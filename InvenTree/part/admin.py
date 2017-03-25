ffb9132de22d92cb5081eccc92dc18a330c2dff3
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-25 23:19:49 +1100

    Added "Part"
    - Has IPN (internal part number)
    - Linked to a PartCategory

--- InvenTree/part/admin.py
+++ InvenTree/part/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import PartCategory
+from .models import PartCategory, Part
 
+admin.site.register(Part)
 admin.site.register(PartCategory)
--- InvenTree/part/models.py
+++ InvenTree/part/models.py
@@ -3,12 +3,26 @@ from __future__ import unicode_literals
 from django.db import models
 
 class PartCategory(models.Model):
-    name = models.CharField(max_length=128)
-    description = models.CharField(max_length=512)
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250)
     parent = models.ForeignKey('self', on_delete=models.CASCADE, blank=True, null=True)
     
     def __str__(self):
         if self.parent:
             return str(self.parent) + "/" + self.name
+        else:
+            return self.name
+        
+class Part(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=250, blank=True)
+    IPN = models.CharField(max_length=100, blank=True)
+    category = models.ForeignKey(PartCategory, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        if self.IPN:
+            return "{name} ({ipn})".format(
+                ipn = self.IPN,
+                name = self.name)
         else:
             return self.name
