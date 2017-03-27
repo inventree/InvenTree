726e8dce4840c91a35adba4c07f16cbb186561f9
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-27 21:14:35 +1100

    Changed admin title

--- InvenTree/InvenTree/urls.py
+++ InvenTree/InvenTree/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 
+admin.site.site_header = "InvenTree Admin"
+
 urlpatterns = [
     url(r'^stock/', include('stock.urls')),
     url(r'^part/', include('part.urls')),
