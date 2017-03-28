f14ae18080e6123338fe0b67aeace80eb96f4dbd
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-28 23:59:57 +1100

    - moved database - added django db test

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -86,7 +86,7 @@ WSGI_APPLICATION = 'InvenTree.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'inventree_db.sqlite3'),
     }
 }
 
