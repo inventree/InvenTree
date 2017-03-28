a082eb2c8181459cdf3278c4360205365e7ff41c
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-28 17:53:54 +1100

    Improved project admin

--- InvenTree/part/admin.py
+++ InvenTree/part/admin.py
@@ -3,11 +3,12 @@ from django.contrib import admin
 from .models import PartCategory, Part
 
 class PartAdmin(admin.ModelAdmin):
+    
     list_display = ('name', 'IPN', 'category')
 
 # Custom form for PartCategory
 class PartCategoryAdmin(admin.ModelAdmin):
-    
+
     list_display = ('name', 'path')
 
     
--- InvenTree/project/admin.py
+++ InvenTree/project/admin.py
@@ -2,6 +2,15 @@ from django.contrib import admin
 
 from .models import ProjectCategory, Project, ProjectPart
 
-admin.site.register(ProjectCategory)
-admin.site.register(Project)
-admin.site.register(ProjectPart)
+class ProjectCategoryAdmin(admin.ModelAdmin):
+    list_display = ('name','path')
+    
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'category')
+    
+class ProjectPartAdmin(admin.ModelAdmin):
+    list_display = ('part', 'project', 'quantity')
+
+admin.site.register(ProjectCategory, ProjectCategoryAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(ProjectPart, ProjectPartAdmin)
