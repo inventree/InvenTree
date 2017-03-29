11b790c3ececa08bbfe01eb817719ad2858b430e
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-29 15:02:59 +1100

    Added Part Templates

--- InvenTree/part/admin.py
+++ InvenTree/part/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import PartCategory, Part
+from .models import PartCategory, Part, PartParameter, PartParameterTemplate
 
 
 class PartAdmin(admin.ModelAdmin):
@@ -12,5 +12,17 @@ class PartCategoryAdmin(admin.ModelAdmin):
     
     list_display = ('name', 'path', 'description')
     
+
+class ParameterTemplateAdmin(admin.ModelAdmin):
+    list_display = ('name', 'units', 'category')
+
+
+class ParameterAdmin(admin.ModelAdmin):
+    list_display = ('part', 'template', 'value')
+
+    
 admin.site.register(Part, PartAdmin)
 admin.site.register(PartCategory, PartCategoryAdmin)
+
+admin.site.register(PartParameter, ParameterAdmin)
+admin.site.register(PartParameterTemplate, ParameterTemplateAdmin)
