774bd75e490f76a2f943b6fbdc8ac7e09afabb9e
Oliver Walters <oliver.henry.walters@gmail.com>
2017-03-29 14:34:41 +1100

    Updated installer script

--- install.py
+++ install.py
@@ -1,6 +1,7 @@
 from __future__ import print_function
 
 import subprocess
+import argparse
 
 def manage(*arg):
     args = ["python", "InvenTree/manage.py"]
@@ -10,12 +11,20 @@ def manage(*arg):
 
     subprocess.call(args)
 
-# Install django requirements
-subprocess.call(["pip", "install", "django", "-q"])
-subprocess.call(["pip", "install", "djangorestframework", "-q"])
+parser = argparse.ArgumentParser(description="Install InvenTree inventory management system")
 
-# Initial database setup
-manage("migrate")
+parser.add_argument('-u', '--update', help='Update only, do not try to install required components', action='store_true')
+    
+args = parser.parse_args()
+
+# If 'update' is specified, don't perform initial installation
+if not args.update:
+    # Install django requirements
+    subprocess.call(["pip", "install", "django", "-q"])
+    subprocess.call(["pip", "install", "djangorestframework", "-q"])
+
+    # Initial database setup
+    manage("migrate")
 
 # Make migrations for all apps
 manage("makemigrations", "part")
@@ -30,5 +39,6 @@ manage("migrate")
 # Check for errors
 manage("check")
 
-print("\n\nAdmin account:\nIf a superuser is not already installed,")
-print("run the command 'python InvenTree/manage.py createsuperuser'")
+if not args.update:
+    print("\n\nAdmin account:\nIf a superuser is not already installed,")
+    print("run the command 'python InvenTree/manage.py createsuperuser'")
