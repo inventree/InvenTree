0df5093440798640471a72f1f2679d6f26062599
Oliver <oliver.henry.walters@gmail.com>
2017-04-01 13:17:17 +1100

    Added docstring for pep checker

--- pep_check.py
+++ pep_check.py
@@ -1,3 +1,12 @@
+"""
+Checks all source files (.py) against PEP8 coding style.
+The following rules are ignored:
+  - W293 - blank lines contain whitespace
+  - E501 - line too long (82 characters)
+
+Run this script before submitting a Pull-Request to check your code.
+"""
+
 import subprocess
 
-subprocess.call(['pep8', '--exclude=migrations', '--ignore=EC04,W293,E501', 'InvenTree'])
+subprocess.call(['pep8', '--exclude=migrations', '--ignore=W293,E501', 'InvenTree'])
