0ca8a751665532fc0fa0bcbf9b22b28e60350c5e
Oliver <oliver.henry.walters@gmail.com>
2017-04-11 23:12:07 +1000

    Added projects API

---
+++ InvenTree/project/serializers.py
@@ -0,0 +1,66 @@
+from rest_framework import serializers
+
+from .models import ProjectCategory, Project, ProjectPart
+
+from part.serializers import PartBriefSerializer
+
+
+class ProjectPartSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProjectPart
+        fields = ('pk',
+                  'part',
+                  'project',
+                  'quantity',
+                  'overage',
+                  'overage_type')
+
+
+class ProjectBriefSerializer(serializers.ModelSerializer):
+    """ Serializer for displaying brief overview of a project
+    """
+
+
+    class Meta:
+        model = Project
+        fields = ('pk',
+                  'name',
+                  'description',
+                  'category')
+
+
+class ProjectDetailSerializer(serializers.ModelSerializer):
+    """ Serializer for detailed project information
+    """
+
+    class Meta:
+        model = Project
+        fields = ('pk',
+                  'name',
+                  'description',
+                  'category')
+
+
+class ProjectCategoryBriefSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = ProjectCategory
+        fields = ('pk','name','description')
+
+
+class ProjectCategoryDetailSerializer(serializers.ModelSerializer):
+
+    projects = ProjectBriefSerializer(many=True)
+
+    children = ProjectCategoryBriefSerializer(many=True)
+
+    class Meta:
+        model = ProjectCategory
+        fields = ('pk',
+                  'name',
+                  'description',
+                  'parent',
+                  'path',
+                  'children',
+                  'projects')
