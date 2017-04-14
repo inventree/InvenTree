2c628c8ab40111fc8fe6216a1dc6986d84dd5b02
Oliver Walters <oliver.henry.walters@gmail.com>
2017-04-14 21:59:52 +1000

    Prepend /api/ to API URL

--- InvenTree/InvenTree/urls.py
+++ InvenTree/InvenTree/urls.py
@@ -16,15 +16,21 @@ def Inventree404(self):
     return Response(content, status=status.HTTP_404_NOT_FOUND)
 
 
-urlpatterns = [
+apipatterns = [
     url(r'^stock/', include('stock.urls')),
     url(r'^part/', include('part.urls')),
     url(r'^supplier/', include('supplier.urls')),
     url(r'^track/', include('track.urls')),
     url(r'^project/', include('project.urls')),
-    url(r'^admin/', admin.site.urls),
-    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 
     # Any other URL
     url(r'', Inventree404)
+]
+
+urlpatterns = [
+    # API URL
+    url(r'^api/', include(apipatterns)),
+
+    url(r'^admin/', admin.site.urls),
+    url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
