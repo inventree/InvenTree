bf1c7125a472187f3e5af88c163afeebc4607bd6
Oliver Walters <oliver.henry.walters@gmail.com>
2017-04-15 00:15:38 +1000

    Added API documentation

--- InvenTree/InvenTree/urls.py
+++ InvenTree/InvenTree/urls.py
@@ -4,6 +4,7 @@ from django.contrib import admin
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework.documentation import include_docs_urls
 
 admin.site.site_header = "InvenTree Admin"
 
@@ -20,6 +21,8 @@ apipatterns = [
 urlpatterns = [
     # API URL
     url(r'^api/', include(apipatterns)),
+
+    url(r'^api-doc/', include_docs_urls(title='InvenTree API')),
 
     url(r'^admin/', admin.site.urls),
     url(r'^auth/', include('rest_framework.urls', namespace='rest_framework')),
