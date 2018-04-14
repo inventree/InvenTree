a31ec388f40f5607413121a5f9e71e006bd856df
Oliver <oliver.henry.walters@gmail.com>
2018-04-14 18:22:31 +1000

    Added static files directory
    - Improved display of part image

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -136,6 +136,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
