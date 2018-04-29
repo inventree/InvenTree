e62bf92bd6717eb71cb73747c74fc306b5c92434
Oliver <oliver.henry.walters@gmail.com>
2018-04-29 23:47:16 +1000

    Notifications work through page-reload / page-redirect
    - Uses session storage

--- InvenTree/static/script/notification.js
+++ InvenTree/static/script/notification.js
@@ -6,3 +6,32 @@ function showAlert(target, message, timeout=5000) {
         $(this).alert(close);
     });
 }
+
+function showCachedAlerts() {
+
+    // Success Message
+    if (sessionStorage.getItem("alert-success")) {
+        showAlert("#alert-success", sessionStorage.getItem("alert-success"));
+        sessionStorage.removeItem("alert-success");
+    }
+
+    // Info Message
+    if (sessionStorage.getItem("alert-info")) {
+        showAlert("#alert-info", sessionStorage.getItem("alert-info"));
+        sessionStorage.removeItem("alert-info");
+    }
+
+    // Warning Message
+    if (sessionStorage.getItem("alert-warning")) {
+        showAlert("#alert-warning", sessionStorage.getItem("alert-warning"));
+        sessionStorage.removeItem("alert-warning");
+    }
+
+    // Danger Message
+    if (sessionStorage.getItem("alert-danger")) {
+        showAlert("#alert-danger", sessionStorage.getItem("alert-danger"));
+        sessionStorage.removeItem("alert-danger");
+    }
+
+    sessionStorage.setItem("alert-danger", 'test');
+}
