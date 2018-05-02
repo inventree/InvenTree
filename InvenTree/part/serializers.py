45510b1adc401244297fb281b8f6ecd22f7c4b0e
Oliver <oliver.henry.walters@gmail.com>
2018-05-02 20:57:04 +1000

    Add category info to part serializer

--- InvenTree/part/serializers.py
+++ InvenTree/part/serializers.py
@@ -8,16 +8,31 @@ class PartSerializer(serializers.ModelSerializer):
     Used when displaying all details of a single component.
     """
 
+    def _category_name(self, part):
+        if part.category:
+            return part.category.name
+        return ''
+
+    def _category_url(self, part):
+        if part.category:
+            return part.category.get_absolute_url()
+        return ''
+
+    category_name = serializers.SerializerMethodField('_category_name')
+    category_url = serializers.SerializerMethodField('_category_url')
+
     class Meta:
         model = Part
         fields = [
+            'pk',
             'url',  # Link to the part detail page
             'name',
             'IPN',
             'URL',  # Link to an external URL (optional)
             'description',
             'category',
-            'category_path',
+            'category_name',
+            'category_url',
             'total_stock',
             'available_stock',
             'units',
