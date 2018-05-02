be0797c6e61b48d3b977f6646c348646a9897bb1
Oliver <oliver.henry.walters@gmail.com>
2018-05-02 23:17:24 +1000

    Bootstrap table to company list

--- InvenTree/company/serializers.py
+++ InvenTree/company/serializers.py
@@ -5,6 +5,8 @@ from .models import Company
 
 class CompanySerializer(serializers.HyperlinkedModelSerializer):
 
+    url = serializers.CharField(source='get_absolute_url', read_only=True)
+
     class Meta:
         model = Company
         fields = '__all__'
