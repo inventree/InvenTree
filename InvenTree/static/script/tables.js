52cc7360b3d2898765443114bd576af986b9ed8f
Oliver <oliver.henry.walters@gmail.com>
2018-05-04 00:48:36 +1000

    More betterer buttons for stock locationdetail

--- InvenTree/static/script/tables.js
+++ InvenTree/static/script/tables.js
@@ -1,9 +1,9 @@
 function editButton(url, text='Edit') {
-    return "<button class='btn btn-success edit-button' type='button' url='" + url + "'>" + text + "</button>";
+    return "<button class='btn btn-success edit-button btn-sm' type='button' url='" + url + "'>" + text + "</button>";
 }
 
 function deleteButton(url, text='Delete') {
-    return "<button class='btn btn-danger delete-button' type='button' url='" + url + "'>" + text + "</button>";
+    return "<button class='btn btn-danger delete-button btn-sm' type='button' url='" + url + "'>" + text + "</button>";
 }
 
 function renderLink(text, url) {
