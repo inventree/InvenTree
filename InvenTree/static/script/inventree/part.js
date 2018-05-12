ceea0533686305077c07c78ffa20ab4227ce2cf4
Oliver <oliver.henry.walters@gmail.com>
2018-05-12 12:44:29 +1000

    Added part API
    - Build allocation table now makes use of this API
    - Further steps towards allocation table

---
+++ InvenTree/static/script/inventree/part.js
@@ -0,0 +1,19 @@
+/* Part API functions
+ * Requires api.js to be loaded first
+ */
+
+function getPartCategoryList(filters={}, options={}) {
+    return inventreeGet('/api/part/category/', filters, options);
+}
+
+function getSupplierPartList(filters={}, options={}) {
+    return inventreeGet('/api/part/supplier/', filters, options);
+}
+
+function getPartList(filters={}, options={}) {
+    return inventreeGet('/api/part/', filters, options);
+}
+
+function getBomList(filters={}, options={}) {
+    return inventreeGet('/api/part/bom/', filters, options);
+}
