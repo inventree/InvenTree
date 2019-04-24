a2d440396803a094cb4021c9b43bdd22a527e67e
Oliver Walters <oliver.henry.walters@gmail.com>
2019-04-24 23:55:11 +1000

    Reverse lookup for company URL
    - Added some simple tests

--- InvenTree/company/tests.py
+++ InvenTree/company/tests.py
@@ -1,3 +1,19 @@
-# from django.test import TestCase
+from django.test import TestCase
 
-# Create your tests here.
+from .models import Company
+
+
+class CompanySimpleTest(TestCase):
+
+    def setUp(self):
+        Company.objects.create(name='ABC Co.',
+                               description='Seller of ABC products',
+                               website='www.abc-sales.com',
+                               address='123 Sales St.',
+                               is_customer=False,
+                               is_supplier=True)
+
+    def test_company_model(self):
+        c = Company.objects.get(pk=1)
+        self.assertEqual(c.name, 'ABC Co.')
+        self.assertEqual(c.get_absolute_url(), '/company/1/')
