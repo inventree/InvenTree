533fdb71c45aec63f81e06b202fa47ab42c42ad2
Oliver Walters <oliver.henry.walters@gmail.com>
2019-04-25 22:04:02 +1000

    Javascript function to render stock tracking table
    - Added extra info to StockItemTracking serializer

--- InvenTree/static/script/inventree/stock.js
+++ InvenTree/static/script/inventree/stock.js
@@ -383,4 +383,93 @@ function loadStockTable(table, options) {
     if (options.buttons) {
         linkButtonsToSelection(table, options.buttons);
     }
-};
+}
+
+
+function loadStockTrackingTable(table, options) {
+
+    var cols = [
+        {
+            field: 'pk',
+            visible: false,
+        },
+        {
+            field: 'date',
+            title: 'Date',
+            sortable: true,
+            formatter: function(value, row, index, field) {
+                var m = moment(value);
+                if (m.isValid()) {
+                    var html = m.format('dddd MMMM Do YYYY') + '<br>' + m.format('h:mm a');
+                    return html;
+                }
+
+                return 'N/A';
+            }
+        },
+    ];
+
+    // If enabled, provide a link to the referenced StockItem
+    if (options.partColumn) {
+        cols.push({
+            field: 'item',
+            title: 'Stock Item',
+            sortable: true,
+            formatter: function(value, row, index, field) {
+                return renderLink(value.part_name, value.url);
+            }
+        });
+    }
+
+    // Stock transaction description
+    cols.push({
+        field: 'title',
+        title: 'Description',
+        sortable: true,
+        formatter: function(value, row, index, field) {
+            var html = "<b>" + value + "</b>";
+
+            if (row.notes) {
+                html += "<br><i>" + row.notes + "</i>";
+            }
+
+            return html;
+        }
+    });
+
+    cols.push({
+        field: 'quantity',
+        title: 'Quantity',
+    });
+
+    cols.push({
+        sortable: true,
+        field: 'user',
+        title: 'User',
+        formatter: function(value, row, index, field) {
+            if (value)
+            {
+                // TODO - Format the user's first and last names
+                return value.username;
+            }
+            else
+            {
+                return "No user information";
+            }
+        }
+    });
+
+    table.bootstrapTable({
+        sortable: true,
+        search: true,
+        method: 'get',
+        rememberOrder: true,
+        queryParams: options.params,
+        columns: cols,
+        url: options.url,
+    });
+
+    if (options.buttons) {
+        linkButtonsToSelection(table, options.buttons);
+    }
+}
