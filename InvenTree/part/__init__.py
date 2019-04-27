aa04941bf162aea43e13dcfb32cfc4f4f1119b5a
Oliver Walters <oliver.henry.walters@gmail.com>
2019-04-28 01:05:45 +1000

    Add module-level docstrings for each app

---
+++ InvenTree/part/__init__.py
@@ -0,0 +1,10 @@
+"""
+The Part module is responsible for Part management.
+
+It includes models for:
+
+- PartCategory
+- Part
+- SupplierPart
+- BomItem
+"""
---
+++ InvenTree/stock/__init__.py
@@ -0,0 +1,9 @@
+"""
+The Stock module is responsible for Stock management.
+
+It includes models for:
+
+- StockLocation
+- StockItem
+- StockItemTracking
+"""
