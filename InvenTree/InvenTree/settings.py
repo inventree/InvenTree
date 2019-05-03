517262b2331bde2e2aac0663a97cd16774512fba
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-04 01:04:30 +1000

    PEP fixes

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 import logging
-import tempfile 
+import tempfile
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
