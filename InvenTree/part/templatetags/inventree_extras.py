4fe3b9d779262d47aed7d498a85cce18d555c132
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-04 11:28:23 +1000

    add docstrings

--- InvenTree/part/templatetags/inventree_extras.py
+++ InvenTree/part/templatetags/inventree_extras.py
@@ -10,19 +10,23 @@ register = template.Library()
 
 @register.simple_tag()
 def multiply(x, y, *args, **kwargs):
+    """ Multiply two numbers together """
     return x * y
 
 
 @register.simple_tag()
 def inventree_version(*args, **kwargs):
+    """ Return InvenTree version string """
     return version.inventreeVersion()
 
 
 @register.simple_tag()
 def inventree_commit(*args, **kwargs):
+    """ Return InvenTree git commit hash string """
     return version.inventreeCommitHash()
 
 
 @register.simple_tag()
 def inventree_github(*args, **kwargs):
+    """ Return URL for InvenTree github site """
     return "https://github.com/InvenTree"
