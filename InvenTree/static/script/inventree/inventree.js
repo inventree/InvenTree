eec0fc34d2bf34a573d19fa6adae232e83b354cc
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-06 21:22:31 +1000

    Provide function callback when file is dropped
    - https://stackoverflow.com/questions/6756583/prevent-browser-from-loading-a-drag-and-dropped-file
    -

--- InvenTree/static/script/inventree/inventree.js
+++ InvenTree/static/script/inventree/inventree.js
@@ -3,6 +3,16 @@ function inventreeDocReady() {
      * This will be called for every page that extends "base.html"
      */
 
+    window.addEventListener("dragover",function(e){
+        e = e || event;
+        e.preventDefault();
+      },false);
+
+    window.addEventListener("drop",function(e){
+        e = e || event;
+        e.preventDefault();
+      },false);
+
     /* Add drag-n-drop functionality to any element
      * marked with the class 'dropzone'
      */
@@ -10,7 +20,19 @@ function inventreeDocReady() {
         $(this).addClass('dragover');
     });
 
-    $('.dropzone').on('dragleave', function() {
+    $('.dropzone').on('dragleave drop', function() {
         $(this).removeClass('dragover');
+    });
+
+    // Callback to launch the 'About' window
+    $('#launch-about').click(function() {
+        var modal = $('#modal-about');
+
+        modal.modal({
+            backdrop: 'static',
+            keyboard: 'false',
+        });
+
+        modal.modal('show');
     });
 }
