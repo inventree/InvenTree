88a1df75d2f4a771d9ffc1bdc0566f0346193d4f
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-08 19:32:00 +1000

    Render part image in the stock location list display

--- InvenTree/static/script/inventree/stock.js
+++ InvenTree/static/script/inventree/stock.js
@@ -347,6 +347,7 @@ function loadStockTable(table, options) {
         search: true,
         method: 'get',
         pagination: true,
+        pageSize: 25,
         rememberOrder: true,
         queryParams: options.params,
         columns: [
@@ -365,7 +366,7 @@ function loadStockTable(table, options) {
                 title: 'Part',
                 sortable: true,
                 formatter: function(value, row, index, field) {
-                    return renderLink(value, row.part.url);
+                    return imageHoverIcon(row.part.image_url) + renderLink(value, row.part.url);
                 }
             },
             {
