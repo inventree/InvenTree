df227a375c1cf5fdd0ad23505799e7c6f7177b9c
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-14 15:42:53 +1000

    Allow some more chars in part names

--- InvenTree/InvenTree/validators.py
+++ InvenTree/InvenTree/validators.py
@@ -8,7 +8,7 @@ from django.utils.translation import gettext_lazy as _
 
 def validate_part_name(value):
     # Prevent some illegal characters in part names
-    for c in ['/', '\\', '|', '#', '$']:
+    for c in ['|', '#', '$']:
         if c in str(value):
             raise ValidationError(
                 _('Invalid character in part name')
