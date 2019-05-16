b77b6d22561556aa00eeb6b06d70fcc9725e6ba5
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-16 22:29:39 +1000

    Display number of supplier parts in supplier list

--- InvenTree/company/serializers.py
+++ InvenTree/company/serializers.py
@@ -25,7 +25,25 @@ class CompanySerializer(serializers.ModelSerializer):
     """ Serializer for Company object (full detail) """
 
     url = serializers.CharField(source='get_absolute_url', read_only=True)
+    part_count = serializers.CharField(read_only=True)
 
     class Meta:
         model = Company
-        fields = '__all__'
+        fields = [
+            'id',
+            'url',
+            'name',
+            'description',
+            'website',
+            'name',
+            'phone',
+            'address',
+            'email',
+            'contact',
+            'URL',
+            'image',
+            'notes',
+            'is_customer',
+            'is_supplier',
+            'part_count'
+        ]
