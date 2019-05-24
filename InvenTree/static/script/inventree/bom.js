fb4eb6d134fc09aa979f0ad9ac54d8e0930c6fc2
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-24 23:05:47 +1000

    Improvements for BOM table buttons
    - Use pictures
    - Direct selection of export format
    - Remove some old and dead code

--- InvenTree/static/script/inventree/bom.js
+++ InvenTree/static/script/inventree/bom.js
@@ -12,52 +12,6 @@ function reloadBomTable(table, options) {
 }
 
 
-function downloadBom(options = {}) {
-
-    var modal = options.modal || "#modal-form";
-    
-    var content = `
-        <b>Select file format</b><br>
-        <div class='controls'>
-        <select id='bom-format' class='select'>
-            <option value='csv'>CSV</option>
-            <option value='tsv'>TSV</option>
-            <option value='xls'>XLS</option>
-            <option value='xlsx'>XLSX</option>
-            <option value='ods'>ODS</option>
-            <option value='yaml'>YAML</option>
-            <option value='json'>JSON</option>
-            <option value='xml'>XML</option>
-            <option value='html'>HTML</option>
-        </select>
-        </div>
-    `;
-
-    openModal({
-        modal: modal,
-        title: "Export Bill of Materials",
-        submit_text: "Download",
-        close_text: "Cancel",
-    });
-
-    modalSetContent(modal, content);
-
-    modalEnable(modal, true);
-
-    $(modal).on('click', '#modal-form-submit', function() {
-        $(modal).modal('hide');
-
-        var format = $(modal).find('#bom-format :selected').val();
-
-        if (options.url) {
-            var url = options.url + "?format=" + format;
-
-            location.href = url;
-        }
-    });
-}
-
-
 function loadBomTable(table, options) {
     /* Load a BOM table with some configurable options.
      * 
@@ -189,10 +143,10 @@ function loadBomTable(table, options) {
     if (options.editable) {
         cols.push({
             formatter: function(value, row, index, field) {
-                var bEdit = "<button title='Edit BOM Item' class='btn btn-primary bom-edit-button btn-sm' type='button' url='/part/bom/" + row.pk + "/edit'><span class='glyphicon glyphicon-small glyphicon-pencil'></span></button>";
+                var bEdit = "<button title='Edit BOM Item' class='btn btn-primary bom-edit-button btn-sm' type='button' url='/part/bom/" + row.pk + "/edit'><span class='glyphicon glyphicon-small glyphicon-edit'></span></button>";
                 var bDelt = "<button title='Delete BOM Item' class='btn btn-danger bom-delete-button btn-sm' type='button' url='/part/bom/" + row.pk + "/delete'><span class='glyphicon glyphicon-small glyphicon-trash'></span></button>";
                 
-                return "<div class='btn-group'>" + bEdit + bDelt + "</div>";
+                return "<div class='btn-group' role='group'>" + bEdit + bDelt + "</div>";
             }
         });
     }
