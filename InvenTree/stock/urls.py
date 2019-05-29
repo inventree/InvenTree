f4a052ee2b83ebb5d121d33f230574f6e9da24ba
Oliver Walters <oliver.henry.walters@gmail.com>
2019-05-29 22:11:03 +1000

    Redirect non-ajax forms to /

--- InvenTree/stock/urls.py
+++ InvenTree/stock/urls.py
@@ -36,7 +36,7 @@ stock_urls = [
 
     url(r'^track/?', views.StockTrackingIndex.as_view(), name='stock-tracking-list'),
 
-    url(r'^move/', views.StockItemMoveMultiple.as_view(), name='stock-item-move-multiple'),
+    url(r'^move/?', views.StockItemMoveMultiple.as_view(), name='stock-item-move-multiple'),
 
     # Individual stock items
     url(r'^item/(?P<pk>\d+)/', include(stock_item_detail_urls)),
