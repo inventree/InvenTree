915395e676afe031758334767d655d8cb14ab4e0
Oliver Walters <oliver.henry.walters@gmail.com>
2019-06-13 20:35:15 +1000

    Create a new supplier part directly from the order form

--- InvenTree/static/script/inventree/order.js
+++ InvenTree/static/script/inventree/order.js
@@ -9,3 +9,32 @@ function removeOrderRowFromOrderWizard(e) {
 
     $('#' + row).remove();
 }
+
+function newSupplierPartFromOrderWizard(e) {
+    /* Create a new supplier part directly from an order form.
+     * Launches a secondary modal and (if successful),
+     * back-populates the selected row.
+     */
+
+    e = e || window.event;
+
+    var src = e.target || e.srcElement;
+
+    var part = $(src).attr('part-id');
+
+    launchModalForm("/supplier-part/new/", {
+        modal: '#modal-form-secondary',
+        data: {
+            part: part,
+        },
+        success: function(response) {
+            /* A new supplier part has been created! */
+
+            var dropdown = '#id_supplier_part_' + part;
+
+            var option = new Option(response.text, response.pk, true, true);
+
+            $('#modal-form').find(dropdown).append(option).trigger('change');
+        },
+    });
+}
