a424a805e42c2142b9ecddd8547d7de49ab74c77
Oliver Walters <oliver.henry.walters@gmail.com>
2019-06-13 22:39:57 +1000

    Bump version string

--- InvenTree/InvenTree/version.py
+++ InvenTree/InvenTree/version.py
@@ -4,7 +4,7 @@ Provides information on the current InvenTree version
 
 import subprocess
 
-INVENTREE_SW_VERSION = "0.0.1"
+INVENTREE_SW_VERSION = "0.0.2"
 
 
 def inventreeVersion():
@@ -14,6 +14,8 @@ def inventreeVersion():
 
 def inventreeCommitHash():
     """ Returns the git commit hash for the running codebase """
+
+    # TODO - This doesn't seem to work when running under gunicorn. Why is this?!
     commit = str(subprocess.check_output('git rev-parse --short HEAD'.split()), 'utf-8').strip()
 
     return commit
