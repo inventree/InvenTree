4be82a31ced886aa08c659fad553e7f9e353d94c
Oliver Walters <oliver.henry.walters@gmail.com>
2019-06-13 20:26:53 +1000

    Remove rows from order form

---
+++ InvenTree/static/script/inventree/order.js
@@ -0,0 +1,11 @@
+function removeOrderRowFromOrderWizard(e) {
+    /* Remove a part selection from an order form. */
+
+    e = e || window.event;
+
+    var src = e.target || e.srcElement;
+
+    var row = $(src).attr('row');
+
+    $('#' + row).remove();
+}
