0857ec61fd594b65be98b9bdfd415ea122da8745
Oliver Walters <oliver.henry.walters@gmail.com>
2019-06-17 19:47:16 +1000

    Add a test fixture for orders

--- InvenTree/order/tests.py
+++ InvenTree/order/tests.py
@@ -1,1 +1,33 @@
-# TODO - Implement tests for the order app
+from django.test import TestCase
+
+from part.models import Part
+
+class OrderTest(TestCase):
+    """
+    Tests to ensure that the order models are functioning correctly.
+    """
+
+    fixtures = [
+        'company',
+        'supplier_part',
+        'category',
+        'part',
+        'location',
+        'stock',
+        'order'
+    ]
+
+    def test_on_order(self):
+        """ There should be 3 separate items on order for the M2x4 LPHS part """
+
+        part = Part.objects.get(name='M2x4 LPHS')
+
+        open_orders = []
+
+        for supplier in part.supplier_parts.all():
+            open_orders += supplier.open_orders()
+
+        self.assertEqual(len(open_orders), 3)
+
+        # Test the total on-order quantity
+        self.assertEqual(part.on_order, 400)
