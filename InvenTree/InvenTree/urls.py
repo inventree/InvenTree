9726ea4f99185b329a768bcbad1a8c822e04dd18
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-07 19:18:18 +1000

    Add serializer / API for currency objects

--- InvenTree/InvenTree/urls.py
+++ InvenTree/InvenTree/urls.py
@@ -19,6 +19,7 @@ from stock.urls import stock_urls
 from build.urls import build_urls
 from order.urls import order_urls
 
+from common.api import common_api_urls
 from part.api import part_api_urls, bom_api_urls
 from company.api import company_api_urls
 from stock.api import stock_api_urls
@@ -39,6 +40,7 @@ from users.urls import user_urls
 admin.site.site_header = "InvenTree Admin"
 
 apipatterns = [
+    url(r'^common/', include(common_api_urls)),
     url(r'^part/', include(part_api_urls)),
     url(r'^bom/', include(bom_api_urls)),
     url(r'^company/', include(company_api_urls)),
