3188b0ab18a1b3a491b8ed9d38eba65036c9a6d2
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-07 20:19:35 +1000

    Edit currency from settings view

--- InvenTree/common/urls.py
+++ InvenTree/common/urls.py
@@ -8,6 +8,9 @@ from . import views
 
 currency_urls = [
     url(r'^new/', views.CurrencyCreate.as_view(), name='currency-create'),
+
+    url(r'^(?P<pk>\d+)/edit/', views.CurrencyEdit.as_view(), name='currency-edit'),
+    #url(r'^(?P<pk>\d+)/delete/', views.CurrencyDelete.as_view(), name='currency-delete'),
 ]
 
 common_urls = [
--- InvenTree/common/views.py
+++ InvenTree/common/views.py
@@ -5,15 +5,23 @@ Django views for interacting with common models
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from InvenTree.views import AjaxCreateView
+from InvenTree.views import AjaxCreateView, AjaxUpdateView
 
-from .models import Currency
-from .forms import CurrencyEditForm
+from . import models
+from . import forms
 
 
 class CurrencyCreate(AjaxCreateView):
     """ View for creating a new Currency object """
 
-    model = Currency
-    form_class = CurrencyEditForm
+    model = models.Currency
+    form_class = forms.CurrencyEditForm
     ajax_form_title = 'Create new Currency'
+
+
+class CurrencyEdit(AjaxUpdateView):
+    """ View for editing an existing Currency object """
+
+    model = models.Currency
+    form_class = forms.CurrencyEditForm
+    ajax_form_title = 'Edit Currency'
