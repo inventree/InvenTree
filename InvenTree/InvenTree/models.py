fb2c347fd4012a6865ed81e357d645265414109d
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-09 08:30:24 +1000

    Removed unused import

--- InvenTree/InvenTree/models.py
+++ InvenTree/InvenTree/models.py
@@ -6,7 +6,6 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.contenttypes.models import ContentType
-from rest_framework.exceptions import ValidationError
 
 from django.db.models.signals import pre_delete
 from django.dispatch import receiver
