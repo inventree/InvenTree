ed20e9d4a16d30521a1731f768126b3d865abb59
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-15 22:04:52 +1000

    Simplify code for exporting PurchaseOrder
    - New resource for managing import/export of POLineItem model

--- InvenTree/order/admin.py
+++ InvenTree/order/admin.py
@@ -2,7 +2,11 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+
 from import_export.admin import ImportExportModelAdmin
+
+from import_export.resources import ModelResource
+from import_export.fields import Field
 
 from .models import PurchaseOrder, PurchaseOrderLineItem
 
@@ -18,7 +22,27 @@ class PurchaseOrderAdmin(ImportExportModelAdmin):
     )
 
 
+class POLineItemResource(ModelResource):
+    """ Class for managing import / export of POLineItem data """
+
+    part_name = Field(attribute='part__part__name', readonly=True)
+
+    manufacturer = Field(attribute='part__manufacturer', readonly=True)
+
+    MPN = Field(attribute='part__MPN', readonly=True)
+
+    SKU = Field(attribute='part__SKU', readonly=True)
+
+    class Meta:
+        model = PurchaseOrderLineItem
+        skip_unchanged = True
+        report_skipped = False
+        clean_model_instances = True
+
+
 class PurchaseOrderLineItemAdmin(ImportExportModelAdmin):
+
+    resource_class = POLineItemResource
 
     list_display = (
         'order',
