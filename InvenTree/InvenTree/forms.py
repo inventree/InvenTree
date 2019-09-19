8a995cc193ba08b875eb4ef832e00e7e0d77dfda
Oliver Walters <oliver.henry.walters@gmail.com>
2019-09-19 14:36:14 +1000

    Ability to edit username

--- InvenTree/InvenTree/forms.py
+++ InvenTree/InvenTree/forms.py
@@ -43,6 +43,7 @@ class EditUserForm(HelperForm):
     class Meta:
         model = User
         fields = [
+            'username',
             'first_name',
             'last_name',
             'email'
