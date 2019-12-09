25e5a64cee334a704746d1a7eaf4d731b66797e4
Oliver Walters <oliver.henry.walters@gmail.com>
2019-12-09 21:19:35 +1100

    Improve filtering / ordering / sorting for purchase-order API

--- InvenTree/order/api.py
+++ InvenTree/order/api.py
@@ -7,8 +7,15 @@ from __future__ import unicode_literals
 
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import generics, permissions
+from rest_framework import filters
+from rest_framework.response import Response
 
+from django.conf import settings
 from django.conf.urls import url
+
+from InvenTree.status_codes import OrderStatus
+
+import os
 
 from .models import PurchaseOrder, PurchaseOrderLineItem
 from .serializers import POSerializer, POLineItemSerializer
@@ -24,17 +31,70 @@ class POList(generics.ListCreateAPIView):
     queryset = PurchaseOrder.objects.all()
     serializer_class = POSerializer
 
+    def list(self, request, *args, **kwargs):
+
+        queryset = self.get_queryset().prefetch_related('supplier', 'lines')
+
+        queryset = self.filter_queryset(queryset)
+
+        # Special filtering for 'status' field
+        if 'status' in request.GET:
+            status = request.GET['status']
+
+            # First attempt to filter by integer value
+            try:
+                status = int(status)
+                queryset = queryset.filter(status=status)
+            except ValueError:
+                try:
+                    value = OrderStatus.value(status)
+                    queryset = queryset.filter(status=value)
+                except ValueError:
+                    pass
+
+        data = queryset.values(
+            'pk',
+            'supplier',
+            'supplier__name',
+            'supplier__image',
+            'reference',
+            'description',
+            'URL',
+            'status',
+            'notes',
+            'creation_date',
+        )
+
+        for item in data:
+
+            order = queryset.get(pk=item['pk'])
+
+            item['supplier__image'] = os.path.join(settings.MEDIA_URL, item['supplier__image'])
+            item['status_text'] = OrderStatus.label(item['status'])
+            item['lines'] = order.lines.count()
+
+        return Response(data)
+
     permission_classes = [
         permissions.IsAuthenticated,
     ]
 
     filter_backends = [
         DjangoFilterBackend,
+        filters.SearchFilter,
+        filters.OrderingFilter,
     ]
 
     filter_fields = [
         'supplier',
     ]
+
+    ordering_fields = [
+        'creation_date',
+        'reference',
+    ]
+
+    ordering = '-creation_date'
 
 
 class PODetail(generics.RetrieveUpdateAPIView):
