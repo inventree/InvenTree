0001b889f0d1b5b4175554892fb5acb112c33429
Oliver Walters <oliver.henry.walters@gmail.com>
2020-04-13 17:37:44 +1000

    Update company API
    - option to include manufacturer_detail
    - improve company serializer
    
    (cherry picked from commit 2ea1e72c448d97a6563c5a8d77fe8eb9f19c3a92)

--- InvenTree/company/serializers.py
+++ InvenTree/company/serializers.py
@@ -17,12 +17,16 @@ class CompanyBriefSerializer(InvenTreeModelSerializer):
 
     url = serializers.CharField(source='get_absolute_url', read_only=True)
 
+    image = serializers.CharField(source='get_thumbnail_url', read_only=True)
+
     class Meta:
         model = Company
         fields = [
             'pk',
             'url',
-            'name'
+            'name',
+            'description',
+            'image',
         ]
 
 
@@ -64,19 +68,27 @@ class SupplierPartSerializer(InvenTreeModelSerializer):
 
     part_detail = PartBriefSerializer(source='part', many=False, read_only=True)
 
-    supplier_name = serializers.CharField(source='supplier.name', read_only=True)
-    supplier_logo = serializers.CharField(source='supplier.get_thumbnail_url', read_only=True)
+    supplier_detail = CompanyBriefSerializer(source='supplier', many=False, read_only=True)
+    manufacturer_detail = CompanyBriefSerializer(source='manufacturer', many=False, read_only=True)
 
     pricing = serializers.CharField(source='unit_pricing', read_only=True)
 
     def __init__(self, *args, **kwargs):
 
         part_detail = kwargs.pop('part_detail', False)
+        supplier_detail = kwargs.pop('supplier_detail', False)
+        manufacturer_detail = kwargs.pop('manufacturer_detail', False)
 
         super(SupplierPartSerializer, self).__init__(*args, **kwargs)
 
         if part_detail is not True:
             self.fields.pop('part_detail')
+
+        if supplier_detail is not True:
+            self.fields.pop('supplier_detail')
+
+        if manufacturer_detail is not True:
+            self.fields.pop('manufacturer_detail')
 
     class Meta:
         model = SupplierPart
@@ -86,11 +98,10 @@ class SupplierPartSerializer(InvenTreeModelSerializer):
             'part',
             'part_detail',
             'supplier',
-            'supplier_name',
-            'supplier_logo',
+            'supplier_detail',
             'SKU',
             'manufacturer',
-            'manufacturer_name',
+            'manufacturer_detail',
             'description',
             'MPN',
             'link',
