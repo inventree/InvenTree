b9d0c60189c0c232fcefeeec7bfcd7028ea354a1
Oliver Walters <oliver.henry.walters@gmail.com>
2020-05-12 08:12:42 +1000

    Pepper

--- InvenTree/stock/api.py
+++ InvenTree/stock/api.py
@@ -697,7 +697,6 @@ class StockTrackingList(generics.ListCreateAPIView):
         headers = self.get_success_headers(serializer.data)
         return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
 
-
     filter_backends = [
         DjangoFilterBackend,
         filters.SearchFilter,
