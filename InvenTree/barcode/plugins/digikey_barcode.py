a72ed11cb02a2cd176dd8bab2c851722812aa1f0
Oliver Walters <oliver.henry.walters@gmail.com>
2020-06-11 11:16:55 +1000

    Skeleton for DigiKey barcode

--- InvenTree/barcode/plugins/digikey_barcode.py
+++ InvenTree/barcode/plugins/digikey_barcode.py
@@ -3,3 +3,17 @@
 """
 DigiKey barcode decoding
 """
+
+from barcode.barcode import BarcodePlugin
+
+
+class DigikeyBarcodePlugin(BarcodePlugin):
+
+    PLUGIN_NAME = "DigikeyBarcode"
+
+    def validate(self):
+        """
+        TODO: Validation of Digikey barcodes.
+        """
+
+        return False
