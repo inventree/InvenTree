aefd70ce490d3a0673b6b82c6efb1dd046bcf46a
Oliver Walters <oliver.henry.walters@gmail.com>
2020-08-16 09:29:45 +1000

    Add admin interface for StockItemLabel

--- InvenTree/label/admin.py
+++ InvenTree/label/admin.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
 from django.contrib import admin
 
-# Register your models here.
+from .models import StockItemLabel
+
+
+class StockItemLabelAdmin(admin.ModelAdmin):
+
+    list_display = ('name', 'description', 'label')
+
+
+admin.site.register(StockItemLabel, StockItemLabelAdmin)
