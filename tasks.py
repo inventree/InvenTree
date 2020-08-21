d8f9f95cb4f7a559a09da215ca9dcae0d133db96
Oliver Walters <oliver.henry.walters@gmail.com>
2020-08-21 21:49:03 +1000

    Add command to run the development server

--- tasks.py
+++ tasks.py
@@ -242,3 +242,13 @@ def backup(c):
 
     manage(c, 'dbbackup')
     manage(c, 'mediabackup')
+
+@task(help={'address': 'Server address:port'})
+def server(c, address="127.0.0.1:8000"):
+    """
+    Launch a (deveopment) server using Django's in-built webserver.
+
+    Note: This is *not* sufficient for a production installation.
+    """
+
+    manage(c, "runserver {address}".format(address=address))
