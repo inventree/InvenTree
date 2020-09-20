81402578c943681c9bee1c7a060ce5c5bebb14cb
alex <alex@kairosaerospace.com>
2020-09-20 00:50:21 -0700

    looks like a test DB gets used so we can't hardcode output IDs for testing

--- InvenTree/build/test_build.py
+++ InvenTree/build/test_build.py
@@ -279,8 +279,7 @@ class BuildBelongsToTest(TestCase):
         allocated_stock = StockItem.objects.filter(build_order=self.build)[0]
         self.assertEqual(allocated_stock.quantity, Decimal(2))
         # Ensure that the component stock now "belongs_to" the build stock item ID
-        self.assertEqual(3, self.build.build_outputs.all()[0].pk)  # confirm that the build output pk == 3
-        self.assertEqual(allocated_stock.belongs_to_id, 3)  # confirm that the component stock items belong to ID 3
+        self.assertEqual(allocated_stock.belongs_to_id, self.build.build_outputs.all()[0].pk)
 
     def test_build_single_output_trackable_component(self):
         self.sub_part = Part.objects.create(
@@ -320,8 +319,7 @@ class BuildBelongsToTest(TestCase):
         allocated_stock = StockItem.objects.filter(build_order=self.build)[0]
         self.assertEqual(allocated_stock.quantity, Decimal(1))
         # Ensure that the component stock now "belongs_to" the build stock item ID
-        self.assertEqual(2, self.build.build_outputs.all()[0].pk)  # confirm that the build output pk == 2
-        self.assertEqual(allocated_stock.belongs_to_id, 2)  # confirm that the component stock items belong to ID 2
+        self.assertEqual(allocated_stock.belongs_to_id, self.build.build_outputs.all()[0].pk)
         self.assertEqual("101", allocated_stock.serial)
 
     def test_build_single_output_trackable_two_components(self):
@@ -368,9 +366,8 @@ class BuildBelongsToTest(TestCase):
         allocated_stock = StockItem.objects.filter(build_order=self.build)
         self.assertEqual(2, len(allocated_stock))
         # Ensure that the component stock now "belongs_to" the build stock item ID
-        self.assertEqual(3, self.build.build_outputs.all()[0].pk)  # confirm that the build output pk == 3
         serials_found = set()
         for subpart_stock in allocated_stock:
-            self.assertEqual(subpart_stock.belongs_to_id, 3)  # confirm that the component stock items belong to ID 3
+            self.assertEqual(subpart_stock.belongs_to_id, self.build.build_outputs.all()[0].pk)
             serials_found.add(subpart_stock.serial)
         self.assertEqual({"101", "102"}, serials_found)
