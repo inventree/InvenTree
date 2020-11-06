8518b0981214ec6d36f92f079fa50f53c0e49fea
Tom Judge <tom@tomjudge.com>
2020-11-06 19:25:12 +0000

    Make tasks their own file

--- InvenTree/InvenTree/celery.py
+++ InvenTree/InvenTree/celery.py
@@ -17,6 +17,4 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 
-@app.task(bind=True)
-def debug_task(self):
-    print(f'Request: {self.request!r}')
+
