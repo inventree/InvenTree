2d36889768ef4129fd4d8bfac166f5dac7bc65fa
Tom Judge <tom@tomjudge.com>
2020-11-07 22:52:08 +0000

    Rename app for a cleaner name space

--- InvenTree/InvenTree/__init__.py
+++ InvenTree/InvenTree/__init__.py
@@ -6,6 +6,6 @@ It provides a number of helper functions and generic classes which are used by I
 
 # This will make sure the app is always imported when
 # Django starts so that shared_task will use this app.
-from .celery import app as celery_app
+from .celery import celery_app as celery_app
 
 __all__ = ('celery_app',)
