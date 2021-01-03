9fa37a915657d3637d53920a348dbc47779ab0a8
Oliver Walters <oliver.henry.walters@gmail.com>
2021-01-03 22:57:39 +1100

    Improve visualisation of global settings

--- InvenTree/common/models.py
+++ InvenTree/common/models.py
@@ -110,7 +110,21 @@ class InvenTreeSetting(models.Model):
             'default': True,
             'validator': bool
         },
-      
+
+        'PART_TEMPLATE': {
+            'name': _('Template'),
+            'description': _('Parts are templates by default'),
+            'default': False,
+            'validator': bool,
+        },
+
+        'PART_ASSEMBLY': {
+            'name': _('Assembly'),
+            'description': _('Parts can be assembled from other components by default'),
+            'default': False,
+            'validator': bool,
+        },
+
         'PART_COMPONENT': {
             'name': _('Component'),
             'description': _('Parts can be used as sub-components by default'),
