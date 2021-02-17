ef902fc313f220660bc517c92ad1b1117308275d
Oliver Walters <oliver.henry.walters@gmail.com>
2021-02-17 23:27:09 +1100

    Add bom_items to build order report context

--- InvenTree/report/models.py
+++ InvenTree/report/models.py
@@ -341,6 +341,7 @@ class BuildReport(ReportTemplateBase):
         return {
             'build': my_build,
             'part': my_build.part,
+            'bom_items': my_build.part.get_bom_items(),
             'reference': my_build.reference,
             'quantity': my_build.quantity,
         }
