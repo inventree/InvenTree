5aea35f8fa0cd999d355a0c2a2b1814436c067d4
Oliver Walters <oliver.henry.walters@gmail.com>
2021-03-31 20:04:18 +1100

    Force TCP

--- InvenTree/InvenTree/ci_mysql.py
+++ InvenTree/InvenTree/ci_mysql.py
@@ -14,6 +14,6 @@ if 'test' in sys.argv:
         'NAME': 'inventree_test_db',
         'USER': 'inventree',
         'PASSWORD': 'password',
-        'HOST': 'localhost',
+        'HOST': '127.0.0.1',
         'PORT': '3306',
     }
