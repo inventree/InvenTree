70703f85888ebe218c5fbcfb86ff8a3bd1d3f39b
Oliver Walters <oliver.henry.walters@gmail.com>
2021-03-31 19:48:20 +1100

    Try localhost

--- InvenTree/InvenTree/ci_mysql.py
+++ InvenTree/InvenTree/ci_mysql.py
@@ -14,6 +14,6 @@ if 'test' in sys.argv:
         'NAME': 'inventree_test_db',
         'USER': 'inventree',
         'PASSWORD': 'password',
-        'HOST': 'mysql',
+        'HOST': 'localhost',
         'PORT': '3306',
     }
