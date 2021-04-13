d5034ece5168977aa3dc258f53f4d657ece3afba
Oliver Walters <oliver.henry.walters@gmail.com>
2021-04-13 20:21:33 +1000

    typo fix

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -524,7 +524,7 @@ EMAIL_HOST_PASSWORD = get_setting(
     email_config.get('password', ''),
 )
 
-EMAIL_SUBJECT_PREFIX = '[InvenTree] ')
+EMAIL_SUBJECT_PREFIX = '[InvenTree] '
 
 EMAIL_USE_LOCALTIME = False
 
