00c5a883838b5d72aff0e9ff3636a5a4099b99ee
Oliver Walters <oliver.henry.walters@gmail.com>
2021-05-12 21:15:53 +1000

    Split environment variables out into a .env file
    - No need to collect "static" file when we are running in DEBUG mode

--- docker/start_dev_server.sh
+++ docker/start_dev_server.sh
@@ -35,16 +35,14 @@ python manage.py wait_for_db
 
 sleep 10
 
-echo "Running InvenTree database migrations and collecting static files..."
+echo "Running InvenTree database migrations..."
 
 # We assume at this stage that the database is up and running
 # Ensure that the database schema are up to date
 python manage.py check || exit 1
 python manage.py migrate --noinput || exit 1
 python manage.py migrate --run-syncdb || exit 1
-python manage.py prerender || exit 1
-python manage.py collectstatic --noinput || exit 1
 python manage.py clearsessions || exit 1
 
 # Launch a development server
-python manage.py runserver -a 0.0.0.0:$INVENTREE_WEB_PORT
+python manage.py runserver 0.0.0.0:$INVENTREE_WEB_PORT
