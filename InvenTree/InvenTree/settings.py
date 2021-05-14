f1f75b45cbc3c9b90718bdc176040f5ccd665dc5
Matthias <matmair@live.de>
2021-05-15 01:02:25 +0200

    using messages for alerts

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -21,6 +21,7 @@ from datetime import datetime
 
 import yaml
 from django.utils.translation import gettext_lazy as _
+from django.contrib.messages import constants as messages
 
 
 def _is_true(x):
@@ -593,3 +594,9 @@ IMPORT_EXPORT_USE_TRANSACTIONS = True
 INTERNAL_IPS = [
     '127.0.0.1',
 ]
+
+MESSAGE_TAGS = {
+    messages.SUCCESS: 'alert alert-block alert-success',
+    messages.ERROR: 'alert alert-block alert-danger',
+    messages.INFO: 'alert alert-block alert-info',
+}
