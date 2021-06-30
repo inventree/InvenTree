a7d60cf5ad4f63202ccc08a8a0f9101b65d156bd
Oliver <oliver.henry.walters@gmail.com>
2021-06-30 10:49:33 +1000

    Exposes BuildOrderAttachment objects to the REST API

--- InvenTree/build/serializers.py
+++ InvenTree/build/serializers.py
@@ -10,13 +10,13 @@ from django.db.models import BooleanField
 
 from rest_framework import serializers
 
-from InvenTree.serializers import InvenTreeModelSerializer
+from InvenTree.serializers import InvenTreeModelSerializer, InvenTreeAttachmentSerializerField
 
 from stock.serializers import StockItemSerializerBrief
 from stock.serializers import LocationSerializer
 from part.serializers import PartSerializer, PartBriefSerializer
 
-from .models import Build, BuildItem
+from .models import Build, BuildItem, BuildOrderAttachment
 
 
 class BuildSerializer(InvenTreeModelSerializer):
@@ -142,4 +142,22 @@ class BuildItemSerializer(InvenTreeModelSerializer):
             'stock_item',
             'stock_item_detail',
             'quantity'
+        ]
+
+
+class BuildAttachmentSerializer(InvenTreeModelSerializer):
+    """
+    Serializer for a BuildAttachment
+    """
+
+    attachment = InvenTreeAttachmentSerializerField(required=True)
+
+    class Meta:
+        model = BuildOrderAttachment
+
+        fields = [
+            'pk',
+            'build',
+            'attachment',
+            'comment'
         ]
