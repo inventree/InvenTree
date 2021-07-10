a15eb90189022ab936b115a81c8bea75a3a509eb
Matthias <matmair@live.de>
2021-07-10 18:20:08 +0200

    permissionstet typo fix

--- InvenTree/users/models.py
+++ InvenTree/users/models.py
@@ -126,7 +126,7 @@ class RuleSet(models.Model):
             'order_salesorder',
             'order_salesorderattachment',
             'order_salesorderlineitem',
-            'order_salesorderadditionallineItem',
+            'order_salesorderadditionallineitem',
             'order_salesorderallocation',
         ]
     }
