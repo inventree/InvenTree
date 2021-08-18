c1ea6dbb9b753603f80df568a5c5f11261fa0c6b
Oliver <oliver.henry.walters@gmail.com>
2021-08-18 12:28:09 +1000

    Remove commented out functionality from the entrypoint command

--- docker/init.sh
+++ docker/init.sh
@@ -36,21 +36,7 @@ if [[ -n "$INVENTREE_PY_ENV" ]]; then
     # e.g docker-compose -f docker-compose.dev.yml run inventree-dev-server invoke install
 fi
 
-# Wait for the InvenTree database to be ready
-# cd ${INVENTREE_MNG_DIR}
-# echo "InvenTree: Waiting for database connection"
-# invoke wait && echo "InvenTree: Database connection successful" || { echo "InvenTree: Failed to connect to db due to errors, aborting"; exit 1; }
-# sleep 5
-
 cd ${INVENTREE_HOME}
-
-# We assume at this stage that the database is up and running
-# echo "InvenTree: Checking database..."
-# invoke check || exit 1
-
-# Can be run as a cron job or directly to clean out expired sessions.
-# cd ${INVENTREE_MNG_DIR}
-# python3 manage.py clearsessions || exit 1
 
 # Launch the CMD *after* the ENTRYPOINT completes
 exec "$@"
