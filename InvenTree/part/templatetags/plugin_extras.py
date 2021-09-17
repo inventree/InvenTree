771c453c404dfe189d312df3a99568d75b4d3f00
Matthias <matthias.mair@oewf.org>
2021-09-17 22:47:49 +0200

    settings UI integration

---
+++ InvenTree/part/templatetags/plugin_extras.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+
+""" This module provides template tags for handeling plugins
+"""
+from django.utils.translation import ugettext_lazy as _
+from django.conf import settings as djangosettings
+
+from django import template
+
+
+register = template.Library()
+
+
+@register.simple_tag()
+def plugin_list(*args, **kwargs):
+    """ Return a list of all installed integration plugins """
+    return djangosettings.INTEGRATION_PLUGIN_LIST
+
+@register.simple_tag()
+def plugin_settings(plugin, *args, **kwargs):
+    """ Return a list of all settings for a plugin """
+    return djangosettings.INTEGRATION_PLUGIN_SETTING.get(plugin)
