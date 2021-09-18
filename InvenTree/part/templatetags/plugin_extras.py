3b2cb43ece01de30c4c1e070b46440946a3c1695
Matthias <matthias.mair@oewf.org>
2021-09-18 04:46:50 +0200

    mixin_enabled check for templates

--- InvenTree/part/templatetags/plugin_extras.py
+++ InvenTree/part/templatetags/plugin_extras.py
@@ -19,3 +19,9 @@ def plugin_list(*args, **kwargs):
 def plugin_settings(plugin, *args, **kwargs):
     """ Return a list of all settings for a plugin """
     return djangosettings.INTEGRATION_PLUGIN_SETTING.get(plugin)
+
+
+@register.simple_tag()
+def mixin_enabled(plugin, key, *args, **kwargs):
+    """ Return if the mixin is existant and configured in the plugin """
+    return plugin.mixin_enabled(key)
