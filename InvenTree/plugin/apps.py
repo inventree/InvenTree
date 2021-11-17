c3ea0f0704e112a3a3934bafce7aca408cf22cf5
Matthias <matthias.mair@oewf.org>
2021-11-17 01:51:13 +0100

    indentations fix

--- InvenTree/plugin/apps.py
+++ InvenTree/plugin/apps.py
@@ -287,8 +287,8 @@ class PluginAppConfig(AppConfig):
                 apps.all_models[plugin_path].pop(model)
 
             # clear the registry for that app
-             # so that the import trick will work on reloading the same plugin
-             # -> the registry is kept for the whole lifecycle
+            # so that the import trick will work on reloading the same plugin
+            # -> the registry is kept for the whole lifecycle
             apps.all_models.pop(app_name)
 
         # remove plugin from installed_apps
