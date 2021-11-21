6533457400701831c524a17419f38b3c97983be7
Matthias <matthias.mair@oewf.org>
2021-11-21 19:08:01 +0100

    always drop out of maintenance on startup

--- InvenTree/plugin/apps.py
+++ InvenTree/plugin/apps.py
@@ -2,6 +2,7 @@
 from __future__ import unicode_literals
 
 from django.apps import AppConfig
+from maintenance_mode.core import set_maintenance_mode
 
 from plugin.registry import plugins
 
@@ -11,5 +12,10 @@ class PluginAppConfig(AppConfig):
 
     def ready(self):
         if not plugins.is_loading:
+            # this is the first startup
             plugins.collect_plugins()
             plugins.load_plugins()
+
+            # drop out of maintenance
+            # makes sure we did not have an error in reloading and maintenance is still active
+            set_maintenance_mode(False)
