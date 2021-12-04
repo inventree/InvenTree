ed5bb3d26471b1f416c30aeaa230a74b28dcb4ef
Matthias <matthias.mair@oewf.org>
2021-12-04 19:49:43 +0100

    turn around template loader order

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -359,9 +359,9 @@ TEMPLATES = [
             ],
             'loaders': [(
                 'django.template.loaders.cached.Loader', [
-                    'django.template.loaders.app_directories.Loader',
-                    'django.template.loaders.filesystem.Loader',
                     'plugin.loader.PluginTemplateLoader',
+                    'django.template.loaders.filesystem.Loader',
+                    'django.template.loaders.app_directories.Loader',
                 ])
             ],
         },
