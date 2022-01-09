f9742ab41d6f75d44f853b7767b37f30fb289512
Matthias <matthias.mair@oewf.org>
2022-01-09 03:34:27 +0100

    add integration test for plugin

---
+++ InvenTree/plugin/samples/integration/test_api_caller.py
@@ -0,0 +1,20 @@
+""" Unit tests for action caller sample"""
+
+from django.test import TestCase
+
+from plugin import plugin_registry
+
+class SampleApiCallerPluginTests(TestCase):
+    """ Tests for SampleApiCallerPluginTests """
+
+    def test_return(self):
+        """check if the external api call works"""
+        # The plugin should be defined
+        self.assertIn('sample-api-caller', plugin_registry.plugins)
+        plg = plugin_registry.plugins['sample-api-caller']
+        self.assertTrue(plg)
+
+        # do an api call
+        result = plg.get_external_url()
+        self.assertTrue(result)
+        self.assertIn('data', result,)
