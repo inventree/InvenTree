5ae757515698feb5ee6923ee33e77ff65f1bb646
Julian-Guinane <julian.guinane@gmail.com>
2022-01-21 11:46:20 +1100

    update for Inventree compatibility

--- InvenTree/plugins/fablab_barcode.py
+++ InvenTree/plugins/fablab_barcode.py
@@ -1,24 +1,24 @@
-"""
-Barcode reader class for part IPNs used in the FabLab.
-Author: Julian Guinane (jgui0653@uni.sydney.edu.au)
-"""
-
 # -*- coding: utf-8 -*-
+"""Barcode reader class for part IPNs used in the FabLab."""
 
 import json
 import barcodenumber
 
-from barcodes.barcode import BarcodePlugin
+from plugin import IntegrationPluginBase
+from plugin.mixins import BarcodeMixin
 
 from part.models import Part
-
 from rest_framework.exceptions import ValidationError
 
 
-class FablabBarcodePlugin(BarcodePlugin):
+class FablabBarcodePlugin(BarcodeMixin, IntegrationPluginBase):
 
     PLUGIN_NAME = "FablabBarcode"
-    BARCODE_STANDARDS = ['EAN', 'EAN13', 'EAN8', 'UPC'] # print(barcodenumber.barcodes()) for list of possible choices
+    BARCODE_STANDARDS = ['EAN', 'EAN13', 'EAN8', 'UPC']  # print(barcodenumber.barcodes()) for list of possible choices
+
+    # metadata
+    AUTHOR = "Julian Guinane (jgui0653@uni.sydney.edu.au)"
+    DESCRIPTION = "Barcode reader class for part IPNs used in the FabLab."
 
     @property
     def name(self):
@@ -31,13 +31,13 @@ class FablabBarcodePlugin(BarcodePlugin):
           * ean8
           * ean13
           * upc (ean12)
-        """       
+        """
 
         # Iterate through each barcode type and check validity
         for codeType in self.BARCODE_STANDARDS:
             if barcodenumber.check_code(codeType, self.data):
                 return True
-        
+
         # Invalid if data does not match any of the standards
         return False
 
@@ -47,4 +47,4 @@ class FablabBarcodePlugin(BarcodePlugin):
             part = Part.objects.get(IPN__iexact=self.data)
             return part
         except (ValueError, Part.DoesNotExist):
-                raise ValidationError({'part', 'Part does not exist'})
+            raise ValidationError({'part', 'Part does not exist'})
