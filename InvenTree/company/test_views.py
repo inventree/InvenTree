440245c812d83d0b1d4e49e14510612d6c5b99e3
Matthias <matthias.mair@oewf.org>
2022-02-13 05:37:28 +0100

    PEP fix

--- InvenTree/company/test_views.py
+++ InvenTree/company/test_views.py
@@ -3,8 +3,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-import json
-
 from django.test import TestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
