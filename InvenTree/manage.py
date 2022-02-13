673435fe901a42a2f6fbd0f14cda97fddced5205
Matthias <matthias.mair@oewf.org>
2022-02-13 04:56:42 +0100

    simpler coverage ignore

--- InvenTree/manage.py
+++ InvenTree/manage.py
@@ -10,13 +10,13 @@ if __name__ == "__main__":
         # The above import may fail for some other reason. Ensure that the
         # issue is really that Django is missing to avoid masking other
         # exceptions on Python 2.
-        try:  # pragma: no cover
-            import django  # pragma: no cover  # noqa: F401
-        except ImportError:  # pragma: no cover
+        try:
+            import django  # noqa: F401
+        except ImportError:
             raise ImportError(
                 "Couldn't import Django. Are you sure it's installed and "
                 "available on your PYTHONPATH environment variable? Did you "
                 "forget to activate a virtual environment?"
-            )  # pragma: no cover
-        raise  # pragma: no cover
+            )
+        raise
     execute_from_command_line(sys.argv)
