6bc53c13088d2744937c7c77bb2ec08005f0e013
Matthias <matthias.mair@oewf.org>
2022-02-13 19:55:50 +0100

    will never be true in testing

--- InvenTree/InvenTree/status.py
+++ InvenTree/InvenTree/status.py
@@ -60,21 +60,21 @@ def is_email_configured():
         configured = False
 
         # Display warning unless in test mode
-        if not settings.TESTING:
+        if not settings.TESTING:  # pragma: no cover
             logger.debug("EMAIL_HOST is not configured")
 
     if not settings.EMAIL_HOST_USER:
         configured = False
 
         # Display warning unless in test mode
-        if not settings.TESTING:
+        if not settings.TESTING:  # pragma: no cover
             logger.debug("EMAIL_HOST_USER is not configured")
 
     if not settings.EMAIL_HOST_PASSWORD:
         configured = False
 
         # Display warning unless in test mode
-        if not settings.TESTING:
+        if not settings.TESTING:  # pragma: no cover
             logger.debug("EMAIL_HOST_PASSWORD is not configured")
 
     return configured
