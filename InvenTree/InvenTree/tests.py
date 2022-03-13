44a2845a752235e52a14bee98162e0ec0ba16885
Matthias <matthias.mair@oewf.org>
2022-03-13 22:35:46 +0100

    add test back in

--- InvenTree/InvenTree/tests.py
+++ InvenTree/InvenTree/tests.py
@@ -453,12 +453,12 @@ class TestSettings(TestCase):
         self.assertEqual(user_count(), 1)
 
         # enough set - duplicate entry
-        #with self.assertRaises(IntegrityError):
-        #    self.env.set('INVENTREE_ADMIN_USER', 'admin')  # set username
-        #    self.env.set('INVENTREE_ADMIN_EMAIL', 'info@example.com')  # set email
-        #    self.env.set('INVENTREE_ADMIN_PASSWORD', 'password123')  # set password
-        #    self.run_reload()
-        #self.assertEqual(user_count(), 1)
+        with self.assertRaises(IntegrityError):
+            self.env.set('INVENTREE_ADMIN_USER', 'admin')  # set username
+            self.env.set('INVENTREE_ADMIN_EMAIL', 'info@example.com')  # set email
+            self.env.set('INVENTREE_ADMIN_PASSWORD', 'password123')  # set password
+            self.run_reload()
+        self.assertEqual(user_count(), 1)
 
         # make sure to clean up
         settings.TESTING_ENV = False
