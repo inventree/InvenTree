547c51b81b768add1ec80437a8b8a26f9734856e
Matthias Mair <66015116+matmair@users.noreply.github.com>
2022-04-06 01:47:11 +0200

    pep fix

--- InvenTree/plugin/builtin/integration/test_core_notifications.py
+++ InvenTree/plugin/builtin/integration/test_core_notifications.py
@@ -1,8 +1,6 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
-from allauth.account.models import EmailAddress
-
 from plugin.models import NotificationUserSetting
 from part.test_part import BaseNotificationIntegrationTest
 from plugin.builtin.integration.core_notifications import CoreNotificationsPlugin
