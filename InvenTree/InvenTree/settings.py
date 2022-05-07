8f1b98670f881dfbc0685b013dac5d7092455417
Matthias <code@mjmair.com>
2022-05-08 00:59:42 +0200

    Adding in app

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -239,6 +239,7 @@ INSTALLED_APPS = [
     'report.apps.ReportConfig',
     'stock.apps.StockConfig',
     'users.apps.UsersConfig',
+    'oobe.apps.OobeConfig',
     'plugin.apps.PluginAppConfig',
     'InvenTree.apps.InvenTreeConfig',       # InvenTree app runs last
 
