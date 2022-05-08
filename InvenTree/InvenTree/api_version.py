6415a20e6e4a6b4ed4e1fb5cacc11a3406f8d4fa
Oliver Walters <oliver.henry.walters@gmail.com>
2022-05-08 13:33:28 +1000

    Update api_version.py

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -4,10 +4,14 @@ InvenTree API version information
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 44
+INVENTREE_API_VERSION = 45
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
+
+v45 -> 2022-05-08 : https://github.com/inventree/InvenTree/pull/2944
+    - Settings are now accessed via the API using their unique key, not their PK
+    - This allows the settings to be accessed without prior knowledge of the PK
 
 v44 -> 2022-05-04 : https://github.com/inventree/InvenTree/pull/2931
     - Converting more server-side rendered forms to the API
