0938c98faf04d1c5e58b2162acea9fc4794456d5
Matthias <code@mjmair.com>
2022-05-08 17:39:40 +0200

    dynamic implementation

--- InvenTree/oobe/forms.py
+++ InvenTree/oobe/forms.py
@@ -5,9 +5,36 @@ Django Forms for interacting with oobe setups
 from django import forms
 
 
-class ContactForm1(forms.Form):
-    subject = forms.CharField(max_length=100)
+class CommentTextWidget(forms.TextInput):
+    """Input for showing a comment as a Text field"""
+    input_type = 'text'
+
+    def render(self, name, value, attrs=None, **kwargs):
+        return '' if value is None else value
+
+
+class CommentTextField(forms.CharField):
+    widget=CommentTextWidget
+    required = False
+
+
+class EmptyForm(forms.Form):
+    pass
+
+
+# region defaults
+# TODO remove
+class ContactForm3(forms.Form):
+    comment = CommentTextField(label='Info text changed')
+    message1 = forms.CharField(widget=forms.Textarea)
     sender = forms.EmailField()
 
-class ContactForm2(forms.Form):
-    message = forms.CharField(widget=forms.Textarea)
+class ContactForm4(forms.Form):
+    sender = forms.EmailField()
+    message2 = forms.CharField(widget=forms.Textarea)
+
+class ContactForm5(forms.Form):
+    sender = forms.EmailField()
+    sender = forms.EmailField()
+    message3 = forms.CharField(widget=forms.Textarea)
+# endregion
--- InvenTree/oobe/views.py
+++ InvenTree/oobe/views.py
@@ -1,19 +1,79 @@
 
+from xml.dom import NotFoundErr
 from django.shortcuts import render
+from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
 
 from common.views import NamedMultiStepFormView
 
-from .forms import ContactForm1, ContactForm2
+from . import forms
 
+
+global_form_list = {
+    'abc': {
+        'steps': [
+            forms.ContactForm3,
+            forms.ContactForm4,
+            forms.ContactForm5,
+        ],
+        'done': _('Done with initial setup')
+    },
+}
 
 class SetupWizard(NamedMultiStepFormView):
-    form_list = [ContactForm1, ContactForm2]
+    form_list = [forms.EmptyForm, ]
     template_name = 'oobe/setup.html'
 
+    # region overrides
+    @classmethod
+    def get_initkwargs(cls, *args, **kwargs):
+        """Override form_list if not supplied"""
+        if not args and not hasattr(kwargs, 'form_list'):
+            kwargs['form_list'] = cls.form_list
+        return super().get_initkwargs(*args, **kwargs)
+    
+    def __init__(self, *args, **kwargs):
+        """Set dynamic flags"""
+        self.from_list_overriden = False
+        super().__init__(*args, **kwargs)
+
+    def get_form_list(self):
+        """Make sure the dynamic form_list is used"""
+        self._set_form_list()
+        return super().get_form_list()
+
+    def get_form(self, step=None, data=None, files=None):
+        """Make sure the dynamic form_list is used"""
+        self._set_form_list()
+        return super().get_form(step, data, files)
+
+    def get_step_url(self, step):
+        """Override url lookup to introduce current setup into lookup"""
+        return reverse(self.url_name, kwargs={'step': step, 'setup': self.kwargs.get('setup')})
+    # endregion
+
+    def _set_form_list(self):
+        """Helper function to make sure the dynamic form_list is used"""
+        if self.from_list_overriden:
+            return
+
+        # Update form_list to use the setup (referenced by slug)
+        # Check if setup slug is valid
+        ref = self.kwargs.get('setup', None)
+        if ref:
+            raise NotFoundErr()
+
+        # Check if steps are valid
+        self.setup_def = global_form_list.get(self.setup_def, None)
+        if not self.setup_def:
+            raise NotFoundErr()
+        self.form_list = self.get_initkwargs(self.setup_def.get('steps'), url_name= self.url_name)['form_list']
+        self.from_list_overriden = True
+
     def done(self, form_list, **kwargs):
+        print(kwargs)
 
         return render(self.request, 'oobe/done.html', {
             'form_data': [form.cleaned_data for form in form_list],
-            'success_message': _('Done with initial setup'),
+            'success_message': self.setup_def.get('done'),
         })
