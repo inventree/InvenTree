2ae5fcf6a8fac89f284a23c564a252eb4fc41858
Matthias <code@mjmair.com>
2022-05-15 00:46:56 +0200

    PEP fix

--- InvenTree/InvenTree/test_middleware.py
+++ InvenTree/InvenTree/test_middleware.py
@@ -60,7 +60,7 @@ class MiddlewareTests(TestCase):
         self.check_path(reverse('settings.js'), HTTP_Authorization=f'Token {token}')
 
         # Request with broken token
-        self.check_path(reverse('settings.js'), 401, HTTP_Authorization=f'Token abcd123')
+        self.check_path(reverse('settings.js'), 401, HTTP_Authorization='Token abcd123')
 
         # should still fail without token
         self.check_path(reverse('settings.js'), 401)
