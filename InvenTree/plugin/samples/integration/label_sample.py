b55c5b777a449636ded4cdb85daefc6c7b69c5af
Oliver Walters <oliver.henry.walters@gmail.com>
2022-05-19 22:03:39 +1000

    Adds a very simple sample plugin for label printing

---
+++ InvenTree/plugin/samples/integration/label_sample.py
@@ -0,0 +1,18 @@
+
+from plugin import InvenTreePlugin
+from plugin.mixins import LabelPrintingMixin
+
+
+class SampleLabelPrinter(LabelPrintingMixin, InvenTreePlugin):
+    """
+    Sample plugin which provides a 'fake' label printer endpoint
+    """
+
+    NAME = "Label Printer"
+    SLUG = "samplelabel"
+    TITLE = "Sample Label Printer"
+    DESCRIPTION = "A sample plugin which provides a (fake) label printer interface"
+    VERSION = "0.1"
+
+    def print_label(self, label, **kwargs):
+        print("OK PRINTING")
