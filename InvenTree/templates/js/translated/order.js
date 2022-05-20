f036252fb9c2f3f3522afd0ed9c27237f569bea0
Maksim Stojkovic <18454392+maksimstojkovic@users.noreply.github.com>
2022-05-21 01:31:01 +1000

    Added completePendingShipments to exported section

--- InvenTree/templates/js/translated/order.js
+++ InvenTree/templates/js/translated/order.js
@@ -24,6 +24,7 @@
     cancelSalesOrder,
     completePurchaseOrder,
     completeShipment,
+    completePendingShipments,
     createSalesOrder,
     createSalesOrderShipment,
     editPurchaseOrderLineItem,
