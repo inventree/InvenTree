e2288317726f795e1c1df2f113233da050b413b6
Matthias Mair <code@mjmair.com>
2022-05-22 03:36:49 +0200

    test the base process of printing

--- InvenTree/plugin/base/label/test_label_mixin.py
+++ InvenTree/plugin/base/label/test_label_mixin.py
@@ -3,6 +3,8 @@
 from django.urls import reverse
 
 from InvenTree.helpers import InvenTreeTestCase
+from label.models import PartLabel
+from part.models import Part
 from plugin.registry import registry
 
 
@@ -82,3 +84,28 @@ class LabelMixinTests(InvenTreeTestCase):
         data = response.data[0]
 
         self.assertEqual(data['key'], 'samplelabel')
+
+    def test_printing_process(self):
+        """Test that a label can be printed"""
+        # Lookup references
+        label = PartLabel.objects.first()
+        part = Part.objects.first()
+        plugin_ref = 'samplelabel'
+
+        # Non-exsisting plugin
+        url = reverse('api-part-label-print', kwargs={'pk': label.pk})
+        self.get(f'{url}?parts={part.pk}&plugin={plugin_ref}123', expected_code=404)
+
+        # Inactive plugin
+        url = reverse('api-part-label-print', kwargs={'pk': label.pk})
+        self.get(f'{url}?parts={part.pk}&plugin={plugin_ref}', expected_code=404)
+
+        # Activate the plugin
+        plugin = registry.get_plugin(plugin_ref)
+
+        config = plugin.plugin_config()
+        config.active = True
+        config.save()
+
+        # Active plugin
+        self.get(f'{url}?parts={part.pk}&plugin=samplelabel', expected_code=200)
