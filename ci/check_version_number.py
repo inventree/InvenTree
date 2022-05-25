fdf029f7875b7472dd83726ce90a7a4c9e528120
Oliver Walters <oliver.henry.walters@gmail.com>
2022-05-25 23:24:56 +1000

    Working on github action

--- ci/check_version_number.py
+++ ci/check_version_number.py
@@ -1,5 +1,17 @@
 """
-On release, ensure that the release tag matches the InvenTree version number!
+Ensure that the release tag matches the InvenTree version number:
+
+master / main branch:
+    - version number must end with 'dev'
+
+stable branch:
+    - version number must *not* end with 'dev'
+    - version number cannot already exist as a release tag
+
+tagged branch:
+    - version number must match tag being built
+    - version number cannot already exist as a release tag
+
 """
 
 import argparse
@@ -11,17 +23,22 @@ if __name__ == '__main__':
 
     here = os.path.abspath(os.path.dirname(__file__))
 
-    for var in [
-        'GITHUB_EVENT_NAME',
-        'GITHUB_REF',
-        'GITHUB_REF_TYPE',
-        'GITHUB_BASE_REF',
-    ]:
-        print(f"'{var}' - '{os.environ.get(var, '')}'")
+    # GITHUB_REF_TYPE may be either 'branch' or 'tag'
+    GITHUB_REF_TYPE = os.environ['GITHUB_REF_TYPE']
+
+    # GITHUB_REF may be either 'refs/heads/<branch>' or 'refs/heads/<tag>'
+    GITHUB_REF = os.environ['GITHUB_REF']
+
+    # GITHUB_BASE_REF is the base branch e.g. 'master' or 'stable'
+    GITHUB_BASE_REF = os.environ['GITHUB_BASE_REF']
 
     version_file = os.path.join(here, '..', 'InvenTree', 'InvenTree', 'version.py')
 
     version = None
+
+    print("GITHUB_REF_TYPE:", GITHUB_REF_TYPE)
+    print("GITHUB_REF:", GITHUB_REF)
+    print("GITHUB_BASE_REF:", GITHUB_BASE_REF)
 
     with open(version_file, 'r') as f:
 
@@ -37,6 +54,32 @@ if __name__ == '__main__':
         version = results[0]
 
     print(f"InvenTree Version: '{version}'")
+
+    if GITHUB_BASE_REF == 'stable' and GITHUB_REF_TYPE == 'branch':
+        print("Checking requirements for 'stable' release")
+
+        pattern = r"^\d+(\.\d+)+$"
+        result = re.match(pattern, version)
+
+        if result is None:
+            print(f"Version number '{version}' does not match required pattern for stable branch")
+            sys.exit(1)
+
+    elif GITHUB_BASE_REF in ['master', 'main'] and GITHUB_REF_TYPE == 'branch':
+        print("Checking requirements for main development branch:")
+
+        pattern = r"^\d+(\.\d+)+ dev$"
+        result = re.match(pattern, version)
+
+        if result is None:
+            print(f"Version number '{version}' does not match required pattern for development branch")
+            sys.exit(1)
+
+    elif GITHUB_REF_TYPE == 'tag':
+        print("Checking requirements for tagged release")
+
+    # error out
+    sys.exit(1)
 
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tag', help='Compare against specified version tag', action='store')
