d1c92a9eda6f9ba76485301684624fd08a29eab7
Oliver <oliver.henry.walters@gmail.com>
2022-05-26 18:24:01 +1000

    Update version.py
    Fix typo?

--- InvenTree/InvenTree/version.py
+++ InvenTree/InvenTree/version.py
@@ -12,7 +12,7 @@ import common.models
 from InvenTree.api_version import INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = "0.8Bum.0 dev"
+INVENTREE_SW_VERSION = "0.8.0 dev"
 
 
 def inventreeInstanceName():
