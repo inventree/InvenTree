fe5ff325998bcd7f20caca2c251f3992558ed98b
Oliver Walters <oliver.henry.walters@gmail.com>
2022-06-05 19:25:37 +1000

    Moving PartCategoryParameterTemplate model to the API
    - Add detail API endpoint
    - Add 'create' action to LIST endpoint

--- InvenTree/part/api.py
+++ InvenTree/part/api.py
@@ -194,7 +194,7 @@ class CategoryMetadata(generics.RetrieveUpdateAPIView):
     queryset = PartCategory.objects.all()
 
 
-class CategoryParameterList(generics.ListAPIView):
+class CategoryParameterList(generics.ListCreateAPIView):
     """API endpoint for accessing a list of PartCategoryParameterTemplate objects.
 
     - GET: Return a list of PartCategoryParameterTemplate objects
@@ -233,6 +233,13 @@ class CategoryParameterList(generics.ListAPIView):
                 pass
 
         return queryset
+
+
+class CategoryParameterDetail(generics.RetrieveUpdateDestroyAPIView):
+    """Detail endpoint fro the PartCategoryParameterTemplate model"""
+
+    queryset = PartCategoryParameterTemplate.objects.all()
+    serializer_class = part_serializers.CategoryParameterTemplateSerializer
 
 
 class CategoryTree(generics.ListAPIView):
@@ -1855,7 +1862,11 @@ part_api_urls = [
     # Base URL for PartCategory API endpoints
     re_path(r'^category/', include([
         re_path(r'^tree/', CategoryTree.as_view(), name='api-part-category-tree'),
-        re_path(r'^parameters/', CategoryParameterList.as_view(), name='api-part-category-parameter-list'),
+
+        re_path(r'^parameters/', include([
+            re_path('^(?P<pk>\d+)/', CategoryParameterDetail.as_view(), name='api-part-category-parameter-detail'),
+            re_path('^.*$', CategoryParameterList.as_view(), name='api-part-category-parameter-list'),
+        ])),
 
         # Category detail endpoints
         re_path(r'^(?P<pk>\d+)/', include([
