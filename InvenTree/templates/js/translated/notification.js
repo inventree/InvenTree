1aae81f453c6750fdebe9bd3135d04f0e7e960fb
Oliver Walters <oliver.henry.walters@gmail.com>
2022-06-24 21:45:58 +1000

    Split basic "show message" function out into new js file

--- InvenTree/templates/js/translated/notification.js
+++ InvenTree/templates/js/translated/notification.js
@@ -1,5 +1,9 @@
 {% load i18n %}
 
+/* globals
+    showMessage,
+*/
+
 /* exported
     loadNotificationTable,
     showAlertOrCache,
@@ -164,63 +168,6 @@ function showCachedAlerts() {
     clearCachedAlerts();
 }
 
-
-/*
- * Display an alert message at the top of the screen.
- * The message will contain a "close" button,
- * and also dismiss automatically after a certain amount of time.
- *
- * arguments:
- * - message: Text / HTML content to display
- *
- * options:
- * - style: alert style e.g. 'success' / 'warning'
- * - timeout: Time (in milliseconds) after which the message will be dismissed
- */
-function showMessage(message, options={}) {
-
-    var style = options.style || 'info';
-
-    var timeout = options.timeout || 5000;
-
-    var target = options.target || $('#alerts');
-
-    var details = '';
-
-    if (options.details) {
-        details = `<p><small>${options.details}</p></small>`;
-    }
-
-    // Hacky function to get the next available ID
-    var id = 1;
-
-    while ($(`#alert-${id}`).exists()) {
-        id++;
-    }
-
-    var icon = '';
-
-    if (options.icon) {
-        icon = `<span class='${options.icon}'></span>`;
-    }
-
-    // Construct the alert
-    var html = `
-    <div id='alert-${id}' class='alert alert-${style} alert-dismissible fade show' role='alert'>
-        ${icon}
-        <b>${message}</b>
-        ${details}
-        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-    </div>
-    `;
-
-    target.append(html);
-
-    // Remove the alert automatically after a specified period of time
-    $(`#alert-${id}`).delay(timeout).slideUp(200, function() {
-        $(this).alert(close);
-    });
-}
 
 var notificationWatcher = null; // reference for the notificationWatcher
 /**
