cc8238b790aadaa71c5b3221473043c5a7af6f62
Oliver <oliver.henry.walters@gmail.com>
2022-07-06 15:28:44 +1000

    Allow BOM table to be filtered by "stock available" parameter (#3301)
    * Allow BOM table to be filtered by "stock available" parameter
    
    * Update API version info
    
    * PEP fixes

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -7,8 +7,12 @@ INVENTREE_API_VERSION = 61
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
 
+v63 -> 2022-07-06 : https://github.com/inventree/InvenTree/pull/3301
+    - Allow BOM List API endpoint to be filtered by "available_stock" paramater
+
 v62 -> 2022-07-05 : https://github.com/inventree/InvenTree/pull/3296
     - Allows search on BOM List API endpoint
+    - Allows ordering on BOM List API endpoint
 
 v61 -> 2022-06-12 : https://github.com/inventree/InvenTree/pull/3183
     - Migrate the "Convert Stock Item" form class to use the API
