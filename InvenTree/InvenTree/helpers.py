5d7d4287ebb1c4746985528d034aff2281fc90d8
Oliver Walters <oliver.henry.walters@gmail.com>
2022-07-24 15:01:52 +1000

    Add unit testing for image downloader function

--- InvenTree/InvenTree/helpers.py
+++ InvenTree/InvenTree/helpers.py
@@ -13,6 +13,7 @@ from django.conf import settings
 from django.contrib.auth.models import Permission
 from django.core.exceptions import FieldError, ValidationError
 from django.core.files.storage import default_storage
+from django.core.validators import URLValidator
 from django.http import StreamingHttpResponse
 from django.test import TestCase
 from django.utils.translation import gettext_lazy as _
@@ -111,23 +112,35 @@ def download_image_from_url(remote_url, timeout=2.5):
         requests.exceptions.Timeout: Connection timed out
         requests.exceptions.HTTPError: Server responded with invalid response code
         ValueError: Server responded with invalid 'Content-Length' value
+        TypeError: Response is not a valid image
     """
 
+    # Check that the provided URL at least looks valid
+    validator = URLValidator()
+    validator(remote_url)
+
+    # Calculate maximum allowable image size (in bytes)
     max_size = int(InvenTreeSetting.get_setting('INVENTREE_DOWNLOAD_IMAGE_MAX_SIZE')) * 1024 * 1024
 
     try:
         response = requests.get(
             remote_url,
             timeout=timeout,
-            allow_redirects=False,
+            allow_redirects=True,
             stream=True,
         )
         # Throw an error if anything goes wrong
         response.raise_for_status()
-    except (requests.exceptions.ConnectionError, requests.exceptions.Timeout) as exc:
-        logger.error(_("Connection Error") + f": {type(exc)}")
+    except requests.exceptions.ConnectionError as exc:
+        raise Exception(_("Connection error") + f": {str(exc)}")
+    except requests.exceptions.Timeout as exc:
         raise exc
     except requests.exceptions.HTTPError:
+        raise requests.exceptions.HTTPError(_("Server responded with invalid status code") + f": {response.status_code}")
+    except Exception as exc:
+        raise Exception(_("Exception occurred") + f": {str(exc)}")
+
+    if response.status_code != 200:
         raise Exception(_("Server responded with invalid status code") + f": {response.status_code}")
 
     try:
@@ -145,11 +158,15 @@ def download_image_from_url(remote_url, timeout=2.5):
     chunk_size = 64 * 1024
 
     for chunk in response.iter_content(chunk_size=chunk_size):
-        fo.write(chunk)
-        dl_size += chunk_size
+        dl_size += len(chunk)
 
         if dl_size > max_size:
             raise ValueError(_("Image download exceeded maximum size"))
+
+        fo.write(chunk)
+
+    if dl_size == 0:
+        raise ValueError(_("Remote server returned empty response"))
 
     # Now, attempt to convert the downloaded data to a valid image file
     # img.verify() will throw an exception if the image is not valid
@@ -157,7 +174,7 @@ def download_image_from_url(remote_url, timeout=2.5):
         img = Image.open(fo).convert()
         img.verify()
     except Exception:
-        raise Exception(_("Supplied URL is not a valid image file"))
+        raise TypeError(_("Supplied URL is not a valid image file"))
 
     return img
 
