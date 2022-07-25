05d7159882ab2941e469db42c9dcc85cc2fae22d
Oliver <oliver.henry.walters@gmail.com>
2022-07-25 13:00:35 +1000

    Pre-fill quantity on part ordering form (#3395)
    * Pre-fill quantity on part ordering form
    
    - Adds new API endpoint for requirement data
    - Load data on the fly when launching ordering form
    
    * Bump API version

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,10 +2,14 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 66
+INVENTREE_API_VERSION = 67
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
+
+v67 -> 2022-07-25 : https://github.com/inventree/InvenTree/pull/3395
+    - Adds a 'requirements' endpoint for Part instance
+    - Provides information on outstanding order requirements for a given part
 
 v66 -> 2022-07-24 : https://github.com/inventree/InvenTree/pull/3393
     - Part images can now be downloaded from a remote URL via the API
