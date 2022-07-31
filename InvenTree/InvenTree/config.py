6251f39708aa17ef1326c383b9ef8e83cd8e4e01
Oliver Walters <oliver.henry.walters@gmail.com>
2022-07-31 12:04:35 +1000

    Simplify settings.py / config.py
    - get_setting function has been streamlined
    - move some functions into config.py

--- InvenTree/InvenTree/config.py
+++ InvenTree/InvenTree/config.py
@@ -2,10 +2,19 @@
 
 import logging
 import os
+import random
 import shutil
+import string
 from pathlib import Path
 
+import yaml
+
 logger = logging.getLogger('inventree')
+
+
+def is_true(x):
+    """Shortcut function to determine if a value "looks" like a boolean"""
+    return str(x).strip().lower() in ['1', 'y', 'yes', 't', 'true', 'on']
 
 
 def get_base_dir() -> Path:
@@ -13,7 +22,7 @@ def get_base_dir() -> Path:
     return Path(__file__).parent.parent.resolve()
 
 
-def get_config_file() -> Path:
+def get_config_file(create=True) -> Path:
     """Returns the path of the InvenTree configuration file.
 
     Note: It will be created it if does not already exist!
@@ -28,7 +37,7 @@ def get_config_file() -> Path:
         # Config file is *not* specified - use the default
         cfg_filename = base_dir.joinpath('config.yaml').resolve()
 
-    if not cfg_filename.exists():
+    if not cfg_filename.exists() and create:
         print("InvenTree configuration file 'config.yaml' not found - creating default file")
 
         cfg_template = base_dir.joinpath("config_template.yaml")
@@ -36,6 +45,17 @@ def get_config_file() -> Path:
         print(f"Created config file {cfg_filename}")
 
     return cfg_filename
+
+
+def load_config_data() -> map:
+    """Load configuration data from the config file."""
+
+    cfg_file = get_config_file()
+
+    with open(cfg_file, 'r') as cfg:
+        data = yaml.safe_load(cfg)
+
+    return data
 
 
 def get_plugin_file():
@@ -64,19 +84,124 @@ def get_plugin_file():
     return PLUGIN_FILE
 
 
-def get_setting(environment_var, backup_val, default_value=None):
+def get_secret_key():
+    """Return the secret key value which will be used by django.
+
+    Following options are tested, in descending order of preference:
+
+    A) Check for environment variable INVENTREE_SECRET_KEY => Use raw key data
+    B) Check for environment variable INVENTREE_SECRET_KEY_FILE => Load key data from file
+    C) Look for default key file "secret_key.txt"
+    D) Create "secret_key.txt" if it does not exist
+    """
+
+    # Look for environment variable
+    if secret_key := os.getenv('INVENTREE_SECRET_KEY'):
+        logger.info("SECRET_KEY loaded by INVENTREE_SECRET_KEY")  # pragma: no cover
+        return secret_key
+
+    # Look for secret key file
+    if secret_key_file := os.getenv('INVENTREE_SECRET_KEY_FILE'):
+        secret_key_file = Path(secret_key_file).resolve()
+    else:
+        # Default location for secret key file
+        secret_key_file = get_base_dir().joinpath("secret_key.txt").resolve()
+
+    if not secret_key_file.exists():
+        logger.info(f"Generating random key file at '{secret_key_file}'")
+
+        # Create a random key file
+        options = string.digits + string.ascii_letters + string.punctuation
+        key = ''.join([random.choice(options) for i in range(100)])
+        secret_key_file.write_text(key)
+
+    logger.info(f"Loading SECRET_KEY from '{secret_key_file}'")
+
+    with open(secret_key_file, 'r') as f:
+        key_data = f.read().strip()
+
+    return key_data
+
+
+def get_setting(env_var=None, config_key=None, default_value=None):
     """Helper function for retrieving a configuration setting value.
 
     - First preference is to look for the environment variable
     - Second preference is to look for the value of the settings file
     - Third preference is the default value
+
+    Arguments:
+        env_var: Name of the environment variable e.g. 'INVENTREE_STATIC_ROOT'
+        config_key: Key to lookup in the configuration file
+        default_value: Value to return if first two options are not provided
+
     """
-    val = os.getenv(environment_var)
 
-    if val is not None:
-        return val
+    # First, try to load from the environment variables
+    if env_var is not None:
+        val = os.getenv(env_var, None)
 
-    if backup_val is not None:
-        return backup_val
+        if val is not None:
+            return val
 
+    # Next, try to load from configuration file
+    if config_key is not None:
+        cfg_data = load_config_data()
+
+        result = None
+
+        # Hack to allow 'path traversal' in configuration
+        for key in config_key.strip().split('.'):
+            if key not in cfg_data:
+                result = None
+                break
+
+            result = cfg_data[key]
+            cfg_data = cfg_data[key]
+
+        if result is not None:
+            return result
+
+    # Finally, return the default value
     return default_value
+
+
+def get_boolean_setting(env_var=None, config_key=None, default_value=False):
+    """Helper function for retreiving a boolean configuration setting"""
+
+    return is_true(get_setting(env_var, config_key, default_value))
+
+
+def get_media_dir(create=True):
+    """Return the absolute path for the 'media' directory (where uploaded files are stored)"""
+
+    md = get_setting('INVENTREE_MEDIA_ROOT', 'media_root')
+
+    if not md:
+        raise FileNotFoundError('INVENTREE_MEDIA_ROOT not specified')
+
+    md = Path(md)
+
+    if create:
+        md.mkdir(parents=True, exist_ok=True)
+
+    return md
+
+
+def get_static_dir(cfg_data=None, create=True):
+    """Return the absolute path for the 'static' directory (where static files are stored)"""
+
+    if not cfg_data:
+        cfg_data = load_config_data()
+
+    sd = get_setting('INVENTREE_STATIC_ROOT', 'static_root')
+
+    if not sd:
+        raise FileNotFoundError('INVENTREE_STATIC_ROOT not specified')
+
+    sd = Path(sd)
+
+    if create:
+        sd.mkdir(parents=True, exist_ok=True)
+
+    return sd
