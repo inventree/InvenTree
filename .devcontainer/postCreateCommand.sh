f254bd1880c0f349327022ee9ffc1977504fe01b
luwol03 <luwol03@gmail.com>
2022-08-07 11:31:26 +0000

    Refactored postCreateCommand into own file

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# create folders
+mkdir -p /workspaces/InvenTree/dev/{commandhistory,plugins}
+cd /workspaces/InvenTree
+
+# create venv
+python3 -m venv dev/venv
+. dev/venv/bin/activate
+
+# setup inventree server
+pip install invoke
+inv update
+inv setup-test --ignore-update --path dev/demo-dataset
