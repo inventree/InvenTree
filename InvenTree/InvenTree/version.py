7f9122af03cc60e798a2d62bdd48e067daec96f3
Oliver Walters <oliver.henry.walters@gmail.com>
2022-08-09 12:05:23 +1000

    Bump version to 0.8.2

--- InvenTree/InvenTree/version.py
+++ InvenTree/InvenTree/version.py
@@ -13,7 +13,7 @@ import common.models
 from InvenTree.api_version import INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = "0.8.1"
+INVENTREE_SW_VERSION = "0.8.2"
 
 
 def inventreeInstanceName():
