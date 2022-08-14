23b74143fd42caaf122eb5a400c8521083bb29bf
Matthias Mair <code@mjmair.com>
2022-08-14 08:40:58 -0700

    do not cover as we turn on all plugins

--- InvenTree/plugin/models.py
+++ InvenTree/plugin/models.py
@@ -167,7 +167,7 @@ class PluginConfig(models.Model):
             return False
 
         # Not loaded plugin
-        return self.plugin.check_is_sample()
+        return self.plugin.check_is_sample()  # pragma: no cover
 
 
 class PluginSetting(common.models.BaseInvenTreeSetting):
