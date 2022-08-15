inventree/InvenTree

commit 794e375009109bec9fe357d90b9aff8eb2e06bc1

Switch to pathlib (#3392)
* switch to pathlib

* more pathlib

* useconvert env to path

* fix typo

* use resolve instead of absolute

* fix gitppod

* also allow parents

* replace more os operations

* fix string replacement feature

* make sure root dirs exsist

* fix replace function

* refactor duplicate code

* reduce code

* make sure dirs exist

* fix typo

* also create parent dirs

* fix match statement

* fix statments expecting string

* return getMigrationFileNames to old behaviour

* fully resolve config file

* make sure comparison works

* use pathlib in tasks

* fix file count test

* reduce code duplication in test + add test for part

* fix test

* re-add os

* Make pathlib usage simpler

tasks.py
@@ -5,6 +5,7 @@ import os
 import pathlib
 import re
 import sys
+from pathlib import Path
 
 from invoke import task
 
@@ -52,23 +53,23 @@ def content_excludes():
     return output
 
 
-def localDir():
+def localDir() -> Path:
     """Returns the directory of *THIS* file.
 
     Used to ensure that the various scripts always run
     in the correct directory.
     """
-    return os.path.dirname(os.path.abspath(__file__))
+    return Path(__file__).parent.resolve()
 
 
 def managePyDir():
     """Returns the directory of the manage.py file."""
-    return os.path.join(localDir(), 'InvenTree')
+    return localDir().joinpath('InvenTree')
 
 
 def managePyPath():
     """Return the path of the manage.py file."""
-    return os.path.join(managePyDir(), 'manage.py')
+    return managePyDir().joinpath('manage.py')
 
 
 def manage(c, cmd, pty: bool = False):
@@ -171,7 +172,7 @@ def translate_stats(c):
 
     The file generated from this is needed for the UI.
     """
-    path = os.path.join('InvenTree', 'script', 'translation_stats.py')
+    path = Path('InvenTree', 'script', 'translation_stats.py')
     c.run(f'python3 {path}')
 
 
@@ -252,12 +253,11 @@ def export_records(c, filename='data.json', overwrite=False, include_permissions
     """
     # Get an absolute path to the file
     if not os.path.isabs(filename):
-        filename = os.path.join(localDir(), filename)
-        filename = os.path.abspath(filename)
+        filename = localDir().joinpath(filename).resolve()
 
     print(f"Exporting database records to file '{filename}'")
 
-    if os.path.exists(filename) and overwrite is False:
+    if filename.exists() and overwrite is False:
         response = input("Warning: file already exists. Do you want to overwrite? [y/N]: ")
         response = str(response).strip().lower()
 
@@ -306,7 +306,7 @@ def import_records(c, filename='data.json', clear=False):
     """Import database records from a file."""
     # Get an absolute path to the supplied filename
     if not os.path.isabs(filename):
-        filename = os.path.join(localDir(), filename)
+        filename = localDir().joinpath(filename)
 
     if not os.path.exists(filename):
         print(f"Error: File '{filename}' does not exist")
@@ -442,8 +442,8 @@ def test_translations(c):
     from django.conf import settings
 
     # setup django
-    base_path = os.getcwd()
-    new_base_path = pathlib.Path('InvenTree').absolute()
+    base_path = Path.cwd()
+    new_base_path = pathlib.Path('InvenTree').resolve()
     sys.path.append(str(new_base_path))
     os.chdir(new_base_path)
     os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InvenTree.settings')
@@ -487,8 +487,8 @@ def test_translations(c):
                     file_new.write(line)
 
     # change out translation files
-    os.rename(file_path, str(file_path) + '_old')
-    os.rename(new_file_path, file_path)
+    file_path.rename(str(file_path) + '_old')
+    new_file_path.rename(file_path)
 
     # compile languages
     print("Compile languages ...")

.gitpod.yml
@@ -7,11 +7,12 @@ tasks:
       export INVENTREE_STATIC_ROOT='/workspace/InvenTree/dev/static'
       export PIP_USER='no'
 
+      sudo apt install gettext
       python3 -m venv venv
       source venv/bin/activate
       pip install invoke
       mkdir dev
-      inv test-setup
+      inv setup-test
       gp sync-done start_server
 
  - name: Start server
@@ -23,6 +24,7 @@ tasks:
       export INVENTREE_MEDIA_ROOT='/workspace/InvenTree/inventree-data/media'
       export INVENTREE_STATIC_ROOT='/workspace/InvenTree/dev/static'
 
+      source venv/bin/activate
       inv server
 
 ports:

InvenTree/InvenTree/config.py
@@ -3,16 +3,17 @@
 import logging
 import os
 import shutil
+from pathlib import Path
 
 logger = logging.getLogger('inventree')
 
 
-def get_base_dir():
+def get_base_dir() -> Path:
     """Returns the base (top-level) InvenTree directory."""
-    return os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+    return Path(__file__).parent.parent.resolve()
 
 
-def get_config_file():
+def get_config_file() -> Path:
     """Returns the path of the InvenTree configuration file.
 
     Note: It will be created it if does not already exist!
@@ -22,16 +23,15 @@ def get_config_file():
     cfg_filename = os.getenv('INVENTREE_CONFIG_FILE')
 
     if cfg_filename:
-        cfg_filename = cfg_filename.strip()
-        cfg_filename = os.path.abspath(cfg_filename)
+        cfg_filename = Path(cfg_filename.strip()).resolve()
     else:
         # Config file is *not* specified - use the default
-        cfg_filename = os.path.join(base_dir, 'config.yaml')
+        cfg_filename = base_dir.joinpath('config.yaml').resolve()
 
-    if not os.path.exists(cfg_filename):
+    if not cfg_filename.exists():
         print("InvenTree configuration file 'config.yaml' not found - creating default file")
 
-        cfg_template = os.path.join(base_dir, "config_template.yaml")
+        cfg_template = base_dir.joinpath("config_template.yaml")
         shutil.copyfile(cfg_template, cfg_filename)
         print(f"Created config file {cfg_filename}")
 
@@ -48,18 +48,18 @@ def get_plugin_file():
 
     if not PLUGIN_FILE:
         # If not specified, look in the same directory as the configuration file
+        config_dir = get_config_file().parent
+        PLUGIN_FILE = config_dir.joinpath('plugins.txt')
+    else:
+        # Make sure we are using a modern Path object
+        PLUGIN_FILE = Path(PLUGIN_FILE)
 
-        config_dir = os.path.dirname(get_config_file())
-
-        PLUGIN_FILE = os.path.join(config_dir, 'plugins.txt')
-
-    if not os.path.exists(PLUGIN_FILE):
+    if not PLUGIN_FILE.exists():
         logger.warning("Plugin configuration file does not exist")
         logger.info(f"Creating plugin file at '{PLUGIN_FILE}'")
 
         # If opening the file fails (no write permission, for example), then this will throw an error
-        with open(PLUGIN_FILE, 'w') as plugin_file:
-            plugin_file.write("# InvenTree Plugins (uses PIP framework to install)\n\n")
+        PLUGIN_FILE.write_text("# InvenTree Plugins (uses PIP framework to install)\n\n")
 
     return PLUGIN_FILE
 

InvenTree/InvenTree/helpers.py
@@ -7,6 +7,7 @@ import os
 import os.path
 import re
 from decimal import Decimal, InvalidOperation
+from pathlib import Path
 from wsgiref.util import FileWrapper
 
 from django.conf import settings
@@ -211,7 +212,7 @@ def getLogoImage(as_file=False, custom=True):
 
     else:
         if as_file:
-            path = os.path.join(settings.STATIC_ROOT, 'img/inventree.png')
+            path = settings.STATIC_ROOT.joinpath('img/inventree.png')
             return f"file://{path}"
         else:
             return getStaticUrl('img/inventree.png')
@@ -687,20 +688,17 @@ def addUserPermissions(user, permissions):
 
 def getMigrationFileNames(app):
     """Return a list of all migration filenames for provided app."""
-    local_dir = os.path.dirname(os.path.abspath(__file__))
-
-    migration_dir = os.path.join(local_dir, '..', app, 'migrations')
-
-    files = os.listdir(migration_dir)
+    local_dir = Path(__file__).parent
+    files = local_dir.joinpath('..', app, 'migrations').iterdir()
 
     # Regex pattern for migration files
-    pattern = r"^[\d]+_.*\.py$"
+    regex = re.compile(r"^[\d]+_.*\.py$")
 
     migration_files = []
 
     for f in files:
-        if re.match(pattern, f):
-            migration_files.append(f)
+        if regex.match(f.name):
+            migration_files.append(f.name)
 
     return migration_files
 

InvenTree/InvenTree/models.py
@@ -437,26 +437,12 @@ class InvenTreeAttachment(models.Model):
         if len(fn) == 0:
             raise ValidationError(_('Filename must not be empty'))
 
-        attachment_dir = os.path.join(
-            settings.MEDIA_ROOT,
-            self.getSubdir()
-        )
-
-        old_file = os.path.join(
-            settings.MEDIA_ROOT,
-            self.attachment.name
-        )
-
-        new_file = os.path.join(
-            settings.MEDIA_ROOT,
-            self.getSubdir(),
-            fn
-        )
-
-        new_file = os.path.abspath(new_file)
+        attachment_dir = settings.MEDIA_ROOT.joinpath(self.getSubdir())
+        old_file = settings.MEDIA_ROOT.joinpath(self.attachment.name)
+        new_file = settings.MEDIA_ROOT.joinpath(self.getSubdir(), fn).resolve()
 
         # Check that there are no directory tricks going on...
-        if os.path.dirname(new_file) != attachment_dir:
+        if new_file.parent != attachment_dir:
             logger.error(f"Attempted to rename attachment outside valid directory: '{new_file}'")
             raise ValidationError(_("Invalid attachment directory"))
 
@@ -473,11 +459,11 @@ class InvenTreeAttachment(models.Model):
         if len(fn.split('.')) < 2:
             raise ValidationError(_("Filename missing extension"))
 
-        if not os.path.exists(old_file):
+        if not old_file.exists():
             logger.error(f"Trying to rename attachment '{old_file}' which does not exist")
             return
 
-        if os.path.exists(new_file):
+        if new_file.exists():
             raise ValidationError(_("Attachment with this filename already exists"))
 
         try:

InvenTree/InvenTree/settings.py
@@ -15,6 +15,7 @@ import random
 import socket
 import string
 import sys
+from pathlib import Path
 
 import django.conf.locale
 from django.core.files.storage import default_storage
@@ -44,7 +45,7 @@ TESTING_ENV = False
 # New requirement for django 3.2+
 DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
+# Build paths inside the project like this: BASE_DIR.joinpath(...)
 BASE_DIR = get_base_dir()
 
 cfg_filename = get_config_file()
@@ -53,7 +54,7 @@ with open(cfg_filename, 'r') as cfg:
     CONFIG = yaml.safe_load(cfg)
 
 # We will place any config files in the same directory as the config file
-config_dir = os.path.dirname(cfg_filename)
+config_dir = cfg_filename.parent
 
 # Default action is to run the system in Debug mode
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -123,19 +124,17 @@ else:
     key_file = os.getenv("INVENTREE_SECRET_KEY_FILE")
 
     if key_file:
-        key_file = os.path.abspath(key_file)  # pragma: no cover
+        key_file = Path(key_file).resolve()  # pragma: no cover
     else:
         # default secret key location
-        key_file = os.path.join(BASE_DIR, "secret_key.txt")
-        key_file = os.path.abspath(key_file)
+        key_file = BASE_DIR.joinpath("secret_key.txt").resolve()
 
-    if not os.path.exists(key_file):  # pragma: no cover
+    if not key_file.exists():  # pragma: no cover
         logger.info(f"Generating random key file at '{key_file}'")
         # Create a random key file
-        with open(key_file, 'w') as f:
-            options = string.digits + string.ascii_letters + string.punctuation
-            key = ''.join([random.choice(options) for i in range(100)])
-            f.write(key)
+        options = string.digits + string.ascii_letters + string.punctuation
+        key = ''.join([random.choice(options) for i in range(100)])
+        key_file.write_text(key)
 
     logger.info(f"Loading SECRET_KEY from '{key_file}'")
 
@@ -146,28 +145,34 @@ else:
         sys.exit(-1)
 
 # The filesystem location for served static files
-STATIC_ROOT = os.path.abspath(
+STATIC_ROOT = Path(
     get_setting(
         'INVENTREE_STATIC_ROOT',
         CONFIG.get('static_root', None)
     )
-)
+).resolve()
 
 if STATIC_ROOT is None:  # pragma: no cover
     print("ERROR: INVENTREE_STATIC_ROOT directory not defined")
     sys.exit(1)
+else:
+    # Ensure the root really is availalble
+    STATIC_ROOT.mkdir(parents=True, exist_ok=True)
 
 # The filesystem location for served static files
-MEDIA_ROOT = os.path.abspath(
+MEDIA_ROOT = Path(
     get_setting(
         'INVENTREE_MEDIA_ROOT',
         CONFIG.get('media_root', None)
     )
-)
+).resolve()
 
 if MEDIA_ROOT is None:  # pragma: no cover
     print("ERROR: INVENTREE_MEDIA_ROOT directory is not defined")
     sys.exit(1)
+else:
+    # Ensure the root really is availalble
+    MEDIA_ROOT.mkdir(parents=True, exist_ok=True)
 
 # List of allowed hosts (default = allow all)
 ALLOWED_HOSTS = CONFIG.get('allowed_hosts', ['*'])
@@ -193,17 +198,17 @@ STATICFILES_DIRS = []
 
 # Translated Template settings
 STATICFILES_I18_PREFIX = 'i18n'
-STATICFILES_I18_SRC = os.path.join(BASE_DIR, 'templates', 'js', 'translated')
-STATICFILES_I18_TRG = os.path.join(BASE_DIR, 'InvenTree', 'static_i18n')
+STATICFILES_I18_SRC = BASE_DIR.joinpath('templates', 'js', 'translated')
+STATICFILES_I18_TRG = BASE_DIR.joinpath('InvenTree', 'static_i18n')
 STATICFILES_DIRS.append(STATICFILES_I18_TRG)
-STATICFILES_I18_TRG = os.path.join(STATICFILES_I18_TRG, STATICFILES_I18_PREFIX)
+STATICFILES_I18_TRG = STATICFILES_I18_TRG.joinpath(STATICFILES_I18_PREFIX)
 
 STATFILES_I18_PROCESSORS = [
     'InvenTree.context.status_codes',
 ]
 
 # Color Themes Directory
-STATIC_COLOR_THEMES_DIR = os.path.join(STATIC_ROOT, 'css', 'color-themes')
+STATIC_COLOR_THEMES_DIR = STATIC_ROOT.joinpath('css', 'color-themes')
 
 # Web URL endpoint for served media files
 MEDIA_URL = '/media/'
@@ -339,10 +344,10 @@ TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            os.path.join(BASE_DIR, 'templates'),
+            BASE_DIR.joinpath('templates'),
             # Allow templates in the reporting directory to be accessed
-            os.path.join(MEDIA_ROOT, 'report'),
-            os.path.join(MEDIA_ROOT, 'label'),
+            MEDIA_ROOT.joinpath('report'),
+            MEDIA_ROOT.joinpath('label'),
         ],
         'OPTIONS': {
             'context_processors': [
@@ -809,7 +814,7 @@ EMAIL_USE_SSL = get_setting(
 EMAIL_TIMEOUT = 60
 
 LOCALE_PATHS = (
-    os.path.join(BASE_DIR, 'locale/'),
+    BASE_DIR.joinpath('locale/'),
 )
 
 TIME_ZONE = get_setting(

InvenTree/InvenTree/tests.py
@@ -745,11 +745,11 @@ class TestSettings(helpers.InvenTreeTestCase):
             'inventree/data/config.yaml',
         ]
 
-        self.assertTrue(any([opt in config.get_config_file().lower() for opt in valid]))
+        self.assertTrue(any([opt in str(config.get_config_file()).lower() for opt in valid]))
 
         # with env set
         with self.in_env_context({'INVENTREE_CONFIG_FILE': 'my_special_conf.yaml'}):
-            self.assertIn('inventree/my_special_conf.yaml', config.get_config_file().lower())
+            self.assertIn('inventree/my_special_conf.yaml', str(config.get_config_file()).lower())
 
     def test_helpers_plugin_file(self):
         """Test get_plugin_file."""
@@ -760,11 +760,11 @@ class TestSettings(helpers.InvenTreeTestCase):
             'inventree/data/plugins.txt',
         ]
 
-        self.assertTrue(any([opt in config.get_plugin_file().lower() for opt in valid]))
+        self.assertTrue(any([opt in str(config.get_plugin_file()).lower() for opt in valid]))
 
         # with env set
         with self.in_env_context({'INVENTREE_PLUGIN_FILE': 'my_special_plugins.txt'}):
-            self.assertIn('my_special_plugins.txt', config.get_plugin_file())
+            self.assertIn('my_special_plugins.txt', str(config.get_plugin_file()))
 
     def test_helpers_setting(self):
         """Test get_setting."""

InvenTree/InvenTree/views.py
@@ -5,7 +5,6 @@ as JSON objects and passing them to modal forms (using jQuery / bootstrap).
 """
 
 import json
-import os
 
 from django.conf import settings
 from django.contrib.auth import password_validation
@@ -638,7 +637,7 @@ class SettingsView(TemplateView):
             ctx["rates_updated"] = None
 
         # load locale stats
-        STAT_FILE = os.path.abspath(os.path.join(settings.BASE_DIR, 'InvenTree/locale_stats.json'))
+        STAT_FILE = settings.BASE_DIR.joinpath('InvenTree/locale_stats.json').abolute()
         try:
             ctx["locale_stats"] = json.load(open(STAT_FILE, 'r'))
         except Exception:

commit d2512608f528b652c997ed2d8539c684680d399d

Fix bug in exporting records
Introduced in #3392

tasks.py
@@ -267,7 +267,7 @@ def export_records(c, filename='data.json', overwrite=False, include_permissions
 
     print(f"Exporting database records to file '{filename}'")
 
-    if filename.exists() and overwrite is False:
+    if Path(filename).is_file() and overwrite is False:
         response = input("Warning: file already exists. Do you want to overwrite? [y/N]: ")
         response = str(response).strip().lower()
 