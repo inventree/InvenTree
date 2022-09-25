9271406ae15effc61afe32747c6b55a4fd5164af
Matthias Mair <code@mjmair.com>
2022-09-25 22:57:19 +0200

    use apphome reference in paths

--- contrib/packager.io/postinstall.sh
+++ contrib/packager.io/postinstall.sh
@@ -2,22 +2,22 @@
 #
 # packager.io postinstall script
 #
-PATH=/opt/inventree/env/bin:/opt/inventree/:/sbin:/bin:/usr/sbin:/usr/bin:
+PATH=${APP_HOME}/env/bin:${APP_HOME}/:/sbin:/bin:/usr/sbin:/usr/bin:
 
 cd ${APP_HOME}
 python3 -m venv env
 
 # default config
-export INVENTREE_MEDIA_ROOT=/opt/inventree/data/media
-export INVENTREE_STATIC_ROOT=/opt/inventree/data/static
-export INVENTREE_PLUGIN_FILE=/opt/inventree/data/plugins.txt
-export INVENTREE_CONFIG_FILE=/opt/inventree/data/config.yaml
+export INVENTREE_MEDIA_ROOT=${APP_HOME}/data/media
+export INVENTREE_STATIC_ROOT=${APP_HOME}/data/static
+export INVENTREE_PLUGIN_FILE=${APP_HOME}/data/plugins.txt
+export INVENTREE_CONFIG_FILE=${APP_HOME}/data/config.yaml
 export INVENTREE_DB_ENGINE=sqlite3
-export INVENTREE_DB_NAME=database.sqlite3
+export INVENTREE_DB_NAME=${APP_HOME}/data/database.sqlite3
 export INVENTREE_PLUGINS_ENABLED=true
 
 # import functions
-. /opt/inventree/contrib/packager.io/functions
+. ${APP_HOME}/contrib/packager.io/functions
 
 # exec postinstall
 debug
