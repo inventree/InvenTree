31219eed08f8111efaef815f9d6f82d7872e27c5
Matthias Mair <code@mjmair.com>
2022-09-25 21:20:23 +0200

    remove activate

--- contrib/packager.io/postinstall.sh
+++ contrib/packager.io/postinstall.sh
@@ -8,11 +8,6 @@ PATH=/opt/inventree/:/sbin:/bin:/usr/sbin:/usr/bin:
 # import functions
 . /opt/inventree/contrib/packager.io/functions
 
-# Activate python virtual environment
-cd ${APP_HOME}
-python3 -m venv virt
-. virt/bin/active
-
 # exec postinstall
 debug
 detect_os
@@ -26,4 +21,3 @@ update_or_install
 start_inventree
 
 final_message
-deactivate
