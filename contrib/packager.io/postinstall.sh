57ff86aa36ad1c6f56293949edd87794bc93501c
Matthias Mair <code@mjmair.com>
2022-09-25 21:55:26 +0200

    use modern python

--- contrib/packager.io/postinstall.sh
+++ contrib/packager.io/postinstall.sh
@@ -5,7 +5,7 @@
 PATH=/opt/inventree/env/bin:/opt/inventree/:/sbin:/bin:/usr/sbin:/usr/bin:
 
 cd ${APP_HOME}
-python -m venv env
+python3 -m venv env
 
 # import functions
 . /opt/inventree/contrib/packager.io/functions
