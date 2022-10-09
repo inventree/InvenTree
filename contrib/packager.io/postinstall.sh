46abe9839e402115e990df77fae12b447c9ca314
Matthias Mair <code@mjmair.com>
2022-10-09 20:15:44 +0200

    keep configs in config file instead of packaging index

--- contrib/packager.io/postinstall.sh
+++ contrib/packager.io/postinstall.sh
@@ -9,12 +9,12 @@ export CONF_DIR=/etc/inventree
 export DATA_DIR=${APP_HOME}/data
 export INVENTREE_MEDIA_ROOT=${DATA_DIR}/media
 export INVENTREE_STATIC_ROOT=${DATA_DIR}/static
+export INVENTREE_PLUGINS_ENABLED=true
 export INVENTREE_PLUGIN_FILE=${CONF_DIR}/plugins.txt
 export INVENTREE_CONFIG_FILE=${CONF_DIR}/config.yaml
 export INVENTREE_SECRET_KEY_FILE=${CONF_DIR}/secret_key.txt
 export INVENTREE_DB_NAME=${DATA_DIR}/database.sqlite3
 export INVENTREE_DB_ENGINE=sqlite3
-export INVENTREE_PLUGINS_ENABLED=true
 
 # import functions
 . ${APP_HOME}/contrib/packager.io/functions
