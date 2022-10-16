be922642e2ae2a4dcc835559c8cb55a2bef685d6
Matthias Mair <code@mjmair.com>
2022-10-17 01:54:06 +0200

    fix merge

--- InvenTree/InvenTree/apps.py
+++ InvenTree/InvenTree/apps.py
@@ -65,12 +65,6 @@ class InvenTreeConfig(AppConfig):
             )
         logger.info("Started background tasks...")
 
-        # Make regular backups
-        InvenTree.tasks.schedule_task(
-            'InvenTree.tasks.run_backup',
-            schedule_type=Schedule.DAILY,
-        )
-
     def update_exchange_rates(self):  # pragma: no cover
         """Update exchange rates each time the server is started.
 
