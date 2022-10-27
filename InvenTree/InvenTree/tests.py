cff4d9745b89d3fa312a9c516579e99c34e9810f
Oliver <oliver.henry.walters@gmail.com>
2022-10-27 14:00:18 +1100

    Serial increment fix (#3863)
    * Add extra unit tests
    
    * Fix logic errors when generating groups of serial numbers
    
    * Fixes for unit tests

--- InvenTree/InvenTree/tests.py
+++ InvenTree/InvenTree/tests.py
@@ -447,10 +447,14 @@ class TestSerialNumberExtraction(TestCase):
         """Test simple serial numbers."""
         e = helpers.extract_serial_numbers
 
+        # Test a range of numbers
         sn = e("1-5", 5, 1)
-        self.assertEqual(len(sn), 5, 1)
+        self.assertEqual(len(sn), 5)
         for i in range(1, 6):
             self.assertIn(str(i), sn)
+
+        sn = e("11-30", 20, 1)
+        self.assertEqual(len(sn), 20)
 
         sn = e("1, 2, 3, 4, 5", 5, 1)
         self.assertEqual(len(sn), 5)
@@ -469,11 +473,6 @@ class TestSerialNumberExtraction(TestCase):
         sn = e("1, 2, TG-4SR-92, 4+", 5, 1)
         self.assertEqual(len(sn), 5)
         self.assertEqual(sn, ['1', '2', "TG-4SR-92", '4', '5'])
-
-        # Test groups are not interpolated with alpha characters
-        sn = e("1, A-2, 3+", 5, 1)
-        self.assertEqual(len(sn), 5)
-        self.assertEqual(sn, ['1', "A-2", '3', '4', '5'])
 
         # Test multiple placeholders
         sn = e("1 2 ~ ~ ~", 5, 2)
@@ -539,6 +538,16 @@ class TestSerialNumberExtraction(TestCase):
         with self.assertRaises(ValidationError):
             e("1, 2, 3, E-5", 5, 1)
 
+        # Extract a range of values with a smaller range
+        with self.assertRaises(ValidationError) as exc:
+            e("11-50", 10, 1)
+            self.assertIn('Range quantity exceeds 10', str(exc))
+
+        # Test groups are not interpolated with alpha characters
+        with self.assertRaises(ValidationError) as exc:
+            e("1, A-2, 3+", 5, 1)
+            self.assertIn('Invalid group range: A-2', str(exc))
+
     def test_combinations(self):
         """Test complex serial number combinations."""
         e = helpers.extract_serial_numbers
@@ -558,6 +567,24 @@ class TestSerialNumberExtraction(TestCase):
         sn = e("~+", 2, 13)
         self.assertEqual(len(sn), 2)
         self.assertEqual(sn, ['14', '15'])
+
+        # Test multiple increment groups
+        sn = e("~+4, 20+4, 30+4", 15, 10)
+        self.assertEqual(len(sn), 15)
+
+        for v in [14, 24, 34]:
+            self.assertIn(str(v), sn)
+
+        # Test multiple range groups
+        sn = e("11-20, 41-50, 91-100", 30, 1)
+        self.assertEqual(len(sn), 30)
+
+        for v in range(11, 21):
+            self.assertIn(str(v), sn)
+        for v in range(41, 51):
+            self.assertIn(str(v), sn)
+        for v in range(91, 101):
+            self.assertIn(str(v), sn)
 
 
 class TestVersionNumber(TestCase):
