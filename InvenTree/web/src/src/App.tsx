82f5b1d75738fe7e18bc7048774499ad04e21880
Matthias Mair <code@mjmair.com>
2022-11-13 00:03:24 +0100

    Add base site

--- InvenTree/web/src/src/App.tsx
+++ InvenTree/web/src/src/App.tsx
@@ -3,7 +3,38 @@ import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core
 import { useColorScheme } from '@mantine/hooks';
 import { Text } from '@mantine/core';
 import { ColorToggle } from './components/ColorToggle';
+import { HeaderTabs } from './components/Layout';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+} from "react-router-dom";
+import ErrorPage from './pages/error';
 
+const user =  {
+    "name": "Matthias Mair",
+    "email": "code@mjmair.com",
+}
+
+const  tabs = [
+    "Home",
+    "Orders",
+    "Education",
+    "Community",
+    "Forums",
+    "Support",
+    "Account",
+    "Helpdesk",
+    "Settings",
+  ]
+
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <div>Hello world!</div>,
+      errorElement: <ErrorPage />,
+    },
+  ]);
 
 export default function App() {
   const preferredColorScheme = useColorScheme();
@@ -14,8 +45,8 @@ export default function App() {
   return (
     <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
       <MantineProvider theme={{ colorScheme }} withGlobalStyles withNormalizeCSS>
-      <Text>Welcome to Mantine!</Text>
-      <ColorToggle />
+      <HeaderTabs tabs={tabs} user={user}></HeaderTabs>
+      <RouterProvider router={router} />
       </MantineProvider>
     </ColorSchemeProvider>
   );
---
+++ InvenTree/web/src/src/components/Layout.tsx
@@ -0,0 +1,169 @@
+import { useState } from 'react';
+import {
+  createStyles,
+  Container,
+  Avatar,
+  UnstyledButton,
+  Group,
+  Text,
+  Menu,
+  Tabs,
+  Burger,
+  ActionIcon,
+} from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import {
+    IconAlien,
+  IconLogout,
+  IconHeart,
+  IconStar,
+  IconMessage,
+  IconSettings,
+  IconPlayerPause,
+  IconTrash,
+  IconSwitchHorizontal,
+  IconChevronDown,
+} from '@tabler/icons';
+import { ColorToggle } from './ColorToggle';
+import InvenTreeIcon from '../assets/inventree.svg';
+
+const useStyles = createStyles((theme) => ({
+  header: {
+    paddingTop: theme.spacing.sm,
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[6] : theme.colors.gray[0],
+    borderBottom: `1px solid ${
+      theme.colorScheme === 'dark' ? 'transparent' : theme.colors.gray[2]
+    }`,
+    marginBottom: 120,
+  },
+
+  mainSection: {
+    paddingBottom: theme.spacing.sm,
+  },
+
+  user: {
+    color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+    padding: `${theme.spacing.xs}px ${theme.spacing.sm}px`,
+    borderRadius: theme.radius.sm,
+    transition: 'background-color 100ms ease',
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+    },
+
+    [theme.fn.smallerThan('xs')]: {
+      display: 'none',
+    },
+  },
+
+  burger: {
+    [theme.fn.largerThan('xs')]: {
+      display: 'none',
+    },
+  },
+
+  userActive: {
+    backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[8] : theme.white,
+  },
+
+  tabs: {
+    [theme.fn.smallerThan('sm')]: {
+      display: 'none',
+    },
+  },
+
+  tabsList: {
+    borderBottom: '0 !important',
+  },
+
+  tab: {
+    fontWeight: 500,
+    height: 38,
+    backgroundColor: 'transparent',
+
+    '&:hover': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[5] : theme.colors.gray[1],
+    },
+
+    '&[data-active]': {
+      backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
+      borderColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[2],
+    },
+  },
+}));
+
+interface HeaderTabsProps {
+  user: { name: string; };
+  tabs: string[];
+}
+
+export function HeaderTabs({ user, tabs }: HeaderTabsProps) {
+  const { classes, theme, cx } = useStyles();
+  const [opened, { toggle }] = useDisclosure(false);
+  const [userMenuOpened, setUserMenuOpened] = useState(false);
+
+  const items = tabs.map((tab) => (
+    <Tabs.Tab value={tab} key={tab}>
+      {tab}
+    </Tabs.Tab>
+  ));
+
+  return (
+    <div className={classes.header}>
+      <Container className={classes.mainSection}>
+        <Group position="apart">
+          <ActionIcon size={28}><img src={InvenTreeIcon} alt="InvenTree Logo" height={28}/></ActionIcon>
+
+          <Burger opened={opened} onClick={toggle} className={classes.burger} size="sm" />
+
+            <Group>
+          <ColorToggle />
+
+          <Menu
+            width={260}
+            position="bottom-end"
+            transition="pop-top-right"
+            onClose={() => setUserMenuOpened(false)}
+            onOpen={() => setUserMenuOpened(true)}
+          >
+            <Menu.Target>
+              <UnstyledButton
+                className={cx(classes.user, { [classes.userActive]: userMenuOpened })}
+              >
+                <Group spacing={7}>
+                  <Text weight={500} size="sm" sx={{ lineHeight: 1 }} mr={3}>
+                    {user.name}
+                  </Text>
+                  <IconChevronDown size={12} stroke={1.5} />
+                </Group>
+              </UnstyledButton>
+            </Menu.Target>
+            <Menu.Dropdown>
+              <Menu.Item icon={<IconHeart size={14} color={theme.colors.red[6]} stroke={1.5} />}>
+                Notifications
+              </Menu.Item>
+
+              <Menu.Label>Settings</Menu.Label>
+              <Menu.Item icon={<IconSettings size={14} stroke={1.5} />}>Account settings</Menu.Item>
+              <Menu.Item icon={<IconLogout size={14} stroke={1.5} />}>Logout</Menu.Item>
+            </Menu.Dropdown>
+          </Menu>
+          </Group>
+        </Group>
+      </Container>
+      <Container>
+        <Tabs
+          defaultValue="Home"
+          variant="outline"
+          classNames={{
+            root: classes.tabs,
+            tabsList: classes.tabsList,
+            tab: classes.tab,
+          }}
+        >
+          <Tabs.List>{items}</Tabs.List>
+        </Tabs>
+      </Container>
+    </div>
+  );
+}
