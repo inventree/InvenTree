f97ad7ac584091b1e7dcaef1f8ce24fde5892e7f
Matthias Mair <code@mjmair.com>
2022-11-21 22:00:58 +0100

    add server state

--- InvenTree/web/src/src/states.tsx
+++ InvenTree/web/src/src/states.tsx
@@ -22,6 +22,35 @@ const user = {
     username: "mjmair",
 };
 
+
+export interface ServerAPIProps {
+    server: null | string;
+    version: null | string;
+    instance: null | string;
+    apiVersion: null | number;
+    worker_running: null | boolean;
+    worker_pending_tasks: null | number;
+    plugins_enabled: null | boolean;
+    active_plugins: PluginProps[];
+}
+
+export interface PluginProps {
+    name: string;
+    slug: string;
+    version: null | string;
+}
+
+const emptyServerAPI: ServerAPIProps = {
+    server: null,
+    version: null,
+    instance: null,
+    apiVersion: null,
+    worker_running: null,
+    worker_pending_tasks: null,
+    plugins_enabled: null,
+    active_plugins: [],
+};
+
 // States
 interface SesstionSettings {
     autoupdate: boolean,
@@ -54,11 +83,15 @@ export const useSessionSettings = create<SesstionSettings>()(
 interface ApiStateProps {
     user: UserProps,
     setUser: (newUser: UserProps) => void,
+    server: ServerAPIProps,
+    setServer: (newServer: ServerAPIProps) => void,
 }
 
 export const useApiState = create<ApiStateProps>((set) => ({
     user: user,
     setUser: (newUser: UserProps) => set({ user: newUser }),
+    server: emptyServerAPI,
+    setServer: (newServer: ServerAPIProps) => set({ server: newServer }),
 }))
 
 interface SessionStateProps {
