eb0fb575dc083b6558ac6c6a1f105f3d1e8ade76
Oliver <oliver.henry.walters@gmail.com>
2022-11-22 14:14:39 +1100

    Prevent caching

--- InvenTree/part/models.py
+++ InvenTree/part/models.py
@@ -2620,9 +2620,9 @@ class PartPricing(models.Model):
             self.internal_cost_max,
         ]
 
-        purchase_history_override = InvenTreeSetting.get_setting('PRICING_PURCHASE_HISTORY_OVERRIDES_SUPPLIER', False)
+        purchase_history_override = InvenTreeSetting.get_setting('PRICING_PURCHASE_HISTORY_OVERRIDES_SUPPLIER', False, cache=False)
 
-        if InvenTreeSetting.get_setting('PRICING_USE_SUPPLIER_PRICING', True):
+        if InvenTreeSetting.get_setting('PRICING_USE_SUPPLIER_PRICING', True, cache=False):
             # Add supplier pricing data, *unless* historical pricing information should override
             if self.purchase_cost_min is None or not purchase_history_override:
                 min_costs.append(self.supplier_price_min)
@@ -2630,7 +2630,7 @@ class PartPricing(models.Model):
             if self.purchase_cost_max is None or not purchase_history_override:
                 max_costs.append(self.supplier_price_max)
 
-        if InvenTreeSetting.get_setting('PRICING_USE_VARIANT_PRICING', True):
+        if InvenTreeSetting.get_setting('PRICING_USE_VARIANT_PRICING', True, cache=False):
             min_costs.append(self.variant_cost_min)
             max_costs.append(self.variant_cost_max)
 
