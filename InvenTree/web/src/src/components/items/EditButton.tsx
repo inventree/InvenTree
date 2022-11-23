89a39c60bca6d3d69ac914cf6ab69303232412b4
Matthias Mair <code@mjmair.com>
2022-11-23 02:12:39 +0100

    update typing

--- InvenTree/web/src/src/components/items/EditButton.tsx
+++ InvenTree/web/src/src/components/items/EditButton.tsx
@@ -1,7 +1,7 @@
 import { ActionIcon } from '@mantine/core';
 import { IconEdit, IconDeviceFloppy } from '@tabler/icons';
 
-export function EditButton(setEditing: () => void, editing: boolean) {
+export function EditButton(setEditing: (value?: React.SetStateAction<boolean> | undefined) => void, editing: boolean) {
     return <ActionIcon onClick={() => setEditing()}>
         {editing ? <IconDeviceFloppy /> : <IconEdit />}
     </ActionIcon>;
