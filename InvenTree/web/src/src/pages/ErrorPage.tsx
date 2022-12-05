435d2fa0527bca70a1c6838d747f2d70dd856014
Matthias Mair <code@mjmair.com>
2022-12-05 02:34:07 +0100

    Add error to title

--- InvenTree/web/src/src/pages/ErrorPage.tsx
+++ InvenTree/web/src/src/pages/ErrorPage.tsx
@@ -1,19 +1,31 @@
-import { Trans } from "@lingui/macro";
+import { Trans, t } from "@lingui/macro";
+import { Container } from "@mantine/core";
+import { useDocumentTitle } from "@mantine/hooks";
+import { useEffect, useState } from "react";
 import { useRouteError } from "react-router-dom";
 import { LanguageContext } from "../context/LanguageContext";
 
 export default function ErrorPage() {
   const error = useRouteError();
+  const [title, setTitle] = useState(t`Error`);
+  useDocumentTitle(title);
+
+  useEffect(() => {
+    if (error && error.statusText) {
+      setTitle(t`Error: ${error.statusText}`);
+    }
+  }, [error])
+
 
   return (
     <LanguageContext>
-      <div id="error-page">
+      <Container>
         <h1><Trans>Error</Trans></h1>
         <p><Trans>Sorry, an unexpected error has occurred.</Trans></p>
         <p>
           <i>{error.statusText || error.message}</i>
         </p>
-      </div>
+      </Container>
     </LanguageContext>
   );
 }
