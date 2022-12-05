04941219cf9647361174024296c44bffb4432e89
Matthias Mair <code@mjmair.com>
2022-12-05 02:28:54 +0100

    add translation for error page

--- InvenTree/web/src/src/pages/ErrorPage.tsx
+++ InvenTree/web/src/src/pages/ErrorPage.tsx
@@ -1,15 +1,19 @@
+import { Trans } from "@lingui/macro";
 import { useRouteError } from "react-router-dom";
+import { LanguageContext } from "../context/LanguageContext";
 
 export default function ErrorPage() {
   const error = useRouteError();
 
   return (
-    <div id="error-page">
-      <h1>Error</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
+    <LanguageContext>
+      <div id="error-page">
+        <h1><Trans>Error</Trans></h1>
+        <p><Trans>Sorry, an unexpected error has occurred.</Trans></p>
+        <p>
+          <i>{error.statusText || error.message}</i>
+        </p>
+      </div>
+    </LanguageContext>
   );
 }
