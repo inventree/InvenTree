73114e93f2df2a00e236ac4ca458eedea2ce6ae1
Oliver Walters <oliver.henry.walters@gmail.com>
2022-12-14 21:15:40 +1100

    Remove debug statements

--- InvenTree/InvenTree/config.py
+++ InvenTree/InvenTree/config.py
@@ -59,15 +59,6 @@ def get_config_file(create=True) -> Path:
 def load_config_data() -> map:
     """Load configuration data from the config file."""
 
-    import sys
-
-    print("load_config_data()")
-    print("- cwd:", os.getcwd())
-    print("- argv:", sys.argv)
-    print("- mod:", sys.modules)
-    print("- exe:", sys.executable)
-    print("- path:", sys.path)
-
     import yaml
 
     cfg_file = get_config_file()
