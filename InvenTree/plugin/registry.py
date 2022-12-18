2cabf1d6c0fa783e7768eabb6f4236f2bb35bf7a
Bben <bbillyben@hotmail.com>
2022-12-18 11:05:13 +0100

    linebreak

--- InvenTree/plugin/registry.py
+++ InvenTree/plugin/registry.py
@@ -565,7 +565,6 @@ class PluginsRegistry:
                         settings.INSTALLED_APPS += [plugin_path]
                         self.installed_apps += [plugin_path]
                         apps_changed = True
-       
             # if apps were changed or force loading base apps -> reload
             if apps_changed or force_reload:
                 # first startup or force loading of base apps -> registry is prob false
