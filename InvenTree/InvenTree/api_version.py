3d9203dee2c92c6d6f9bc7e3ff7c050b17e4bb93
miggland <miggland@users.noreply.github.com>
2022-12-21 09:54:13 +0100

    Increment API version

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,7 +2,7 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 83
+INVENTREE_API_VERSION = 84
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
