2ff28b24af547c0b7b9e05337c46d159cf8ab85d
Matthias Mair <code@mjmair.com>
2023-01-15 15:27:33 +0100

    bump API version

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,10 +2,13 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 87
+INVENTREE_API_VERSION = 88
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
+v88 -> 2023-01-15 : https://github.com/inventree/InvenTree/pull/4214
+    - Adds updated field to SupplierPart API
+    - Adds API date orddering for supplier part list
 v87 -> 2023-01-04 : https://github.com/inventree/InvenTree/pull/4067
     - Add API date filter for stock table on Expiry date
 
