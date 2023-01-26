c838bddc197504574191ca39b42439e3ce711af4
Oliver <oliver.henry.walters@gmail.com>
2023-01-26 22:34:19 +1100

    Handle case where function is called with empty user (#4259)

--- InvenTree/part/templatetags/inventree_extras.py
+++ InvenTree/part/templatetags/inventree_extras.py
@@ -166,8 +166,11 @@ def inventree_in_debug_mode(*args, **kwargs):
 @register.simple_tag()
 def inventree_show_about(user, *args, **kwargs):
     """Return True if the about modal should be shown."""
-    if InvenTreeSetting.get_setting('INVENTREE_RESTRICT_ABOUT') and not user.is_superuser:
-        return False
+    if InvenTreeSetting.get_setting('INVENTREE_RESTRICT_ABOUT'):
+        # Return False if the user is not a superuser, or no user information is provided
+        if not user or not user.is_superuser:
+            return False
+
     return True
 
 
