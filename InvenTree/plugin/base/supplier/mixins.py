86ea44c5c91a9c97234d240286fdcd9b72eedd50
Matthias Mair <code@mjmair.com>
2023-02-08 01:35:52 +0100

    move connection fnc

--- InvenTree/plugin/base/supplier/mixins.py
+++ InvenTree/plugin/base/supplier/mixins.py
@@ -50,6 +50,32 @@ class SupplierMixin:
         """Setup web connections for this plugin."""
         return getattr(self, 'CONNECTIONS', None)
 
+    # TODO @matmair clean up this mess
+    def get_con(self, key: str, ref: str = None, default=None):
+        """Get webconnection setting data."""
+        def ret_default(val):
+            if not val and default:
+                return default
+            return val
+
+        ref = ref if ref else self.STD_CONNECTION
+        qs = self.db.webconnections.filter(connection_key=ref)
+        ret = [a.settings.get(key=key).value for a in qs]
+        if len(ret) == 1:
+            return ret_default(ret[0])
+        return ret_default(ret)
+
+    # TODO @matmair clean up this mess
+    def set_con(self, key: str, val, ref: str = None):
+        """Set webconnection setting data."""
+        ref = ref if ref else self.STD_CONNECTION
+        qs = self.db.webconnections.filter(connection_key=ref)
+        if len(qs) > 1:
+            raise NotImplementedError('This function is not implemented!')
+        setting = qs[0].settings.get(key=key)
+        setting.value = val
+        setting.save()
+
     def search_action(self, term: str, exact: bool = False, safe_results: bool = True) -> SearchRunResult:
         """Run search against supplier and return results."""
         raise MixinNotImplementedError('The `search_action` function must be enabled for search integration.')
