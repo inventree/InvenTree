dc47dfdb79ecff2ac9f105904983c5abd8775394
Oliver <oliver.henry.walters@gmail.com>
2023-02-16 08:41:17 +1100

    Increment API version

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,10 +2,13 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 94
+INVENTREE_API_VERSION = 95
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
+
+v95 -> 2023-02-16 : https://github.com/inventree/InvenTree/pull/4346
+    - Adds "CompanyAttachment" model (and associated API endpoints)
 
 v94 -> 2023-02-10 : https://github.com/inventree/InvenTree/pull/4327
     - Adds API endpoints for the "Group" auth model
