6e6a2b7113ed8a4d38f3d55a4064b6d116fe85fd
Simon Kühling <mail@simonkuehling.de>
2023-02-24 13:42:32 +0100

    bump API version number

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,10 +2,13 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 97
+INVENTREE_API_VERSION = 98
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
+
+v98 -> 2023-02-24 : https://github.com/inventree/InvenTree/pull/4408
+    - Adds "responsible" filter to Build API
 
 v97 -> 2023-02-20 : https://github.com/inventree/InvenTree/pull/4377
     - Adds "external" attribute to StockLocation model
