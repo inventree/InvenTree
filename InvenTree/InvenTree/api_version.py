cacaf523947bdd83078082aaff6e24a998208c4e
Simon Kühling <mail@simonkuehling.de>
2023-03-04 21:37:13 +0100

    bump API version

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,10 +2,13 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 99
+INVENTREE_API_VERSION = 100
 
 """
 Increment this API version number whenever there is a significant change to the API that any clients need to know about
+
+v100 -> 2023-03-04 : https://github.com/inventree/InvenTree/pull/4452
+     - Adds bulk delete of PurchaseOrderLineItems to API
 
 v99 -> 2023-03-03 : https://github.com/inventree/InvenTree/pull/4445
     - Adds sort by "responsible" to PurchaseOrderAPI
