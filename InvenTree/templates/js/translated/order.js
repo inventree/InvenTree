153b9c0abe9af1cef1486659d80781d89fc86e65
Oliver <oliver.henry.walters@gmail.com>
2023-03-16 16:09:18 +1100

    Render extra line table for return order
    - Refactor existing functions into a single generic function
    - Reduces repeated JS code a lot

--- InvenTree/templates/js/translated/order.js
+++ InvenTree/templates/js/translated/order.js
@@ -14,6 +14,7 @@
     removeOrderRowFromOrderWizard,
     removePurchaseOrderLineItem,
     loadOrderTotal,
+    loadExtraLineTable,
     extraLineFields,
     reloadTotal,
 */
@@ -122,3 +123,197 @@ function reloadTotal() {
         }
     );
 };
+
+
+/*
+ * Load a table displaying "extra" line items for a given order.
+ * Used for all external order types (e.g. PurchaseOrder / SalesOrder / ReturnOrder)
+ *
+ * options:
+ *  - table: The DOM ID of the table element
+ *  - order: The ID of the related order (required)
+ *  - name: The unique 'name' for this table
+ *  - url: The API URL for the extra line item model (required)
+ *  - filtertarget: The DOM ID for the filter list element
+ */
+function loadExtraLineTable(options={}) {
+
+    const table = options.table;
+
+    options.params = options.params || {};
+
+    // Filtering
+    options.params.order = options.order;
+
+    var filters = {};
+
+    if (options.name) {
+        filters = loadTableFilters(options.name);
+    }
+
+    for (var key in options.params) {
+        filters[key] = options.params[key];
+    }
+
+    setupFilterList(
+        options.name,
+        $(table),
+        options.filtertarget,
+        {
+            download: true
+        }
+    );
+
+    // Helper function to reload table
+    function reloadExtraLineTable() {
+        $(table).bootstrapTable('refresh');
+        reloadTotal();
+    }
+
+    // Configure callback functions once the table is loaded
+    function setupCallbacks() {
+
+        if (options.allow_edit) {
+
+            // Callback to duplicate line item
+            $(table).find('.button-duplicate').click(function() {
+                var pk = $(this).attr('pk');
+
+                inventreeGet(`${options.url}${pk}/`, {}, {
+                    success: function(data) {
+
+                        var fields = extraLineFields();
+
+                        constructForm(options.url, {
+                            method: 'POST',
+                            fields: fields,
+                            data: data,
+                            title: '{% trans "Duplicate Line" %}',
+                            onSuccess: reloadExtraLineTable,
+                        });
+                    }
+                });
+            });
+
+            // Callback to edit line item
+            // Callback for editing lines
+            $(table).find('.button-edit').click(function() {
+                var pk = $(this).attr('pk');
+
+                constructForm(`${options.url}${pk}/`, {
+                    fields: extraLineFields(),
+                    title: '{% trans "Edit Line" %}',
+                    onSuccess: reloadExtraLineTable,
+                });
+            });
+        }
+
+        if (options.allow_delete) {
+            // Callback for deleting lines
+            $(table).find('.button-delete').click(function() {
+                var pk = $(this).attr('pk');
+
+                constructForm(`${options.url}${pk}/`, {
+                    method: 'DELETE',
+                    title: '{% trans "Delete Line" %}',
+                    onSuccess: reloadExtraLineTable,
+                });
+            });
+        }
+    }
+
+    $(table).inventreeTable({
+        url: options.url,
+        name: options.name,
+        sidePagination: 'server',
+        onPostBody: setupCallbacks,
+        formatNoMatches: function() {
+            return '{% trans "No line items found" %}';
+        },
+        queryParams: filters,
+        original: options.params,
+        showFooter: true,
+        uniqueId: 'pk',
+        columns: [
+            {
+                sortable: true,
+                field: 'reference',
+                title: '{% trans "Reference" %}',
+                switchable: false,
+            },
+            {
+                sortable: true,
+                switchable: false,
+                field: 'quantity',
+                title: '{% trans "Quantity" %}',
+                footerFormatter: function(data) {
+                    return data.map(function(row) {
+                        return +row['quantity'];
+                    }).reduce(function(sum, i) {
+                        return sum + i;
+                    }, 0);
+                },
+            },
+            {
+                sortable: true,
+                field: 'price',
+                title: '{% trans "Unit Price" %}',
+                formatter: function(value, row) {
+                    return formatCurrency(row.price, {
+                        currency: row.price_currency,
+                    });
+                }
+            },
+            {
+                field: 'total_price',
+                sortable: true,
+                switchable: true,
+                title: '{% trans "Total Price" %}',
+                formatter: function(value, row) {
+                    return formatCurrency(row.price * row.quantity, {
+                        currency: row.price_currency,
+                    });
+                },
+                footerFormatter: function(data) {
+                    return calculateTotalPrice(
+                        data,
+                        function(row) {
+                            return row.price ? row.price * row.quantity : null;
+                        },
+                        function(row) {
+                            return row.price_currency;
+                        }
+                    );
+                }
+            },
+            {
+                field: 'notes',
+                title: '{% trans "Notes" %}',
+            },
+            {
+                field: 'buttons',
+                switchable: false,
+                formatter: function(value, row, index, field) {
+
+                    var html = `<div class='btn-group float-right' role='group'>`;
+
+                    if (options.allow_edit || options.allow_delete) {
+                        var pk = row.pk;
+
+                        if (options.allow_edit) {
+                            html += makeIconButton('fa-clone', 'button-duplicate', pk, '{% trans "Duplicate line" %}');
+                            html += makeIconButton('fa-edit icon-blue', 'button-edit', pk, '{% trans "Edit line" %}');
+                        }
+
+                        if (options.allow_delete) {
+                            html += makeIconButton('fa-trash-alt icon-red', 'button-delete', pk, '{% trans "Delete line" %}', );
+                        }
+                    }
+
+                    html += `</div>`;
+                    return html;
+                }
+            },
+        ]
+    })
+}
