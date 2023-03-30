903b0feae6e8bcc091371a46d76f776ae93528ab
Oliver Walters <oliver.henry.walters@gmail.com>
2023-03-30 23:12:51 +1100

    Add API endpoint and serializer for issuing a SalesOrder

--- InvenTree/order/serializers.py
+++ InvenTree/order/serializers.py
@@ -731,6 +731,19 @@ class SalesOrderSerializer(TotalPriceMixin, AbstractOrderSerializer, InvenTreeMo
     customer_detail = CompanyBriefSerializer(source='customer', many=False, read_only=True)
 
 
+class SalesOrderIssueSerializer(serializers.Serializer):
+    """Serializer for issuing a SalesOrder"""
+
+    class Meta:
+        """Metaclass options"""
+        fields = []
+
+    def save(self):
+        """Save the serializer to 'issue' the order"""
+        order = self.context['order']
+        order.issue_order()
+
+
 class SalesOrderAllocationSerializer(InvenTreeModelSerializer):
     """Serializer for the SalesOrderAllocation model.
 
