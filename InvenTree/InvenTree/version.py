7052241dd62addcd360da47a1cd39d829a1d5975
Oliver <oliver.henry.walters@gmail.com>
2023-04-04 08:25:53 +1000

    Bump version number to 0.10.2

--- InvenTree/InvenTree/version.py
+++ InvenTree/InvenTree/version.py
@@ -13,7 +13,7 @@ import common.models
 from InvenTree.api_version import INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = "0.10.1"
+INVENTREE_SW_VERSION = "0.10.2"
 
 
 def inventreeInstanceName():
