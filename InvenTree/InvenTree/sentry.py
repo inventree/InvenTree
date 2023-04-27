3aeed70de8a6ba490afe03aa2a4b2daac5185721
Oliver Walters <oliver.henry.walters@gmail.com>
2023-04-27 22:39:18 +1000

    Cleanup

--- InvenTree/InvenTree/sentry.py
+++ InvenTree/InvenTree/sentry.py
@@ -4,7 +4,16 @@ import logging
 
 from django.core.exceptions import Http404
 
+import sentry_sdk
+from sentry_sdk.integrations.django import DjangoIntegration
+
 logger = logging.getLogger('inventree')
+
+
+def default_sentry_dsn():
+    """Return the default Sentry.io DSN for InvenTree"""
+
+    return 'https://3928ccdba1d34895abde28031fd00100@o378676.ingest.sentry.io/6494600'
 
 
 def sentry_before_send(event, hint):
@@ -18,8 +27,26 @@ def sentry_before_send(event, hint):
     if 'exc_info' in hint:
         exc_type, exc_value, tb = hint['exc_info']
         if exc_type in ignore_errors:
+            logger.debug("sentry_before_send: Ignoring error type '{exc_type}'")
             return None
 
-    logger.info('Sending error to Sentry.io: {e}'.format(e=event))
+    logger.info(f'Sending error to sentry.io: {event}')
 
     return event
+
+
+def init_sentry(dsn, sample_rate, tags):
+    """Initialize sentry.io error reporting"""
+
+    logger.info("Initializing sentry.io integration")
+
+    sentry_sdk.init(
+        dsn=dsn,
+        integrations=[DjangoIntegration()],
+        traces_sample_rate=sample_rate,
+        send_default_pii=True,
+        before_send=sentry_before_send,
+    )
+
+    for key, val in tags.items():
+        sentry_sdk.set_tag(f'inventree_{key}', val)
