ec51b1c837441609c1ac6aec3e53c04e5540388e
martin <martin@iggland.com>
2023-05-01 12:56:36 +0000

    Add flag to API which allows using pack size when adding stock items manually

--- InvenTree/stock/serializers.py
+++ InvenTree/stock/serializers.py
@@ -123,6 +123,7 @@ class StockItemSerializer(InvenTree.serializers.InvenTreeModelSerializer):
             'updated',
             'purchase_price',
             'purchase_price_currency',
+            'use_pack_size',
         ]
 
         """
@@ -137,11 +138,29 @@ class StockItemSerializer(InvenTree.serializers.InvenTreeModelSerializer):
             'updated',
         ]
 
+        """
+        Fields used when creating a stock item
+        """
+        extra_kwargs = {
+            'use_pack_size': {'write_only': True},
+        }
+
     part = serializers.PrimaryKeyRelatedField(
         queryset=part_models.Part.objects.all(),
         many=False, allow_null=False,
         help_text=_("Base Part"),
         label=_("Part"),
+    )
+
+    """
+    Field used when creating a stock item
+    """
+    use_pack_size = serializers.BooleanField(
+        write_only=True,
+        required=False,
+        allow_null=True,
+        help_text=_("Use pack size when adding: the quantity defined is the number of packs"),
+        label=("Use pack size"),
     )
 
     def validate_part(self, part):
