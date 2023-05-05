c29e58aeaa30cd7058a529b24de1e9f163d05406
Oliver <oliver.henry.walters@gmail.com>
2023-05-05 22:40:20 +1000

    Bug fix for zero quantity pricing (#4766)
    - Fix default value for formatPriceRange method
    - Display empty value in table
    
    (cherry picked from commit 07d9c2264e2555d55edcc0873ba3de30549bb6fe)

--- InvenTree/templates/js/translated/stock.js
+++ InvenTree/templates/js/translated/stock.js
@@ -2063,6 +2063,10 @@ function loadStockTable(table, options) {
                 currency = baseCurrency();
             }
 
+            if (row.quantity <= 0) {
+                return '-';
+            }
+
             return formatPriceRange(
                 min_price,
                 max_price,
