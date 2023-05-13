5be74aa9d9185ea2071c766fcdb579f0949ccfe1
martin <martin@iggland.com>
2023-05-13 07:07:14 +0000

    Add delivery date setting to Shipment model

--- InvenTree/order/test_sales_order.py
+++ InvenTree/order/test_sales_order.py
@@ -257,6 +257,13 @@ class SalesOrderTest(TestCase):
         # Shipment should have default reference of '1'
         self.assertEqual('1', order_2.pending_shipments()[0].reference)
 
+    def test_shipment_delivery(self):
+        """Test the shipment delivery settings"""
+
+        # Shipment delivery date should be empty before setting date
+        self.assertIsNone(self.shipment.delivery_date)
+        self.assertIsFalse(self.shipment.is_delivered)
+
     def test_overdue_notification(self):
         """Test overdue sales order notification"""
 
