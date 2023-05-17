b9e936e29275fd05cc601d9ff74a3cab37d52c57
LavissaWoW <lavissawow@gmail.com>
2023-05-18 01:05:00 +0200

    iDots

--- InvenTree/company/serializers.py
+++ InvenTree/company/serializers.py
@@ -115,7 +115,7 @@ class CompanySerializer(RemoteImageMixin, InvenTreeModelSerializer):
 
         return queryset
 
-    primary_address = AddressSerializer()
+    primary_address = AddressSerializer(required=False)
 
     url = serializers.CharField(source='get_absolute_url', read_only=True)
 
