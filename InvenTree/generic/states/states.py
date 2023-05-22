310c2c3e1067becadfad53f966111786e1c6acd4
Matthias Mair <code@mjmair.com>
2023-05-22 23:38:19 +0200

    cleanup BaseEnum

--- InvenTree/generic/states/states.py
+++ InvenTree/generic/states/states.py
@@ -3,41 +3,28 @@ import enum
 
 
 class BaseEnum(enum.Enum):
-    """
-    An `Enum` capabile of having its members have docstrings
-
-    Should be passed in the form of:
-      value, docstring
+    """An `Enum` capabile of having its members have docstrings.
 
     Based on https://stackoverflow.com/questions/19330460/how-do-i-put-docstrings-on-enums
     """
 
     def __new__(cls, *args):
+        """Assign values on creation."""
         obj = object.__new__(cls)
         obj._value_ = args[0]
         return obj
 
     def __eq__(self, obj):
+        """Override equality operator to allow comparison with int."""
         if type(self) == type(obj):
             return super().__eq__(obj)
         return self.value == obj
 
     def __ne__(self, obj):
+        """Override inequality operator to allow comparison with int."""
         if type(self) == type(obj):
             return super().__ne__(obj)
         return self.value != obj
-
-    def __init__(self, *args):
-        """
-        Creates a generic enumeration with potential assigning of a member docstring
-
-        Should be passed in the form of:
-          value, docstring
-        Or:
-          docstring
-        """
-        if len(args) == 2 and isinstance(args[-1], str):
-            self.__doc__ = args[-1]
 
 
 class StatusCode(BaseEnum):
