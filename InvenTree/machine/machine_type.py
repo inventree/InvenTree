10a9372872e4ae22f044ed19eb881a7020984bca
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-05-31 21:19:40 +0000

    Refactor helper mixins into own file in InvenTree app

--- InvenTree/machine/machine_type.py
+++ InvenTree/machine/machine_type.py
@@ -1,10 +1,6 @@
-import inspect
-from pathlib import Path
 from typing import TYPE_CHECKING, Dict, Literal
 
-from django.conf import settings
-
-from plugin import registry as plg_registry
+from InvenTree.helpers_mixin import ClassProviderMixin, ClassValidationMixin
 
 # Import only for typechecking, otherwise this throws cyclic import errors
 if TYPE_CHECKING:
@@ -16,64 +12,6 @@ else:  # pragma: no cover
 
     class SettingsKeyType:
         pass
-
-
-# TODO: move to better destination (helpers)
-class ClassValidationMixin:
-    """Mixin to validate class attributes and overrides.
-
-    Class attributes:
-        required_attributes: List of class attributes that need to be defined
-        required_overrides: List of functions that need override
-    """
-
-    required_attributes = []
-    required_overrides = []
-
-    @classmethod
-    def validate(cls):
-        def attribute_missing(key):
-            return not hasattr(cls, key) or getattr(cls, key) == ""
-
-        def override_missing(base_implementation):
-            return base_implementation == getattr(cls, base_implementation.__name__, None)
-
-        missing_attributes = list(filter(attribute_missing, cls.required_attributes))
-        missing_overrides = list(filter(override_missing, cls.required_overrides))
-
-        errors = []
-
-        if len(missing_attributes) > 0:
-            errors.append(f"did not provide the following attributes: {', '.join(missing_attributes)}")
-        if len(missing_overrides) > 0:
-            errors.append(f"did not override the required attributes: {', '.join(map(lambda attr: attr.__name__, missing_overrides))}")
-
-        if len(errors) > 0:
-            raise NotImplementedError(f"'{cls}' " + " and ".join(errors))
-
-
-class ClassProviderMixin:
-    @classmethod
-    def get_provider_file(cls):
-        """File that contains the Class definition."""
-        return inspect.getfile(cls)
-
-    @classmethod
-    def get_provider_plugin(cls):
-        """Plugin that contains the Class definition, otherwise None."""
-        for plg in plg_registry.plugins.values():
-            if plg.package_path == cls.__module__:
-                return plg
-
-    @classmethod
-    def get_is_builtin(cls):
-        """Is this Class build in the Inventree source code?"""
-        try:
-            Path(cls.get_provider_file()).relative_to(settings.BASE_DIR)
-            return True
-        except ValueError:
-            # Path(...).relative_to throws an ValueError if its not relative to the InvenTree source base dir
-            return False
 
 
 class BaseDriver(ClassValidationMixin, ClassProviderMixin):
