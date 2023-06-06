b0f7e49f2e9fa51d92ad65a073b32d0ce734391a
Matthias Mair <code@mjmair.com>
2023-06-06 23:26:12 +0200

    Revert "make currency choices independend"
    This reverts commit ab84a7ff830bfdf8df2235ec11f5a83e05533a10.

--- InvenTree/part/migrations/0055_auto_20201110_1001.py
+++ InvenTree/part/migrations/0055_auto_20201110_1001.py
@@ -2,7 +2,6 @@
 
 from django.db import migrations
 import djmoney.models.fields
-import common.models
 
 
 class Migration(migrations.Migration):
@@ -20,6 +19,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='partsellpricebreak',
             name='price_currency',
-            field=djmoney.models.fields.CurrencyField(choices=common.settings.currency_code_mappings, default='USD', editable=False, max_length=3),
+            field=djmoney.models.fields.CurrencyField(choices=[('AUD', 'Australian Dollar'), ('CAD', 'Canadian Dollar'), ('EUR', 'Euro'), ('NZD', 'New Zealand Dollar'), ('GBP', 'Pound Sterling'), ('USD', 'US Dollar'), ('JPY', 'Yen')], default='USD', editable=False, max_length=3),
         ),
     ]
