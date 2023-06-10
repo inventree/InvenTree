feccb6ce772124cb9bfc1888258e56fa68211ded
Matthias Mair <code@mjmair.com>
2023-06-10 20:25:12 +0200

    include version info

---
+++ contrib/packager.io/before.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#
+# packager.io before script
+#
+
+set -eux
+uname -a
+ruby -v
+env
+
+VERSION="$APP_PKG_VERSION-$APP_PKG_ITERATION"
+echo "Setting VERSION information to $_VERSION"
+echo "$VERSION" > VERSION
+
+# cleanup
+script/build/cleanup.sh
