a4542586cfb3f52b5a78daa1257eb4207771fae2
Matthias Mair <code@mjmair.com>
2023-06-26 22:28:07 +0200

    move envs to qc

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -26,8 +26,7 @@ export default defineConfig({
 
   /* Run your local dev server before starting the tests */
   webServer: {
-    command:
-      'export INVENTREE_DEBUG=True && exportINVENTREE_LOG_LEVEL=DEBUG && export TESTING=True && invoke server -a 127.0.0.1:8000 -c',
+    command: 'invoke server -a 127.0.0.1:8000 -c',
     url: 'http://127.0.0.1:8000/api/',
     reuseExistingServer: !process.env.CI,
     stdout: 'pipe',
