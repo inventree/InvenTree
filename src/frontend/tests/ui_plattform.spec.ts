fb49dfea40a2824acd3c3c03a872f933c9cf7820
Matthias Mair <code@mjmair.com>
2023-06-26 23:22:58 +0200

    reduce wait time

--- src/frontend/tests/ui_plattform.spec.ts
+++ src/frontend/tests/ui_plattform.spec.ts
@@ -10,10 +10,8 @@ test('Basic Platform UI test', async ({ page }) => {
   await page.getByLabel('username').fill('allaccess');
   await page.getByLabel('password').fill('nolimits');
   await page.click('button', { text: 'Sign In' });
-  await page.waitForURL('**/index/');
 
   await page.goto('./platform/');
-  await page.waitForURL('./platform/');
 
   await expect(page).toHaveTitle('InvenTree Demo Server');
   await expect(
