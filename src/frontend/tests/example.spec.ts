cd54f596a61a1617347e31ab9dd67cdc89826829
Matthias Mair <code@mjmair.com>
2023-06-26 21:16:19 +0200

    add test for class ui

--- src/frontend/tests/example.spec.ts
+++ src/frontend/tests/example.spec.ts
@@ -1,8 +1,31 @@
 import { test, expect } from '@playwright/test';
+import exp from 'constants';
 
-test('check test output', async ({ page }) => {
+test('Check class index site', async ({ page }) => {
   await page.goto('./api/');
-  await page.goto('./platform/');
+  await page.goto('./index/');
+  await expect(page).toHaveTitle('InvenTree Demo Server | Sign In');
+  await expect(
+    page.getByRole('heading', { name: 'InvenTree Demo Server' })
+  ).toBeVisible();
 
-  //await expect(page).toHaveTitle(/Welcome to the new frontend!/);
+  await page.getByLabel('username').fill('admin');
+  await page.getByLabel('password').fill('inventree');
+  await page.click('button', { text: 'Sign In' });
+  await page.waitForURL('./index/');
+
+  await page.waitForLoadState('networkidle');
+  await expect(page).toHaveTitle('InvenTree Demo Server | Index');
+  await expect(page.getByRole('button', { name: 'admin' })).toBeVisible();
+  await expect(
+    page.getByRole('link', { name: 'Parts', exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole('link', { name: 'Stock', exact: true })
+  ).toBeVisible();
+  await expect(
+    page.getByRole('link', { name: 'Build', exact: true })
+  ).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Buy' })).toBeVisible();
+  await expect(page.getByRole('button', { name: 'Sell' })).toBeVisible();
 });
