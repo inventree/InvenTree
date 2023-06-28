d3dab6ccaf09a6650b48b4307710623445793672
Matthias Mair <code@mjmair.com>
2023-06-28 06:58:41 +0200

    use vendor splitting

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { splitVendorChunkPlugin } from 'vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -8,7 +9,8 @@ export default defineConfig({
       babel: {
         plugins: ['macros']
       }
-    })
+    }),
+    splitVendorChunkPlugin()
   ],
   build: {
     manifest: true,
