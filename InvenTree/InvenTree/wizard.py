cb194578bad7c311ed76b3a04e3faf96ba98a131
Oliver Walters <oliver.henry.walters@gmail.com>
2023-06-30 22:54:01 +1000

    Register some more models

--- InvenTree/InvenTree/wizard.py
+++ InvenTree/InvenTree/wizard.py
@@ -5,8 +5,12 @@
 
 import data_wizard
 
+import order.models
+import order.serializers
 import part.models
 import part.serializers
+import stock.models
+import stock.serializers
 
 # Register Part models
 data_wizard.register(part.models.Part, part.serializers.PartSerializer)
@@ -14,3 +18,15 @@ data_wizard.register(part.models.PartCategory, part.serializers.CategorySerializ
 data_wizard.register(part.models.PartParameterTemplate, part.serializers.PartParameterTemplateSerializer)
 data_wizard.register(part.models.PartParameter, part.serializers.PartParameterSerializer)
 data_wizard.register(part.models.BomItem, part.serializers.BomItemSerializer)
+
+# Register Order models
+data_wizard.register(order.models.SalesOrder, order.serializers.SalesOrderSerializer)
+data_wizard.register(order.models.SalesOrderLineItem, order.serializers.SalesOrderLineItemSerializer)
+data_wizard.register(order.models.PurchaseOrder, order.serializers.PurchaseOrderSerializer)
+data_wizard.register(order.models.PurchaseOrderLineItem, order.serializers.PurchaseOrderLineItemSerializer)
+data_wizard.register(order.models.ReturnOrder, order.serializers.ReturnOrderSerializer)
+data_wizard.register(order.models.ReturnOrderLineItem, order.serializers.ReturnOrderLineItemSerializer)
+
+# Register Stock models
+data_wizard.register(stock.models.StockItem, stock.serializers.StockItemSerializer)
+data_wizard.register(stock.models.StockLocation, stock.serializers.LocationSerializer)
