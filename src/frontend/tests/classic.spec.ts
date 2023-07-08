6d1fd9707c50929ff84f1829704f882edb1e0e6e
Matthias Mair <code@mjmair.com>
2023-07-09 01:08:55 +0200

    add import order settings

--- src/frontend/tests/classic.spec.ts
+++ src/frontend/tests/classic.spec.ts
@@ -1,4 +1,4 @@
-import { test, expect } from '@playwright/test';
+import { expect, test } from '@playwright/test';
 import exp from 'constants';
 
 test('Check classic index site', async ({ page }) => {
