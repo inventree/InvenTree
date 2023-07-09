c15e99404be38d1e930b00733f4ab5ee946af40e
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-09 22:42:17 +1000

    Add "parttable" component

--- src/frontend/src/pages/Index/Part.tsx
+++ src/frontend/src/pages/Index/Part.tsx
@@ -1,18 +1,24 @@
 import { Trans } from '@lingui/macro';
 import { Group } from '@mantine/core';
 
+import { Text } from '@mantine/core';
+import { DataTable } from 'mantine-datatable';
+
 import { PlaceholderPill } from '../../components/items/Placeholder';
 import { StylishText } from '../../components/items/StylishText';
+
+import { PartTable } from '../../components/tables/PartTable';
 
 export default function Part() {
   return (
     <>
-      <Group>
-        <StylishText>
-          <Trans>Part</Trans>
-        </StylishText>
-        <PlaceholderPill />
-      </Group>
+    <Group>
+      <StylishText>
+        <Trans>Parts</Trans>
+      </StylishText>
+      <PlaceholderPill />
+    </Group>
+    <PartTable />
     </>
   );
 }
