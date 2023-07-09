d1ae84abe451d462fe7b16f9e87e64465a5acd4a
Matthias Mair <code@mjmair.com>
2023-07-09 19:31:31 +0200

    fix tests

--- src/frontend/tests/ui_plattform.spec.ts
+++ src/frontend/tests/ui_plattform.spec.ts
@@ -1,7 +1,7 @@
 import { expect, test } from '@playwright/test';
 
 test('Basic Platform UI test', async ({ page }) => {
-  await page.goto('./index/');
+  await page.goto('./platform/');
   await expect(page).toHaveTitle('InvenTree Demo Server | Sign In');
   await expect(
     page.getByRole('heading', { name: 'InvenTree Demo Server' })
@@ -10,9 +10,13 @@ test('Basic Platform UI test', async ({ page }) => {
   await page.getByLabel('username').fill('allaccess');
   await page.getByLabel('password').fill('nolimits');
   await page.click('button', { text: 'Sign In' });
-
+  await page.waitForURL('**/platform/');
+  await page.getByLabel('username').fill('allaccess');
+  await page.getByLabel('password').fill('nolimits');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForURL('**/platform/home');
   await page.goto('./platform/');
 
   await expect(page).toHaveTitle('InvenTree Demo Server');
-  await expect(page.getByRole('heading', { name: 'Home' })).toBeVisible();
+  await expect(page.getByText('Home').nth(1)).toBeVisible();
 });
