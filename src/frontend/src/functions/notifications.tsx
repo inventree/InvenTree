1f1f74821eb1b248316c2b290f6308a0bafc0f88
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-11 21:28:50 +1000

    Add "not yet implemented" notification message

---
+++ src/frontend/src/functions/notifications.tsx
@@ -0,0 +1,14 @@
+
+import { t } from '@lingui/macro';
+import { notifications } from '@mantine/notifications';
+
+/**
+ * Show a notification that the feature is not yet implemented
+ */
+export function notYetImplemented() {
+    notifications.show({
+        title: t`Not implemented`,
+        message: t`This feature is not yet implemented`,
+        color: 'red',
+    });
+}
