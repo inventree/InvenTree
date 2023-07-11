6ff4060b0ac1490dc69de4cf87cdaa62bfc88a32
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-11 21:27:49 +1000

    Enable mantine notification system

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -3,8 +3,14 @@ import ReactDOM from 'react-dom/client';
 
 import App from './App';
 
+import { MantineProvider } from '@mantine/core';
+import { Notifications } from '@mantine/notifications';
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider withNormalizeCSS withGlobalStyles>
+      <Notifications />
+      <App />
+    </MantineProvider>
   </React.StrictMode>
 );
