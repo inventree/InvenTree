ec418ad1dd3613aac0c5d6abc53bc8c6c330366e
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-14 19:07:07 +1000

    Update sample plugin

--- InvenTree/plugin/samples/integration/label_sample.py
+++ InvenTree/plugin/samples/integration/label_sample.py
@@ -24,14 +24,11 @@ class SampleLabelPrinter(LabelPrintingMixin, InvenTreePlugin):
         """
         # Test that the expected kwargs are present
         print(f"Printing Label: {kwargs['filename']} (User: {kwargs['user']})")
-        print(f"Width: {kwargs['width']} x Height: {kwargs['height']}")
-
-        print("kwargs:", kwargs)
 
         pdf_data = kwargs['pdf_data']
         png_file = self.render_to_png(label=None, pdf_data=pdf_data)
 
-        filename = 'label.pdf'
+        filename = 'sample.pdf'
 
         # Dump the PDF to a local file
         with open(filename, 'wb') as pdf_out:
