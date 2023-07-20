41f7657ec8bafaf6e30fdd592fd1dadd95abbc5c
Matthias Mair <code@mjmair.com>
2023-07-21 01:07:03 +0200

    added language toggle component

---
+++ src/frontend/src/components/items/LanguageToggle.tsx
@@ -0,0 +1,47 @@
+import { ActionIcon, Group, Select } from '@mantine/core';
+import { useDisclosure } from '@mantine/hooks';
+import { IconLanguage } from '@tabler/icons-react';
+import { useEffect, useState } from 'react';
+
+import { Locales, languages } from '../../contexts/LanguageContext';
+import { useLocalState } from '../../states/LocalState';
+
+export function LanguageToggle() {
+  const [open, toggle] = useDisclosure();
+  const [value, setValue] = useState<Locales | null>(null);
+  const [locale, setLanguage] = useLocalState((state) => [
+    state.language,
+    state.setLanguage
+  ]);
+
+  // change global language on change
+  useEffect(() => {
+    if (value === null) return;
+    setLanguage(value);
+  }, [value]);
+
+  // set language on component load
+  useEffect(() => {
+    setValue(locale);
+  }, [locale]);
+
+  return (
+    <Group
+      position="center"
+      style={{
+        border: open === true ? `1px dashed ` : ``,
+        margin: open === true ? 2 : 12,
+        padding: open === true ? 8 : 0
+      }}
+    >
+      <ActionIcon onClick={() => toggle.toggle()} size="lg">
+        <IconLanguage />
+      </ActionIcon>
+      {open && (
+        <Group>
+          <Select w={80} data={languages} value={value} onChange={setValue} />
+        </Group>
+      )}
+    </Group>
+  );
+}
