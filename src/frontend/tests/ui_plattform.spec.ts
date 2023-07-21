9d2b755e5099a163b31f2a9b58b0eaf57ba947b4
Matthias Mair <code@mjmair.com>
2023-07-21 22:28:33 +0200

    revert unneeded change

--- src/frontend/tests/ui_plattform.spec.ts
+++ src/frontend/tests/ui_plattform.spec.ts
@@ -4,8 +4,8 @@ test('Basic Platform UI test', async ({ page }) => {
   await page.goto('./platform/');
   await expect(page).toHaveTitle('InvenTree Demo Server');
   await page.waitForURL('**/platform/');
-  await page.getByLabel('Username').fill('allaccess');
-  await page.getByLabel('Password').fill('nolimits');
+  await page.getByLabel('username').fill('allaccess');
+  await page.getByLabel('password').fill('nolimits');
   await page.getByRole('button', { name: 'Log in' }).click();
   await page.waitForURL('**/platform/home');
   await page.goto('./platform/');
