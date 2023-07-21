fe0d3c9d17ec50354f70ca6687587c37abe0b0d9
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-22 01:15:14 +1000

    Spelling fix

--- src/frontend/src/components/tables/part/PartTable.tsx
+++ src/frontend/src/components/tables/part/PartTable.tsx
@@ -1,62 +1,59 @@
 import { t } from '@lingui/macro';
-
-import { shortenString } from '../../../functions/tables';
-
-import { ThumbnailHoverCard } from '../../items/Thumbnail';
-
-import { InvenTreeTable } from '../InvenTreeTable';
-import { notYetImplemented } from '../../../functions/notifications';
-
 import { Text } from '@mantine/core';
+
+import { notYetImplemented } from '../../../functions/notifications';
+import { shortenString } from '../../../functions/tables';
+import { ThumbnailHoverCard } from '../../items/Thumbnail';
 import { TableColumn } from '../Column';
 import { TableFilter } from '../Filter';
-
+import { InvenTreeTable } from '../InvenTreeTable';
 
 /**
  * Construct a list of columns for the part table
  */
-function partTableColumns() : TableColumn[] {
+function partTableColumns(): TableColumn[] {
   return [
     {
       accessor: 'name',
       sortable: true,
       title: t`Part`,
-      render: function(record: any) {
-
+      render: function (record: any) {
         // TODO - Link to the part detail page
-        return <ThumbnailHoverCard
-          src={record.thumbnail || record.image}
-          text={record.name}
-          link=""
-        />;
+        return (
+          <ThumbnailHoverCard
+            src={record.thumbnail || record.image}
+            text={record.name}
+            link=""
+          />
+        );
       }
     },
     {
       accessor: 'IPN',
       title: t`IPN`,
       sortable: true,
-      switchable: true,
+      switchable: true
     },
     {
       accessor: 'units',
       sortable: true,
       title: t`Units`,
-      switchable: true,
+      switchable: true
     },
     {
       accessor: 'description',
       title: t`Description`,
       sortable: true,
-      switchable: true,
+      switchable: true
     },
     {
       accessor: 'category',
       title: t`Category`,
       sortable: true,
-      render: function(record: any) {
+      render: function (record: any) {
         // TODO: Link to the category detail page
         return shortenString({
-          str: record.category_detail.pathstring,
+          str: record.category_detail.pathstring
         });
       }
     },
@@ -64,22 +61,22 @@ function partTableColumns() : TableColumn[] {
       accessor: 'total_in_stock',
       title: t`Stock`,
       sortable: true,
-      switchable: true,
+      switchable: true
     },
     {
       accessor: 'price_range',
       title: t`Price Range`,
       sortable: false,
       switchable: true,
-      render: function(record: any) {
+      render: function (record: any) {
         // TODO: Render price range
-        return "-- price --";
+        return '-- price --';
       }
     },
     {
       accessor: 'link',
       title: t`Link`,
-      switchable: true,
+      switchable: true
     }
   ];
 }
@@ -87,44 +84,40 @@ function partTableColumns() : TableColumn[] {
 /**
  * Construct a set of filters for the part table
  */
-function partTableFilters() : TableFilter[] {
+function partTableFilters(): TableFilter[] {
   return [
     {
       name: 'active',
       label: t`Active`,
       description: t`Filter by part active status`,
-      type: 'boolean',
-    },
+      type: 'boolean'
+    }
   ];
 }
-
 
 /**
  * PartListTable - Displays a list of parts, based on the provided parameters
  * @param {Object} params - The query parameters to pass to the API
- * @returns 
+ * @returns
  */
-export function PartListTable({
-    params={}
-  }: {
-    params?: any;
-  }) {
+export function PartListTable({ params = {} }: { params?: any }) {
+  let tableParams = Object.assign({}, params);
 
-    let tableParams = Object.assign({}, params);
-    
-    // Add required query parmeters
-    tableParams.category_detail = true;
+  // Add required query parameters
+  tableParams.category_detail = true;
 
-    return <InvenTreeTable
-        url='part/'
-        params={tableParams}
-        enableDownload
-        tableKey='part-table'
-        printingActions={[
-          <Text onClick={notYetImplemented}>Hello</Text>,
-          <Text onClick={notYetImplemented}>World</Text>
-        ]}
-        columns={partTableColumns()}
-        customFilters={partTableFilters()}
-    />;
+  return (
+    <InvenTreeTable
+      url="part/"
+      params={tableParams}
+      enableDownload
+      tableKey="part-table"
+      printingActions={[
+        <Text onClick={notYetImplemented}>Hello</Text>,
+        <Text onClick={notYetImplemented}>World</Text>
+      ]}
+      columns={partTableColumns()}
+      customFilters={partTableFilters()}
+    />
+  );
 }
