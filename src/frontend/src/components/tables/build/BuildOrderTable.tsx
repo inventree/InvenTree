44e5a000ea712b69421257ac9ce2070f39333abf
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-22 23:12:18 +1000

    Cleanup build order table

--- src/frontend/src/components/tables/build/BuildOrderTable.tsx
+++ src/frontend/src/components/tables/build/BuildOrderTable.tsx
@@ -1,8 +1,10 @@
 import { t } from '@lingui/macro';
 import { Progress } from '@mantine/core';
+import { useMemo } from 'react';
 
 import { ThumbnailHoverCard } from '../../items/Thumbnail';
 import { TableColumn } from '../Column';
+import { TableFilter } from '../Filter';
 import { InvenTreeTable } from '../InvenTreeTable';
 
 /**
@@ -98,22 +100,36 @@ function buildOrderTableColumns(): TableColumn[] {
   ];
 }
 
+function buildOrderTableFilters(): TableFilter[] {
+  return [];
+}
+
+function buildOrderTableParams(params: any): any {
+  return {
+    ...params,
+    part_detail: true
+  };
+}
+
 /*
  * Construct a table of build orders, according to the provided parameters
  */
 export function BuildOrderTable({ params = {} }: { params?: any }) {
   // Add required query parameters
-  let tableParams = { ...params };
+  let tableParams = useMemo(() => buildOrderTableParams(params), [params]);
+  let tableColumns = useMemo(() => buildOrderTableColumns(), []);
+  let tableFilters = useMemo(() => buildOrderTableFilters(), []);
 
   tableParams.part_detail = true;
 
   return (
     <InvenTreeTable
       url="build/"
-      params={tableParams}
       enableDownload
       tableKey="build-order-table"
-      columns={buildOrderTableColumns()}
+      params={tableParams}
+      columns={tableColumns}
+      customFilters={tableFilters}
     />
   );
 }
