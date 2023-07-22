4a815f21b77508cec54f76d6aae823ea0dbd1ea9
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-23 00:00:56 +1000

    Add very basic structure for generic form

---
+++ src/frontend/src/components/forms/GenericForm.tsx
@@ -0,0 +1,65 @@
+import { Trans, t } from '@lingui/macro';
+import { Input, NumberInput, Text, TextInput } from '@mantine/core';
+import { Group } from '@mantine/core';
+
+/**
+ * Define a "generic" form field.
+ */
+export type GenericFormField = {
+  name: string;
+  label: string;
+  value?: any;
+  type: string;
+  required: boolean;
+  placeholder?: string;
+  help_text?: string;
+  icon?: string;
+  errors?: string[];
+};
+
+/**
+ * Construct a form input based on a GenericFormField definition object
+ * @param field - A GenericFormField object
+ */
+export function GenericFormInput({ field }: { field: GenericFormField }) {
+  switch (field.type) {
+    case 'text':
+    case 'string':
+      return (
+        <TextInput
+          placeholder={field.placeholder || undefined}
+          error={field.errors?.length ?? 0 > 0}
+        />
+      );
+    default:
+      return (
+        <TextInput
+          error
+          disabled
+          placeholder={`Unknown field type: ${field.type}`}
+        />
+      );
+  }
+}
+
+/**
+ * Construct a form field input group
+ */
+export function GenericFormInputGroup({ field }: { field: GenericFormField }) {
+  return (
+    <Group>
+      <Text>{field.label}</Text>
+      <GenericFormInput field={field} />
+    </Group>
+  );
+}
+
+/**
+ *
+ * Construct a "generic" form, where the fields are defined by the caller.
+ */
+export function GenericForm({ fields }: { fields: GenericFormField[] }) {
+  return (
+    <Group>{fields.map((field) => GenericFormInputGroup({ field }))}</Group>
+  );
+}
