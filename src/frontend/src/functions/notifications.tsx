78f79f4ba3bf7358bc0b4cba046fc48bf2c41398
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-23 09:59:12 +1000

    Format updates

--- src/frontend/src/functions/notifications.tsx
+++ src/frontend/src/functions/notifications.tsx
@@ -1,4 +1,3 @@
-
 import { t } from '@lingui/macro';
 import { notifications } from '@mantine/notifications';
 
@@ -6,9 +5,9 @@ import { notifications } from '@mantine/notifications';
  * Show a notification that the feature is not yet implemented
  */
 export function notYetImplemented() {
-    notifications.show({
-        title: t`Not implemented`,
-        message: t`This feature is not yet implemented`,
-        color: 'red',
-    });
+  notifications.show({
+    title: t`Not implemented`,
+    message: t`This feature is not yet implemented`,
+    color: 'red'
+  });
 }
