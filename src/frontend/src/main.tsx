af0fe45a132a73702a2766a2b3f4d868899c28d2
Matthias Mair <code@mjmair.com>
2023-07-27 01:36:39 +0200

    add debug msg

--- src/frontend/src/main.tsx
+++ src/frontend/src/main.tsx
@@ -10,6 +10,8 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 );
 
 // Redirect to /platform if on / - while building for netlify
+console.log(process.env.NETLIFY_PREVIEW);
+console.log(process.env);
 if (process.env.NETLIFY_PREVIEW == 'TRUE') {
   if (window.location.pathname === '/') {
     window.location.replace('/platform');
