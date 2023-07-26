ef880db07d769f2ac8601d4312534ab2c297f7bb
Oliver Walters <oliver.henry.walters@gmail.com>
2023-07-26 15:41:28 +1000

    Perform debounced query

--- src/frontend/src/components/nav/SearchDrawer.tsx
+++ src/frontend/src/components/nav/SearchDrawer.tsx
@@ -3,7 +3,10 @@ import { Drawer, TextInput } from '@mantine/core';
 import { Loader } from '@mantine/core';
 import { useDebouncedValue } from '@mantine/hooks';
 import { IconBackspace, IconSearch } from '@tabler/icons-react';
+import { useQuery } from '@tanstack/react-query';
 import { useEffect, useState } from 'react';
+
+import { api } from '../../App';
 
 /**
  * Construct a drawer which provides quick-search functionality
@@ -20,8 +23,57 @@ export function SearchDrawer({
   const [searchText] = useDebouncedValue(value, 500);
 
   useEffect(() => {
-    console.log(`Search text: ${searchText}`);
+    // TODO: Implement search functionality
+    refetch();
   }, [searchText]);
+
+  // Function for performing the actual search query
+  const performSearch = async () => {
+    console.log('Performing search:', searchText);
+    // Return empty result set if no search text
+    if (!searchText) {
+      return [];
+    }
+
+    let params: any = {
+      offset: 0,
+      limit: 10,
+      search: searchText,
+      searchRegex: false, // TODO: Make this configurable
+      searchWhole: false // TODO: Make this configurable
+    };
+
+    // TODO: Implement specific search functionality based on user settings (and permissions)
+    params.part = {};
+    params.supplierpart = {};
+    params.manufacturerpart = {};
+    params.partcategory = {};
+    params.stock = {};
+    params.stocklocation = {};
+    params.build = {};
+    params.company = {};
+    params.purchaseorder = {};
+    params.salesorder = {};
+    params.returnorder = {};
+
+    return api
+      .post(`/search/`, params)
+      .then(function (response) {
+        console.log('results:', response);
+        return response.data;
+      })
+      .catch(function (error) {
+        console.error(error);
+        return [];
+      });
+  };
+
+  // Search query manager
+  const { data, isError, isFetching, isLoading, refetch } = useQuery(
+    ['search', searchText],
+    performSearch,
+    {}
+  );
 
   return (
     <Drawer
@@ -36,7 +88,7 @@ export function SearchDrawer({
           radius="xs"
           value={value}
           onChange={(event) => setValue(event.currentTarget.value)}
-          icon={<IconSearch size="0.8rem" />}
+          icon={<IconSearch size="0.8rem" onClick={() => refetch()} />}
           rightSection={
             value && <IconBackspace color="red" onClick={() => setValue('')} />
           }
@@ -44,7 +96,7 @@ export function SearchDrawer({
         />
       }
     >
-      <Loader />
+      {isFetching && <Loader />}
     </Drawer>
   );
 }
