00b25bb8db8fabb991368b8ee2b9d60c6ce3df58
Oliver <oliver.henry.walters@gmail.com>
2023-08-03 13:52:12 +1000

    Handle case where calculation may be performed on invalid values

--- InvenTree/stock/api.py
+++ InvenTree/stock/api.py
@@ -677,8 +677,13 @@ class StockList(APIDownloadMixin, ListCreateDestroyAPIView):
                 else:
                     if bool(data.get('use_pack_size')):
                         quantity = data['quantity'] = supplier_part.base_quantity(quantity)
+
                         # Divide purchase price by pack size, to save correct price per stock item
-                        data['purchase_price'] = float(data['purchase_price']) / float(supplier_part.pack_quantity_native)
+                        if data['purchase_price'] and supplier_part.pack_quantity_native:
+                            try:
+                                data['purchase_price'] = float(data['purchase_price']) / float(supplier_part.pack_quantity_native)
+                            except ValueError:
+                                pass
 
         # Now remove the flag from data, so that it doesn't interfere with saving
         # Do this regardless of results above
