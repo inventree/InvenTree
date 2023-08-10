e5a13aa9aa7e1ef839967623e54fe80e177ae1fd
Matthias Mair <code@mjmair.com>
2023-08-10 22:44:07 +0200

    refactored OwnerRenderer

--- src/frontend/src/components/renderer/ownerRenderer.tsx
+++ src/frontend/src/components/renderer/ownerRenderer.tsx
@@ -1,16 +1,33 @@
 import { t } from '@lingui/macro';
-import { Anchor, Group, Text, Tooltip } from '@mantine/core';
-import { IconUser, IconUsersGroup } from '@tabler/icons-react';
+import { Anchor, Group, MantineSize, Text, Tooltip } from '@mantine/core';
+import {
+  IconQuestionMark,
+  IconUser,
+  IconUsersGroup
+} from '@tabler/icons-react';
+
+function OwnerIcon({
+  record,
+  size
+}: {
+  record: any;
+  size: MantineSize | number;
+}) {
+  switch (record.label) {
+    case t`group`:
+      return <IconUsersGroup size={size} />;
+    case t`user`:
+      return <IconUser size={size} />;
+    default:
+      return <IconQuestionMark size={size} />;
+  }
+}
 
 export function ownerRenderer(record: any) {
   return record ? (
     <Tooltip label={record.label} position="bottom">
       <Group spacing={0}>
-        {record.label == t`group` ? (
-          <IconUsersGroup size={12} />
-        ) : (
-          <IconUser size={12} />
-        )}
+        <OwnerIcon record={record} size={16} />
         <Text>{record.name}</Text>
       </Group>
     </Tooltip>
