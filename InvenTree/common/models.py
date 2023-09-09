af7fc0baf24548645b571a8e8f9c5c6862207d6b
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2023-09-09 18:50:43 +0000

    Fix test debug

--- InvenTree/common/models.py
+++ InvenTree/common/models.py
@@ -615,7 +615,7 @@ class BaseInvenTreeSetting(models.Model):
         elif self.is_bool():
             self.value = self.as_bool()
 
-        definition = self.__class__.get_setting_definition(self.key, **kwargs)
+        definition = self.__class__.get_setting_definition(self.key, **self.get_filters_for_instance())
         validator = self.__class__.get_setting_validator(self.key, **{**self.get_filters_for_instance(), **kwargs})
         print("DEBUG LINE #1", str(self), kwargs, self.get_filters_for_instance(), definition, validator)
 
