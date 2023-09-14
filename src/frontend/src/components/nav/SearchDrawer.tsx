db7025c87bad1aaed4f5e4af72815913a70fca54
Oliver Walters <oliver.henry.walters@gmail.com>
2023-09-15 00:13:42 +1000

    Cleanup links in search bar

--- src/frontend/src/components/nav/SearchDrawer.tsx
+++ src/frontend/src/components/nav/SearchDrawer.tsx
@@ -2,6 +2,7 @@ import { Trans, t } from '@lingui/macro';
 import {
   ActionIcon,
   Alert,
+  Anchor,
   Center,
   Checkbox,
   Divider,
@@ -209,13 +210,13 @@ function QueryResultGroup({
         <Divider />
         <Stack>
           {query.results.results.map((result: any) => (
-            <div onClick={() => onResultClick(query.name, result.pk)}>
+            <Anchor onClick={() => onResultClick(query.name, result.pk)}>
               <RenderInstance
                 key={`${query.name}-${result.pk}`}
                 instance={result}
                 model={query.name}
               />
-            </div>
+            </Anchor>
           ))}
         </Stack>
         <Space />
