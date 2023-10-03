85ace32f152e450fdebe9d0af3b332d42fe07e3d
Bobbe <maxschlecht@web.de>
2023-10-03 03:49:51 +0200

    Fix TME qrcode regex

--- InvenTree/plugin/builtin/suppliers/tme.py
+++ InvenTree/plugin/builtin/suppliers/tme.py
@@ -34,24 +34,24 @@ class TMEPlugin(SupplierBarcodeMixin, InvenTreePlugin):
                 QRCODE_FIELD_NAME_MAP.get(field_name, field_name): value
                 for field_name, value in TME_PARSE_QRCODE_REGEX.findall(barcode_data)
             }
-        elif barcode_split := TME_BARCODE2D_REGEX.findall(barcode_data):
+        elif barcode_split := TME_PARSE_BARCODE2D_REGEX.findall(barcode_data):
             barcode_fields = self.parse_ecia_barcode2d(barcode_split)
         else:
             return None
-
-        if po_number := barcode_fields.get("purchase_order_number"):
-            barcode_fields["purchase_order_number"] = po_number.split("/")[0]
 
         sku = barcode_fields.get("supplier_part_number")
         if not (supplier_part := self.get_supplier_part(sku)):
             return None
 
+        if po_number := barcode_fields.get("purchase_order_number"):
+            barcode_fields["purchase_order_number"] = po_number.split("/")[0]
+
         return supplier_part, barcode_fields
 
 
-TME_IS_QRCODE_REGEX = re.compile(r"^(\S+(\s+|$))+")
+TME_IS_QRCODE_REGEX = re.compile(r"([^\s:]+:[^\s:]+\s+)+PN:[^\s:]+\s+(\S+(\s|$)+)+")
 TME_PARSE_QRCODE_REGEX = re.compile(r"([^\s:]+):([^\s:]+)(?:\s+|$)")
-TME_BARCODE2D_REGEX = re.compile(r"([^\s]+)(?:\s+|$)")
+TME_PARSE_BARCODE2D_REGEX = re.compile(r"([^\s]+)(?:\s+|$)")
 QRCODE_FIELD_NAME_MAP = {
     "PN": "supplier_part_number",
     "PO": "purchase_order_number",
