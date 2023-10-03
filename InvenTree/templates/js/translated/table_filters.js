78905a45c76f3d1f52459c4b410216325a6f68ef
Oliver <oliver.henry.walters@gmail.com>
2023-10-03 19:44:10 +1100

    Plugin missing fix (#5653)
    * Add 'is_installed' property to PluginConfig model
    
    - Allow us to show users which plugins are "outdated"
    
    * Cleanup plugin table
    
    - Display clearly if a plugin is no longer installed
    
    * Fixes for plugin table
    
    * Revert change due to failing CI

--- InvenTree/templates/js/translated/table_filters.js
+++ InvenTree/templates/js/translated/table_filters.js
@@ -471,6 +471,10 @@ function getPluginTableFilters() {
             type: 'bool',
             title: '{% trans "Sample" %}',
         },
+        installed: {
+            type: 'bool',
+            title: '{% trans "Installed" %}'
+        },
     };
 }
 
