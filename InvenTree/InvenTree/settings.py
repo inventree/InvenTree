0d4c59558623d51c1ce9057eedc1c910379f3184
Oliver Walters <oliver.henry.walters@gmail.com>
2023-10-05 23:28:19 +1100

    Custom token auth handler
    - Correctly handles revoked tokens
    - Correctly handles expired tokens

--- InvenTree/InvenTree/settings.py
+++ InvenTree/InvenTree/settings.py
@@ -375,7 +375,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
-        'rest_framework.authentication.TokenAuthentication',
+        'users.authentication.ApiTokenAuthentication',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
     'DEFAULT_PERMISSION_CLASSES': (
