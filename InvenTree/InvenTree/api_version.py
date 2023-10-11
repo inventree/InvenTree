c35ff4e5be578d9534a619459c73c3eca32403e9
Oliver <oliver.henry.walters@gmail.com>
2023-10-11 16:23:20 +1100

    Update api version notes

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -10,6 +10,7 @@ Increment this API version number whenever there is a significant change to the 
 v127 -> 2023-10-10 : https://github.com/inventree/InvenTree/pull/5679
     - Settings keys are no longer case sensitive
     - Include settings units in API serializer
+    - Add "admin" and "superuser" status to user serializer
 
 v136 -> 2023-09-23 : https://github.com/inventree/InvenTree/pull/5595
     - Adds structural to StockLocation and PartCategory tree endpoints
