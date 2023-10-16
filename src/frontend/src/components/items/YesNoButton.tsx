d98b1bb2c7f4d65f7e15ef52c6580be9c7abea06
Oliver <oliver.henry.walters@gmail.com>
2023-10-17 00:54:55 +1100

    Fix <YesNoButton> (#5711)
    - Add missing destructuring

--- src/frontend/src/components/items/YesNoButton.tsx
+++ src/frontend/src/components/items/YesNoButton.tsx
@@ -1,7 +1,7 @@
 import { t } from '@lingui/macro';
 import { Badge } from '@mantine/core';
 
-export function YesNoButton(value: any) {
+export function YesNoButton({ value }: { value: any }) {
   const bool =
     String(value).toLowerCase().trim() in ['true', '1', 't', 'y', 'yes'];
 
