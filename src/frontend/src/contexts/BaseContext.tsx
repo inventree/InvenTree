36d079c3f552895618b34b1316cf8e47057584f2
Oliver Walters <oliver.henry.walters@gmail.com>
2023-10-16 23:33:00 +1100

    Update BaseContext

--- src/frontend/src/contexts/BaseContext.tsx
+++ src/frontend/src/contexts/BaseContext.tsx
@@ -3,15 +3,12 @@ import { QueryClientProvider } from '@tanstack/react-query';
 import { queryClient } from '../App';
 import { LanguageContext } from './LanguageContext';
 import { ThemeContext } from './ThemeContext';
-import { InvenTreeUserContext } from './UserContext';
 
 export const BaseContext = ({ children }: { children: any }) => {
   return (
     <QueryClientProvider client={queryClient}>
       <LanguageContext>
-        <InvenTreeUserContext>
-          <ThemeContext>{children}</ThemeContext>
-        </InvenTreeUserContext>
+        <ThemeContext>{children}</ThemeContext>
       </LanguageContext>
     </QueryClientProvider>
   );
