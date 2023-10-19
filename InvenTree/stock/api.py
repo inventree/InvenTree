d2a313bda97804a184b93ede656ba4963e80dcc7
Oliver <oliver.henry.walters@gmail.com>
2023-10-19 11:46:32 +1100

    Handle ValueError when creating stock item (#5751)

--- InvenTree/stock/api.py
+++ InvenTree/stock/api.py
@@ -677,7 +677,11 @@ class StockList(APIDownloadMixin, ListCreateDestroyAPIView):
                     if bool(data.get('use_pack_size')):
                         quantity = data['quantity'] = supplier_part.base_quantity(quantity)
                         # Divide purchase price by pack size, to save correct price per stock item
-                        data['purchase_price'] = float(data['purchase_price']) / float(supplier_part.pack_quantity_native)
+                        try:
+                            data['purchase_price'] = float(data['purchase_price']) / float(supplier_part.pack_quantity_native)
+                        except ValueError:
+                            # If the purchase price is not a number, ignore it
+                            pass
 
         # Now remove the flag from data, so that it doesn't interfere with saving
         # Do this regardless of results above
