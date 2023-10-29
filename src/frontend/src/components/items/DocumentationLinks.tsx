36766907d2d2259cc861a2e970bd7568dfce0053
Matthias Mair <code@mjmair.com>
2023-10-29 18:25:55 +0100

    made it possible to use an action instead of a link

--- src/frontend/src/components/items/DocumentationLinks.tsx
+++ src/frontend/src/components/items/DocumentationLinks.tsx
@@ -29,24 +29,53 @@ export function DocumentationLinks({
 }: {
   links: DocumentationLinkItem[];
 }) {
+  const DocumentationLinkRenderer = ({
+    link
+  }: {
+    link: DocumentationLinkItem;
+  }) => {
+    const content = (
+      <Text size="sm" fw={500}>
+        {link.title}
+      </Text>
+    );
+
+    const Linker = ({ children }: { children: any }) => {
+      if (link.link) return;
+      <Anchor href={link.link} key={link.id}>
+        {children}
+      </Anchor>;
+
+      if (link.action)
+        return (
+          <Anchor component="button" type="button" onClick={link.action}>
+            {children}
+          </Anchor>
+        );
+
+      console.log('Neither link nor action found for link:', link);
+      return children;
+    };
+
+    return (
+      <Linker>
+        {link.placeholder ? (
+          <Group>
+            {content}
+            <PlaceholderPill />
+          </Group>
+        ) : (
+          content
+        )}
+      </Linker>
+    );
+  };
+
   return (
     <SimpleGrid cols={2} spacing={0}>
       {links.map((link) => (
         <DocTooltip key={link.id} text={link.description}>
-          <Anchor href={link.link} key={link.id}>
-            {link.placeholder ? (
-              <Group>
-                <Text size="sm" fw={500}>
-                  {link.title}
-                </Text>
-                <PlaceholderPill />
-              </Group>
-            ) : (
-              <Text size="sm" fw={500}>
-                {link.title}
-              </Text>
-            )}
-          </Anchor>
+          <DocumentationLinkRenderer link={link} />
         </DocTooltip>
       ))}
     </SimpleGrid>
