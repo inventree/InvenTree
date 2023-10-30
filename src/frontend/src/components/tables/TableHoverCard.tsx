e4e1aae7db62954cb8d83e466b41a7250a838553
Oliver Walters <oliver.henry.walters@gmail.com>
2023-10-30 22:05:52 +1100

    Adds <TableHoverCard> component
    - Allows us to display more information in a table cell, without clutter

---
+++ src/frontend/src/components/tables/TableHoverCard.tsx
@@ -0,0 +1,43 @@
+import { Divider, Group, HoverCard, Stack } from '@mantine/core';
+import { IconInfoCircle } from '@tabler/icons-react';
+
+/*
+ * A custom hovercard element for displaying extra information in a table cell.
+ * If a table cell has extra information available,
+ * it can be displayed as a drop-down hovercard when the user hovers over the cell.
+ */
+export function TableHoverCard({
+  value, // The value of the cell
+  extra, // The extra information to display
+  title // The title of the hovercard
+}: {
+  value: any;
+  extra?: any;
+  title?: string;
+}) {
+  // If no extra information presented, just return the raw value
+  if (!extra) {
+    return value;
+  }
+
+  return (
+    <HoverCard>
+      <HoverCard.Target>
+        <Group spacing="xs" position="apart">
+          {value}
+          <IconInfoCircle size="16" />
+        </Group>
+      </HoverCard.Target>
+      <HoverCard.Dropdown>
+        <Stack spacing="xs">
+          <Group spacing="xs" position="left">
+            <IconInfoCircle size="16" />
+            {title}
+          </Group>
+          <Divider />
+          {extra}
+        </Stack>
+      </HoverCard.Dropdown>
+    </HoverCard>
+  );
+}
