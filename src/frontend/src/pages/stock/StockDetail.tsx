c90af001f71268d4e4e0e8365fbc36bb0d7d2b01
Oliver Walters <oliver.henry.walters@gmail.com>
2023-10-31 23:32:01 +1100

    Add some placeholder actions for StockItem page

--- src/frontend/src/pages/stock/StockDetail.tsx
+++ src/frontend/src/pages/stock/StockDetail.tsx
@@ -4,15 +4,31 @@ import {
   IconBookmark,
   IconBoxPadding,
   IconChecklist,
+  IconCircleCheck,
+  IconCircleMinus,
+  IconCirclePlus,
+  IconCopy,
+  IconDots,
+  IconEdit,
   IconHistory,
   IconInfoCircle,
+  IconLink,
   IconNotes,
+  IconPackages,
   IconPaperclip,
-  IconSitemap
+  IconQrcode,
+  IconSitemap,
+  IconTransfer,
+  IconTrash,
+  IconUnlink
 } from '@tabler/icons-react';
 import { useMemo, useState } from 'react';
 import { useParams } from 'react-router-dom';
 
+import {
+  ActionDropdown,
+  BarcodeActionDropdown
+} from '../../components/items/ActionDropdown';
 import { PlaceholderPanel } from '../../components/items/Placeholder';
 import { PageDetail } from '../../components/nav/PageDetail';
 import { PanelGroup, PanelType } from '../../components/nav/PanelGroup';
@@ -21,9 +37,12 @@ import { AttachmentTable } from '../../components/tables/general/AttachmentTable
 import { NotesEditor } from '../../components/widgets/MarkdownEditor';
 import { useInstance } from '../../hooks/UseInstance';
 import { ApiPaths, apiUrl } from '../../states/ApiState';
+import { useUserState } from '../../states/UserState';
 
 export default function StockDetail() {
   const { id } = useParams();
+
+  const user = useUserState();
 
   const [treeOpen, setTreeOpen] = useState(false);
 
@@ -120,6 +139,82 @@ export default function StockDetail() {
     [stockitem]
   );
 
+  const stockActions = useMemo(
+    () => /* TODO: Disable actions based on user permissions*/ [
+      <BarcodeActionDropdown
+        actions={[
+          {
+            icon: <IconQrcode />,
+            name: t`View`,
+            tooltip: t`View part barcode`
+          },
+          {
+            icon: <IconLink />,
+            name: t`Link Barcode`,
+            tooltip: t`Link custom barcode to stock item`,
+            disabled: stockitem?.barcode_hash
+          },
+          {
+            icon: <IconUnlink />,
+            name: t`Unlink Barcode`,
+            tooltip: t`Unlink custom barcode from stock item`,
+            disabled: !stockitem?.barcode_hash
+          }
+        ]}
+      />,
+      <ActionDropdown
+        key="operations"
+        tooltip={t`Stock Operations`}
+        icon={<IconPackages />}
+        actions={[
+          {
+            name: t`Count`,
+            tooltip: t`Count stock`,
+            icon: <IconCircleCheck color="green" />
+          },
+          {
+            name: t`Add`,
+            tooltip: t`Add stock`,
+            icon: <IconCirclePlus color="green" />
+          },
+          {
+            name: t`Remove`,
+            tooltip: t`Remove stock`,
+            icon: <IconCircleMinus color="red" />
+          },
+          {
+            name: t`Transfer`,
+            tooltip: t`Transfer stock`,
+            icon: <IconTransfer color="blue" />
+          }
+        ]}
+      />,
+      <ActionDropdown
+        key="stock"
+        // tooltip={t`Stock Actions`}
+        icon={<IconDots />}
+        actions={[
+          {
+            name: t`Duplicate`,
+            tooltip: t`Duplicate stock item`,
+            icon: <IconCopy />
+          },
+          {
+            name: t`Edit`,
+            tooltip: t`Edit stock item`,
+            icon: <IconEdit color="blue" />
+          },
+          {
+            name: t`Delete`,
+            tooltip: t`Delete stock item`,
+            icon: <IconTrash color="red" />
+          }
+        ]}
+      />
+    ],
+    [id, stockitem, user]
+  );
+
   return (
     <Stack>
       <LoadingOverlay visible={instanceQuery.isFetching} />
@@ -129,8 +224,9 @@ export default function StockDetail() {
         selectedLocation={stockitem?.location}
       />
       <PageDetail
-        title={t`Stock Items`}
-        subtitle={stockitem.part_detail?.full_name ?? 'name goes here'}
+        title={t`Stock Item`}
+        subtitle={stockitem.part_detail?.full_name}
+        imageUrl={stockitem.part_detail?.thumbnail}
         detail={
           <Alert color="teal" title="Stock Item">
             <Text>Quantity: {stockitem.quantity ?? 'idk'}</Text>
@@ -140,6 +236,7 @@ export default function StockDetail() {
         breadcrumbAction={() => {
           setTreeOpen(true);
         }}
+        actions={stockActions}
       />
       <PanelGroup pageKey="stockitem" panels={stockPanels} />
     </Stack>
