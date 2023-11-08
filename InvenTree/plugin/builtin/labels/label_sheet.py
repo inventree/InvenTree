e1dd5b4e17b10198083eb582e0a3791a21413b16
Oliver Walters <oliver.henry.walters@gmail.com>
2023-11-08 20:55:11 +1100

    Add custom printing options serializer

--- InvenTree/plugin/builtin/labels/label_sheet.py
+++ InvenTree/plugin/builtin/labels/label_sheet.py
@@ -2,9 +2,23 @@
 
 from django.utils.translation import gettext_lazy as _
 
+from rest_framework import serializers
+
+import report.helpers
 from label.models import LabelTemplate
 from plugin import InvenTreePlugin
 from plugin.mixins import LabelPrintingMixin, SettingsMixin
+
+
+class LabelPrintingOptionsSerializer(serializers.Serializer):
+    """Custom printing options for the label sheet plugin"""
+
+    page_size = serializers.ChoiceField(
+        choices=report.helpers.report_page_size_options(),
+        default='A4',
+        label=_('Page Size'),
+        help_text=_('Page size for the label sheet')
+    )
 
 
 class InvenTreeLabelSheetPlugin(LabelPrintingMixin, SettingsMixin, InvenTreePlugin):
@@ -23,6 +37,8 @@ class InvenTreeLabelSheetPlugin(LabelPrintingMixin, SettingsMixin, InvenTreePlug
     BLOCKING_PRINT = True
 
     SETTINGS = {}
+
+    PrintingOptionsSerializer = LabelPrintingOptionsSerializer
 
     def print_labels(self, label: LabelTemplate, items: list, request, **kwargs):
         """Handle printing of the provided labels"""
