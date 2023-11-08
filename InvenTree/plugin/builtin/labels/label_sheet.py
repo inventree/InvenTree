12c14c9c90e335097ee6730303b15d877f985c31
Oliver Walters <oliver.henry.walters@gmail.com>
2023-11-08 22:18:31 +1100

    Extract page size and column size

--- InvenTree/plugin/builtin/labels/label_sheet.py
+++ InvenTree/plugin/builtin/labels/label_sheet.py
@@ -1,5 +1,7 @@
 """Label printing plugin which supports printing multiple labels on a single page"""
 
+import math
+
 from django.core.exceptions import ValidationError
 from django.utils.translation import gettext_lazy as _
 
@@ -19,6 +21,12 @@ class LabelPrintingOptionsSerializer(serializers.Serializer):
         default='A4',
         label=_('Page Size'),
         help_text=_('Page size for the label sheet')
+    )
+
+    landscape = serializers.BooleanField(
+        default=False,
+        label=_('Landscape'),
+        help_text=_('Print the label sheet in landscape mode')
     )
 
 
@@ -44,9 +52,32 @@ class InvenTreeLabelSheetPlugin(LabelPrintingMixin, SettingsMixin, InvenTreePlug
     def print_labels(self, label: LabelTemplate, items: list, request, **kwargs):
         """Handle printing of the provided labels"""
 
+        printing_options = kwargs['printing_options']
+
+        # Extract page size for the label sheet
+        page_size_code = printing_options.get('page_size', 'A4')
+        landscape = printing_options.get('landscape', False)
+        page_size = report.helpers.page_size(page_size_code)
+
+        page_width, page_height = page_size
+
+        if landscape:
+            page_width, page_height = page_height, page_width
+
+        # Calculate number of rows and columns
+        n_cols = math.floor(page_width / label.width)
+        n_rows = math.floor(page_height / label.height)
+        n_cells = n_cols * n_rows
+
+        print("printing:", n_cols, n_rows, '->', n_cells)
+
         for item in items:
             output = label.render_as_string(request, target_object=item)
 
             print("output:", output)
 
         raise ValidationError("oh no we don't")
+
+    def print_page(self, label: LabelTemplate, items: list, request, **kwargs):
+        """Print a single page of labels"""
+        ...
