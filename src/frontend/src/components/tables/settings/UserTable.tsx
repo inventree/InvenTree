4a2430c7a012c64a4e454bc2be19385bb1245517
Matthias Mair <code@mjmair.com>
2023-11-12 15:18:28 +0100

    remove actions as they are not usable after refactor

--- src/frontend/src/components/tables/settings/UserTable.tsx
+++ src/frontend/src/components/tables/settings/UserTable.tsx
@@ -1,24 +1,15 @@
 import { t } from '@lingui/macro';
 import { Text } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { notifications } from '@mantine/notifications';
-import { IconCheck } from '@tabler/icons-react';
 import { useCallback, useMemo, useState } from 'react';
 
-import { api } from '../../../App';
 import {
   openCreateApiForm,
   openDeleteApiForm,
   openEditApiForm
 } from '../../../functions/forms';
-import {
-  invalidResponse,
-  permissionDenied
-} from '../../../functions/notifications';
-import { InvenTreeStyle } from '../../../globalStyle';
 import { useTableRefresh } from '../../../hooks/TableRefresh';
 import { ApiPaths, apiUrl } from '../../../states/ApiState';
-import { useUserState } from '../../../states/UserState';
 import { AddItemButton } from '../../buttons/AddItemButton';
 import { TableColumn } from '../Column';
 import { BooleanColumn } from '../ColumnRenderers';
@@ -88,108 +79,7 @@ export function UserTable() {
   }, []);
 
   const rowActions = useCallback((record: any): RowAction[] => {
-    const [user] = useUserState((state) => [state.user]);
-    const { theme } = InvenTreeStyle();
-
-    function setPermission(pk: number, new_role: UserRole) {
-      let data = {};
-      switch (new_role) {
-        case UserRole.REGULAR:
-          data = {
-            is_staff: false,
-            is_superuser: false
-          };
-          break;
-        case UserRole.STAFF:
-          data = {
-            is_staff: true,
-            is_superuser: false
-          };
-          break;
-        case UserRole.SUPERUSER:
-          data = {
-            is_staff: true,
-            is_superuser: true
-          };
-          break;
-      }
-      api
-        .patch(`${apiUrl(ApiPaths.user_list)}${pk}/`, data)
-        .then(() => {
-          notifications.show({
-            title: t`User permission changed successfully`,
-            message: t`Set to ${new_role}`,
-            color: 'green',
-            icon: <IconCheck size="1rem" />
-          });
-          refreshTable();
-        })
-        .catch((error) => {
-          if (error.response.status === 403) {
-            permissionDenied();
-          } else {
-            console.log(error);
-            invalidResponse(error.response.status);
-          }
-        });
-    }
-    function setActive(pk: number, active: boolean) {
-      api
-        .patch(`${apiUrl(ApiPaths.user_list)}${pk}/`, {
-          is_active: active
-        })
-        .then(() => {
-          notifications.show({
-            title: t`Changed user active status successfully`,
-            message: t`Set to ${active}`,
-            color: 'green',
-            icon: <IconCheck size="1rem" />
-          });
-          refreshTable();
-        })
-        .catch((error) => {
-          if (error.response.status === 403) {
-            permissionDenied();
-          } else {
-            console.log(error);
-            invalidResponse(error.response.status);
-          }
-        });
-    }
-
     return [
-      {
-        title: t`Make regular user`,
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        onClick: () => {
-          setPermission(record.pk, UserRole.REGULAR);
-        },
-        hidden: !user?.is_staff || !(record.is_superuser && record.is_staff)
-      },
-      {
-        title: t`Make staff user`,
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        onClick: () => {
-          setPermission(record.pk, UserRole.STAFF);
-        },
-        hidden: !user?.is_staff || record.is_staff
-      },
-      {
-        title: t`Make superuser`,
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        onClick: () => {
-          setPermission(record.pk, UserRole.SUPERUSER);
-        },
-        hidden: !user?.is_superuser || record.is_superuser
-      },
-      {
-        title: record.is_active ? t`Deactivate user` : t`Activate user`,
-        color: theme.colorScheme === 'dark' ? theme.white : theme.black,
-        onClick: () => {
-          setActive(record.pk, !record.is_active);
-        },
-        hidden: !user?.is_superuser
-      },
       RowEditAction({
         onClick: () => {
           openEditApiForm({
