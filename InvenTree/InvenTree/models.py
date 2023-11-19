cbba00d2a5dc0b69bc8743d462abfa864df24b16
Oliver Walters <oliver.henry.walters@gmail.com>
2023-11-19 22:46:36 +1100

    Refactor existing barcode API endpoints
    - Expose fields using proper DRF serializers
    - API endpoints are now self documenting
    - Validation is handled by serializer models
    - Serializers and endpoints are extensible
    - Extended existing unit tests

--- InvenTree/InvenTree/models.py
+++ InvenTree/InvenTree/models.py
@@ -963,11 +963,13 @@ class InvenTreeBarcodeMixin(models.Model):
 
     def format_barcode(self, **kwargs):
         """Return a JSON string for formatting a QR code for this model instance."""
-        return InvenTree.helpers.MakeBarcode(
+        barcode = InvenTree.helpers.MakeBarcode(
             self.__class__.barcode_model_type(),
             self.pk,
             **kwargs
         )
+
+        return str(barcode)
 
     @property
     def barcode(self):
