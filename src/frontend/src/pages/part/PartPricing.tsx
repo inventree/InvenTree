8a6b61e2b3450000a26f39e059f2d01a1c66a468
Oliver <oliver.henry.walters@gmail.com>
2023-11-21 15:06:39 +1100

    Rough in details for part pricing panel

--- src/frontend/src/pages/part/PartPricing.tsx
+++ src/frontend/src/pages/part/PartPricing.tsx
@@ -1,6 +1,26 @@
-import { Stack } from '@mantine/core';
+import { t } from '@lingui/macro';
+import { Divider, Paper, Stack } from '@mantine/core';
 
+import { StylishText } from '../../components/items/StylishText';
 import { useUserState } from '../../states/UserState';
+
+function PricingGroup({
+  children,
+  title
+}: {
+  children?: React.ReactNode;
+  title: string;
+}) {
+  return (
+    <Paper radius="sm" p="md">
+      <Stack spacing="xs">
+        <StylishText size="xl">{title}</StylishText>
+        <Divider />
+        {children}
+      </Stack>
+    </Paper>
+  );
+}
 
 /*
  * Render a pricing panel for a single part
@@ -8,5 +28,19 @@ import { useUserState } from '../../states/UserState';
 export function PartPricingPanel({ part }: { part: any }) {
   const user = useUserState();
 
-  return <Stack spacing="xs">todo</Stack>;
+  return (
+    <Stack spacing="xs">
+      <PricingGroup title={t`Pricing Overview`}></PricingGroup>
+      <PricingGroup title={t`Internal Pricing`}></PricingGroup>
+      {part.assembly && <PricingGroup title={t`BOM Pricing`}></PricingGroup>}
+      {part.purchaseable && (
+        <PricingGroup title={t`Supplier Pricing`}></PricingGroup>
+      )}
+      {part.purchaseable && (
+        <PricingGroup title={t`Purchase History`}></PricingGroup>
+      )}
+      {part.salable && <PricingGroup title={t`Sale Pricing`}></PricingGroup>}
+      {part.salable && <PricingGroup title={t`Sale History`}></PricingGroup>}
+    </Stack>
+  );
 }
