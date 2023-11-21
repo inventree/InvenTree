bef4805fe30b2921dbf4071625429dcc9dda83d0
Oliver <oliver.henry.walters@gmail.com>
2023-11-21 14:45:09 +1100

    Add PartPricingPanel skeleton

---
+++ src/frontend/src/pages/part/PartPricing.tsx
@@ -0,0 +1,12 @@
+import { Stack } from '@mantine/core';
+
+import { useUserState } from '../../states/UserState';
+
+/*
+ * Render a pricing panel for a single part
+ */
+export function PartPricingPanel({ part }: { part: any }) {
+  const user = useUserState();
+
+  return <Stack spacing="xs">todo</Stack>;
+}
