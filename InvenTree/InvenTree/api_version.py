43434fc530e8a1047e1325972cae7c7982d8c15c
miggland <miggland@users.noreply.github.com>
2023-11-26 10:52:03 +0100

    Extend API for labels and reports to allow creating new items (#5982)
    * Extend API for labels and reports to allow creating new items
    
    * Increment API_VERSION
    
    * Missed something..

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -2,10 +2,13 @@
 
 
 # InvenTree API version
-INVENTREE_API_VERSION = 155
+INVENTREE_API_VERSION = 156
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v156 -> 2023-11-26 : https://github.com/inventree/InvenTree/pull/5982
+    - Add POST endpoint for report and label creation
 
 v155 -> 2023-11-24 : https://github.com/inventree/InvenTree/pull/5979
     - Add "creation_date" field to Part instance serializer
