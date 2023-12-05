c67cfa448a6f43049a30f0dd14c121ff8f33f268
LavissaWoW <lavissawow@gmail.com>
2023-12-05 23:33:46 +0000

    Remove unused code

--- src/frontend/src/components/nav/ItemDetails.tsx
+++ src/frontend/src/components/nav/ItemDetails.tsx
@@ -1,16 +1,6 @@
 import { Paper } from '@mantine/core';
 
 import { DetailsImage } from '../images/DetailsImage';
-import { Thumbnail } from '../images/Thumbnail';
-
-function defineFields(part: any) {
-  return [
-    {
-      accessor: 'image',
-      type: 'image'
-    }
-  ];
-}
 
 const imageActions = {
   selectExisting: true,
@@ -19,9 +9,6 @@ const imageActions = {
 };
 
 export function ItemDetails({ params = {} }: { params?: any }) {
-  console.log(params);
-  const fields = defineFields(params);
-
   return (
     <Paper withBorder={true}>
       <DetailsImage imageActions={imageActions} src={params.image} />
