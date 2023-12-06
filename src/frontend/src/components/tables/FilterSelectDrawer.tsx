52a4fefe013c85df1a776e29584197134d1f0301
Oliver Walters <oliver.henry.walters@gmail.com>
2023-12-07 00:01:46 +1100

    Add callback to remove selected filter

--- src/frontend/src/components/tables/FilterSelectDrawer.tsx
+++ src/frontend/src/components/tables/FilterSelectDrawer.tsx
@@ -4,10 +4,12 @@ import {
   Divider,
   Drawer,
   Group,
+  Paper,
   Stack,
   Text,
   Tooltip
 } from '@mantine/core';
+import { useCallback, useEffect } from 'react';
 
 import { TableState } from '../../hooks/UseTable';
 import { StylishText } from '../items/StylishText';
@@ -16,20 +18,35 @@ import { TableFilter } from './Filter';
 /*
  * Render a single table filter item
  */
-function FilterItem({ flt }: { flt: TableFilter }) {
+function FilterItem({
+  flt,
+  tableState
+}: {
+  flt: TableFilter;
+  tableState: TableState;
+}) {
+  const removeFilter = useCallback(() => {
+    let newFilters = tableState.activeFilters.filter(
+      (f) => f.name !== flt.name
+    );
+    tableState.setActiveFilters(newFilters);
+  }, [flt]);
+
   return (
-    <Group position="apart" key={flt.name}>
-      <Stack spacing="xs">
-        <Text size="sm">{flt.label}</Text>
-        <Text size="xs">{flt.description}</Text>
-      </Stack>
-      <Group position="right">
-        <Text size="xs">{flt.value}</Text>
-        <Tooltip label={t`Remove filter`} withinPortal={true}>
-          <CloseButton size="sm" color="red" />
-        </Tooltip>
+    <Paper p="sm" shadow="sm" radius="xs">
+      <Group position="apart" key={flt.name}>
+        <Stack spacing="xs">
+          <Text size="sm">{flt.label}</Text>
+          <Text size="xs">{flt.description}</Text>
+        </Stack>
+        <Group position="right">
+          <Text size="xs">{flt.value}</Text>
+          <Tooltip label={t`Remove filter`} withinPortal={true}>
+            <CloseButton size="md" color="red" onClick={removeFilter} />
+          </Tooltip>
+        </Group>
       </Group>
-    </Group>
+    </Paper>
   );
 }
 
@@ -53,10 +70,10 @@ export function FilterSelectDrawer({
       onClose={onClose}
       title={<StylishText size="lg">{t`Table Filters`}</StylishText>}
     >
-      <Stack spacing="sm">
+      <Stack spacing="xs">
         <Divider />
         {tableState.activeFilters.map((f) => (
-          <FilterItem flt={f} />
+          <FilterItem flt={f} tableState={tableState} />
         ))}
       </Stack>
     </Drawer>
