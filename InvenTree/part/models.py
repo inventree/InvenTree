aa56d2f42049b4139a1b93c29eb3f84fcc16c936
Oliver <oliver.henry.walters@gmail.com>
2023-12-11 14:49:03 +1100

    Handle IntegrityError when saving PartPricing object

--- InvenTree/part/models.py
+++ InvenTree/part/models.py
@@ -2432,7 +2432,11 @@ class PartPricing(common.models.MetaMixin):
             # If something has happened to the Part model, might throw an error
             pass
 
-        super().save(*args, **kwargs)
+        try:
+            super().save(*args, **kwargs)
+        except IntegrityError:
+            # This error may be thrown if there is already duplicate pricing data
+            pass
 
     def update_bom_cost(self, save=True):
         """Recalculate BOM cost for the referenced Part instance.
