473e75eda205793769946e923748356ffd7e5b4b
Matthias Mair <code@mjmair.com>
2024-01-07 22:52:30 +0100

    fix wrong url response

--- InvenTree/InvenTree/version.py
+++ InvenTree/InvenTree/version.py
@@ -105,7 +105,7 @@ def inventreeDocUrl():
 
 def inventreeAppUrl():
     """Return URL for InvenTree app site."""
-    return (f'{inventreeDocUrl()}/app/app',)
+    return f'{inventreeDocUrl()}/app/app'
 
 
 def inventreeCreditsUrl():
