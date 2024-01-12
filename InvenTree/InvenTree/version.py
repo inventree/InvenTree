0e2cf97a67de600ff8ae3fc1c8eaef3993f73e24
Oliver <oliver.henry.walters@gmail.com>
2024-01-13 00:03:12 +1100

    Update version.py
    Bump version number to 0.13.2

--- InvenTree/InvenTree/version.py
+++ InvenTree/InvenTree/version.py
@@ -19,7 +19,7 @@ from dulwich.repo import NotGitRepository, Repo
 from .api_version import INVENTREE_API_TEXT, INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = "0.13.1"
+INVENTREE_SW_VERSION = "0.13.2"
 
 # Discover git
 try:
