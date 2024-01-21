917a88c6f43c5549c6ddc036ad738f3f17c8a459
Lavissa <lavissawow@gmail.com>
2024-01-21 20:56:08 +0100

    Add check for KeyError on reading plugin commit (#6300)
    * Add check for KeyError on reading plugin commit
    
    * .
    
    * .

--- InvenTree/plugin/helpers.py
+++ InvenTree/plugin/helpers.py
@@ -145,6 +145,8 @@ def get_git_log(path):
                 datetime.datetime.fromtimestamp(commit.author_time).isoformat(),
                 commit.message.decode().split('\n')[0],
             ]
+        except KeyError as err:
+            logger.debug('No HEAD tag found in git repo at path %s', path)
         except NotGitRepository:
             pass
 
