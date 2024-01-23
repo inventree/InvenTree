0aa8a36ea70860d33b6948368de314f88f520752
Matthias Mair <code@mjmair.com>
2024-01-23 23:28:17 +0100

    Added more icons

--- src/frontend/src/components/buttons/SooButton.tsx
+++ src/frontend/src/components/buttons/SooButton.tsx
@@ -1,22 +1,47 @@
 import { Button } from '@mantine/core';
 import {
+  IconBrandAzure,
+  IconBrandBitbucket,
+  IconBrandDiscord,
+  IconBrandFacebook,
+  IconBrandFlickr,
   IconBrandGithub,
+  IconBrandGitlab,
   IconBrandGoogle,
+  IconBrandReddit,
+  IconBrandTwitch,
+  IconBrandTwitter,
   IconLogin
 } from '@tabler/icons-react';
 
 import { Provider } from '../../states/states';
 
+const brandIcons: { [key: string]: JSX.Element } = {
+  google: <IconBrandGoogle />,
+  github: <IconBrandGithub />,
+  facebook: <IconBrandFacebook />,
+  discord: <IconBrandDiscord />,
+  twitter: <IconBrandTwitter />,
+  bitbucket: <IconBrandBitbucket />,
+  flickr: <IconBrandFlickr />,
+  gitlab: <IconBrandGitlab />,
+  reddit: <IconBrandReddit />,
+  twitch: <IconBrandTwitch />,
+  microsoft: <IconBrandAzure />
+};
+
 export function SooButton({ provider }: { provider: Provider }) {
-  let icon = <IconLogin />;
-  if (provider.id === 'google') {
-    icon = <IconBrandGoogle />;
-  } else if (provider.id === 'github') {
-    icon = <IconBrandGithub />;
-  }
   return (
-    <Button leftIcon={icon} radius="xl" component="a" href={provider.connect}>
+    <Button
+      leftIcon={getBrandIcon(provider)}
+      radius="xl"
+      component="a"
+      href={provider.connect}
+    >
       {provider.display_name}{' '}
     </Button>
   );
+}
+function getBrandIcon(provider: Provider) {
+  return brandIcons[provider.id] || <IconLogin />;
 }
