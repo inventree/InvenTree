8a59c90c09966cb1c4358b2efa325c4833d68573
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2024-01-27 09:33:50 +0000

    Removed only partially working django admin in favor of the PUI admin center implementation

--- InvenTree/machine/admin.py
+++ InvenTree/machine/admin.py
@@ -1,25 +1,7 @@
-from django import forms
 from django.contrib import admin
-from django.utils.translation import gettext_lazy as _
+from django.http.request import HttpRequest
 
 from machine import models
-from machine.registry import registry
-
-# Note: Most of this code here is only for developing as there is no UI for machines *yet*.
-
-
-class MachineConfigAdminForm(forms.ModelForm):
-    def get_machine_type_choices():
-        return [(machine_type.SLUG, machine_type.NAME) for machine_type in registry.machine_types.values()]
-
-    def get_driver_choices():
-        return [(driver.SLUG, driver.NAME) for driver in registry.drivers.values()]
-
-    # TODO: add conditional choices like shown here
-    # Ref: https://www.reddit.com/r/django/comments/18cj55/conditional_choices_for_model_field_based_on/
-    # Ref: https://gist.github.com/blackrobot/4956070
-    driver = forms.ChoiceField(label=_("Driver"), choices=get_driver_choices)
-    machine_type = forms.ChoiceField(label=_("Machine Type"), choices=get_machine_type_choices)
 
 
 class MachineSettingInline(admin.TabularInline):
@@ -27,54 +9,38 @@ class MachineSettingInline(admin.TabularInline):
 
     model = models.MachineSetting
 
-    read_only_fields = [
-        'key',
-        'config_type'
-    ]
-
-    def get_extra(self, request, obj, **kwargs):
-        if getattr(obj, 'machine', None) is not None:
-            # TODO: improve this mechanism
-            machine_settings = getattr(obj.machine, "MACHINE_SETTINGS", {})
-            driver_settings = getattr(obj.machine.driver, "MACHINE_SETTINGS", {})
-            count = len(machine_settings.keys()) + len(driver_settings.keys())
-            if obj.settings.count() != count:
-                return count
-        return 0
+    read_only_fields = ['key', 'config_type']
 
     def has_add_permission(self, request, obj):
         """The machine settings should not be meddled with manually."""
-        return True  # TODO: change back
+        return False
 
 
 @admin.register(models.MachineConfig)
 class MachineConfigAdmin(admin.ModelAdmin):
     """Custom admin with restricted id fields."""
 
-    form = MachineConfigAdminForm
-    list_filter = ["active"]
-    list_display = ["name", "machine_type", "driver", "initialized", "active", "no_errors", "get_machine_status"]
-    readonly_fields = ["initialized", "is_driver_available", "get_admin_errors", "get_machine_status"]
+    list_filter = ['active']
+    list_display = [
+        'name',
+        'machine_type',
+        'driver',
+        'initialized',
+        'active',
+        'no_errors',
+        'get_machine_status',
+    ]
+    readonly_fields = [
+        'initialized',
+        'is_driver_available',
+        'get_admin_errors',
+        'get_machine_status',
+    ]
     inlines = [MachineSettingInline]
 
     def get_readonly_fields(self, request, obj):
         # if update, don't allow changes on machine_type and driver
         if obj is not None:
-            return ["machine_type", "driver", *self.readonly_fields]
+            return ['machine_type', 'driver', *self.readonly_fields]
 
         return self.readonly_fields
-
-    def get_inline_formsets(self, request, formsets, inline_instances, obj):
-        formsets = super().get_inline_formsets(request, formsets, inline_instances, obj)
-
-        if getattr(obj, 'machine', None) is not None:
-            machine_settings = getattr(obj.machine, "MACHINE_SETTINGS", {})
-            driver_settings = getattr(obj.machine.driver, "MACHINE_SETTINGS", {})
-            settings = [(s, models.MachineSetting.ConfigType.MACHINE) for s in machine_settings] + [(s, models.MachineSetting.ConfigType.DRIVER) for s in driver_settings]
-            for form, (setting, typ) in zip(formsets[0].forms, settings):
-                if form.fields["key"].initial is None:
-                    form.fields["key"].initial = setting
-                if form.fields["config_type"].initial is None:
-                    form.fields["config_type"].initial = typ
-
-        return formsets
--- InvenTree/machine/machine_types/LabelPrintingMachineType.py
+++ InvenTree/machine/machine_types/LabelPrintingMachineType.py
@@ -6,31 +6,31 @@ from machine.machine_type import BaseDriver, BaseMachineType, MachineStatus
 class BaseLabelPrintingDriver(BaseDriver):
     """Base label printing driver."""
 
-    machine_type = "label_printer"
+    machine_type = 'label_printer'
 
     def print_label(self):
         """This function must be overridden."""
-        raise NotImplementedError("The `print_label` function must be overridden!")
+        raise NotImplementedError('The `print_label` function must be overridden!')
 
     def print_labels(self):
         """This function must be overridden."""
-        raise NotImplementedError("The `print_labels` function must be overridden!")
+        raise NotImplementedError('The `print_labels` function must be overridden!')
 
     requires_override = [print_label]
 
 
 class LabelPrintingMachineType(BaseMachineType):
-    SLUG = "label_printer"
-    NAME = _("Label Printer")
-    DESCRIPTION = _("Device used to print labels")
+    SLUG = 'label_printer'
+    NAME = _('Label Printer')
+    DESCRIPTION = _('Directly print labels for various items.')
 
     base_driver = BaseLabelPrintingDriver
 
     class LabelPrinterStatus(MachineStatus):
-        CONNECTED = 100, _("Connected"), "success"
-        PRINTING = 101, _("Printing"), "primary"
-        PAPER_MISSING = 301, _("Paper missing"), "warning"
-        DISCONNECTED = 400, _("Disconnected"), "danger"
+        CONNECTED = 100, _('Connected'), 'success'
+        PRINTING = 101, _('Printing'), 'primary'
+        PAPER_MISSING = 301, _('Paper missing'), 'warning'
+        DISCONNECTED = 400, _('Disconnected'), 'danger'
 
     MACHINE_STATUS = LabelPrinterStatus
 
