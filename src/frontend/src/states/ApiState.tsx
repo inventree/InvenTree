a4ca3716008ac8dd543afd286a46a6c4488a655d
Matthias Mair <code@mjmair.com>
2024-01-30 21:14:29 +0100

    remove unneeded import

--- src/frontend/src/states/ApiState.tsx
+++ src/frontend/src/states/ApiState.tsx
@@ -2,7 +2,6 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 import { api } from '../App';
-import { statusCodeList } from '../defaults/backendMappings';
 import { emptyServerAPI } from '../defaults/defaults';
 import { ApiEndpoints } from '../enums/ApiEndpoints';
 import { AuthProps, ServerAPIProps } from './states';
