d8338d85a7c0ff93a0d23f4750e2bf4f464cca3a
Lukas <76838159+wolflu05@users.noreply.github.com>
2024-02-01 14:27:32 +0100

    Fix pre-commit

--- InvenTree/plugin/urls.py
+++ InvenTree/plugin/urls.py
@@ -14,9 +14,12 @@ def get_plugin_urls():
     urls = []
 
     # Only allow custom routing if the setting is enabled
-    if InvenTreeSetting.get_setting(
-        'ENABLE_PLUGINS_URL', False, create=False, cache=False
-    ) or settings.PLUGIN_TESTING:
+    if (
+        InvenTreeSetting.get_setting(
+            'ENABLE_PLUGINS_URL', False, create=False, cache=False
+        )
+        or settings.PLUGIN_TESTING
+    ):
         for plugin in registry.plugins.values():
             if plugin.mixin_enabled('urls'):
                 urls.append(plugin.urlpatterns)
