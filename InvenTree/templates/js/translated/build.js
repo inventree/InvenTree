c9c93bce39e8c58921c3212b7577a9357aa3236b
Oliver <oliver.henry.walters@gmail.com>
2024-02-02 14:58:11 +1100

    BuildOrder: Fix bug where test templates are requested for a non-trackable output (#6387)

--- InvenTree/templates/js/translated/build.js
+++ InvenTree/templates/js/translated/build.js
@@ -1094,7 +1094,7 @@ function loadBuildOutputTable(build_info, options={}) {
     var params = options.params || {};
 
     // test templates for the part being assembled
-    let test_templates = null;
+    let test_templates = [];
 
     // tracked line items for this build
     let has_tracked_lines = false;
@@ -1138,6 +1138,9 @@ function loadBuildOutputTable(build_info, options={}) {
                         test_templates.push(item);
                     }
                 });
+            },
+            error: function() {
+                test_templates = [];
             }
         }
     );
