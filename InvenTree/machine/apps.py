60fbb85b01506cf67d92e29cc7dd371df7148231
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2024-02-05 09:10:13 +0000

    Fix testing?

--- InvenTree/machine/apps.py
+++ InvenTree/machine/apps.py
@@ -3,9 +3,11 @@
 import logging
 
 from django.apps import AppConfig
+from django.db.utils import OperationalError, ProgrammingError
 
 from InvenTree.ready import (
     canAppAccessDatabase,
+    isImportingData,
     isInMainThread,
     isPluginRegistryLoaded,
     isRunningMigrations,
@@ -26,11 +28,16 @@ class MachineConfig(AppConfig):
             or not isPluginRegistryLoaded()
             or not isInMainThread()
             or isRunningMigrations()
+            or isImportingData()
         ):
             logger.debug('Machine app: Skipping machine loading sequence')
             return
 
         from machine import registry
 
-        logger.info('Loading InvenTree machines')
-        registry.initialize()
+        try:
+            logger.info('Loading InvenTree machines')
+            registry.initialize()
+        except (OperationalError, ProgrammingError):
+            # Database might not yet be ready
+            logger.warn('Database was not ready for initializing machines')
