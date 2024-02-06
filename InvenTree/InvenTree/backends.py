fb0e01c45a94febf78e6a8ff08816a7cc9c1dfe9
Oliver Walters <oliver.henry.walters@gmail.com>
2024-02-06 08:45:50 +0000

    Custom maintenance mode backend

---
+++ InvenTree/InvenTree/backends.py
@@ -0,0 +1,47 @@
+"""Custom backend implementations."""
+
+import logging
+
+from django.db.utils import IntegrityError, OperationalError, ProgrammingError
+
+from maintenance_mode.backends import AbstractStateBackend
+
+import common.models
+import InvenTree.helpers
+
+logger = logging.getLogger('inventree')
+
+
+class InvenTreeMaintenanceModeBackend(AbstractStateBackend):
+    """Custom backend for managing state of maintenance mode.
+
+    Stores the current state of the maintenance mode in the database,
+    using an InvenTreeSetting object.
+    """
+
+    SETTING_KEY = '_MAINTENANCE_MODE'
+
+    def get_value(self) -> bool:
+        """Get the current state of the maintenance mode.
+
+        Returns:
+            bool: True if maintenance mode is active, False otherwise.
+        """
+        value = InvenTree.helpers.str2bool(
+            common.models.InvenTreeSetting.get_setting(
+                self.SETTING_KEY, False, create=False, cache=False
+            )
+        )
+
+        logger.debug('Maintenance mode state: {state}'.format(state=value))
+
+        return value
+
+    def set_value(self, value: bool):
+        """Set the state of the maintenance mode."""
+        logger.debug('Setting maintenance mode state: {state}'.format(state=value))
+
+        try:
+            common.models.InvenTreeSetting.set_setting(self.SETTING_KEY, value)
+        except (IntegrityError, OperationalError, ProgrammingError):
+            logger.warning('Failed to set maintenance mode state in database')
