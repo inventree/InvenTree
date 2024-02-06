06e414f93ea473ff8278f63a91fa982f1f9cbb96
Matthias Mair <code@mjmair.com>
2024-02-06 01:05:04 +0100

    remove tz from self.creation if TZ is off

--- InvenTree/common/models.py
+++ InvenTree/common/models.py
@@ -13,7 +13,7 @@ import math
 import os
 import re
 import uuid
-from datetime import datetime, timedelta, timezone
+from datetime import datetime, timedelta
 from enum import Enum
 from secrets import compare_digest
 from typing import Any, Callable, Dict, List, Tuple, TypedDict, Union
@@ -2857,20 +2857,17 @@ class NotificationMessage(models.Model):
         def is_naive(d):
             return d.tzinfo is None or d.tzinfo.utcoffset(d) is None
 
-        current = (
-            datetime.utcnow().replace(tzinfo=timezone.utc)
-            if settings.TESTING
-            else now()
+        current = now()
+        creation = (
+            self.creation if settings.USE_TZ else self.creation.replace(tzinfo=None)
         )
         try:
-            delta = current - self.creation
+            delta = now() - creation
             return delta.seconds
         except TypeError as _e:
             print(self)
             print(f'current is {is_aware(current)} aware, {is_naive(current)} naive')
-            print(
-                f'creation is {is_aware(self.creation)} aware, {is_naive(self.creation)} naive'
-            )
+            print(f'creation is {is_aware(creation)} aware, {is_naive(creation)} naive')
             raise _e
 
     def age_human(self):
