d5fc87e67d4a9d075ce5c07455650cb0f08daeae
Oliver Walters <oliver.henry.walters@gmail.com>
2024-02-10 10:16:49 +1100

    Adjust maintenance mode backend
    - Save a timestamp to the setting, after which maintenance mode is not active
    - Fallback to account for possibility that race condition / exception leaves maintenance mode active

--- InvenTree/InvenTree/backends.py
+++ InvenTree/InvenTree/backends.py
@@ -1,5 +1,6 @@
 """Custom backend implementations."""
 
+import datetime
 import logging
 import time
 
@@ -30,26 +31,45 @@ class InvenTreeMaintenanceModeBackend(AbstractStateBackend):
         """
         try:
             setting = common.models.InvenTreeSetting.objects.get(key=self.SETTING_KEY)
-            value = InvenTree.helpers.str2bool(setting.value)
+            value = str(setting.value).strip()
         except common.models.InvenTreeSetting.DoesNotExist:
             # Database is accessible, but setting is not available - assume False
-            value = False
+            return False
         except (IntegrityError, OperationalError, ProgrammingError):
             # Database is inaccessible - assume we are not in maintenance mode
-            logger.warning('Failed to read maintenance mode state - assuming True')
-            value = True
+            logger.debug('Failed to read maintenance mode state - assuming True')
+            return True
 
-        logger.debug('Maintenance mode state: %s', value)
+        # Extract timestamp from string
+        try:
+            # If the timestamp is in the past, we are now *out* of maintenance mode
+            timestamp = datetime.datetime.fromisoformat(value)
+            return timestamp > datetime.datetime.now()
+        except ValueError:
+            # If the value is not a valid timestamp, assume maintenance mode is not active
+            return False
 
-        return value
-
-    def set_value(self, value: bool, retries: int = 5):
-        """Set the state of the maintenance mode."""
+    def set_value(self, value: bool, retries: int = 5, minutes: int = 5):
+        """Set the state of the maintenance mode.
+        
+        Instead of simply writing "true" or "false" to the setting,
+        we write a timestamp to the setting, which is used to determine
+        when maintenance mode will elapse.
+        This ensures that we will always *exit* maintenance mode after a certain time period.
+        """
         logger.debug('Setting maintenance mode state: %s', value)
+
+        if value:
+            # Save as isoformat
+            timestamp = datetime.datetime.now() + datetime.timedelta(minutes=minutes)
+            timestamp = timestamp.isoformat()
+        else:
+            # Blank timestamp means maintenance mode is not active
+            timestamp = ''
 
         while retries > 0:
             try:
-                common.models.InvenTreeSetting.set_setting(self.SETTING_KEY, value)
+                common.models.InvenTreeSetting.set_setting(self.SETTING_KEY, timestamp)
 
                 # Read the value back to confirm
                 if self.get_value() == value:
