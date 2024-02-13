9ffd3053688094af30bb1249abbde90459f1c945
Oliver Walters <oliver.henry.walters@gmail.com>
2024-02-13 20:45:00 +1100

    Revert "remove argparse"
    This reverts commit 4665805340d068a5e17bd7f60addac930383acf5.

--- docs/docs/exctract_schema.py
+++ docs/docs/exctract_schema.py
@@ -1,5 +1,6 @@
 """Extract API schema and split into smaller subsections."""
 
+import argparse
 import os
 import textwrap
 
@@ -133,3 +134,12 @@ def parse_api_file(filename: str):
 
     # Finally, generate an index file for the API schema
     generate_index_file(data['info']['version'])
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description=__doc__)
+    parser.add_argument('input', help='Input API schema file (.yml)')
+
+    args = parser.parse_args()
+
+    parse_api_file(args.input)
