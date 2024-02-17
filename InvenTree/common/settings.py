3c6c1a43c029f429859b689bfaed01b350a4454b
Oliver <oliver.henry.walters@gmail.com>
2024-02-18 09:50:07 +1100

    Update settings.py
    Formatting fix

--- InvenTree/common/settings.py
+++ InvenTree/common/settings.py
@@ -62,4 +62,6 @@ def prevent_build_output_complete_on_incompleted_tests():
     """Returns True if the completion of the build outputs is disabled until the required tests are passed."""
     from common.models import InvenTreeSetting
 
-    return InvenTreeSetting.get_setting('PREVENT_BUILD_COMPLETION_HAVING_INCOMPLETED_TESTS', False, create=False)
+    return InvenTreeSetting.get_setting(
+        'PREVENT_BUILD_COMPLETION_HAVING_INCOMPLETED_TESTS', False, create=False
+    )
