d4d79077c759aa69bc653b7899b435c7d4d3b0fb
wolflu05 <76838159+wolflu05@users.noreply.github.com>
2024-02-23 09:42:48 +0100

    Moved default tempaltes in default folder

--- src/frontend/src/pages/Index/Settings/AdminCenter/TemplateManagementPanel.tsx
+++ src/frontend/src/pages/Index/Settings/AdminCenter/TemplateManagementPanel.tsx
@@ -5,6 +5,10 @@ import { useMemo } from 'react';
 import { TemplatePreviewProps } from '../../../../components/editors/TemplateEditor/TemplateEditor';
 import { ApiFormFieldSet } from '../../../../components/forms/fields/ApiFormField';
 import { PanelGroup } from '../../../../components/nav/PanelGroup';
+import {
+  defaultLabelTemplate,
+  defaultReportTemplate
+} from '../../../../defaults/templates';
 import { ApiEndpoints } from '../../../../enums/ApiEndpoints';
 import { ModelType } from '../../../../enums/ModelType';
 import { InvenTreeIcon, InvenTreeIconType } from '../../../../functions/icons';
@@ -25,60 +29,6 @@ type TemplateType = {
     preview: TemplatePreviewProps;
   }[];
 };
-
-const defaultLabelTemplate = `{% extends "label/label_base.html" %}
-
-{% load l10n i18n barcode %}
-
-{% block style %}
-
-.qr {
-    position: absolute;
-    left: 0mm;
-    top: 0mm;
-    {% localize off %}
-    height: {{ height }}mm;
-    width: {{ height }}mm;
-    {% endlocalize %}
-}
-
-{% endblock style %}
-
-{% block content %}
-<img class='qr' alt="{% trans 'QR Code' %}" src='{% qrcode qr_data %}'>
-
-{% endblock content %}
-`;
-
-const defaultReportTemplate = `{% extends "report/inventree_report_base.html" %}
-
-{% load i18n report barcode inventree_extras %}
-
-{% block page_margin %}
-margin: 2cm;
-margin-top: 4cm;
-{% endblock page_margin %}
-
-{% block bottom_left %}
-content: "v{{ report_revision }} - {{ date.isoformat }}";
-{% endblock bottom_left %}
-
-{% block bottom_center %}
-content: "{% inventree_version shortstring=True %}";
-{% endblock bottom_center %}
-
-{% block style %}
-<!-- Custom style -->
-{% endblock style %}
-
-{% block header_content %}
-<!-- Custom header content -->
-{% endblock header_content %}
-
-{% block page_content %}
-<!-- Custom page content -->
-{% endblock page_content %}
-`;
 
 export default function TemplateManagementPanel() {
   const templateTypes = useMemo(() => {
