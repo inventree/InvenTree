03c191e781e61f16c794ca1a7f1df742462e25e0
Matthias Mair <code@mjmair.com>
2024-02-26 12:46:02 +0100

    use s3 switch to set storage backend

--- InvenTree/InvenTree/storage_backends.py
+++ InvenTree/InvenTree/storage_backends.py
@@ -5,14 +5,14 @@ from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
-class StaticStorage(S3Boto3Storage):
+class S3StaticStorage(S3Boto3Storage):
     """Static assets storage for InvenTree."""
 
     location = settings.STATIC_LOCATION
     default_acl = 'public-read'
 
 
-class PublicMediaStorage(S3Boto3Storage):
+class S3PublicMediaStorage(S3Boto3Storage):
     """Public media storage for InvenTree."""
 
     location = settings.PUBLIC_MEDIA_LOCATION
@@ -20,10 +20,20 @@ class PublicMediaStorage(S3Boto3Storage):
     file_overwrite = False
 
 
-class PrivateMediaStorage(S3Boto3Storage):
+class S3PrivateMediaStorage(S3Boto3Storage):
     """Private media storage for InvenTree."""
 
     location = settings.PRIVATE_MEDIA_LOCATION
     default_acl = 'private'
     file_overwrite = False
     custom_domain = False
+
+
+if settings.USE_S3:
+    StaticStorage = S3StaticStorage
+    PublicMediaStorage = S3PublicMediaStorage
+    PrivateMediaStorage = S3PrivateMediaStorage
+else:
+    StaticStorage = settings.default_storage
+    PublicMediaStorage = settings.default_storage
+    PrivateMediaStorage = settings.default_storage
