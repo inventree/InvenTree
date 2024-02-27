88c9cce2e3816ecb17dea987c018784638b46aa7
Matthias Mair <code@mjmair.com>
2024-02-27 01:08:02 +0100

    move even more functions out

--- InvenTree/generic/templating/apps.py
+++ InvenTree/generic/templating/apps.py
@@ -1,5 +1,7 @@
 """Shared templating code."""
 
+import logging
+import os
 import warnings
 
 from django.core.exceptions import AppRegistryNotReady
@@ -8,8 +10,11 @@ from django.db.utils import IntegrityError, OperationalError, ProgrammingError
 
 from maintenance_mode.core import maintenance_mode_on, set_maintenance_mode
 
+import InvenTree.helpers
 from InvenTree.config import ensure_dir
 from InvenTree.files import MEDIA_STORAGE_DIR
+
+logger = logging.getLogger('inventree')
 
 
 class TemplatingMixin:
@@ -22,6 +27,10 @@ class TemplatingMixin:
     def get_src_dir(self, ref, ref_name):
         """Get the source directory for the default templates."""
         raise NotImplementedError('get_src_dir must be implemented')
+
+    def get_new_obj_data(self, data, filename):
+        """Get the data for a new template db object."""
+        raise NotImplementedError('get_new_obj_data must be implemented')
 
     # Standardized code
     def ready(self):
@@ -66,3 +75,57 @@ class TemplatingMixin:
         # Copy each template across (if required)
         for entry in data:
             self.create_template_file(model, src_dir, entry, ref_name)
+
+    def create_template_file(self, model, src_dir, data, ref_name):
+        """Ensure a label template is in place."""
+        # Destination filename
+        filename = os.path.join(ref, 'inventree', ref_name, data['file'])
+
+        src_file = src_dir.joinpath(data['file'])
+        dst_file = MEDIA_STORAGE_DIR.joinpath(filename)
+
+        do_copy = False
+
+        if not dst_file.exists():
+            logger.info("%s template '%s' is not present", ref, filename)
+            do_copy = True
+        else:
+            # Check if the file contents are different
+            src_hash = InvenTree.helpers.hash_file(src_file)
+            dst_hash = InvenTree.helpers.hash_file(dst_file)
+
+            if src_hash != dst_hash:
+                logger.info("Hash differs for '%s'", filename)
+                do_copy = True
+
+        if do_copy:
+            logger.info("Copying %s template '%s'", ref, dst_file)
+            # Ensure destination dir exists
+            try:
+                dst_file.parent.mkdir(parents=True, exist_ok=True)
+            except FileExistsError:
+                pass
+
+            # Copy file
+            try:
+                default_storage.save(filename, src_file.open('rb'))
+            except FileExistsError:
+                pass
+
+        # Check if a file matching the template already exists
+        try:
+            if model.objects.filter(**{db_ref: filename}).exists():
+                return  # pragma: no cover
+        except Exception:
+            logger.exception(
+                "Failed to query %s for '%s' - you should run 'invoke update' first!",
+                ref,
+                filename,
+            )
+
+        logger.info("Creating entry for %s '%s'", model, data.get('name'))
+
+        try:
+            model.objects.create(**self.get_new_obj_data(data, filename))
+        except Exception:
+            logger.warning("Failed to create %s '%s'", ref, data['name'])
--- InvenTree/label/apps.py
+++ InvenTree/label/apps.py
@@ -101,64 +101,14 @@ class LabelConfig(TemplatingMixin, AppConfig):
         """Get the source directory."""
         return TEMPLATES_DIR.joinpath(ref, 'templates', ref, ref_name)
 
-    def create_template_file(self, model, src_dir, data, ref_name):
-        """Ensure a label template is in place."""
-        # Destination filename
-        filename = os.path.join(ref, 'inventree', ref_name, data['file'])
-
-        src_file = src_dir.joinpath(data['file'])
-        dst_file = MEDIA_STORAGE_DIR.joinpath(filename)
-
-        do_copy = False
-
-        if not dst_file.exists():
-            logger.info("%s template '%s' is not present", ref, filename)
-            do_copy = True
-        else:
-            # Check if the file contents are different
-            src_hash = InvenTree.helpers.hash_file(src_file)
-            dst_hash = InvenTree.helpers.hash_file(dst_file)
-
-            if src_hash != dst_hash:
-                logger.info("Hash differs for '%s'", filename)
-                do_copy = True
-
-        if do_copy:
-            logger.info("Copying %s template '%s'", ref, dst_file)
-            # Ensure destination dir exists
-            try:
-                dst_file.parent.mkdir(parents=True, exist_ok=True)
-            except FileExistsError:
-                pass
-
-            # Copy file
-            try:
-                default_storage.save(filename, src_file.open('rb'))
-            except FileExistsError:
-                pass
-
-        # Check if a file matching the template already exists
-        try:
-            if model.objects.filter(**{db_ref: filename}).exists():
-                return  # pragma: no cover
-        except Exception:
-            logger.exception(
-                "Failed to query %s for '%s' - you should run 'invoke update' first!",
-                ref,
-                filename,
-            )
-
-        logger.info("Creating entry for %s '%s'", model, data.get('name'))
-
-        try:
-            model.objects.create(
-                name=data['name'],
-                description=data['description'],
-                label=filename,
-                filters='',
-                enabled=True,
-                width=data['width'],
-                height=data['height'],
-            )
-        except Exception:
-            logger.warning("Failed to create %s '%s'", ref, data['name'])
+    def get_new_obj_data(self, data, filename):
+        """Get the data for a new template db object."""
+        return {
+            'name': data['name'],
+            'description': data['description'],
+            'label': filename,
+            'filters': '',
+            'enabled': True,
+            'width': data['width'],
+            'height': data['height'],
+        }
