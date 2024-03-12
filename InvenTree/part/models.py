7c840bf4ea09259081bb2f34e9e69b59312fd93d
Oliver Walters <oliver.henry.walters@gmail.com>
2024-03-12 22:49:24 +0000

    Ensure test template name would generate valid key

--- InvenTree/part/models.py
+++ InvenTree/part/models.py
@@ -3428,6 +3428,13 @@ class PartTestTemplate(InvenTree.models.InvenTreeMetadataModel):
 
         self.key = helpers.generateTestKey(self.test_name)
 
+        if len(self.key) == 0:
+            raise ValidationError({
+                'test_name': _(
+                    'Invalid template name - must include at least one alphanumeric character'
+                )
+            })
+
         self.validate_unique()
         super().clean()
 
