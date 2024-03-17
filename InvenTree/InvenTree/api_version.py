5aceb03ca0e78e5a72e3abfb6e30815322fd8e5c
Miklos Marton <martonmiklosqdev@gmail.com>
2024-03-17 22:44:24 +0100

    Fix API version again

--- InvenTree/InvenTree/api_version.py
+++ InvenTree/InvenTree/api_version.py
@@ -1,7 +1,7 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 183
+INVENTREE_API_VERSION = 184
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
