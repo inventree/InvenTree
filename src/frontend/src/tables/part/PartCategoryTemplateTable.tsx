fef527a6ab0b7931805c69b5d3106aa90be3a698
Oliver Walters <oliver.henry.walters@gmail.com>
2024-03-20 12:12:23 +0000

    Add functions to create / edit / delete via table

--- src/frontend/src/tables/part/PartCategoryTemplateTable.tsx
+++ src/frontend/src/tables/part/PartCategoryTemplateTable.tsx
@@ -1,10 +1,17 @@
 import { t } from '@lingui/macro';
 import { Group, Text } from '@mantine/core';
-import { useCallback, useMemo } from 'react';
+import { useCallback, useMemo, useState } from 'react';
+import { set } from 'react-hook-form';
 
 import { AddItemButton } from '../../components/buttons/AddItemButton';
+import { ApiFormFieldSet } from '../../components/forms/fields/ApiFormField';
 import { ApiEndpoints } from '../../enums/ApiEndpoints';
 import { UserRoles } from '../../enums/Roles';
+import {
+  useCreateApiFormModal,
+  useDeleteApiFormModal,
+  useEditApiFormModal
+} from '../../hooks/UseForm';
 import { useTable } from '../../hooks/UseTable';
 import { apiUrl } from '../../states/ApiState';
 import { useUserState } from '../../states/UserState';
@@ -16,6 +23,38 @@ import { RowDeleteAction, RowEditAction } from '../RowActions';
 export default function PartCategoryTemplateTable({}: {}) {
   const table = useTable('part-category-parameter-templates');
   const user = useUserState();
+
+  const formFields: ApiFormFieldSet = useMemo(() => {
+    return {
+      category: {},
+      parameter_template: {},
+      default_value: {}
+    };
+  }, []);
+
+  const [selectedTemplate, setSelectedTemplate] = useState<number>(0);
+
+  const newTemplate = useCreateApiFormModal({
+    url: ApiEndpoints.category_parameter_list,
+    title: t`Add Category Parameter`,
+    fields: formFields,
+    onFormSuccess: table.refreshTable
+  });
+
+  const editTemplate = useEditApiFormModal({
+    url: ApiEndpoints.category_parameter_list,
+    pk: selectedTemplate,
+    title: t`Edit Category Parameter`,
+    fields: formFields,
+    onFormSuccess: (record: any) => table.updateRecord(record)
+  });
+
+  const deleteTemplate = useDeleteApiFormModal({
+    url: ApiEndpoints.category_parameter_list,
+    pk: selectedTemplate,
+    title: t`Delete Category Parameter`,
+    onFormSuccess: table.refreshTable
+  });
 
   const tableFilters: TableFilter[] = useMemo(() => {
     // TODO
@@ -44,6 +83,10 @@ export default function PartCategoryTemplateTable({}: {}) {
         sortable: true,
         switchable: false,
         render: (record: any) => {
+          if (!record?.default_value) {
+            return '-';
+          }
+
           let units = '';
 
           if (record?.parameter_template_detail?.units) {
@@ -67,13 +110,15 @@ export default function PartCategoryTemplateTable({}: {}) {
         RowEditAction({
           hidden: !user.hasChangeRole(UserRoles.part),
           onClick: () => {
-            // TODO
+            setSelectedTemplate(record.pk);
+            editTemplate.open();
           }
         }),
         RowDeleteAction({
           hidden: !user.hasDeleteRole(UserRoles.part),
           onClick: () => {
-            // TODO
+            setSelectedTemplate(record.pk);
+            deleteTemplate.open();
           }
         })
       ];
@@ -85,9 +130,7 @@ export default function PartCategoryTemplateTable({}: {}) {
     return [
       <AddItemButton
         tooltip={t`Add Category Parameter`}
-        onClick={() => {
-          // TODO
-        }}
+        onClick={() => newTemplate.open()}
         hidden={!user.hasAddRole(UserRoles.part)}
       />
     ];
@@ -95,6 +138,9 @@ export default function PartCategoryTemplateTable({}: {}) {
 
   return (
     <>
+      {newTemplate.modal}
+      {editTemplate.modal}
+      {deleteTemplate.modal}
       <InvenTreeTable
         url={apiUrl(ApiEndpoints.category_parameter_list)}
         tableState={table}
