e5d6470a974bf74fed8d53cefff333431921e33e
Matthias Mair <code@mjmair.com>
2024-03-25 22:04:19 +0100

    add contenttypes

--- InvenTree/plugin/samples_app/app_sample/models.py
+++ InvenTree/plugin/samples_app/app_sample/models.py
@@ -1,9 +1,8 @@
 """Models for sample plugin."""
 
 from django.contrib.auth.models import User
-
-# from django.contrib.contenttypes.fields import GenericForeignKey
-# from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+from django.contrib.contenttypes.models import ContentType
 from django.db import models
 from django.urls import reverse
 from django.utils.translation import gettext_lazy as _
@@ -27,9 +26,9 @@ class SampleModel(InvenTree.models.MetadataMixin, models.Model):
         related_name='sample_plugin',
     )
     boolean_field = models.BooleanField(default=False, verbose_name=_('Boolean field'))
-    # content_type = models.ForeignKey('ContentType', on_delete=models.CASCADE)
-    # object_id = models.PositiveIntegerField()
-    # content_object = GenericForeignKey('content_type', 'object_id')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
 
     class Meta:
         """Meta class."""
