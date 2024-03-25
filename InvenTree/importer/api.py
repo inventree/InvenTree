8baf848c3b98c84f3766939399d2863c6ba6ec2e
Oliver Walters <oliver.henry.walters@gmail.com>
2024-03-25 11:26:22 +0000

    Add detail endpoints

--- InvenTree/importer/api.py
+++ InvenTree/importer/api.py
@@ -5,11 +5,18 @@ from django.urls import include, path
 import importer.models
 import importer.serializers
 from InvenTree.filters import SEARCH_ORDER_FILTER
-from InvenTree.mixins import ListAPI, ListCreateAPI
+from InvenTree.mixins import ListAPI, ListCreateAPI, RetrieveUpdateDestroyAPI
 
 
 class DataImportSessionList(ListCreateAPI):
     """API endpoint for accessing a list of DataImportSession objects."""
+
+    queryset = importer.models.DataImportSession.objects.all()
+    serializer_class = importer.serializers.DataImportSessionSerializer
+
+
+class DataImportSessionDetail(RetrieveUpdateDestroyAPI):
+    """Detail endpoint for a single DataImportSession object."""
 
     queryset = importer.models.DataImportSession.objects.all()
     serializer_class = importer.serializers.DataImportSessionSerializer
@@ -24,6 +31,13 @@ class DataImportColumnMappingList(ListCreateAPI):
     filter_backends = SEARCH_ORDER_FILTER
 
     filterset_fields = ['session']
+
+
+class DataImportColumnMappingDetail(RetrieveUpdateDestroyAPI):
+    """Detail endpoint for a single DataImportColumnMap object."""
+
+    queryset = importer.models.DataImportColumnMap.objects.all()
+    serializer_class = importer.serializers.DataImportColumnMapSerializer
 
 
 class DataImportRowList(ListAPI):
@@ -41,25 +55,49 @@ class DataImportRowList(ListAPI):
     ordering = 'row_index'
 
 
+class DataImportRowDetail(RetrieveUpdateDestroyAPI):
+    """Detail endpoint for a single DataImportRow object."""
+
+    queryset = importer.models.DataImportRow.objects.all()
+    serializer_class = importer.serializers.DataImportRowSerializer
+
+
 importer_api_urls = [
     path(
         'session/',
         include([
-            path('', DataImportSessionList.as_view(), name='api-importer-session-list')
+            path(
+                '<int:pk>/',
+                DataImportSessionDetail.as_view(),
+                name='api-import-session-detail',
+            ),
+            path('', DataImportSessionList.as_view(), name='api-importer-session-list'),
         ]),
     ),
     path(
         'column-mapping/',
         include([
             path(
+                '<int:pk>/',
+                DataImportColumnMappingDetail.as_view(),
+                name='api-importer-mapping-detail',
+            ),
+            path(
                 '',
                 DataImportColumnMappingList.as_view(),
                 name='api-importer-mapping-list',
-            )
+            ),
         ]),
     ),
     path(
         'row/',
-        include([path('', DataImportRowList.as_view(), name='api-importer-row-list')]),
+        include([
+            path(
+                '<int:pk>/',
+                DataImportRowDetail.as_view(),
+                name='api-importer-row-detail',
+            ),
+            path('', DataImportRowList.as_view(), name='api-importer-row-list'),
+        ]),
     ),
 ]
