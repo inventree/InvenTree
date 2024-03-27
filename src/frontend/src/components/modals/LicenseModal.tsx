e3709e868dffab108fe7d81feb8357e78a170b54
Oliver Walters <oliver.henry.walters@gmail.com>
2024-03-27 11:36:55 +0000

    Handle errors when fetching license information

--- src/frontend/src/components/modals/LicenseModal.tsx
+++ src/frontend/src/components/modals/LicenseModal.tsx
@@ -1,6 +1,14 @@
-import { Trans } from '@lingui/macro';
-import { Accordion } from '@mantine/core';
-import { ContextModalProps } from '@mantine/modals';
+import { Trans, t } from '@lingui/macro';
+import {
+  Accordion,
+  Alert,
+  Divider,
+  LoadingOverlay,
+  Stack,
+  Text
+} from '@mantine/core';
+import { ContextModalProps, closeModal } from '@mantine/modals';
+import { notifications } from '@mantine/notifications';
 import { useQuery } from '@tanstack/react-query';
 
 import { api } from '../../App';
@@ -10,21 +18,42 @@ import { apiUrl } from '../../states/ApiState';
 export function LicenseModal({}: ContextModalProps<{
   modalBody: string;
 }>) {
-  const { isLoading, data } = useQuery({
+  const { data, isFetching, isError } = useQuery({
     queryKey: ['license'],
-    queryFn: () => api.get(apiUrl(ApiEndpoints.license)).then((res) => res.data)
+    queryFn: () =>
+      api
+        .get(apiUrl(ApiEndpoints.license))
+        .then((res) => res.data)
+        .catch(() => {})
   });
-  if (isLoading) return <Trans>Loading</Trans>;
+
   return (
-    <Accordion variant="contained" defaultValue="customization">
-      {Object.keys(data).map((item, key) => (
-        <Accordion.Item key={key} value={item}>
-          <Accordion.Control>{item}</Accordion.Control>
-          <Accordion.Panel>
-            <p style={{ whiteSpace: 'pre-line' }}>{data[item]}</p>
-          </Accordion.Panel>
-        </Accordion.Item>
-      ))}
-    </Accordion>
+    <Stack spacing="xs">
+      <Divider />
+      <LoadingOverlay visible={isFetching} />
+      {isFetching && (
+        <Text>
+          <Trans>Loading license information</Trans>
+        </Text>
+      )}
+      {isError ? (
+        <Alert color="red" title={t`Error`}>
+          <Text>
+            <Trans>Failed to fetch license information</Trans>
+          </Text>
+        </Alert>
+      ) : (
+        <Accordion variant="contained" defaultValue="customization">
+          {Object.keys(data ?? []).map((item, key) => (
+            <Accordion.Item key={key} value={item}>
+              <Accordion.Control>{item}</Accordion.Control>
+              <Accordion.Panel>
+                <p style={{ whiteSpace: 'pre-line' }}>{data[item]}</p>
+              </Accordion.Panel>
+            </Accordion.Item>
+          ))}
+        </Accordion>
+      )}
+    </Stack>
   );
 }
