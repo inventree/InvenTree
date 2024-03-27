aeffb701894f4e75550e505a830208359c0885c0
Matthias Mair <code@mjmair.com>
2024-03-27 21:27:17 +0100

    add test for serving and logging into pui from django / cui tech stack

--- src/frontend/tests/ui_plattform.spec.ts
+++ src/frontend/tests/ui_plattform.spec.ts
@@ -1,4 +1,18 @@
+import { classicUrl } from './baseFixtures';
 import { expect, test } from './baseFixtures.js';
+
+test('Basic Platform UI test via django', async ({ page }) => {
+  await page.goto(`${classicUrl}/platform/`);
+  await expect(page).toHaveTitle('InvenTree Demo Server');
+  await page.waitForURL('**/platform/');
+  await page.getByLabel('username').fill('allaccess');
+  await page.getByLabel('password').fill('nolimits');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForURL('**/platform');
+  await page.goto(`${classicUrl}/platform/`);
+
+  await expect(page).toHaveTitle('InvenTree Demo Server');
+});
 
 test('Basic Platform UI test', async ({ page }) => {
   await page.goto('./platform/');
