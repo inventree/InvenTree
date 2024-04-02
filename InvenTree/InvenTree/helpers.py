99fbead187bc1f5304fe72bb548692c1bed078d0
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-02 00:40:20 +0000

    Simplify is_bool function

--- InvenTree/InvenTree/helpers.py
+++ InvenTree/InvenTree/helpers.py
@@ -248,11 +248,7 @@ def str2int(text, default=None):
 
 def is_bool(text):
     """Determine if a string value 'looks' like a boolean."""
-    if str2bool(text, True):
-        return True
-    elif str2bool(text, False):
-        return True
-    return False
+    return str2bool(text, True) or str2bool(text, False)
 
 
 def isNull(text):
