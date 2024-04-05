f774f41c83c6e18857beb2091d679bfa557ed05f
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-05 04:58:48 +0000

    Update API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 186
+INVENTREE_API_VERSION = 187
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v187 - 2024-04-05 : https://github.com/inventree/InvenTree/pull/6911
+    - Adds API endpoints for managing data import
 
 v186 - 2024-03-26 : https://github.com/inventree/InvenTree/pull/6855
     - Adds license information to the API
