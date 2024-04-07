738a62392f68f1035b82ccfeaacf9a4d2212d8c8
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-07 05:14:11 +0000

    Prevent session auth on login view
    - Existing (invalid) session token causes 403

--- src/backend/InvenTree/users/api.py
+++ src/backend/InvenTree/users/api.py
@@ -11,6 +11,8 @@ from django.views.generic.base import RedirectView
 from dj_rest_auth.views import LoginView, LogoutView
 from drf_spectacular.utils import OpenApiResponse, extend_schema, extend_schema_view
 from rest_framework import exceptions, permissions
+from rest_framework.authentication import BasicAuthentication
+from rest_framework.decorators import authentication_classes
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
@@ -203,6 +205,18 @@ class GroupList(ListCreateAPI):
     search_fields = ['name']
 
     ordering_fields = ['name']
+
+
+@authentication_classes([BasicAuthentication])
+@extend_schema_view(
+    post=extend_schema(
+        responses={200: OpenApiResponse(description='User successfully logged in')}
+    )
+)
+class Login(LoginView):
+    """API view for logging in via API."""
+
+    ...
 
 
 @extend_schema_view(
