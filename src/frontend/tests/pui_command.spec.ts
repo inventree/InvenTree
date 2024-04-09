b3764e8b11f6fbc9a6cab9df419bdaebbc638eb9
Matthias Mair <code@mjmair.com>
2024-04-09 23:32:21 +0200

    use var to determine action key

--- src/frontend/tests/pui_command.spec.ts
+++ src/frontend/tests/pui_command.spec.ts
@@ -1,4 +1,4 @@
-import { expect, test } from './baseFixtures.js';
+import { expect, systemKey, test } from './baseFixtures.js';
 
 test('PUI - Quick Command', async ({ page }) => {
   await page.goto('./platform/');
@@ -16,7 +16,7 @@ test('PUI - Quick Command', async ({ page }) => {
   await page.waitForTimeout(500);
 
   // Open Spotlight with Keyboard Shortcut
-  await page.keyboard.press('Control+K');
+  await page.keyboard.press(`${systemKey}+K`);
   await page.getByText('Go to the InvenTree dashboard').click();
   await page
     .locator('div')
@@ -33,7 +33,7 @@ test('PUI - Quick Command', async ({ page }) => {
   await page.waitForURL('**/platform');
 
   // Open Spotlight with Keyboard Shortcut and Search
-  await page.keyboard.press('Control+K');
+  await page.keyboard.press(`${systemKey}+K`);
   await page.getByPlaceholder('Search...').fill('Dashboard');
   await page.getByPlaceholder('Search...').press('Tab');
   await page.getByPlaceholder('Search...').press('Enter');
