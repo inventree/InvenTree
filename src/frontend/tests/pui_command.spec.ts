7b58070e92b4b032f3f4e43f3685143a574aef73
Matthias Mair <code@mjmair.com>
2024-04-09 23:40:27 +0200

    split out keyboard based tests

--- src/frontend/tests/pui_command.spec.ts
+++ src/frontend/tests/pui_command.spec.ts
@@ -42,9 +42,29 @@ test('PUI - Quick Command', async ({ page }) => {
   await page.getByPlaceholder('Search...').press('Tab');
   await page.getByPlaceholder('Search...').press('Enter');
   await page.waitForURL('**/platform/dashboard');
+});
 
-  // Reset
+test('PUI - Quick Command - no keys', async ({ page }) => {
   await page.goto('./platform/');
+  await expect(page).toHaveTitle('InvenTree');
+  await page.waitForURL('**/platform/');
+  await page.getByLabel('username').fill('allaccess');
+  await page.getByLabel('password').fill('nolimits');
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForURL('**/platform');
+
+  await expect(page).toHaveTitle('InvenTree');
+  await page.waitForURL('**/platform/');
+  // wait for the page to load - 0.5s
+  await page.waitForTimeout(500);
+
+  // Open Spotlight with Button
+  await page.getByRole('button', { name: 'Open spotlight' }).click();
+  await page.getByRole('button', { name: 'Home Go to the home page' }).click();
+  await page
+    .getByRole('heading', { name: 'Welcome to your Dashboard,' })
+    .click();
+  await page.waitForURL('**/platform');
 
   // Use navigation menu
   await page.getByRole('button', { name: 'Open spotlight' }).click();
