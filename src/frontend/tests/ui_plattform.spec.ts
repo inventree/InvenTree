9176087e487ae136c6ed1cbed3141c37cc5713a7
Matthias Mair <code@mjmair.com>
2024-04-10 18:53:46 +0200

    fix assert url

--- src/frontend/tests/ui_plattform.spec.ts
+++ src/frontend/tests/ui_plattform.spec.ts
@@ -21,7 +21,7 @@ test('PUI - Basic test', async ({ page }) => {
   await page.getByLabel('username').fill('allaccess');
   await page.getByLabel('password').fill('nolimits');
   await page.getByRole('button', { name: 'Log in' }).click();
-  await page.waitForURL('**/platform');
+  await page.waitForURL('**/platform/home');
   await page.goto('./platform/');
 
   await expect(page).toHaveTitle('InvenTree');
