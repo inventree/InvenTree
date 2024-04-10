8805bd2936892e5a80df00caeaf8cb029c66da13
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-10 10:25:32 +0000

    Refactor BomPricingPanel
    - Table drives the data

--- src/frontend/src/pages/part/pricing/BomPricingPanel.tsx
+++ src/frontend/src/pages/part/pricing/BomPricingPanel.tsx
@@ -9,7 +9,11 @@ import { ApiEndpoints } from '../../../enums/ApiEndpoints';
 import { ModelType } from '../../../enums/ModelType';
 import { getDetailUrl } from '../../../functions/urls';
 import { useInstance } from '../../../hooks/UseInstance';
+import { useTable } from '../../../hooks/UseTable';
+import { apiUrl } from '../../../states/ApiState';
+import { TableColumn } from '../../../tables/Column';
 import { PartColumn } from '../../../tables/ColumnRenderers';
+import { InvenTreeTable } from '../../../tables/InvenTreeTable';
 
 export default function BomPricingPanel({
   part,
@@ -18,69 +22,66 @@ export default function BomPricingPanel({
   part: any;
   pricing: any;
 }): ReactNode {
-  const {
-    instance: bomData,
-    refreshInstance,
-    instanceQuery
-  } = useInstance({
-    hasPrimaryKey: false,
-    endpoint: ApiEndpoints.bom_list,
-    params: {
-      part: part?.pk,
-      has_pricing: true,
-      sub_part_detail: true
-    },
-    defaultValue: []
-  });
+  const table = useTable('pricing-bom');
 
-  const bomPricingData: any[] = useMemo(() => {
-    const pricing = bomData.map((entry: any) => {
-      return {
-        entry: entry,
-        quantity: entry.quantity,
-        name: entry.sub_part_detail?.name,
-        pmin: parseFloat(entry.pricing_min ?? entry.pricing_max ?? 0),
-        pmax: parseFloat(entry.pricing_max ?? entry.pricing_min ?? 0)
-      };
-    });
-
-    return pricing;
-  }, [part, pricing, bomData]);
-
-  const columns: DataTableColumn<any>[] = useMemo(() => {
+  const columns: TableColumn[] = useMemo(() => {
     return [
       {
         accessor: 'name',
         title: t`Component`,
+        sortable: true,
+        switchable: false,
         render: (record: any) =>
           PartColumn(
-            record.entry.sub_part_detail,
-            getDetailUrl(ModelType.part, record.entry.sub_part_detail.pk)
+            record.sub_part_detail,
+            getDetailUrl(ModelType.part, record.sub_part_detail.pk)
           )
       },
       {
         accessor: 'quantity',
         title: t`Quantity`,
+        sortable: true,
+        switchable: false,
         render: (record: any) => formatDecimal(record.quantity)
       },
       {
-        accessor: 'pmin',
+        accessor: 'pricing_min',
+        sortable: false,
+        switchable: false,
         title: t`Minimum Price`,
         render: (record: any) =>
-          formatCurrency(record.quantity * record.pmin, {
+          formatCurrency(record.quantity * record.pricing_min, {
             currency: pricing?.currency
           })
       },
       {
-        accessor: 'pmax',
+        accessor: 'pricing_max',
         title: t`Maximum Price`,
+        sortable: false,
+        switchable: false,
         render: (record: any) =>
-          formatCurrency(record.quantity * record.pmax, {
+          formatCurrency(record.quantity * record.pricing_max, {
             currency: pricing?.currency
           })
       }
     ];
   }, [part, pricing]);
+
+  const bomPricingData: any[] = useMemo(() => {
+    const pricing = table.records
+      .map((entry: any) => {
+        return {
+          entry: entry,
+          quantity: entry.quantity,
+          name: entry.sub_part_detail?.name,
+          pmin: parseFloat(entry.pricing_min ?? entry.pricing_max ?? 0),
+          pmax: parseFloat(entry.pricing_max ?? entry.pricing_min ?? 0)
+        };
+      })
+      .sort((a, b) => a.pmax - b.pmax);
+
+    return pricing;
+  }, [table.records]);
 
   // TODO: Enable record selection (toggle which items appear in BOM pricing wheel)
   // TODO: Different colors for each element in the pie chart
@@ -88,9 +89,19 @@ export default function BomPricingPanel({
 
   return (
     <Stack spacing="xs">
-      <LoadingOverlay visible={instanceQuery.isLoading} />
       <SimpleGrid cols={2}>
-        <DataTable records={bomPricingData} columns={columns} />
+        <InvenTreeTable
+          tableState={table}
+          url={apiUrl(ApiEndpoints.bom_list)}
+          columns={columns}
+          props={{
+            params: {
+              part: part?.pk,
+              sub_part_detail: true,
+              has_pricing: true
+            }
+          }}
+        />
         <ResponsiveContainer width="100%" height={500}>
           <PieChart>
             <Pie
