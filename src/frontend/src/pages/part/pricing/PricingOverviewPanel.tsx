a4a9c6276b9c94aec92915ad23a23e8b29534a7e
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-10 11:48:39 +0000

    Update pricing overview panel

--- src/frontend/src/pages/part/pricing/PricingOverviewPanel.tsx
+++ src/frontend/src/pages/part/pricing/PricingOverviewPanel.tsx
@@ -1,7 +1,19 @@
 import { t } from '@lingui/macro';
-import { SimpleGrid, Stack } from '@mantine/core';
+import { Alert, Group, Paper, SimpleGrid, Stack, Text } from '@mantine/core';
+import { notifications } from '@mantine/notifications';
+import {
+  IconBuildingWarehouse,
+  IconCalendar,
+  IconChartDonut,
+  IconExclamationCircle,
+  IconList,
+  IconReportAnalytics,
+  IconShape,
+  IconShoppingCart,
+  IconTriangleSquareCircle
+} from '@tabler/icons-react';
 import { DataTable, DataTableColumn } from 'mantine-datatable';
-import { ReactNode, useMemo } from 'react';
+import { ReactNode, useCallback, useMemo } from 'react';
 import {
   Bar,
   BarChart,
@@ -12,9 +24,14 @@ import {
   YAxis
 } from 'recharts';
 
-import { formatCurrency } from '../../../defaults/formatters';
+import { api } from '../../../App';
+import { formatCurrency, renderDate } from '../../../defaults/formatters';
+import { ApiEndpoints } from '../../../enums/ApiEndpoints';
+import { cancelEvent } from '../../../functions/events';
+import { apiUrl } from '../../../states/ApiState';
 
 interface PricingOverviewEntry {
+  icon: ReactNode;
   name: string;
   title: string;
   min_value: number | null | undefined;
@@ -34,12 +51,23 @@ export default function PricingOverviewPanel({
     return [
       {
         accessor: 'title',
-        title: t`Pricing Category`
+        title: t`Pricing Category`,
+        render: (record: PricingOverviewEntry) => {
+          return (
+            <Group position="left" spacing="xs">
+              {record.icon}
+              <Text weight={700}>{record.title}</Text>
+            </Group>
+          );
+        }
       },
       {
         accessor: 'min_value',
         title: t`Minimum`,
         render: (record: PricingOverviewEntry) => {
+          if (record?.min_value === null || record?.min_value === undefined) {
+            return '-';
+          }
           return formatCurrency(record?.min_value, {
             currency: record.currency ?? pricing?.currency
           });
@@ -49,6 +77,10 @@ export default function PricingOverviewPanel({
         accessor: 'max_value',
         title: t`Maximum`,
         render: (record: PricingOverviewEntry) => {
+          if (record?.max_value === null || record?.max_value === undefined) {
+            return '-';
+          }
+
           return formatCurrency(record?.max_value, {
             currency: record.currency ?? pricing?.currency
           });
@@ -62,40 +94,55 @@ export default function PricingOverviewPanel({
       {
         name: 'internal',
         title: t`Internal Pricing`,
-        min_value: pricing?.internal_min,
-        max_value: pricing?.internal_max
+        icon: <IconList />,
+        min_value: pricing?.internal_cost_min,
+        max_value: pricing?.internal_cost_max
       },
       {
         name: 'bom',
         title: t`BOM Pricing`,
+        icon: <IconChartDonut />,
         min_value: pricing?.bom_cost_min,
         max_value: pricing?.bom_cost_max
       },
       {
         name: 'purchase',
         title: t`Purchase Pricing`,
+        icon: <IconShoppingCart />,
         min_value: pricing?.purchase_cost_min,
         max_value: pricing?.purchase_cost_max
       },
       {
         name: 'supplier',
         title: t`Supplier Pricing`,
+        icon: <IconBuildingWarehouse />,
         min_value: pricing?.supplier_price_min,
         max_value: pricing?.supplier_price_max
       },
       {
         name: 'variants',
         title: t`Variant Pricing`,
+        icon: <IconTriangleSquareCircle />,
         min_value: pricing?.variant_cost_min,
         max_value: pricing?.variant_cost_max
       },
       {
+        name: 'override',
+        title: t`Override Pricing`,
+        icon: <IconExclamationCircle />,
+        min_value: pricing?.override_min,
+        max_value: pricing?.override_max
+      },
+      {
         name: 'overall',
         title: t`Overall Pricing`,
+        icon: <IconReportAnalytics />,
         min_value: pricing?.overall_min,
         max_value: pricing?.overall_max
       }
-    ];
+    ].filter((entry) => {
+      return entry.min_value !== null || entry.max_value !== null;
+    });
   }, [part, pricing]);
 
   // TODO: Add display of "last updated"
@@ -104,7 +151,16 @@ export default function PricingOverviewPanel({
   return (
     <Stack spacing="xs">
       <SimpleGrid cols={2}>
-        <DataTable records={overviewData} columns={columns} />
+        <Stack spacing="xs">
+          {pricing?.updated && (
+            <Paper p="xs">
+              <Alert color="blue" title={t`Last Updated`}>
+                <Text>{renderDate(pricing.updated)}</Text>
+              </Alert>
+            </Paper>
+          )}
+          <DataTable records={overviewData} columns={columns} />
+        </Stack>
         <ResponsiveContainer width="100%" height={500}>
           <BarChart data={overviewData}>
             <XAxis dataKey="title" />
