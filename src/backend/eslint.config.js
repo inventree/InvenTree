46824c2835e479977bb0501c2f96316e1fc1e498
Matthias Mair <code@mjmair.com>
2024-04-11 23:18:40 +0200

    fix config for new style

--- src/backend/eslint.config.js
+++ src/backend/eslint.config.js
@@ -1,31 +1,35 @@
-/* eslint-env node */
-module.exports = {
-  env: {
-    commonjs: false,
-    browser: true,
-    es2021: true,
-    jquery: true,
-  },
-  extends: ['eslint:recommended'],
-  parserOptions: {
-    ecmaVersion: 12
-  },
+// eslint.config.js
+import js from "@eslint/js";
+import globals from "globals";
 
-  rules: {
-    "no-var": "off",
-    "guard-for-in": "off",
-    "no-trailing-spaces": "off",
-    "camelcase": "off",
-    "padded-blocks": "off",
-    "prefer-const": "off",
-    "max-len": "off",
-    "require-jsdoc": "off",
-    "valid-jsdoc": "off",
-    "no-multiple-empty-lines": "off",
-    "comma-dangle": "off",
-    "no-unused-vars": "off",
-    "no-useless-escape": "off",
-    "prefer-spread": "off",
-    "indent": ["error", 4]
+export default [
+  js.configs.recommended,
+  {
+    languageOptions: {
+      ecmaVersion: 12,
+      sourceType: "module",
+      globals: {
+        ...globals.browser,
+        ...globals.es2021,
+        ...globals.jquery,
+      }
+    },
+    rules: {
+      "no-var": "off",
+      "guard-for-in": "off",
+      "no-trailing-spaces": "off",
+      "camelcase": "off",
+      "padded-blocks": "off",
+      "prefer-const": "off",
+      "max-len": "off",
+      "require-jsdoc": "off",
+      "valid-jsdoc": "off",
+      "no-multiple-empty-lines": "off",
+      "comma-dangle": "off",
+      "no-unused-vars": "off",
+      "no-useless-escape": "off",
+      "prefer-spread": "off",
+      "indent": ["error", 4]
+    }
   }
-};
+];
