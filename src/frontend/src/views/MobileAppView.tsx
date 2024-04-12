6456b930196098c0a44609b4769332203a4d2d54
Matthias Mair <code@mjmair.com>
2024-04-12 08:41:58 +0200

    only show exception on dev

--- src/frontend/src/views/MobileAppView.tsx
+++ src/frontend/src/views/MobileAppView.tsx
@@ -3,6 +3,7 @@ import { Anchor, Center, Container, Stack, Text, Title } from '@mantine/core';
 
 import { BaseContext } from '../contexts/BaseContext';
 import { docLinks } from '../defaults/links';
+import { IS_DEV } from '../main';
 import { useLocalState } from '../states/LocalState';
 
 export default function MobileAppView() {
@@ -29,9 +30,11 @@ export default function MobileAppView() {
             <Anchor href={docLinks.app}>
               <Trans>Read the docs</Trans>
             </Anchor>
-            <Text onClick={ignore}>
-              <Trans>Ignore and continue to Desktop view</Trans>
-            </Text>
+            {IS_DEV && (
+              <Text onClick={ignore}>
+                <Trans>Ignore and continue to Desktop view</Trans>
+              </Text>
+            )}
           </Stack>
         </Container>
       </Center>
