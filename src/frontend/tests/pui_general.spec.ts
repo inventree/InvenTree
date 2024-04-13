9e9272becefd5534d07daeeaa38e743760545803
Matthias Mair <code@mjmair.com>
2024-04-13 23:30:34 +0200

    add missing tabs

--- src/frontend/tests/pui_general.spec.ts
+++ src/frontend/tests/pui_general.spec.ts
@@ -25,9 +25,12 @@ test('PUI - Parts', async ({ page }) => {
   await page.getByRole('tab', { name: 'Scheduling' }).click();
   await page.getByRole('tab', { name: 'Stocktake' }).click();
   await page.getByRole('tab', { name: 'Related Parts' }).click();
+  await page.getByRole('tab', { name: 'Attachments' }).click();
+  await page.getByRole('tab', { name: 'Notes' }).click();
   await page.getByText('1551ACLR').click();
   await page.getByRole('tab', { name: 'Part Details' }).click();
   await page.getByRole('tab', { name: 'Parameters' }).click();
+  await page.getByRole('tab', { name: 'Stock' }).click();
   await page.getByRole('tab', { name: 'Allocations' }).click();
   await page.getByRole('tab', { name: 'Used In' }).click();
   await page.getByRole('tab', { name: 'Pricing' }).click();
