ae3c436088c85b6b19fb98c2b3a03f7e9aaac209
Matthias Mair <code@mjmair.com>
2024-04-14 02:01:50 +0200

    Add test for settings + admin center

--- src/frontend/tests/pui_general.spec.ts
+++ src/frontend/tests/pui_general.spec.ts
@@ -166,7 +166,62 @@ test('PUI - Admin', async ({ page }) => {
   await page.getByRole('button', { name: 'Log in' }).click();
   await page.waitForURL('**/platform');
 
+  // User settings
   await page.getByRole('button', { name: 'admin' }).click();
+  await page.getByRole('menuitem', { name: 'Account settings' }).click();
+  await page.getByRole('tab', { name: 'Security' }).click();
+  //await page.getByRole('tab', { name: 'Dashboard' }).click();
+  await page.getByRole('tab', { name: 'Display Options' }).click();
+  await page.getByText('Date Format').waitFor();
+  await page.getByRole('tab', { name: 'Search' }).click();
+  await page.getByText('Regex Search').waitFor();
+  await page.getByRole('tab', { name: 'Notifications' }).click();
+  await page.getByRole('tab', { name: 'Reporting' }).click();
+  await page.getByText('Inline report display').waitFor();
+
+  // System Settings
+  await page.getByRole('link', { name: 'Switch to System Setting' }).click();
+  await page.getByText('Base URL', { exact: true }).waitFor();
+  await page.getByRole('tab', { name: 'Login' }).click();
+  await page.getByRole('tab', { name: 'Barcodes' }).click();
+  await page.getByRole('tab', { name: 'Notifications' }).click();
+  await page.getByRole('tab', { name: 'Pricing' }).click();
+  await page.getByRole('tab', { name: 'Labels' }).click();
+  await page.getByRole('tab', { name: 'Reporting' }).click();
+  await page.getByRole('tab', { name: 'Part Categories' }).click();
+  //wait page.locator('#mantine-9hqbwrml8-tab-parts').click();
+  //await page.locator('#mantine-9hqbwrml8-tab-stock').click();
+  await page.getByRole('tab', { name: 'Stocktake' }).click();
+  await page.getByRole('tab', { name: 'Build Orders' }).click();
+  await page.getByRole('tab', { name: 'Purchase Orders' }).click();
+  await page.getByRole('tab', { name: 'Sales Orders' }).click();
+  await page.getByRole('tab', { name: 'Return Orders' }).click();
+
+  // Admin Center
+  await page.getByRole('button', { name: 'admin' }).click();
+  await page.getByRole('menuitem', { name: 'Admin Center' }).click();
+  await page.getByRole('tab', { name: 'Background Tasks' }).click();
+  await page.getByRole('tab', { name: 'Error Reports' }).click();
+  await page.getByRole('tab', { name: 'Currencies' }).click();
+  await page.getByRole('tab', { name: 'Project Codes' }).click();
+  await page.getByRole('tab', { name: 'Custom Units' }).click();
+  await page.getByRole('tab', { name: 'Part Parameters' }).click();
+  await page.getByRole('tab', { name: 'Category Parameters' }).click();
+  await page.getByRole('tab', { name: 'Templates' }).click();
+  await page.getByRole('tab', { name: 'Plugins' }).click();
+  await page.getByRole('tab', { name: 'Machines' }).click();
+});
+
+test('PUI - Language / Color', async ({ page }) => {
+  await page.goto('./platform/');
+  await expect(page).toHaveTitle('InvenTree');
+  await page.waitForURL('**/platform/*');
+  await page.getByLabel('username').fill(user.username);
+  await page.getByLabel('password').fill(user.password);
+  await page.getByRole('button', { name: 'Log in' }).click();
+  await page.waitForURL('**/platform');
+
+  await page.getByRole('button', { name: 'Ally Access' }).click();
   await page.getByRole('menuitem', { name: 'Logout' }).click();
   await page.getByRole('button', { name: 'Send me an email' }).click();
   await page.getByRole('button').nth(3).click();
