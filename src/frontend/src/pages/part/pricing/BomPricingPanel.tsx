d3a2eced975abb78f7a86cbcab135d4bf1eeb2de
Oliver <oliver.henry.walters@gmail.com>
2024-04-16 01:05:56 +1000

    [PUI] pricing tab (#6985)
    * Add recharts package
    
    - Brings us in-line with mantine v7
    
    * Add skeleton pricing page
    
    * Fetch pricing data
    
    * Rough implementation of variant pricing chart
    
    - Needs better labels for tooltip and axis
    
    * Cleanup
    
    * More cleanup
    
    * Improve rendering
    
    * Add pricing overview
    
    * Add pie chart for BOM pricing
    
    - Needs extra work!
    
    * Split components into separate files
    
    * Backend: allow ordering parts by pricing
    
    * Bump API version
    
    * Update VariantPricingPanel:
    
    - Table drives data selection now
    
    * Refactor BomPricingPanel
    
    - Table drives the data
    
    * Allow BomItemList to be sorted by pricing too
    
    * Sort bom table
    
    * Make record index available to render function
    
    * Refactor BomPricingPanel
    
    - Better rendering of pie chart
    
    * Update pricing overview panel
    
    * Further updates
    
    - Expose "pricing_updated" column to API endpoints
    - Allow ordering by "pricing_updated" column
    
    * Update API endpoint for PurchaseOrderLineItem
    
    * Implement PurchaseOrderHistory panel
    
    * Cleanup PurchaseHistoryPanel
    
    * Enhance API for SupplierPriceBreak
    
    * Implement SupplierPricingPanel
    
    * Fix for getDetailUrl
    
    - Take base URL into account also!
    
    * Further fixes for getDetailUrl
    
    * Fix number form field
    
    * Implement SupplierPriceBreakTable
    
    * Tweaks for StockItemTable
    
    * Ensure frontend is translated when compiling static files
    
    * Fixes for BomPricingPanel
    
    * Simplify price rendering for bom table
    
    * Update BomItem serializer
    
    - Add pricing_min_total
    - Add pricing_max_total
    - Fix existing 1+N query issue
    
    * Use values provided by API
    
    * Fix BomItem serializer lookup
    
    * Refactor pricing charts
    
    * Fix for VariantPricingPanel
    
    * Remove unused imports
    
    * Implement SalePriceBreak table
    
    - Refactor the InternalPriceBreak table to be generic
    
    * Allow price breaks to be ordered by 'price'
    
    * Display alert for no available data
    
    * Update backend API filters
    
    * Allow ordering by customer
    
    * Implement SaleHistoryPanel
    
    * Allow user to select pie or bar chart for BOM pricing detail
    
    * Remove extra padding

---
+++ src/frontend/src/pages/part/pricing/BomPricingPanel.tsx
@@ -0,0 +1,203 @@
+import { t } from '@lingui/macro';
+import { SegmentedControl, SimpleGrid, Stack } from '@mantine/core';
+import { ReactNode, useMemo, useState } from 'react';
+import {
+  Bar,
+  BarChart,
+  Cell,
+  Legend,
+  Pie,
+  PieChart,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis
+} from 'recharts';
+
+import { CHART_COLORS } from '../../../components/charts/colors';
+import { formatDecimal, formatPriceRange } from '../../../defaults/formatters';
+import { ApiEndpoints } from '../../../enums/ApiEndpoints';
+import { useTable } from '../../../hooks/UseTable';
+import { apiUrl } from '../../../states/ApiState';
+import { TableColumn } from '../../../tables/Column';
+import { DateColumn, PartColumn } from '../../../tables/ColumnRenderers';
+import { InvenTreeTable } from '../../../tables/InvenTreeTable';
+import { NoPricingData } from './PricingPanel';
+
+// Display BOM data as a pie chart
+function BomPieChart({ data }: { data: any[] }) {
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <PieChart>
+        <Pie
+          data={data}
+          dataKey="total_price_min"
+          nameKey="name"
+          innerRadius={20}
+          outerRadius={100}
+        >
+          {data.map((_entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={CHART_COLORS[index % CHART_COLORS.length]}
+            />
+          ))}
+        </Pie>
+        <Pie
+          data={data}
+          dataKey="total_price_max"
+          nameKey="name"
+          innerRadius={120}
+          outerRadius={240}
+        >
+          {data.map((_entry, index) => (
+            <Cell
+              key={`cell-${index}`}
+              fill={CHART_COLORS[index % CHART_COLORS.length]}
+            />
+          ))}
+        </Pie>
+        <Tooltip />
+      </PieChart>
+    </ResponsiveContainer>
+  );
+}
+
+// Display BOM data as a bar chart
+function BomBarChart({ data }: { data: any[] }) {
+  return (
+    <ResponsiveContainer width="100%" height={500}>
+      <BarChart data={data}>
+        <XAxis dataKey="name" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar
+          dataKey="total_price_min"
+          fill={CHART_COLORS[0]}
+          label={t`Minimum Total Price`}
+        />
+        <Bar
+          dataKey="total_price_max"
+          fill={CHART_COLORS[1]}
+          label={t`Maximum Total Price`}
+        />
+      </BarChart>
+    </ResponsiveContainer>
+  );
+}
+
+export default function BomPricingPanel({
+  part,
+  pricing
+}: {
+  part: any;
+  pricing: any;
+}): ReactNode {
+  const table = useTable('pricing-bom');
+
+  const columns: TableColumn[] = useMemo(() => {
+    return [
+      {
+        accessor: 'name',
+        title: t`Component`,
+        sortable: true,
+        switchable: false,
+        render: (record: any) => PartColumn(record.sub_part_detail)
+      },
+      {
+        accessor: 'quantity',
+        title: t`Quantity`,
+        sortable: true,
+        switchable: false,
+        render: (record: any) => formatDecimal(record.quantity)
+      },
+      {
+        accessor: 'unit_price',
+        ordering: 'pricing_max',
+        sortable: true,
+        switchable: true,
+        title: t`Unit Price`,
+        render: (record: any) => {
+          return formatPriceRange(record.pricing_min, record.pricing_max, {
+            currency: pricing?.currency
+          });
+        }
+      },
+      {
+        accessor: 'total_price',
+        title: t`Total Price`,
+        ordering: 'pricing_max_total',
+        sortable: true,
+        switchable: false,
+        render: (record: any) => {
+          return formatPriceRange(
+            record.pricing_min_total,
+            record.pricing_max_total,
+            {
+              currency: pricing?.currency
+            }
+          );
+        }
+      },
+      DateColumn({
+        accessor: 'pricing_updated',
+        title: t`Updated`,
+        sortable: true,
+        switchable: true
+      })
+    ];
+  }, [part, pricing]);
+
+  const bomPricingData: any[] = useMemo(() => {
+    const pricing = table.records.map((entry: any) => {
+      return {
+        name: entry.sub_part_detail?.name,
+        unit_price_min: parseFloat(entry.pricing_min ?? 0),
+        unit_price_max: parseFloat(entry.pricing_max ?? 0),
+        total_price_min: parseFloat(entry.pricing_min_total ?? 0),
+        total_price_max: parseFloat(entry.pricing_max_total ?? 0)
+      };
+    });
+
+    return pricing;
+  }, [table.records]);
+
+  const [chartType, setChartType] = useState<string>('pie');
+
+  return (
+    <Stack spacing="xs">
+      <SimpleGrid cols={2}>
+        <InvenTreeTable
+          tableState={table}
+          url={apiUrl(ApiEndpoints.bom_list)}
+          columns={columns}
+          props={{
+            params: {
+              part: part?.pk,
+              sub_part_detail: true,
+              has_pricing: true
+            },
+            enableSelection: false
+          }}
+        />
+        {bomPricingData.length > 0 ? (
+          <Stack spacing="xs">
+            {chartType == 'bar' && <BomBarChart data={bomPricingData} />}
+            {chartType == 'pie' && <BomPieChart data={bomPricingData} />}
+            <SegmentedControl
+              value={chartType}
+              onChange={setChartType}
+              data={[
+                { value: 'pie', label: t`Pie Chart` },
+                { value: 'bar', label: t`Bar Chart` }
+              ]}
+            />
+          </Stack>
+        ) : (
+          <NoPricingData />
+        )}
+      </SimpleGrid>
+    </Stack>
+  );
+}
