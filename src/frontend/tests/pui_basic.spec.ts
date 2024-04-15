cdd9d300918c7ab38189db8552595c9d5f437469
Matthias Mair <code@mjmair.com>
2024-04-15 21:39:06 +0200

    make test less flaky

--- src/frontend/tests/pui_basic.spec.ts
+++ src/frontend/tests/pui_basic.spec.ts
@@ -4,7 +4,7 @@ import { classicUrl, user } from './defaults.js';
 test('PUI - Basic test via django', async ({ page }) => {
   await page.goto(`${classicUrl}/platform/`);
   await expect(page).toHaveTitle('InvenTree Demo Server');
-  await page.waitForURL('**/platform/');
+  await page.waitForURL('**/platform/*');
   await page.getByLabel('username').fill(user.username);
   await page.getByLabel('password').fill(user.password);
   await page.getByRole('button', { name: 'Log in' }).click();
@@ -17,7 +17,7 @@ test('PUI - Basic test via django', async ({ page }) => {
 test('PUI - Basic test', async ({ page }) => {
   await page.goto('./platform/');
   await expect(page).toHaveTitle('InvenTree');
-  await page.waitForURL('**/platform/');
+  await page.waitForURL('**/platform/*');
   await page.getByLabel('username').fill(user.username);
   await page.getByLabel('password').fill(user.password);
   await page.getByRole('button', { name: 'Log in' }).click();
