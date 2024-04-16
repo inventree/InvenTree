b55f4542e7952dd69c0cd0cb9555ef72c4482981
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-16 10:05:35 +0000

    Trying something

--- src/frontend/tests/pui_basic.spec.ts
+++ src/frontend/tests/pui_basic.spec.ts
@@ -5,7 +5,7 @@ test('PUI - Basic test via django', async ({ page }) => {
   await page.goto(`${classicUrl}/platform/logout/`);
   await page.goto(`${classicUrl}/platform/login/`);
   await expect(page).toHaveTitle('InvenTree Demo Server');
-  await page.waitForURL('**/platform/');
+  await page.waitForURL('**/platform/login');
   await page.getByLabel('username').fill(user.username);
   await page.getByLabel('password').fill(user.password);
   await page.getByRole('button', { name: 'Log in' }).click();
@@ -19,7 +19,7 @@ test('PUI - Basic test', async ({ page }) => {
   await page.goto('./platform/logout/');
   await page.goto('./platform/login/');
   await expect(page).toHaveTitle('InvenTree');
-  await page.waitForURL('**/platform/');
+  await page.waitForURL('**/platform/login');
   await page.getByLabel('username').fill(user.username);
   await page.getByLabel('password').fill(user.password);
   await page.getByRole('button', { name: 'Log in' }).click();
