32ea476d5374d315549390b155e1714910a06155
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-16 07:02:59 +0000

    Cleanup

--- src/frontend/src/components/forms/AuthenticationForm.tsx
+++ src/frontend/src/components/forms/AuthenticationForm.tsx
@@ -12,8 +12,6 @@ import {
 } from '@mantine/core';
 import { useForm } from '@mantine/form';
 import { useDisclosure } from '@mantine/hooks';
-import { notifications } from '@mantine/notifications';
-import { IconCheck } from '@tabler/icons-react';
 import { useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 
