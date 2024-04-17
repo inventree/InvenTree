f08e0898e3adf2901842d1866b36143d0ed52bc1
Matthias Mair <code@mjmair.com>
2024-04-18 00:29:00 +0200

    add test for Dashboard

--- src/frontend/tests/pages/pui_index.spec.ts
+++ src/frontend/tests/pages/pui_index.spec.ts
@@ -74,3 +74,15 @@ test('PUI - Pages - Index - Playground', async ({ page }) => {
   await page.getByRole('textbox').fill('50');
   await page.getByText('Attention needed').waitFor();
 });
+
+test('PUI - Pages - Index - Dashboard', async ({ page }) => {
+  await doQuickLogin(page);
+  await page.goto(`${baseUrl}/`);
+
+  // Dashboard auto update
+  await page.getByRole('tab', { name: 'Dashboard' }).click();
+  await page.getByText('Autoupdate').click();
+  await page.waitForTimeout(500);
+  await page.getByText('Autoupdate').click();
+  await page.getByText('This page is a replacement').waitFor();
+});
