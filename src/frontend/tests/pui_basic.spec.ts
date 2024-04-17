d8422c082c7ccae0fb80780a107d1884bef499ca
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-17 00:49:29 +0000

    Fix basic tests

--- src/frontend/tests/pui_basic.spec.ts
+++ src/frontend/tests/pui_basic.spec.ts
@@ -1,30 +1,31 @@
 import { expect, test } from './baseFixtures.js';
 import { classicUrl, user } from './defaults.js';
 
+const BASE_URL = `${classicUrl}/platform`;
+
 test('PUI - Basic test via django', async ({ page }) => {
-  await page.goto(`${classicUrl}/platform/logout/`);
-  await page.goto(`${classicUrl}/platform/login/`);
-  await expect(page).toHaveTitle('InvenTree Demo Server');
-  await page.waitForURL('**/platform/login');
+  await page.goto(`${BASE_URL}/logout/`, { timeout: 5000 });
+  await page.goto(`${BASE_URL}/login/`, { timeout: 5000 });
+  await expect(page).toHaveTitle(RegExp('^InvenTree.*$'));
   await page.getByLabel('username').fill(user.username);
   await page.getByLabel('password').fill(user.password);
   await page.getByRole('button', { name: 'Log in' }).click();
   await page.waitForURL('**/platform/*');
-  await page.goto(`${classicUrl}/platform/`);
+  await page.goto(`${BASE_URL}/home`, { timeout: 5000 });
 
   await expect(page).toHaveTitle('InvenTree Demo Server');
 });
 
 test('PUI - Basic test', async ({ page }) => {
-  await page.goto('./platform/logout/');
-  await page.goto('./platform/login/');
-  await expect(page).toHaveTitle('InvenTree');
+  await page.goto(`${BASE_URL}/logout`);
+  await page.goto(`${BASE_URL}/login`);
+  await expect(page).toHaveTitle(RegExp('^InvenTree.*$'));
   await page.waitForURL('**/platform/login');
   await page.getByLabel('username').fill(user.username);
   await page.getByLabel('password').fill(user.password);
   await page.getByRole('button', { name: 'Log in' }).click();
   await page.waitForURL('**/platform/home');
-  await page.goto('./platform/');
+  await page.goto(`${BASE_URL}/home`);
 
-  await expect(page).toHaveTitle('InvenTree');
+  await expect(page).toHaveTitle(RegExp('^InvenTree'));
 });
