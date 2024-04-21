38a486688af191190dbbe998ff4a66118b3d87fa
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-21 12:25:04 +0000

    Provide model choices in admin interface

--- src/backend/InvenTree/report/admin.py
+++ src/backend/InvenTree/report/admin.py
@@ -2,6 +2,7 @@
 
 from django.contrib import admin
 
+from .helpers import report_model_options
 from .models import (
     BillOfMaterialsReport,
     BuildReport,
@@ -21,6 +22,13 @@ class ReportAdmin(admin.ModelAdmin):
     """Admin class for the ReportTemplate model."""
 
     list_display = ('name', 'description', 'model_type', 'enabled')
+
+    def formfield_for_dbfield(self, db_field, request, **kwargs):
+        """Provide custom choices for 'model_type' field."""
+        if db_field.name == 'model_type':
+            db_field.choices = report_model_options()
+
+        return super().formfield_for_dbfield(db_field, request, **kwargs)
 
 
 @admin.register(
