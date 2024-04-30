9d64bba8d398006d7ea197f5bf420e9c7be6b157
Oliver Walters <oliver.henry.walters@gmail.com>
2024-04-30 10:17:41 +0000

    Report printing works from PUI now!

--- src/frontend/src/components/buttons/PrintingActions.tsx
+++ src/frontend/src/components/buttons/PrintingActions.tsx
@@ -1,13 +1,15 @@
 import { t } from '@lingui/macro';
 import { ActionIcon, Menu, Tooltip } from '@mantine/core';
+import { notifications } from '@mantine/notifications';
 import { IconPrinter, IconReport, IconTags } from '@tabler/icons-react';
 import { useCallback, useMemo } from 'react';
 
 import { api } from '../../App';
 import { ApiEndpoints } from '../../enums/ApiEndpoints';
 import { ModelType } from '../../enums/ModelType';
-import { useApiFormModal, useCreateApiFormModal } from '../../hooks/UseForm';
+import { useCreateApiFormModal } from '../../hooks/UseForm';
 import { apiUrl } from '../../states/ApiState';
+import { useLocalState } from '../../states/LocalState';
 
 export function PrintingActions({
   items,
@@ -20,6 +22,8 @@ export function PrintingActions({
   enableReports?: boolean;
   modelType?: ModelType;
 }) {
+  const { host } = useLocalState.getState();
+
   const enabled = useMemo(() => items.length > 0, [items]);
 
   const printLabels = useCallback(() => {
@@ -35,15 +39,6 @@ export function PrintingActions({
 
   const printReports = useCallback(() => {
     reportModal.open();
-
-    // // Fetch available report templates
-    // api.get(apiUrl(ApiEndpoints.report_list), {
-    //   params: {
-    //     enabled: true,
-    //     model_type: modelType,
-    //     items: items.join(',')
-    //   }
-    // });
   }, [items]);
 
   if (!modelType) {
@@ -69,6 +64,30 @@ export function PrintingActions({
       items: {
         hidden: true,
         value: items
+      }
+    },
+    onFormSuccess: (response: any) => {
+      if (!response.complete) {
+        // TODO: Periodically check for completion
+        notifications.show({
+          title: t`Error`,
+          message: t`The report could not be generated`,
+          color: 'red'
+        });
+        return;
+      }
+
+      notifications.show({
+        title: t`Success`,
+        message: t`Report printing completed successfully`,
+        color: 'green'
+      });
+
+      if (response.output) {
+        // An output file was generated
+        const url = `${host}${response.output}`;
+        console.log('url:', response.output, '->', url);
+        window.open(url, '_blank');
       }
     }
   });
