c5a27e63d201363097e9ea514f8d2b4dc457301a
Matthias Mair <code@mjmair.com>
2024-05-01 17:38:43 +0200

    fix test

--- src/frontend/tests/modals.spec.ts
+++ src/frontend/tests/modals.spec.ts
@@ -8,7 +8,7 @@ test('PUI - Modals as admin', async ({ page }) => {
   await page.getByRole('button', { name: 'Open spotlight' }).click();
   await page
     .getByRole('button', {
-      name: 'Server Information About this Inventree instance'
+      name: 'About this Inventree instance'
     })
     .click();
   await page.getByRole('cell', { name: 'Instance Name' }).waitFor();
@@ -20,7 +20,7 @@ test('PUI - Modals as admin', async ({ page }) => {
   await page.getByRole('button', { name: 'Open spotlight' }).click();
   await page
     .getByRole('button', {
-      name: 'License Information Licenses for dependencies of the service'
+      name: 'Licenses for dependencies of the service'
     })
     .click();
   await page.getByText('License Information').first().waitFor();
@@ -31,7 +31,7 @@ test('PUI - Modals as admin', async ({ page }) => {
   await page.getByRole('tab', { name: 'frontend Packages' }).click();
   await page.getByRole('button', { name: '@sentry/utils MIT' }).click();
   await page
-    .getByLabel('@sentry/utilsMIT7.109.0')
+    .getByLabel('@sentry/utilsMIT')
     .getByText('Copyright (c) 2019 Sentry (')
     .waitFor();
 
@@ -43,27 +43,17 @@ test('PUI - Modals as admin', async ({ page }) => {
 
   // use about
   await page.getByRole('button', { name: 'Open spotlight' }).click();
-  await page
-    .getByRole('button', { name: 'About InvenTree About the InvenTree org' })
-    .click();
+  await page.getByRole('button', { name: 'About the InvenTree org' }).click();
   await page.getByRole('cell', { name: 'InvenTree Version' }).click();
 
   await page.goto('./platform/');
 
   // qr code modal
   await page.getByRole('button', { name: 'Open QR code scanner' }).click();
-  await page
-    .locator('div')
-    .filter({ hasText: /^Scan QR code$/ })
-    .getByRole('button')
-    .click();
+  await page.getByRole('banner').getByRole('button').click();
   await page.getByRole('button', { name: 'Open QR code scanner' }).click();
   await page.getByRole('button', { name: 'Close modal' }).click();
   await page.getByRole('button', { name: 'Open QR code scanner' }).click();
   await page.waitForTimeout(500);
-  await page
-    .locator('div')
-    .filter({ hasText: /^Scan QR code$/ })
-    .getByRole('button')
-    .click();
+  await page.getByRole('banner').getByRole('button').click();
 });
