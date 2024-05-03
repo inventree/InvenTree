5784eda2b224b69c9fbac045c920211bd8b704ca
Oliver Walters <oliver.henry.walters@gmail.com>
2024-05-03 09:48:17 +0000

    Improve query efficiency for build endpoints

--- src/backend/InvenTree/build/api.py
+++ src/backend/InvenTree/build/api.py
@@ -103,15 +103,31 @@ class BuildFilter(rest_filters.FilterSet):
         return queryset.filter(project_code=None)
 
 
-class BuildList(APIDownloadMixin, ListCreateAPI):
+class BuildMixin:
+    """Mixin class for Build API endpoints."""
+
+    queryset = Build.objects.all()
+    serializer_class = build.serializers.BuildSerializer
+
+    def get_queryset(self):
+        """Return the queryset for the Build API endpoints."""
+        queryset = super().get_queryset()
+
+        queryset = queryset.prefetch_related(
+            'responsible',
+            'issued_by',
+        )
+
+        return queryset
+
+
+class BuildList(APIDownloadMixin, BuildMixin, ListCreateAPI):
     """API endpoint for accessing a list of Build objects.
 
     - GET: Return list of objects (with filters)
     - POST: Create a new Build object
     """
 
-    queryset = Build.objects.all()
-    serializer_class = build.serializers.BuildSerializer
     filterset_class = BuildFilter
 
     filter_backends = SEARCH_ORDER_FILTER_ALIAS
@@ -223,11 +239,8 @@ class BuildList(APIDownloadMixin, ListCreateAPI):
         return self.serializer_class(*args, **kwargs)
 
 
-class BuildDetail(RetrieveUpdateDestroyAPI):
+class BuildDetail(BuildMixin, RetrieveUpdateDestroyAPI):
     """API endpoint for detail view of a Build object."""
-
-    queryset = Build.objects.all()
-    serializer_class = build.serializers.BuildSerializer
 
     def destroy(self, request, *args, **kwargs):
         """Only allow deletion of a BuildOrder if the build status is CANCELLED"""
