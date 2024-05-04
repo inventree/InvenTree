39f507743d86933945bc6988f985a800ff9f7bc6
Oliver Walters <oliver.henry.walters@gmail.com>
2024-05-04 04:09:26 +0000

    Remove outdated comment

--- src/backend/InvenTree/InvenTree/settings.py
+++ src/backend/InvenTree/InvenTree/settings.py
@@ -269,7 +269,6 @@ MIDDLEWARE = CONFIG.get(
 )
 
 # In DEBUG mode, add support for django-querycount
-# We also force this option when in testing mode
 # Ref: https://github.com/bradmontgomery/django-querycount
 if DEBUG and get_boolean_setting(
     'INVENTREE_DEBUG_QUERYCOUNT', 'debug_querycount', False
