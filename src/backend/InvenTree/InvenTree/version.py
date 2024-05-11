4095e84990f4ef59dd073d9d8bd17558c9e90cd4
Oliver <oliver.henry.walters@gmail.com>
2024-05-11 12:25:47 +1000

    Revert "Fixed the app/app link to point to the latest app version. (#7195)"
    This reverts commit b88457a39e77f72f39ecb5ea82cb3975177f222b.

--- src/backend/InvenTree/InvenTree/version.py
+++ src/backend/InvenTree/InvenTree/version.py
@@ -104,7 +104,7 @@ def inventreeDocUrl():
 
 def inventreeAppUrl():
     """Return URL for InvenTree app site."""
-    return f'https://docs.inventree.org/en/latest/app/app/'
+    return f'{inventreeDocUrl()}/app/app/'
 
 
 def inventreeCreditsUrl():
