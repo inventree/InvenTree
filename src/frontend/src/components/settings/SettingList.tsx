2fa0517c5e220b4df62ac65f4aab5cfe57708f17
Oliver Walters <oliver.henry.walters@gmail.com>
2024-05-16 00:21:26 +0000

    Refactor settings management:
    - Use proper hooked form
    - Reduce code duplication
    - Run single callback for each <SettingList>

--- src/frontend/src/components/settings/SettingList.tsx
+++ src/frontend/src/components/settings/SettingList.tsx
@@ -1,8 +1,19 @@
-import { Trans } from '@lingui/macro';
+import { Trans, t } from '@lingui/macro';
 import { Stack, Text } from '@mantine/core';
-import React, { useEffect, useMemo, useRef } from 'react';
+import { notifications } from '@mantine/notifications';
+import React, {
+  useCallback,
+  useEffect,
+  useMemo,
+  useRef,
+  useState
+} from 'react';
 import { useStore } from 'zustand';
 
+import { api } from '../../App';
+import { ModelType } from '../../enums/ModelType';
+import { useEditApiFormModal } from '../../hooks/UseForm';
+import { apiUrl } from '../../states/ApiState';
 import {
   SettingsStateProps,
   createMachineSettingsState,
@@ -10,6 +21,8 @@ import {
   useGlobalSettingsState,
   useUserSettingsState
 } from '../../states/SettingsState';
+import { Setting } from '../../states/states';
+import { ApiFormFieldType } from '../forms/fields/ApiFormField';
 import { SettingItem } from './SettingItem';
 
 /**
@@ -33,8 +46,80 @@ export function SettingList({
     [settingsState?.settings]
   );
 
+  const [setting, setSetting] = useState<Setting | undefined>(undefined);
+
+  const editSettingModal = useEditApiFormModal({
+    url: settingsState.endpoint,
+    pk: setting?.key,
+    pathParams: settingsState.pathParams,
+    title: t`Edit Setting`,
+    fields: {
+      value: {
+        value: setting?.value ?? '',
+        field_type:
+          setting?.type ?? setting?.choices ?? 0 > 0 ? 'choice' : 'string',
+        label: setting?.name,
+        description: setting?.description,
+        api_url: setting?.api_url ?? '',
+        model: (setting?.model_name?.split('.')[1] as ModelType) ?? null,
+        choices: setting?.choices ?? undefined
+      }
+    },
+    successMessage: t`Setting ${setting?.key} updated successfully`,
+    onFormSuccess: () => {
+      settingsState.fetchSettings();
+      onChange?.();
+    }
+  });
+
+  // Callback for editing a single setting instance
+  const onValueEdit = useCallback(
+    (setting: Setting) => {
+      setSetting(setting);
+      editSettingModal.open();
+    },
+    [editSettingModal]
+  );
+
+  // Callback for toggling a single boolean setting instance
+  const onValueToggle = useCallback(
+    (setting: Setting, value: boolean) => {
+      api
+        .patch(
+          apiUrl(settingsState.endpoint, setting.key, settingsState.pathParams),
+          {
+            value: value
+          }
+        )
+        .then(() => {
+          notifications.hide('setting');
+          notifications.show({
+            title: t`Setting updated`,
+            message: t`Setting ${setting.key} updated successfully`,
+            color: 'green',
+            id: 'setting'
+          });
+          onChange?.();
+        })
+        .catch((error) => {
+          notifications.hide('setting');
+          notifications.show({
+            title: t`Error editing setting`,
+            message: error.message,
+            color: 'red',
+            id: 'setting'
+          });
+        })
+        .finally(() => {
+          settingsState.fetchSettings();
+        });
+    },
+    [settingsState]
+  );
+
   return (
     <>
+      {editSettingModal.modal}
       <Stack gap="xs">
         {(keys || allKeys).map((key, i) => {
           const setting = settingsState?.settings?.find(
@@ -48,7 +133,8 @@ export function SettingList({
                   settingsState={settingsState}
                   setting={setting}
                   shaded={i % 2 === 0}
-                  onChange={onChange}
+                  onEdit={onValueEdit}
+                  onToggle={onValueToggle}
                 />
               ) : (
                 <Text size="sm" style={{ fontStyle: 'italic' }} color="red">
