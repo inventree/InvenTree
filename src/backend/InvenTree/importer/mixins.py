a6b9416ba1eba8e81e2f7eed996783a5c495bc58
Oliver Walters <oliver.henry.walters@gmail.com>
2024-06-08 09:18:49 +0000

    Add shim class to process an individual row before exporting it

--- src/backend/InvenTree/importer/mixins.py
+++ src/backend/InvenTree/importer/mixins.py
@@ -9,57 +9,6 @@ import importer.operations
 from InvenTree.helpers import DownloadFile, GetExportFormats, current_date
 
 
-class DataExportViewMixin:
-    """Mixin class for exporting a dataset via the API.
-
-    Adding this mixin to an API view allows the user to export the dataset to file in a variety of formats.
-
-    We achieve this by overriding the 'get' method, and checking for the presence of the required query parameter.
-    """
-
-    EXPORT_QUERY_PARAMETER = 'export'
-
-    def export_data(self, export_format):
-        """Export the data in the specified format.
-
-        Use the provided serializer to generate the data, and return it as a file download.
-        """
-        serializer_class = self.get_serializer_class()
-
-        if not issubclass(serializer_class, DataExportSerializerMixin):
-            raise TypeError(
-                'Serializer class must inherit from DataExportSerialierMixin'
-            )
-
-        queryset = self.filter_queryset(self.get_queryset())
-
-        serializer = serializer_class(exporting=True)
-        serializer.initial_data = queryset
-
-        # Export dataset with a second copy of the serializer
-        # This is because when we pass many=True, the returned class is a ListSerializer
-        data = serializer_class(queryset, many=True, exporting=True).data
-
-        filename = serializer.get_exported_filename(export_format)
-        datafile = serializer.export_to_file(data, export_format)
-
-        return DownloadFile(datafile, filename=filename)
-
-    def get(self, request, *args, **kwargs):
-        """Override the 'get' method to check for the export query parameter."""
-        if export_format := request.query_params.get(self.EXPORT_QUERY_PARAMETER, None):
-            export_format = str(export_format).strip().lower()
-            if export_format in GetExportFormats():
-                return self.export_data(export_format)
-            else:
-                raise ValidationError({
-                    self.EXPORT_QUERY_PARAMETER: _('Invalid export format')
-                })
-
-        # If the export query parameter is not present, return the default response
-        return super().get(request, *args, **kwargs)
-
-
 class DataImportSerializerMixin:
     """Mixin class for adding data import functionality to a DRF serializer."""
 
@@ -118,14 +67,6 @@ class DataExportSerializerMixin:
 
         super().__init__(*args, **kwargs)
 
-        print(
-            'DataExportSerializerMixin:',
-            self.__class__.__name__,
-            ':',
-            'exporting =',
-            exporting,
-        )
-
         if exporting:
             # Exclude fields which are not required for data export
             for field in self.get_export_exclude_fields(**kwargs):
@@ -174,6 +115,10 @@ class DataExportSerializerMixin:
         """Optional method to arrange the export headers."""
         return headers
 
+    def process_row(self, row):
+        """Optional method to process a row before exporting it."""
+        return row
+
     def export_to_file(self, data, file_format):
         """Export the queryset to a file in the specified format.
 
@@ -201,6 +146,7 @@ class DataExportSerializerMixin:
         dataset = tablib.Dataset(headers=headers)
 
         for row in data:
+            row = self.process_row(row)
             dataset.append([row.get(field, None) for field in field_names])
 
         return dataset.export(file_format)
@@ -212,3 +158,54 @@ class DataImportExportSerializerMixin(
     """Mixin class for adding data import/export functionality to a DRF serializer."""
 
     pass
+
+
+class DataExportViewMixin:
+    """Mixin class for exporting a dataset via the API.
+
+    Adding this mixin to an API view allows the user to export the dataset to file in a variety of formats.
+
+    We achieve this by overriding the 'get' method, and checking for the presence of the required query parameter.
+    """
+
+    EXPORT_QUERY_PARAMETER = 'export'
+
+    def export_data(self, export_format):
+        """Export the data in the specified format.
+
+        Use the provided serializer to generate the data, and return it as a file download.
+        """
+        serializer_class = self.get_serializer_class()
+
+        if not issubclass(serializer_class, DataExportSerializerMixin):
+            raise TypeError(
+                'Serializer class must inherit from DataExportSerialierMixin'
+            )
+
+        queryset = self.filter_queryset(self.get_queryset())
+
+        serializer = serializer_class(exporting=True)
+        serializer.initial_data = queryset
+
+        # Export dataset with a second copy of the serializer
+        # This is because when we pass many=True, the returned class is a ListSerializer
+        data = serializer_class(queryset, many=True, exporting=True).data
+
+        filename = serializer.get_exported_filename(export_format)
+        datafile = serializer.export_to_file(data, export_format)
+
+        return DownloadFile(datafile, filename=filename)
+
+    def get(self, request, *args, **kwargs):
+        """Override the 'get' method to check for the export query parameter."""
+        if export_format := request.query_params.get(self.EXPORT_QUERY_PARAMETER, None):
+            export_format = str(export_format).strip().lower()
+            if export_format in GetExportFormats():
+                return self.export_data(export_format)
+            else:
+                raise ValidationError({
+                    self.EXPORT_QUERY_PARAMETER: _('Invalid export format')
+                })
+
+        # If the export query parameter is not present, return the default response
+        return super().get(request, *args, **kwargs)
