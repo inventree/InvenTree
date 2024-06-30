29e675576c47bc1efe500a119954c38e9b9ba36e
LavissaWoW <lavissawow@gmail.com>
2024-06-30 18:44:56 +0000

    .

--- src/frontend/tests/pages/pui_purchasing.spec.ts
+++ src/frontend/tests/pages/pui_purchasing.spec.ts
@@ -1,16 +1,10 @@
+import { Page } from '@playwright/test';
+
 import { test } from '../baseFixtures';
 import { adminuser, baseUrl } from '../defaults';
 import { doLogin, doQuickLogin } from '../login';
 
-test('PUI - Pages - Purchasing - Detail', async ({ page }) => {
-  await doQuickLogin(page);
-
-  // All state altering settings disabled
-  await page.goto(`${baseUrl}/purchasing/purchase-order/12/detail`);
-  await page.getByText('PO0012').waitFor();
-  await page.getByRole('button', { name: 'Issue Order' }).isEnabled();
-
-  // Enable Ready setting
+async function enableReady(page: Page) {
   await doLogin(page, adminuser.username, adminuser.password);
   await page.goto(`${baseUrl}/settings/system/purchaseorders`);
   await page.getByText('Enable Ready Status').waitFor();
@@ -18,9 +12,69 @@ test('PUI - Pages - Purchasing - Detail', async ({ page }) => {
     .locator('div', { hasText: 'Enable Ready Status' })
     .getByRole('switch')
     .click();
+}
 
-  await doLogin(page);
+async function enableApprovals(page: Page) {
+  await doLogin(page, adminuser.username, adminuser.password);
+  await page.goto(`${baseUrl}/settings/system/purchaseorders`);
+  await page.getByText('Purchase Order Approvals').waitFor();
+  await page
+    .locator('div', { hasText: 'Purchase Order Approvals' })
+    .getByRole('switch')
+    .click();
+}
+
+async function setApprover(page: Page) {
+  await doLogin(page, adminuser.username, adminuser.password);
+  await page.goto(`${baseUrl}/settings/system/purchaseorders`);
+  await page.getByText('Master approval group').waitFor();
+  await page
+    .locator('div', { hasText: 'Master approval group' })
+    .getByRole('button')
+    .click();
+  await page.getByLabel('Master approval group').fill('all access');
+  await page.getByRole('button', { name: 'Submit' }).click();
+}
+
+test('PUI - Pages - Purchasing - Pending transitions', async ({ page }) => {
+  await doQuickLogin(page);
+
+  // All state altering settings disabled
   await page.goto(`${baseUrl}/purchasing/purchase-order/12/detail`);
-  await page.getByText('PO0012').waitFor();
+  await page.getByText('Purchase Order: PO0012').waitFor();
+  await page.getByRole('button', { name: 'Issue Order' }).isEnabled();
+
+  enableReady(page);
+
+  // Check that Ready Button is present
+  await page.goto(`${baseUrl}/purchasing/purchase-order/12/detail`);
+  await page.getByText('Purchase Order: PO0012').waitFor();
   await page.getByRole('button', { name: 'Ready' }).isEnabled();
+
+  await enableApprovals(page);
+
+  await page.goto(`${baseUrl}/purchasing/purchase-order/12/detail`);
+  await page.getByText('Purchase Order: PO0012').waitFor();
+  await page.getByRole('button', { name: 'Request Approval' }).isEnabled();
+});
+
+test('PUI - Pages - Purchasing - In Approval transitions', async ({ page }) => {
+  await enableApprovals(page);
+
+  await page.goto(`${baseUrl}/purchasing/purchase-order/12/detail`);
+  await page.getByText('Purchase Order: PO0012').waitFor();
+  await page.getByRole('button', { name: 'Request Approval' }).click();
+
+  await page.getByText('Request approval of order').waitFor();
+  await page.getByRole('button', { name: 'Submit' }).click();
+
+  await page.getByText('Purchase Order: PO0012').waitFor();
+  await page.getByRole('button', { name: 'Approve' }).isDisabled();
+  await page.getByRole('button', { name: 'Recall' }).isEnabled();
+
+  await setApprover(page);
+
+  await page.goto(`${baseUrl}/purchasing/purchase-order/12/detail`);
+  await page.getByRole('button', { name: 'Approve' }).isEnabled();
+  await page.getByRole('button', { name: 'Reject' }).isEnabled();
 });
