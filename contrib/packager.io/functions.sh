ecf232bffaae226f03f71f64a3863f0a935f0d4f
Matthias Mair <code@mjmair.com>
2024-07-04 02:00:06 +0200

    fix missing var assigment

--- contrib/packager.io/functions.sh
+++ contrib/packager.io/functions.sh
@@ -344,8 +344,8 @@ function final_message() {
 
 function update_checks() {
   echo "# Running upgrade"
-  old_version = $1
-  old_version_rev = $(echo $old_version | cut -d' ' -f2)
+  local old_version = $1
+  local old_version_rev = $(echo $old_version | cut -d' ' -f2)
   echo "# Old version is: ${old_version} - release: ${old_version_rev}"
 
   local ABORT=false
