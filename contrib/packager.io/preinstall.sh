7dcbebe7a9a7654f9e622ac0c26436b33552f67a
Matthias Mair <code@mjmair.com>
2024-07-08 10:47:04 +0200

    Update preinstall.sh

--- contrib/packager.io/preinstall.sh
+++ contrib/packager.io/preinstall.sh
@@ -4,10 +4,14 @@
 #
 PATH=${APP_HOME}/env/bin:${APP_HOME}/:/sbin:/bin:/usr/sbin:/usr/bin:
 
-# import functions
-. ${APP_HOME}/contrib/packager.io/functions.sh
-
 # Envs that should be passed to setup commands
 export SETUP_ENVS=PATH,APP_HOME,INVENTREE_MEDIA_ROOT,INVENTREE_STATIC_ROOT,INVENTREE_BACKUP_DIR,INVENTREE_PLUGINS_ENABLED,INVENTREE_PLUGIN_FILE,INVENTREE_CONFIG_FILE,INVENTREE_SECRET_KEY_FILE,INVENTREE_DB_ENGINE,INVENTREE_DB_NAME,INVENTREE_DB_USER,INVENTREE_DB_PASSWORD,INVENTREE_DB_HOST,INVENTREE_DB_PORT,INVENTREE_ADMIN_USER,INVENTREE_ADMIN_EMAIL,INVENTREE_ADMIN_PASSWORD,SETUP_NGINX_FILE,SETUP_ADMIN_PASSWORD_FILE,SETUP_NO_CALLS,SETUP_DEBUG,SETUP_EXTRA_PIP,SETUP_PYTHON
 
-clear_precompiled
+if test -f "${APP_HOME}/env/bin/pip"; then
+  echo "# Clearing precompiled files"
+else
+  echo "# No python environment found - skipping"
+  return
+fi
+
+sudo -u ${APP_USER} --preserve-env=$SETUP_ENVS bash -c "cd ${APP_HOME} && invoke clear-generated"
