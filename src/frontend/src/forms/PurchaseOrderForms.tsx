97f9c727900be4bdeafbdf214e27815e1ce96f4f
Matthias Mair <code@mjmair.com>
2024-07-13 18:31:07 +0200

    Do not pass children as props

--- src/frontend/src/forms/PurchaseOrderForms.tsx
+++ src/frontend/src/forms/PurchaseOrderForms.tsx
@@ -319,7 +319,7 @@ function LineItemFormRow({
       <Modal
         opened={opened}
         onClose={close}
-        title={<StylishText children={t`Scan Barcode`} />}
+        title={<StylishText>{t`Scan Barcode`}</StylishText>}
       >
         <FocusTrap>
           <TextInput
