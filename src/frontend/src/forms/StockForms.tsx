2d1bfbfbd56fbcd4c45c44699d9888dee44978fc
Matthias Mair <code@mjmair.com>
2024-07-13 18:25:02 +0200

    clenaup empty object patterns

--- src/frontend/src/forms/StockForms.tsx
+++ src/frontend/src/forms/StockForms.tsx
@@ -850,7 +850,7 @@ export function useDeleteStockItem(props: StockOperationProps) {
   });
 }
 
-export function stockLocationFields({}: {}): ApiFormFieldSet {
+export function stockLocationFields(): ApiFormFieldSet {
   let fields: ApiFormFieldSet = {
     parent: {
       description: t`Parent stock location`,
