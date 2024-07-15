9da45c7576916e3056c6195ffa8c21ddb7cdb53e
Matthias Mair <code@mjmair.com>
2024-07-15 23:54:10 +0200

    Matmair/issue7338 (#205)
    * Package frontend in deb
    
    * Add artifact download
    
    * remove 0.8.0 check
    
    * remove array casting
    
    * fix format once more
    
    * another try
    
    * add brackets again
    
    * add version
    
    * and bash
    
    * and shell
    
    * more debuging
    
    * various style fixes
    
    * small fixes
    
    * and ls for prosperity
    
    * debug
    
    * maybe git as source?
    
    * fix download cmd?
    
    * debug a bit
    
    * debug a bit more
    
    * remove sha download - is not working with GHA restrictions
    
    * write version number
    
    * check if a new frontend must be dowloaded
    
    * write versions into frontend packages

--- contrib/packager.io/before.sh
+++ contrib/packager.io/before.sh
@@ -5,6 +5,7 @@
 
 set -eu
 
+REPO="InvenTree/InvenTree"
 VERSION="$APP_PKG_VERSION-$APP_PKG_ITERATION"
 echo "Setting VERSION information to $VERSION"
 echo "$VERSION" > VERSION
@@ -17,11 +18,15 @@ echo "Getting info from github for commit $SHA"
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/InvenTree/InvenTree/commits/$SHA > commit.json
+  https://api.github.com/repos/$REPO/commits/$SHA > commit.json
 curl -L \
   -H "Accept: application/vnd.github+json" \
   -H "X-GitHub-Api-Version: 2022-11-28" \
-  https://api.github.com/repos/InvenTree/InvenTree/commits/$SHA/branches-where-head > branches.json
+  https://api.github.com/repos/$REPO/commits/$SHA/branches-where-head > branches.json
+curl -L \
+  -H "Accept: application/vnd.github+json" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://api.github.com/repos/$REPO/commits/$APP_PKG_VERSION > tag.json
 
 # Extract info
 echo "Extracting info from github"
@@ -29,6 +34,7 @@ DATE=$(jq -r '.commit.committer.date' commit.json)
 BRANCH=$(jq -r '.[].name' branches.json)
 NODE_ID=$(jq -r '.node_id' commit.json)
 SIGNATURE=$(jq -r '.commit.verification.signature' commit.json)
+FULL_SHA=$(jq -r '.sha' commit.json)
 
 echo "Write VERSION information"
 echo "INVENTREE_COMMIT_HASH='$SHA'" >> VERSION
@@ -41,3 +47,15 @@ echo "SIGNATURE='$SIGNATURE'" >> VERSION
 
 echo "Written VERSION information"
 cat VERSION
+
+# Try to get frontend
+# Check if tag sha is the same as the commit sha
+TAG_SHA=$(jq -r '.sha' tag.json)
+if [ "$TAG_SHA" != "$FULL_SHA" ]; then
+  echo "Tag sha is not the same as commit sha, can not download frontend"
+else
+  echo "Getting frontend from github via tag"
+  curl https://github.com/$REPO/releases/download/$APP_PKG_VERSION/frontend-build.zip -L frontend.zip
+  unzip frontend.zip -d src/backend/InvenTree/web/static/web
+  echo "Unzipped frontend"
+fi
