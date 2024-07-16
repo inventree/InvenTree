5543709197ec7c6c8f58ecf1fa67cb0f99d0eebe
Matthias Mair <code@mjmair.com>
2024-07-16 02:30:49 +0200

    adjust publisher

--- contrib/packager.io/before.sh
+++ contrib/packager.io/before.sh
@@ -6,7 +6,7 @@
 set -eu
 
 # The sha is the second element in APP_PKG_ITERATION
-REPO="matmair/InvenTree"
+REPO="inventree/InvenTree"
 VERSION="$APP_PKG_VERSION-$APP_PKG_ITERATION"
 SHA=$(echo $APP_PKG_ITERATION | cut -d'.' -f2)
 
