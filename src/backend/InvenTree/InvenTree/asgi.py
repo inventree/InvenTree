6f432c30e5812908fb230aface0ab4ba1739a8c3
Matthias Mair <code@mjmair.com>
2024-07-28 21:24:23 +0200

    cleanup code following sonar scan reports

--- src/backend/InvenTree/InvenTree/asgi.py
+++ src/backend/InvenTree/InvenTree/asgi.py
@@ -8,7 +8,6 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/asgi/
 
 import os
 
-# from django.conf import settings
 from django.core.asgi import get_asgi_application
 
 from channels.auth import AuthMiddlewareStack
@@ -20,9 +19,6 @@ os.environ.setdefault(
 )  # pragma: no cover
 
 from InvenTree import routing  # noqa: E402
-
-# Set up django
-# settings.configure()
 
 application = ProtocolTypeRouter({
     'http': get_asgi_application(),
