3b269b0967d2f0fe8558e90400f6944c543a2ec6
Oliver Walters <oliver.henry.walters@gmail.com>
2024-08-01 06:54:40 +0000

    Implement sales order line item table

---
+++ src/frontend/src/tables/sales/SalesOrderLineItemTable.tsx
@@ -0,0 +1,173 @@
+import { t } from '@lingui/macro';
+import { Text } from '@mantine/core';
+import { ReactNode, useCallback, useMemo } from 'react';
+
+import { ProgressBar } from '../../components/items/ProgressBar';
+import { formatCurrency } from '../../defaults/formatters';
+import { ApiEndpoints } from '../../enums/ApiEndpoints';
+import { ModelType } from '../../enums/ModelType';
+import { useTable } from '../../hooks/UseTable';
+import { apiUrl } from '../../states/ApiState';
+import { useUserState } from '../../states/UserState';
+import { TableColumn } from '../Column';
+import { DateColumn, PartColumn } from '../ColumnRenderers';
+import { InvenTreeTable } from '../InvenTreeTable';
+import { TableHoverCard } from '../TableHoverCard';
+
+export default function SalesOrderLineItemTable({
+  orderId
+}: {
+  orderId: number;
+}) {
+  const user = useUserState();
+  const table = useTable('sales-order-line-item');
+
+  const tableColumns: TableColumn[] = useMemo(() => {
+    return [
+      {
+        accessor: 'part',
+        sortable: true,
+        switchable: false,
+        render: (record: any) => PartColumn(record?.part_detail)
+      },
+      {
+        accessor: 'part_detail.IPN',
+        title: t`IPN`,
+        switchable: true
+      },
+      {
+        accessor: 'part_detail.description',
+        title: t`Description`,
+        sortable: false,
+        switchable: true
+      },
+      {
+        accessor: 'reference',
+        sortable: false,
+        switchable: true
+      },
+      {
+        accessor: 'quantity',
+        sortable: true
+      },
+      {
+        accessor: 'sale_price',
+        render: (record: any) =>
+          formatCurrency(record.sale_price, {
+            currency: record.sale_price_currency
+          })
+      },
+      {
+        accessor: 'total_price',
+        title: t`Total Price`,
+        render: (record: any) =>
+          formatCurrency(record.sale_price, {
+            currency: record.sale_price_currency,
+            multiplier: record.quantity
+          })
+      },
+      DateColumn({
+        accessor: 'target_date',
+        sortable: true,
+        title: t`Target Date`
+      }),
+      {
+        accessor: 'stock',
+        title: t`Available Stock`,
+        render: (record: any) => {
+          let part_stock = record?.available_stock ?? 0;
+          let variant_stock = record?.available_variant_stock ?? 0;
+          let available = part_stock + variant_stock;
+
+          let required = Math.max(
+            record.quantity - record.allocated - record.shipped,
+            0
+          );
+
+          let color: string | undefined = undefined;
+          let text: string = `${available}`;
+
+          let extra: ReactNode[] = [];
+
+          if (available <= 0) {
+            color = 'red';
+            text = t`No stock available`;
+          } else if (available < required) {
+            color = 'orange';
+          }
+
+          if (variant_stock > 0) {
+            extra.push(<Text size="sm">{t`Includes variant stock`}</Text>);
+          }
+
+          return (
+            <TableHoverCard
+              value={<Text color={color}>{text}</Text>}
+              extra={extra}
+              title={t`Stock Information`}
+            />
+          );
+        }
+      },
+      {
+        accessor: 'allocated',
+        render: (record: any) => (
+          <ProgressBar
+            progressLabel={true}
+            value={record.allocated}
+            maximum={record.quantity}
+          />
+        )
+      },
+      {
+        accessor: 'shipped',
+        render: (record: any) => (
+          <ProgressBar
+            progressLabel={true}
+            value={record.shipped}
+            maximum={record.quantity}
+          />
+        )
+      },
+      {
+        accessor: 'notes'
+      },
+      {
+        accessor: 'link'
+      }
+    ];
+  }, []);
+
+  const tableActions = useMemo(() => {
+    return [];
+  }, [user]);
+
+  const rowActions = useCallback(
+    (record: any) => {
+      return [];
+    },
+    [user]
+  );
+
+  return (
+    <>
+      <InvenTreeTable
+        url={apiUrl(ApiEndpoints.sales_order_line_list)}
+        tableState={table}
+        columns={tableColumns}
+        props={{
+          enableSelection: true,
+          enableDownload: true,
+          params: {
+            order: orderId,
+            part_detail: true
+          },
+          rowActions: rowActions,
+          tableActions: tableActions,
+          modelType: ModelType.part,
+          modelField: 'part'
+        }}
+      />
+    </>
+  );
+}
