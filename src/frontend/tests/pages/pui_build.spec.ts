5c08f5749d357272bbeac8ac30fed79d503752f7
Oliver Walters <oliver.henry.walters@gmail.com>
2024-08-05 20:33:21 +1000

    Playwright tests for build order

--- src/frontend/tests/pages/pui_build.spec.ts
+++ src/frontend/tests/pages/pui_build.spec.ts
@@ -9,7 +9,42 @@ test('PUI - Pages - Build Order', async ({ page }) => {
 
   // Navigate to the correct build order
   await page.getByRole('tab', { name: 'Build', exact: true }).click();
+
+  // We have now loaded the "Build Order" table. Check for some expected texts
+  await page.getByText('On Hold').waitFor();
+  await page.getByText('Pending').first().waitFor();
+
+  // Load a particular build order
+  await page.getByRole('cell', { name: 'BO0017' }).click();
+
+  // This build order should be "on hold"
+  await page.getByText('On Hold').first().waitFor();
+  await page.getByRole('button', { name: 'Issue Order' }).click();
+  await page.getByRole('button', { name: 'Cancel' }).click();
+
+  await page.waitForTimeout(5000);
+  return;
+
+  // Back to the build list
+  await page.getByLabel('breadcrumb-0-build-orders').click();
+
+  // Load a different build order
   await page.getByRole('cell', { name: 'BO0011' }).click();
+
+  // This build order should be "in production"
+  await page.getByText('Production').first().waitFor();
+  await page.getByRole('button', { name: 'Complete Order' }).click();
+  await page.getByText('Accept Unallocated').waitFor();
+  await page.getByRole('button', { name: 'Cancel' }).click();
+
+  // Check for other expected actions
+  await page.getByLabel('action-menu-build-order-').click();
+  await page.getByLabel('action-menu-build-order-actions-edit').waitFor();
+  await page.getByLabel('action-menu-build-order-actions-duplicate').waitFor();
+  await page.getByLabel('action-menu-build-order-actions-hold').waitFor();
+  await page.getByLabel('action-menu-build-order-actions-cancel').click();
+  await page.getByText('Remove Incomplete Outputs').waitFor();
+  await page.getByRole('button', { name: 'Cancel' }).click();
 
   // Click on some tabs
   await page.getByRole('tab', { name: 'Attachments' }).click();
