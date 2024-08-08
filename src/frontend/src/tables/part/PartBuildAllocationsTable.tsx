2ed4cf7e16215df95f7803892aad1ee4a0d1ba2b
Oliver Walters <oliver.henry.walters@gmail.com>
2024-08-08 02:52:57 +0000

    Implement build order allocation table

--- src/frontend/src/tables/part/PartBuildAllocationsTable.tsx
+++ src/frontend/src/tables/part/PartBuildAllocationsTable.tsx
@@ -1,4 +1,17 @@
+import { t } from '@lingui/macro';
+import { useMemo } from 'react';
+
+import { ApiEndpoints } from '../../enums/ApiEndpoints';
+import { ModelType } from '../../enums/ModelType';
 import { useTable } from '../../hooks/UseTable';
+import { apiUrl } from '../../states/ApiState';
+import { TableColumn } from '../Column';
+import {
+  LocationColumn,
+  ReferenceColumn,
+  StatusColumn
+} from '../ColumnRenderers';
+import { InvenTreeTable } from '../InvenTreeTable';
 
 export default function PartBuildAlloctionsTable({
   partId
@@ -7,5 +20,49 @@ export default function PartBuildAlloctionsTable({
 }) {
   const table = useTable('partbuildallocations');
 
-  return <></>;
+  const tableColumns: TableColumn[] = useMemo(() => {
+    return [
+      ReferenceColumn({
+        accessor: 'build_detail.reference',
+        title: t`Build Order`,
+        switchable: false
+      }),
+      {
+        accessor: 'build_detail.title',
+        title: t`Description`
+      },
+      StatusColumn({
+        accessor: 'build_detail.status',
+        model: ModelType.build
+      }),
+      {
+        accessor: 'quantity',
+        switchable: false,
+        sortable: true
+      },
+      LocationColumn({
+        accessor: 'location_detail'
+      })
+    ];
+  }, []);
+
+  return (
+    <>
+      <InvenTreeTable
+        url={apiUrl(ApiEndpoints.build_item_list)}
+        columns={tableColumns}
+        tableState={table}
+        props={{
+          params: {
+            part: partId,
+            build_detail: true,
+            location_detail: true,
+            stock_detail: true
+          },
+          modelField: 'build',
+          modelType: ModelType.build
+        }}
+      />
+    </>
+  );
 }
