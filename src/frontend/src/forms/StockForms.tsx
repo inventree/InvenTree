ca638297619dd4315c0277df9a57b3d3dfc3fdbb
Matthias Mair <code@mjmair.com>
2024-08-23 18:42:33 +0200

    Revert changes from https://github.com/inventree/InvenTree/pull/7965

--- src/frontend/src/forms/StockForms.tsx
+++ src/frontend/src/forms/StockForms.tsx
@@ -138,7 +138,7 @@ export function useStockFields({
         value: batchCode,
         onValueChange: (value) => setBatchCode(value)
       },
-      status: {},
+      status_custom_key: {},
       expiry_date: {
         // TODO: icon
       },
