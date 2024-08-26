95a9debcae9b88381ac8447d905df868b80eaefd
Matthias Mair <code@mjmair.com>
2024-08-27 00:47:35 +0200

    reduce code duplication by using BarcodeInput

--- src/frontend/src/components/modals/QrCodeModal.tsx
+++ src/frontend/src/components/modals/QrCodeModal.tsx
@@ -1,30 +1,13 @@
 import { Trans, t } from '@lingui/macro';
-import {
-  Badge,
-  Button,
-  Container,
-  Group,
-  ScrollArea,
-  Space,
-  Stack,
-  Text
-} from '@mantine/core';
-import {
-  useDocumentVisibility,
-  useListState,
-  useLocalStorage
-} from '@mantine/hooks';
+import { Button, ScrollArea, Stack, Text } from '@mantine/core';
+import { useListState } from '@mantine/hooks';
 import { ContextModalProps } from '@mantine/modals';
 import { showNotification } from '@mantine/notifications';
-import { IconX } from '@tabler/icons-react';
-import { Html5Qrcode } from 'html5-qrcode';
-import { CameraDevice } from 'html5-qrcode/camera/core';
-import { Html5QrcodeResult } from 'html5-qrcode/core';
-import { useEffect, useState } from 'react';
 
 import { api } from '../../App';
 import { ApiEndpoints } from '../../enums/ApiEndpoints';
 import { apiUrl } from '../../states/ApiState';
+import { BarcodeInput } from '../items/BarcodeInput';
 
 function ScanArea({ values }: Readonly<{ values: string[] }>) {
   if (values.length == 0)
@@ -46,40 +29,9 @@ export function QrCodeModal({
   context,
   id
 }: Readonly<ContextModalProps<{ modalBody: string }>>) {
-  const [qrCodeScanner, setQrCodeScanner] = useState<Html5Qrcode | null>(null);
-  const [camId, setCamId] = useLocalStorage<CameraDevice | null>({
-    key: 'camId',
-    defaultValue: null
-  });
-  const [scanningEnabled, setScanningEnabled] = useState<boolean>(false);
-  const [wasAutoPaused, setWasAutoPaused] = useState<boolean>(false);
-  const documentState = useDocumentVisibility();
-
   const [values, handlers] = useListState<string>([]);
 
-  // Mount QR code once we are loaded
-  useEffect(() => {
-    setQrCodeScanner(new Html5Qrcode('reader'));
-  }, []);
-
-  // Stop/star when leaving or reentering page
-  useEffect(() => {
-    if (scanningEnabled && documentState === 'hidden') {
-      stopScanning();
-      setWasAutoPaused(true);
-    } else if (wasAutoPaused && documentState === 'visible') {
-      startScanning();
-      setWasAutoPaused(false);
-    }
-  }, [documentState]);
-
-  // Scanner functions
-  function onScanSuccess(
-    decodedText: string,
-    decodedResult: Html5QrcodeResult
-  ) {
-    qrCodeScanner?.pause();
-
+  function onScanAction(decodedText: string) {
     handlers.append(decodedText);
     api
       .post(apiUrl(ApiEndpoints.barcode), { barcode: decodedText })
@@ -93,114 +45,19 @@ export function QrCodeModal({
           window.location.href = response.data.url;
         }
       });
-
-    qrCodeScanner?.resume();
-  }
-
-  function onScanFailure(error: string) {
-    if (
-      error !=
-      'QR code parse error, error = NotFoundException: No MultiFormat Readers were able to detect the code.'
-    ) {
-      console.warn(`Code scan error = ${error}`);
-    }
-  }
-
-  function selectCamera() {
-    Html5Qrcode.getCameras()
-      .then((devices) => {
-        if (devices?.length) {
-          setCamId(devices[0]);
-        }
-      })
-      .catch((err) => {
-        showNotification({
-          title: t`Error while getting camera`,
-          message: err,
-          color: 'red',
-          icon: <IconX />
-        });
-      });
-  }
-
-  function startScanning() {
-    if (camId && qrCodeScanner) {
-      qrCodeScanner
-        .start(
-          camId.id,
-          { fps: 10, qrbox: { width: 250, height: 250 } },
-          (decodedText, decodedResult) => {
-            onScanSuccess(decodedText, decodedResult);
-          },
-          (errorMessage) => {
-            onScanFailure(errorMessage);
-          }
-        )
-        .catch((err: string) => {
-          showNotification({
-            title: t`Error while scanning`,
-            message: err,
-            color: 'red',
-            icon: <IconX />
-          });
-        });
-      setScanningEnabled(true);
-    }
-  }
-
-  function stopScanning() {
-    if (qrCodeScanner && scanningEnabled) {
-      qrCodeScanner.stop().catch((err: string) => {
-        showNotification({
-          title: t`Error while stopping`,
-          message: err,
-          color: 'red',
-          icon: <IconX />
-        });
-      });
-      setScanningEnabled(false);
-    }
   }
 
   return (
-    <Stack>
-      <Group>
-        <Text size="sm">{camId?.label}</Text>
-        <Space style={{ flex: 1 }} />
-        <Badge>{scanningEnabled ? t`Scanning` : t`Not scanning`}</Badge>
-      </Group>
-      <Container px={0} id="reader" w={'100%'} mih="300px" />
-      {!camId ? (
-        <Button onClick={() => selectCamera()}>
-          <Trans>Select Camera</Trans>
-        </Button>
-      ) : (
-        <>
-          <Group>
-            <Button
-              style={{ flex: 1 }}
-              onClick={() => startScanning()}
-              disabled={camId != undefined && scanningEnabled}
-            >
-              <Trans>Start scanning</Trans>
-            </Button>
-            <Button
-              style={{ flex: 1 }}
-              onClick={() => stopScanning()}
-              disabled={!scanningEnabled}
-            >
-              <Trans>Stop scanning</Trans>
-            </Button>
-          </Group>
-          <ScanArea values={values} />
-        </>
-      )}
+    <Stack gap="xs">
+      <BarcodeInput onScan={onScanAction} />
+
+      <ScanArea values={values} />
       <Button
         fullWidth
         mt="md"
         color="red"
         onClick={() => {
-          stopScanning();
+          // stopScanning();
           context.closeModal(id);
         }}
       >
