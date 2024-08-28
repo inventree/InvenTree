327405f9637340b6195ad09d70c985e12ad0603b
Matthias Mair <code@mjmair.com>
2024-08-28 22:02:03 +0200

    add breacrumbs to company subpages

--- src/frontend/src/pages/company/CompanyDetail.tsx
+++ src/frontend/src/pages/company/CompanyDetail.tsx
@@ -56,6 +56,7 @@ import { StockItemTable } from '../../tables/stock/StockItemTable';
 export type CompanyDetailProps = {
   title: string;
   breadcrumbs: Breadcrumb[];
+  last_crumb_url: string;
 };
 
 /**
@@ -341,6 +342,12 @@ export default function CompanyDetail(props: Readonly<CompanyDetailProps>) {
             actions={companyActions}
             imageUrl={company.image}
             breadcrumbs={props.breadcrumbs}
+            last_crumb={[
+              {
+                name: company.name,
+                url: `${props.last_crumb_url}/${company.pk}/`
+              }
+            ]}
             badges={badges}
             editAction={editCompany.open}
             editEnabled={user.hasChangePermission(ModelType.company)}
