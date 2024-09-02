3f1399e40b24014e81234dbed4220c9a8615d9f7
Matthias Mair <code@mjmair.com>
2024-09-03 01:28:20 +0200

    extend tests

--- src/frontend/tests/settings/selectionList.spec.ts
+++ src/frontend/tests/settings/selectionList.spec.ts
@@ -5,12 +5,19 @@ test('PUI - Admin - Parameter', async ({ page }) => {
   await doQuickLogin(page, 'admin', 'inventree');
   await page.getByRole('button', { name: 'admin' }).click();
   await page.getByRole('menuitem', { name: 'Admin Center' }).click();
-
   await page.getByRole('tab', { name: 'Part Parameters' }).click();
+
+  // Add selection list
+  await page.getByRole('button', { name: 'Selection Lists' }).click();
+  await page.getByLabel('action-button-add-selection-').click();
+  await page.getByLabel('text-field-name').fill('some list');
+  await page.getByLabel('text-field-description').fill('Listdescription');
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.getByRole('cell', { name: 'some list' }).waitFor();
+
+  // Add parameter
   await page.getByLabel('action-button-add-parameter-').click();
-  await page.getByLabel('text-field-name').click();
   await page.getByLabel('text-field-name').fill('my custom parameter');
-  await page.getByLabel('text-field-description').click();
   await page.getByLabel('text-field-description').fill('description');
   await page
     .locator('div')
@@ -18,7 +25,7 @@ test('PUI - Admin - Parameter', async ({ page }) => {
     .nth(2)
     .click();
   await page
-    .getByRole('option', { name: 'some list List with some' })
+    .getByRole('option', { name: 'some list' })
     .locator('div')
     .first()
     .click();
