f02104eecaca94c739ba6f01fafc30f2d5143c76
Matthias Mair <code@mjmair.com>
2024-09-04 00:07:43 +0200

    and more testing

--- src/frontend/tests/settings/selectionList.spec.ts
+++ src/frontend/tests/settings/selectionList.spec.ts
@@ -1,4 +1,5 @@
 import { test } from '../baseFixtures';
+import { baseUrl } from '../defaults';
 import { doQuickLogin } from '../login';
 
 test('PUI - Admin - Parameter', async ({ page }) => {
@@ -14,9 +15,10 @@ test('PUI - Admin - Parameter', async ({ page }) => {
   await page.getByLabel('text-field-description').fill('Listdescription');
   await page.getByRole('button', { name: 'Submit' }).click();
   await page.getByRole('cell', { name: 'some list' }).waitFor();
+  await page.waitForTimeout(200);
 
   // Add parameter
-  await page.getByLabel('action-button-add-parameter-').click({ force: true });
+  await page.getByLabel('action-button-add-parameter-').click();
   await page.getByLabel('text-field-name').fill('my custom parameter');
   await page.getByLabel('text-field-description').fill('description');
   await page
@@ -31,4 +33,27 @@ test('PUI - Admin - Parameter', async ({ page }) => {
     .click();
   await page.getByRole('button', { name: 'Submit' }).click();
   await page.getByRole('cell', { name: 'my custom parameter' }).click();
+
+  // Fill parameter
+  await page.goto(`${baseUrl}/part/104/parameters/`);
+  await page.getByLabel('Parameters').getByText('Parameters').waitFor();
+  await page.getByLabel('action-button-add-parameter').click();
+  await page.waitForTimeout(200);
+  await page.getByText('New Part Parameter').waitFor();
+  // await page.getByText('Blue Square Table').waitFor();
+  await page
+    .getByText('Template *Parameter')
+    .locator('div')
+    .filter({ hasText: /^Search\.\.\.$/ })
+    .nth(2)
+    .click();
+  await page
+    .getByText('Template *Parameter')
+    .locator('div')
+    .filter({ hasText: /^Search\.\.\.$/ })
+    .locator('input')
+    .fill('my custom parameter');
+  await page.getByRole('option', { name: 'my custom parameter' }).click();
+  await page.getByLabel('choice-field-data').fill('2');
+  await page.getByRole('button', { name: 'Submit' }).click();
 });
