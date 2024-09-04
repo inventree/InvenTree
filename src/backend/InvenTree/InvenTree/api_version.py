fcfabed7f420ae5d56149cc65e85dd172c741f24
Miklos Marton <martonmiklosqdev@gmail.com>
2024-09-04 07:24:51 +0200

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 249
+INVENTREE_API_VERSION = 250
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v250 - 2024-09-04 : https://github.com/inventree/InvenTree/pull/8061
+    - Add complete build items when all test passed option for parts
 
 v249 - 2024-08-23 : https://github.com/inventree/InvenTree/pull/7978
     - Sort status enums
