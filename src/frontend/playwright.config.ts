14f7f8f46816958043719b491266679cf9e332e9
Matthias Mair <code@mjmair.com>
2024-09-09 01:31:15 +0200

    more retries but shorter runs

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  timeout: 90000,
+  timeout: 45000,
   forbidOnly: !!process.env.CI,
-  retries: process.env.CI ? 1 : 0,
+  retries: process.env.CI ? 2 : 0,
   workers: process.env.CI ? 2 : undefined,
   reporter: process.env.CI ? [['html', { open: 'never' }], ['github']] : 'list',
 
