f8c87e7d6c190cd2f0b1ae039485a1d69cc0dda8
Matthias Mair <code@mjmair.com>
2024-09-09 02:47:51 +0200

    Update playwright.config.ts

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -3,10 +3,10 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  timeout: 45000,
+  timeout: 90000,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 2 : undefined,
+  workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI ? [['html', { open: 'never' }], ['github']] : 'list',
 
   /* Configure projects for major browsers */
