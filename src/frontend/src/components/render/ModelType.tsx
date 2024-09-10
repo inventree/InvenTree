109a81b5f1c8f5be024281e4b3514bf57913be44
Matthias Mair <code@mjmair.com>
2024-09-10 08:54:10 +0200

    materialise labels

--- src/frontend/src/components/render/ModelType.tsx
+++ src/frontend/src/components/render/ModelType.tsx
@@ -256,10 +256,14 @@ export const ModelInformationDict: ModelDict = {
 };
 
 /*
- * Extract model definition given the provided type
+ * Extract model definition given the provided type - returns translatable strings for labels as string, not functions
  * @param type - ModelType to extract information from
  * @returns ModelInformationInterface
  */
 export function getModelInfo(type: ModelType): ModelInformationInterface {
-  return ModelInformationDict[type];
+  return {
+    ...ModelInformationDict[type],
+    label: ModelInformationDict[type].label(),
+    label_multiple: ModelInformationDict[type].label_multiple()
+  };
 }
