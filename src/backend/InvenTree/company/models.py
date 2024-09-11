44fa01f83b072274f4928572a9caa447bd4d50b8
Oliver <oliver.henry.walters@gmail.com>
2024-09-11 10:26:38 +1000

    Improve logic for automatically updating part pricing (#8090)
    * Improve logic for automatically updating part pricing
    
    * Simplify logic
    
    * Update unit tests to ensure pricing flows upwards
    
    * Unit test update
    
    * Add unit tests for handling of "multi level" BOM pricing
    
    * ADjust unit tests
    
    * Improve efficiency of operation
    
    * Adjust testing for pricing
    
    - Only allow pricing updates in testing if TESTING_PRICING flag is set
    
    * Tweak when pricing updates are performed
    
    * More tweaks

--- src/backend/InvenTree/company/models.py
+++ src/backend/InvenTree/company/models.py
@@ -1052,7 +1052,10 @@ class SupplierPriceBreak(common.models.PriceBreak):
 )
 def after_save_supplier_price(sender, instance, created, **kwargs):
     """Callback function when a SupplierPriceBreak is created or updated."""
-    if InvenTree.ready.canAppAccessDatabase() and not InvenTree.ready.isImportingData():
+    if (
+        InvenTree.ready.canAppAccessDatabase(allow_test=settings.TESTING_PRICING)
+        and not InvenTree.ready.isImportingData()
+    ):
         if instance.part and instance.part.part:
             instance.part.part.schedule_pricing_update(create=True)
 
@@ -1064,6 +1067,9 @@ def after_save_supplier_price(sender, instance, created, **kwargs):
 )
 def after_delete_supplier_price(sender, instance, **kwargs):
     """Callback function when a SupplierPriceBreak is deleted."""
-    if InvenTree.ready.canAppAccessDatabase() and not InvenTree.ready.isImportingData():
+    if (
+        InvenTree.ready.canAppAccessDatabase(allow_test=settings.TESTING_PRICING)
+        and not InvenTree.ready.isImportingData()
+    ):
         if instance.part and instance.part.part:
             instance.part.part.schedule_pricing_update(create=False)
