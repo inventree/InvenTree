d9082a25cf320e4649ea0302179c7d6ea01df5ff
Oliver Walters <oliver.henry.walters@gmail.com>
2024-10-01 14:32:39 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 263
+INVENTREE_API_VERSION = 264
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+264 - 2024-10-02 : https://github.com/inventree/InvenTree/pull/8228
+    - Extend fields available in the PluginConfig API endpoints
 
 263 - 2024-09-30 : https://github.com/inventree/InvenTree/pull/8194
     - Adds Sales Order Shipment report
