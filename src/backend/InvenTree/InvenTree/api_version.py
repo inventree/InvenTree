d285099cb063328507b632f5f230c7e15c0cea25
Oliver Walters <oliver.henry.walters@gmail.com>
2024-10-07 10:56:22 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 264
+INVENTREE_API_VERSION = 265
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+265 - 2024-10-07 : https://github.com/inventree/InvenTree/pull/8249
+    - Tweak SalesOrderShipment API for more efficient data retrieval
 
 264 - 2024-10-03 : https://github.com/inventree/InvenTree/pull/8231
     - Adds Sales Order Shipment attachment model type
