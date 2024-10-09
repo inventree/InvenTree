67434b319b61f97f2c64d5ae9b663eb9b9ff3c12
Oliver <oliver.henry.walters@gmail.com>
2024-10-09 16:33:25 +1100

    [PUI] Build output labels (#8261)
    * Enable printing actions from "build order output" table
    
    * Adjust action order

--- src/frontend/src/tables/build/BuildOutputTable.tsx
+++ src/frontend/src/tables/build/BuildOutputTable.tsx
@@ -231,12 +231,6 @@ export default function BuildOutputTable({
 
   const tableActions = useMemo(() => {
     return [
-      <AddItemButton
-        key="add-build-output"
-        tooltip={t`Add Build Output`}
-        hidden={!user.hasAddRole(UserRoles.build)}
-        onClick={addBuildOutput.open}
-      />,
       <ActionButton
         key="complete-selected-outputs"
         tooltip={t`Complete selected outputs`}
@@ -269,6 +263,12 @@ export default function BuildOutputTable({
           setSelectedOutputs(table.selectedRecords);
           cancelBuildOutputsForm.open();
         }}
+      />,
+      <AddItemButton
+        key="add-build-output"
+        tooltip={t`Add Build Output`}
+        hidden={!user.hasAddRole(UserRoles.build)}
+        onClick={addBuildOutput.open}
       />
     ];
   }, [user, table.selectedRecords, table.hasSelectedRecords]);
@@ -449,6 +449,8 @@ export default function BuildOutputTable({
             is_building: true,
             build: buildId
           },
+          enableLabels: true,
+          enableReports: true,
           modelType: ModelType.stockitem,
           dataFormatter: formatRecords,
           tableActions: tableActions,
