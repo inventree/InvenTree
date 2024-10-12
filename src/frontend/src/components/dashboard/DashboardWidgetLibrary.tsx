c863eab4bc65285c821f6df0d0b4c1776d9f6049
Oliver Walters <oliver.henry.walters@gmail.com>
2024-10-12 14:33:20 +0000

    Add some more widgets to the library

--- src/frontend/src/components/dashboard/DashboardWidgetLibrary.tsx
+++ src/frontend/src/components/dashboard/DashboardWidgetLibrary.tsx
@@ -1,6 +1,8 @@
 import { t } from '@lingui/macro';
 
 import { ModelType } from '../../enums/ModelType';
+import DisplayWidget from '../widgets/DisplayWidget';
+import GetStartedWidget from '../widgets/GetStartedWidget';
 import { DashboardWidgetProps } from './DashboardWidget';
 import QueryCountDashboardWidget from './widgets/QueryCountDashboardWidget';
 
@@ -84,10 +86,36 @@ export function BuiltinQueryCountWidgets(): DashboardWidgetProps[] {
   ];
 }
 
+export function BuiltinGettingStartedWidgets(): DashboardWidgetProps[] {
+  return [
+    {
+      label: 'getting-started',
+      minWidth: 5,
+      minHeight: 4,
+      render: () => <GetStartedWidget />
+    }
+  ];
+}
+
+export function BuiltingSettinsWidgets(): DashboardWidgetProps[] {
+  return [
+    {
+      label: 'color-settings',
+      minWidth: 3,
+      minHeight: 2,
+      render: () => <DisplayWidget />
+    }
+  ];
+}
+
 /**
  *
  * @returns A list of built-in dashboard widgets
  */
 export default function BuiltinDashboardWidgets(): DashboardWidgetProps[] {
-  return [...BuiltinQueryCountWidgets()];
+  return [
+    ...BuiltinQueryCountWidgets(),
+    ...BuiltinGettingStartedWidgets(),
+    ...BuiltingSettinsWidgets()
+  ];
 }
