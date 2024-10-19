09f81f56e0f82bf9c5fa4af3bc7255b4604f94fb
Oliver Walters <oliver.henry.walters@gmail.com>
2024-10-19 03:17:21 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 269
+INVENTREE_API_VERSION = 270
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v270 - 2024-10-19 : https://github.com/inventree/InvenTree/pull/8307
+    - Adds missing date fields from order API endpoint(s)
 
 v269 - 2024-10-16 : https://github.com/inventree/InvenTree/pull/8295
     - Adds "include_variants" filter to the BuildOrder API endpoint
