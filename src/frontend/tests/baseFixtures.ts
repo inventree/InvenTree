7ef36c18173f4a10937186543c9865b1df325fce
Matthias Mair <code@mjmair.com>
2024-10-20 01:06:44 +0200

    fix imports

--- src/frontend/tests/baseFixtures.ts
+++ src/frontend/tests/baseFixtures.ts
@@ -1,7 +1,7 @@
-import * as crypto from 'crypto';
-import * as fs from 'fs';
-import os from 'os';
-import * as path from 'path';
+import * as crypto from 'node:crypto';
+import * as fs from 'node:fs';
+import os from 'node:os';
+import * as path from 'node:path';
 import { test as baseTest } from '@playwright/test';
 
 const istanbulCLIOutput = path.join(process.cwd(), '.nyc_output');
