ebaf86dfdb65bef4d116fc8c1c5b934752c57b7c
Oliver Walters <oliver.henry.walters@gmail.com>
2024-10-25 04:18:47 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 271
+INVENTREE_API_VERSION = 272
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v272 - 2024-10-25 : https://github.com/inventree/InvenTree/pull/8343
+    - Adjustments to BuildLine API serializers
 
 v271 - 2024-10-22 : https://github.com/inventree/InvenTree/pull/8331
     - Fixes for SalesOrderLineItem endpoints
