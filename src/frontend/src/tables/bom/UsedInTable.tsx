7bd77a062e984cd5bb6f2b497371c20752059175
Oliver Walters <oliver.henry.walters@gmail.com>
2024-11-08 22:26:59 +0000

    Another table tweak

--- src/frontend/src/tables/bom/UsedInTable.tsx
+++ src/frontend/src/tables/bom/UsedInTable.tsx
@@ -57,7 +57,7 @@ export function UsedInTable({
           let units = record.sub_part_detail?.units;
 
           return (
-            <Group justify="space-between" grow>
+            <Group justify="space-between" grow wrap="nowrap">
               <Text>{quantity}</Text>
               {units && <Text size="xs">{units}</Text>}
             </Group>
