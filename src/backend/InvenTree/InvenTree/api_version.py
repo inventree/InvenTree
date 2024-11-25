594dc49b84284db71aa2504db2eb65c22910dac7
Matthias Mair <code@mjmair.com>
2024-11-25 23:25:06 +0100

    Bump backend deps (#8559)
    * bump backend deps
    
    * bump api

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 284
+INVENTREE_API_VERSION = 285
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v285 - 2024-11-25 : https://github.com/inventree/InvenTree/pull/8559
+    - Adds better description for registration endpoints
 
 v284 - 2024-11-25 : https://github.com/inventree/InvenTree/pull/8544
     - Adds new date filters to the StockItem API
