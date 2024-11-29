ba64d50cdbcdbf5233bbeaa4ed2a5af5a9f86db0
Oliver Walters <oliver.henry.walters@gmail.com>
2024-11-29 00:38:52 +0000

    Add 'quantity' to serializer

--- src/backend/InvenTree/order/serializers.py
+++ src/backend/InvenTree/order/serializers.py
@@ -2040,6 +2040,7 @@ class ReturnOrderLineItemSerializer(
             'order_detail',
             'item',
             'item_detail',
+            'quantity',
             'received_date',
             'outcome',
             'part_detail',
@@ -2070,9 +2071,11 @@ class ReturnOrderLineItemSerializer(
             self.fields.pop('part_detail', None)
 
     order_detail = ReturnOrderSerializer(source='order', many=False, read_only=True)
+
     item_detail = stock.serializers.StockItemSerializer(
         source='item', many=False, read_only=True
     )
+
     part_detail = PartBriefSerializer(source='item.part', many=False, read_only=True)
 
     price = InvenTreeMoneySerializer(allow_null=True)
