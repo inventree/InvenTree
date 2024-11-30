23c45296fb5575f1d9c2edfcd5d6a9e5fcb4e78e
Oliver Walters <oliver.henry.walters@gmail.com>
2024-11-30 09:57:11 +0000

    Simple placeholder table

--- src/frontend/src/components/wizards/OrderPartsWizard.tsx
+++ src/frontend/src/components/wizards/OrderPartsWizard.tsx
@@ -1,7 +1,46 @@
 import { t } from '@lingui/macro';
-import { Stack, Text } from '@mantine/core';
+import { NumberInput, Table } from '@mantine/core';
 import { useCallback, useEffect, useState } from 'react';
 import useWizard from '../../hooks/UseWizard';
+import { PartColumn } from '../../tables/ColumnRenderers';
+
+function SelectPartsStep({
+  parts
+}: {
+  parts: any[];
+}) {
+  return (
+    <Table striped withColumnBorders withTableBorder>
+      <Table.Thead>
+        <Table.Tr>
+          <Table.Th>{t`Part`}</Table.Th>
+          <Table.Th>{t`IPN`}</Table.Th>
+          <Table.Th>{t`Description`}</Table.Th>
+          <Table.Th>{t`Quantity`}</Table.Th>
+        </Table.Tr>
+      </Table.Thead>
+      <Table.Tbody>
+        {parts.map((part) => (
+          <Table.Tr key={part.pk}>
+            <Table.Td>
+              <PartColumn part={part} />
+            </Table.Td>
+            <Table.Td>{part.IPN}</Table.Td>
+            <Table.Td>{part.description}</Table.Td>
+            <Table.Td>
+              <NumberInput
+                min={0}
+                max={100}
+                defaultValue={0}
+                placeholder={t`Quantity`}
+              />
+            </Table.Td>
+          </Table.Tr>
+        ))}
+      </Table.Tbody>
+    </Table>
+  );
+}
 
 export default function OrderPartsWizard({
   parts
@@ -16,12 +55,7 @@ export default function OrderPartsWizard({
 
   const renderStep = useCallback(
     (step: number) => {
-      return (
-        <Stack gap='xs'>
-          <Text>Step: {step}</Text>
-          <Text>Parts: {selectedParts.length}</Text>
-        </Stack>
-      );
+      return <SelectPartsStep parts={selectedParts} />;
     },
     [selectedParts]
   );
