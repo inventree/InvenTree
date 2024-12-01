b621c76b305631d2c078050add3d033b80c93a62
Matthias Mair <code@mjmair.com>
2024-12-02 00:19:53 +0100

    make logic simpler

--- src/backend/InvenTree/stock/models.py
+++ src/backend/InvenTree/stock/models.py
@@ -1359,7 +1359,7 @@ class StockItem(
         if self.installed_item_count() > 0:
             return False
 
-        return not self.sales_order is not None
+        return self.sales_order is None
 
     def get_installed_items(self, cascade: bool = False) -> set[StockItem]:
         """Return all stock items which are *installed* in this one!
@@ -1542,7 +1542,7 @@ class StockItem(
         if self.belongs_to is not None:
             return False
 
-        return not self.sales_order is not None
+        return self.sales_order is None
 
     @property
     def tracking_info_count(self):
