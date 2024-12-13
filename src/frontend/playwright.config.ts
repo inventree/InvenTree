36e3b4e992803dc60da56173ee396e10b8f61284
Matthias Mair <code@mjmair.com>
2024-12-14 00:43:42 +0100

    re-introduce firefox testing

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -16,6 +16,12 @@ export default defineConfig({
       use: {
         ...devices['Desktop Chrome']
       }
+    },
+    {
+      name: 'firefox',
+      use: {
+        ...devices['Desktop Firefox']
+      }
     }
   ],
 
