69bf47f0af5bf3c3caffaf34eb2d7984a502e066
Matthias Mair <code@mjmair.com>
2024-12-14 13:19:32 +0100

    no timeouts

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -3,9 +3,9 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   fullyParallel: true,
-  timeout: 60000,
+  timeout: 90000,
   forbidOnly: !!process.env.CI,
-  retries: 1,
+  retries: 0,
   workers: process.env.CI ? 3 : undefined,
   reporter: process.env.CI ? [['html', { open: 'never' }], ['github']] : 'list',
 
