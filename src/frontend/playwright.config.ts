5aae1e076218f6fdd4680926e28ac4bdacb592a8
Matthias Mair <code@mjmair.com>
2024-12-14 11:08:36 +0100

    only run n firefox

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -12,12 +12,6 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: {
-        ...devices['Desktop Chrome']
-      }
-    },
-    {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox']
