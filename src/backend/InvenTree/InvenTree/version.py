3041d9050b6669aedc8363d5f0d373212e5fc372
Oliver <oliver.henry.walters@gmail.com>
2024-12-17 09:16:38 +1100

    Bump version number (#8677)
    - Update to 0.18.0 dev
    - Will likely become 1.0.0 dev

--- src/backend/InvenTree/InvenTree/version.py
+++ src/backend/InvenTree/InvenTree/version.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from .api_version import INVENTREE_API_TEXT, INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = '0.17.0 dev'
+INVENTREE_SW_VERSION = '0.18.0 dev'
 
 
 logger = logging.getLogger('inventree')
