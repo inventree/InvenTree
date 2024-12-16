d42435c8410397c4278de229fb7d9d1bf99bb128
Oliver <oliver.henry.walters@gmail.com>
2024-12-17 07:39:49 +1100

    [Refactor] Barcode scanning (#8658)
    * Enhance SupplierPart barcode mixin
    
    - Provide richer response based on available data
    
    * Add comment
    
    * Add new fields to BarcodePOReceiveSerializer
    
    * Add "supplier" information to serializer
    
    * Add 'is_completed' method for PurchaseOrderLineItem
    
    * Refactor SupplierBarcodeMixin
    
    * Tweak error message
    
    * Logic fix
    
    * Fix response data
    
    * Improved error checking
    
    * Bump API version
    
    * Bump API version
    
    * Error handling
    
    - Improve get_supplier_part method
    - Error handling
    - Wrap calls to external plugins
    
    * Enhanced unit testing and exception handling
    
    * More exception handling
    
    * Fix circula imports
    
    * Improve unit tests
    
    * Adjust filtering
    
    * Remove outdated tests
    
    * Remove success code for matching item

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 292
+INVENTREE_API_VERSION = 293
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v293 - 2024-12-14 : https://github.com/inventree/InvenTree/pull/8658
+    - Adds new fields to the supplier barcode API endpoints
 
 v292 - 2024-12-03 : https://github.com/inventree/InvenTree/pull/8625
     - Add "on_order" and "in_stock" annotations to SupplierPart API
