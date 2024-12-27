d389aa2f7a061dd0babd910a550685be0aad97e8
Oliver Walters <oliver.henry.walters@gmail.com>
2024-12-27 23:18:40 +0000

    Enhance status filter for orders

--- src/backend/InvenTree/stock/api.py
+++ src/backend/InvenTree/stock/api.py
@@ -571,7 +571,10 @@ class StockFilter(rest_filters.FilterSet):
     status = rest_filters.NumberFilter(label=_('Status Code'), method='filter_status')
 
     def filter_status(self, queryset, name, value):
-        """Filter by integer status code."""
+        """Filter by integer status code.
+
+        Note: Also account for the possibility of a custom status code.
+        """
         q1 = Q(status=value, status_custom_key__isnull=True)
         q2 = Q(status_custom_key=value)
 
