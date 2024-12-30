031059ed1303de2ed84d3945b53ef4cf94f12a8f
Oliver Walters <oliver.henry.walters@gmail.com>
2024-12-30 05:37:59 +0000

    Add basic check for model type support

--- src/backend/InvenTree/plugin/base/integration/DataExportMixin.py
+++ src/backend/InvenTree/plugin/base/integration/DataExportMixin.py
@@ -2,4 +2,27 @@
 
 
 class DataExportMixin:
-    """Mixin which provides support for custom data export functionality."""
+    """Mixin which provides support for custom data export functionality.
+
+    This mixin class allows plugins to implement custom "export" functionality for a queryset.
+    """
+
+    class MixinMeta:
+        """Meta options for this mixin."""
+
+        MIXIN_NAME = 'DataExport'
+
+    def __init__(self):
+        """Register mixin."""
+        super().__init__()
+        self.add_mixin('export', True, __class__)
+
+    def supports_model(self, model: str) -> bool:
+        """Check if this mixin supports the given model.
+
+        Arguments:
+            model: The qualified app.model type to check e.g. 'part.partcategory'
+
+        By default, all model types are supported
+        """
+        return True
