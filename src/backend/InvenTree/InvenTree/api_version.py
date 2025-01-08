7ef84c87a2c49b1eb130d627b7b0d03fa5c1f548
Matthias Mair <code@mjmair.com>
2025-01-08 19:56:33 +0100

    update api version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,13 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 298
+INVENTREE_API_VERSION = 299
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
-v296 - 2024-12-22 : https://github.com/inventree/InvenTree/pull/6293
+v299 - 2025-01-10 : https://github.com/inventree/InvenTree/pull/6293
     - Removes a considerable amount of old auth endpoints
     - Introduces allauth based REST API
 
