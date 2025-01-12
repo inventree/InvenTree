2c2dcc0a5dff7d68c873f607a619d0fdad0df4a7
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-12 02:29:14 +0000

    Revert again
    - Use element size, not screen size

--- src/frontend/src/components/details/ItemDetails.tsx
+++ src/frontend/src/components/details/ItemDetails.tsx
@@ -1,10 +1,16 @@
 import { Paper, SimpleGrid } from '@mantine/core';
+import { useElementSize } from '@mantine/hooks';
 import type React from 'react';
+import { useMemo } from 'react';
 
 export function ItemDetailsGrid(props: React.PropsWithChildren<{}>) {
+  const { ref, width } = useElementSize();
+
+  const cols = useMemo(() => (width > 700 ? 2 : 1), [width]);
+
   return (
     <Paper p='xs'>
-      <SimpleGrid cols={{ base: 1, md: 2 }} spacing='xs' verticalSpacing='xs'>
+      <SimpleGrid cols={cols} spacing='xs' verticalSpacing='xs' ref={ref}>
         {props.children}
       </SimpleGrid>
     </Paper>
