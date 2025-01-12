637a2579e6cd1c278c88ab36bd8a42b07800e3ec
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-12 02:27:25 +0000

    Cleaner implementation

--- src/frontend/src/components/details/ItemDetails.tsx
+++ src/frontend/src/components/details/ItemDetails.tsx
@@ -1,16 +1,10 @@
 import { Paper, SimpleGrid } from '@mantine/core';
-import { useElementSize } from '@mantine/hooks';
 import type React from 'react';
-import { useMemo } from 'react';
 
 export function ItemDetailsGrid(props: React.PropsWithChildren<{}>) {
-  const { ref, width } = useElementSize();
-
-  const cols = useMemo(() => (width > 700 ? 2 : 1), [width]);
-
   return (
     <Paper p='xs'>
-      <SimpleGrid cols={cols} spacing='xs' verticalSpacing='xs' ref={ref}>
+      <SimpleGrid cols={{ base: 1, md: 2 }} spacing='xs' verticalSpacing='xs'>
         {props.children}
       </SimpleGrid>
     </Paper>
