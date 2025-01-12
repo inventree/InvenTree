24c0677246548d88d62a0cf9f746fef2b342a08c
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-12 08:42:45 +0000

    Simplify <ItemDetailsGrid />

--- src/frontend/src/components/details/ItemDetails.tsx
+++ src/frontend/src/components/details/ItemDetails.tsx
@@ -1,16 +1,15 @@
 import { Paper, SimpleGrid } from '@mantine/core';
-import { useElementSize } from '@mantine/hooks';
 import type React from 'react';
-import { useMemo } from 'react';
 
 export function ItemDetailsGrid(props: React.PropsWithChildren<{}>) {
-  const { ref, width } = useElementSize();
-
-  const cols = useMemo(() => (width > 700 ? 2 : 1), [width]);
-
   return (
     <Paper p='xs'>
-      <SimpleGrid cols={cols} spacing='xs' verticalSpacing='xs' ref={ref}>
+      <SimpleGrid
+        cols={{ base: 1, '750px': 2 }}
+        type='container'
+        spacing='xs'
+        verticalSpacing='xs'
+      >
         {props.children}
       </SimpleGrid>
     </Paper>
