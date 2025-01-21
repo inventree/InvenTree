64260452e42a218de75393c4866521e83af791b0
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-21 08:35:08 +0000

    Add locale support

--- src/frontend/src/components/calendar/Calendar.tsx
+++ src/frontend/src/components/calendar/Calendar.tsx
@@ -1,16 +1,22 @@
+import allLocales from '@fullcalendar/core/locales-all';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
+import { useLocalState } from '../../states/LocalState';
 
 export default function Calendar({
   events
 }: {
   events: any[];
 }) {
+  const [locale] = useLocalState((s) => [s.language]);
+
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView='dayGridMonth'
       events={events}
+      locales={allLocales}
+      locale={locale}
     />
   );
 }
