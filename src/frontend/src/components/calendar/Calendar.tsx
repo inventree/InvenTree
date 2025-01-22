1b224275921281337684f7e15cbd045b6cde0773
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-22 11:02:20 +0000

    Fetch build orders based on calendar dates

--- src/frontend/src/components/calendar/Calendar.tsx
+++ src/frontend/src/components/calendar/Calendar.tsx
@@ -1,22 +1,19 @@
+import type { CalendarOptions } from '@fullcalendar/core';
 import allLocales from '@fullcalendar/core/locales-all';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
 import { useLocalState } from '../../states/LocalState';
 
-export default function Calendar({
-  events
-}: {
-  events: any[];
-}) {
+export default function Calendar(props: CalendarOptions) {
   const [locale] = useLocalState((s) => [s.language]);
 
   return (
     <FullCalendar
       plugins={[dayGridPlugin]}
       initialView='dayGridMonth'
-      events={events}
       locales={allLocales}
       locale={locale}
+      {...props}
     />
   );
 }
--- src/frontend/src/pages/build/BuildCalendar.tsx
+++ src/frontend/src/pages/build/BuildCalendar.tsx
@@ -1,5 +1,50 @@
+import type { DatesSetArg } from '@fullcalendar/core';
+import { t } from '@lingui/macro';
+import { useQuery } from '@tanstack/react-query';
+import dayjs from 'dayjs';
+import { useState } from 'react';
+import { api } from '../../App';
 import Calendar from '../../components/calendar/Calendar';
+import { ApiEndpoints } from '../../enums/ApiEndpoints';
+import { showApiErrorMessage } from '../../functions/notifications';
+import { apiUrl } from '../../states/ApiState';
 
 export default function BuildCalendar() {
-  return <Calendar events={[]} />;
+  const [minDate, setMinDate] = useState<Date | null>(null);
+  const [maxDate, setMaxDate] = useState<Date | null>(null);
+
+  const onDatesChange = (dateInfo: DatesSetArg) => {
+    if (!!dateInfo.start) {
+      setMinDate(dayjs(dateInfo.start).subtract(1, 'month').toDate());
+    }
+
+    if (!!dateInfo.end) {
+      setMaxDate(dayjs(dateInfo.end).add(1, 'month').toDate());
+    }
+  };
+
+  const buildQuery = useQuery({
+    enabled: !!minDate && !!maxDate,
+    queryKey: ['builds', minDate, maxDate],
+    queryFn: async () =>
+      api
+        .get(apiUrl(ApiEndpoints.build_order_list), {
+          params: {
+            outstanding: true,
+            min_date: minDate?.toISOString().split('T')[0],
+            max_date: maxDate?.toISOString().split('T')[0]
+          }
+        })
+        .then((response) => {
+          return response.data ?? [];
+        })
+        .catch((error) => {
+          showApiErrorMessage({
+            error: error,
+            title: t`Error fetching build orders`
+          });
+        })
+  });
+
+  return <Calendar events={[]} datesSet={onDatesChange} />;
 }
