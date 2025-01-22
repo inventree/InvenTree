b97d60afd2b91af457e28da3ecf015a3fea3838e
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-22 11:11:27 +0000

    Display builds

--- src/frontend/src/pages/build/BuildCalendar.tsx
+++ src/frontend/src/pages/build/BuildCalendar.tsx
@@ -2,7 +2,7 @@ import type { DatesSetArg } from '@fullcalendar/core';
 import { t } from '@lingui/macro';
 import { useQuery } from '@tanstack/react-query';
 import dayjs from 'dayjs';
-import { useState } from 'react';
+import { useMemo, useState } from 'react';
 import { api } from '../../App';
 import Calendar from '../../components/calendar/Calendar';
 import { ApiEndpoints } from '../../enums/ApiEndpoints';
@@ -46,5 +46,35 @@ export default function BuildCalendar() {
         })
   });
 
-  return <Calendar events={[]} datesSet={onDatesChange} />;
+  const events = useMemo(() => {
+    const today = dayjs().toISOString().split('T')[0];
+
+    return (
+      buildQuery.data?.map((build: any) => {
+        const start: string = build.start_date || today;
+        const end: string = build.target_date || build.start_date || today;
+
+        const inProgress =
+          dayjs(start).isBefore(today) && dayjs(end).isAfter(today);
+        const overdue = dayjs(end).isBefore(today);
+
+        const color: string = overdue
+          ? 'var(--mantine-color-red-3)'
+          : inProgress
+            ? 'var(--mantine-color-green-3)'
+            : 'var(--mantine-color-blue-3)';
+
+        return {
+          id: build.pk,
+          title: build.reference,
+          description: build.title,
+          start: start,
+          end: end,
+          borderColor: color
+        };
+      }) ?? []
+    );
+  }, [buildQuery.data]);
+
+  return <Calendar events={events} datesSet={onDatesChange} />;
 }
