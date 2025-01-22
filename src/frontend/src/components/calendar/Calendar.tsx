d0379c295983ad993410069b18184da26f69ed10
Oliver Walters <oliver.henry.walters@gmail.com>
2025-01-22 11:34:52 +0000

    Working on custom header

--- src/frontend/src/components/calendar/Calendar.tsx
+++ src/frontend/src/components/calendar/Calendar.tsx
@@ -2,18 +2,57 @@ import type { CalendarOptions } from '@fullcalendar/core';
 import allLocales from '@fullcalendar/core/locales-all';
 import dayGridPlugin from '@fullcalendar/daygrid';
 import FullCalendar from '@fullcalendar/react';
+import { t } from '@lingui/macro';
+import { Group, Stack } from '@mantine/core';
+import {
+  IconCalendarDot,
+  IconChevronLeft,
+  IconChevronRight
+} from '@tabler/icons-react';
 import { useLocalState } from '../../states/LocalState';
+import { ActionButton } from '../buttons/ActionButton';
+import { StylishText } from '../items/StylishText';
 
 export default function Calendar(props: CalendarOptions) {
   const [locale] = useLocalState((s) => [s.language]);
 
   return (
-    <FullCalendar
-      plugins={[dayGridPlugin]}
-      initialView='dayGridMonth'
-      locales={allLocales}
-      locale={locale}
-      {...props}
-    />
+    <Stack gap='xs'>
+      <Group justify='space-between' gap='xs'>
+        <Group gap='xs' justify='left'>
+          <ActionButton
+            icon={<IconChevronLeft />}
+            onClick={() => {}}
+            tooltipAlignment='top'
+            tooltip={t`Previous month`}
+          />
+          <ActionButton
+            icon={<IconCalendarDot />}
+            onClick={() => {}}
+            tooltipAlignment='top'
+            tooltip={t`Today`}
+          />
+          <ActionButton
+            icon={<IconChevronRight />}
+            onClick={() => {}}
+            tooltipAlignment='top'
+            tooltip={t`Next month`}
+          />
+        </Group>
+        <StylishText size='lg'>MONTHY</StylishText>
+        <Group justify='right' gap='xs' wrap='nowrap'>
+          <div>hello world</div>
+        </Group>
+      </Group>
+      <FullCalendar
+        plugins={[dayGridPlugin]}
+        initialView='dayGridMonth'
+        locales={allLocales}
+        locale={locale}
+        headerToolbar={false}
+        footerToolbar={false}
+        {...props}
+      />
+    </Stack>
   );
 }
