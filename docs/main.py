e8714dfb7146de366f9de7698636e37a878403b7
Matthias Mair <code@mjmair.com>
2025-01-27 21:47:19 +0100

    fix formatting

--- docs/main.py
+++ docs/main.py
@@ -321,10 +321,11 @@ def define_env(env):
 
         ret_data = ''
         for ref in [['Tags', TAGS], ['Filters', FILTERS]]:
-            ret_data += f'## {ref[0]}\n\n| Namespace | Name | Description |\n| --- | --- | --- |\n'
+            ret_data += f'### {ref[0]}\n\n| Namespace | Name | Description |\n| --- | --- | --- |\n'
             for value in ref[1]:
                 title = value['title'].replace('\n', ' ')
                 ret_data += f'| {value["library"]} | {value["name"]} | {title} |\n'
             ret_data += '\n'
+        ret_data += '\n'
 
         return ret_data
