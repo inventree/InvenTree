9442330aaded8c5d349db2f486e2ca77111d57c7
Oliver Walters <oliver.henry.walters@gmail.com>
2025-02-01 05:46:10 +0000

    Cleanup ServerInfo list

--- src/frontend/src/components/forms/InstanceOptions.tsx
+++ src/frontend/src/components/forms/InstanceOptions.tsx
@@ -1,12 +1,28 @@
-import { Trans } from '@lingui/macro';
-import { Divider, Group, Select, Text, Title } from '@mantine/core';
+import { Trans, t } from '@lingui/macro';
+import {
+  ActionIcon,
+  Divider,
+  Group,
+  Paper,
+  Select,
+  Table,
+  Text
+} from '@mantine/core';
 import { useToggle } from '@mantine/hooks';
-import { IconCheck } from '@tabler/icons-react';
+import {
+  IconApi,
+  IconCheck,
+  IconInfoCircle,
+  IconPlugConnected,
+  IconServer,
+  IconServerSpark
+} from '@tabler/icons-react';
 
 import { useServerApiState } from '../../states/ApiState';
 import { useLocalState } from '../../states/LocalState';
 import type { HostList } from '../../states/states';
 import { EditButton } from '../buttons/EditButton';
+import { StylishText } from '../items/StylishText';
 import { HostOptionsForm } from './HostOptionsForm';
 
 export function InstanceOptions({
@@ -39,45 +55,46 @@ export function InstanceOptions({
 
   return (
     <>
-      <Title order={3}>
-        <Trans>Select destination instance</Trans>
-      </Title>
-      <Group>
+      <Paper p='xl' withBorder>
+        <StylishText size='xl'>{t`Select Server`}</StylishText>
+        <Divider p='xs' />
         <Group>
-          <Select
-            value={hostKey}
-            onChange={ChangeHost}
-            data={hostListData}
-            disabled={HostListEdit}
-          />
+          <Group>
+            <Select
+              value={hostKey}
+              onChange={ChangeHost}
+              data={hostListData}
+              disabled={HostListEdit}
+            />
+            <EditButton
+              setEditing={setHostListEdit}
+              editing={HostListEdit}
+              disabled={HostListEdit}
+            />
+          </Group>
           <EditButton
-            setEditing={setHostListEdit}
-            editing={HostListEdit}
+            setEditing={setHostEdit}
+            editing={true}
             disabled={HostListEdit}
+            saveIcon={<IconCheck />}
           />
         </Group>
-        <EditButton
-          setEditing={setHostEdit}
-          editing={true}
-          disabled={HostListEdit}
-          saveIcon={<IconCheck />}
-        />
-      </Group>
 
-      {HostListEdit ? (
-        <>
-          <Divider my={'sm'} />
-          <Text>
-            <Trans>Edit possible host options</Trans>
-          </Text>
-          <HostOptionsForm data={hostList} saveOptions={SaveOptions} />
-        </>
-      ) : (
-        <>
-          <Divider my={'sm'} />
-          <ServerInfo hostList={hostList} hostKey={hostKey} />
-        </>
-      )}
+        {HostListEdit ? (
+          <>
+            <Divider my={'sm'} />
+            <Text>
+              <Trans>Edit possible host options</Trans>
+            </Text>
+            <HostOptionsForm data={hostList} saveOptions={SaveOptions} />
+          </>
+        ) : (
+          <>
+            <Divider my={'sm'} />
+            <ServerInfo hostList={hostList} hostKey={hostKey} />
+          </>
+        )}
+      </Paper>
     </>
   );
 }
@@ -91,27 +108,66 @@ function ServerInfo({
 }>) {
   const [server] = useServerApiState((state) => [state.server]);
 
+  const items: any[] = [
+    {
+      key: 'server',
+      label: t`Server`,
+      value: hostList[hostKey]?.host,
+      icon: <IconServer />
+    },
+    {
+      key: 'name',
+      label: t`Name`,
+      value: server.instance,
+      icon: <IconInfoCircle />
+    },
+    {
+      key: 'version',
+      label: t`Version`,
+      value: server.version,
+      icon: <IconInfoCircle />
+    },
+    {
+      key: 'api',
+      label: t`API Version`,
+      value: server.apiVersion,
+      icon: <IconApi />
+    },
+    {
+      key: 'plugins',
+      label: t`Plugins`,
+      value: server.plugins_enabled ? t`Enabled` : t`Disabled`,
+      icon: <IconPlugConnected />,
+      color: server.plugins_enabled ? 'green' : 'red'
+    },
+    {
+      key: 'worker',
+      label: t`Worker`,
+      value: server.worker_running ? t`Running` : t`Stopped`,
+      icon: <IconServerSpark />,
+      color: server.worker_running ? 'green' : 'red'
+    }
+  ];
+
   return (
-    <Text>
-      {hostList[hostKey]?.host}
-      <br />
-      <Trans>Version: {server.version}</Trans>
-      <br />
-      <Trans>API:{server.apiVersion}</Trans>
-      <br />
-      <Trans>Name: {server.instance}</Trans>
-      <br />
-      <Trans>
-        State:{' '}
-        <Text span c={server.worker_running ? 'green' : 'red'}>
-          worker
-        </Text>{' '}
-        ({server.worker_pending_tasks}),{' '}
-        <Text span c={server.plugins_enabled ? 'green' : 'red'}>
-          plugins
-        </Text>
-        {server.plugins_enabled ? ` (${server.active_plugins.length})` : null}
-      </Trans>
-    </Text>
+    <Table>
+      <Table.Tbody>
+        {items.map((item) => (
+          <Table.Tr key={item.key}>
+            <Table.Td>
+              <ActionIcon size='xs' variant='transparent' color={item.color}>
+                {item.icon}
+              </ActionIcon>
+            </Table.Td>
+            <Table.Td>
+              <Text size='sm'>{item.label}</Text>
+            </Table.Td>
+            <Table.Td>
+              <Text size='sm'>{item.value}</Text>
+            </Table.Td>
+          </Table.Tr>
+        ))}
+      </Table.Tbody>
+    </Table>
   );
 }
