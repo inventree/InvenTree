df82c4916c69ef8dee0a23e3764ca67c9613660f
Oliver Walters <oliver.henry.walters@gmail.com>
2025-02-11 09:02:38 +0000

    Further devcontainer updates
    - Bump postgresql image from 13 to 15
    - Store psql data in the dev/psql directory
    - Install required frontend packages

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,6 +7,19 @@ git config --global --add safe.directory /home/inventree
 python3 -m venv /home/inventree/dev/venv --system-site-packages --upgrade-deps
 . /home/inventree/dev/venv/bin/activate
 
+# remove existing gitconfig created by "Avoiding Dubious Ownership" step
+# so that it gets copied from host to the container to have your global
+# git config in container
+rm -f /home/vscode/.gitconfig
+
+# Fix issue related to CFFI version mismatch
+pip uninstall cffi -y
+sudo apt remove --purge -y python3-cffi
+pip install --no-cache-dir --force-reinstall --ignore-installed cffi
+
+# Upgrade pip
+python3 -m pip install --upgrade pip
+
 # Ensure the correct invoke is available
 pip3 install --ignore-installed --upgrade invoke Pillow
 
@@ -22,15 +35,5 @@ invoke dev.setup-dev
 # Install required frontend packages
 invoke int.frontend-install
 
-# remove existing gitconfig created by "Avoiding Dubious Ownership" step
-# so that it gets copied from host to the container to have your global
-# git config in container
-rm -f /home/vscode/.gitconfig
-
-# Fix issue related to CFFI version mismatch
-pip uninstall cffi -y
-sudo apt remove --purge -y python3-cffi
-pip install --no-cache-dir --force-reinstall --ignore-installed cffi
-
-# Upgrade pip
-python3 -m pip install --upgrade pip
+# Install playwright dependencies
+cd src/frontend && sudo npx playwright install-deps
