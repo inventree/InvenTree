0d8754dd4e689166f55e9a8a6d47201fdd2006d0
Oliver Walters <oliver.henry.walters@gmail.com>
2025-02-16 01:16:22 +0000

    Run cleanup task on boot

--- src/backend/InvenTree/exporter/apps.py
+++ src/backend/InvenTree/exporter/apps.py
@@ -8,3 +8,18 @@ class ExporterConfig(AppConfig):
 
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'exporter'
+
+    def ready(self):
+        """Run any code that needs to be executed when the app is loaded."""
+        super().ready()
+
+        self.cleanup()
+
+    def cleanup(self):
+        """Cleanup any old export files."""
+        try:
+            from exporter.tasks import cleanup_old_export_outputs
+
+            cleanup_old_export_outputs()
+        except Exception:
+            pass
