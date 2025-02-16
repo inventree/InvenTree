b259302457579ffdf0d405bd68d70d88f1e40562
Matthias Mair <code@mjmair.com>
2025-02-16 21:22:04 +0100

    extend datamatrix testing

--- src/backend/InvenTree/report/test_tags.py
+++ src/backend/InvenTree/report/test_tags.py
@@ -462,3 +462,12 @@ class BarcodeTagTest(TestCase):
         # Test empty tag
         with self.assertRaises(ValueError):
             barcode_tags.datamatrix('')
+
+        # Failure cases with wrong args
+        datamatrix = barcode_tags.datamatrix(
+            'hello world', border='abc', fill_color='aaaaaaa', back_color='aaaaaaa'
+        )
+        self.assertEqual(
+            datamatrix,
+            'data:image/png;charset=utf-8;base64,iVBORw0KGgoAAAANSUhEUgAAABYAAAAWCAIAAABL1vtsAAAAqElEQVR4nN1UQQ6AMAgrxv9/GQ9mpJYSY/QkBxM3KLUUA0i8i+1l/dcQiXj09CwSEU2aQJ7nE8ou2faVUXoPZSEkq+dZKVxWg4UqxUHnVdkp6IdwMXMulGvzNBDMk4WwPSrUF3LNnQNZBJmOsZaVXa44QSEKnvWb5mIgKon1E1H6aPyOcIa15uhONP9aR4hSCiGmYAoYpj4uO+vK4+ybMhr8Nkjmn/z4Dvoldi8uJu4iAAAAAElFTkSuQmCC',
+        )
