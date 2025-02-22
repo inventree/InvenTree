74c8307637874d69883d41d6f5f85ae52efecb00
Asterix\Oliver <oliver@currawongeng.com>
2025-02-22 01:46:23 +0000

    backend: allow split search by company type

--- src/backend/InvenTree/InvenTree/api.py
+++ src/backend/InvenTree/InvenTree/api.py
@@ -504,6 +504,9 @@ class APISearchView(GenericAPIView):
         return {
             'build': build.api.BuildList,
             'company': company.api.CompanyList,
+            'supplier': company.api.CompanyList,
+            'manufacturer': company.api.CompanyList,
+            'customer': company.api.CompanyList,
             'manufacturerpart': company.api.ManufacturerPartList,
             'supplierpart': company.api.SupplierPartList,
             'part': part.api.PartList,
@@ -514,6 +517,14 @@ class APISearchView(GenericAPIView):
             'salesordershipment': order.api.SalesOrderShipmentList,
             'stockitem': stock.api.StockList,
             'stocklocation': stock.api.StockLocationList,
+        }
+
+    def get_result_filters(self):
+        """Provide extra filtering options for particular search groups."""
+        return {
+            'supplier': {'is_supplier': True},
+            'manufacturer': {'is_manufacturer': True},
+            'customer': {'is_customer': True},
         }
 
     def post(self, request, *args, **kwargs):
@@ -534,6 +545,8 @@ class APISearchView(GenericAPIView):
         if 'search' not in data:
             raise ValidationError({'search': 'Search term must be provided'})
 
+        search_filters = self.get_result_filters()
+
         for key, cls in self.get_result_types().items():
             # Only return results which are specifically requested
             if key in data:
@@ -541,6 +554,11 @@ class APISearchView(GenericAPIView):
 
                 for k, v in pass_through_params.items():
                     params[k] = request.data.get(k, v)
+
+                # Add in any extra filters for this particular search type
+                if key in search_filters:
+                    for k, v in search_filters[key].items():
+                        params[k] = v
 
                 # Enforce json encoding
                 params['format'] = 'json'
