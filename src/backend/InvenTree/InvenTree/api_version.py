3a36d64ae4b8274883d21953954e51d92b37e6b4
Oliver Walters <oliver.henry.walters@gmail.com>
2025-02-22 22:32:24 +0000

    Fix API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,7 +1,7 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 314
+INVENTREE_API_VERSION = 315
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
