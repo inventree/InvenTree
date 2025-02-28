81486a49f913688fac68948cd6fb324f9509e461
Matthias Mair <code@mjmair.com>
2025-02-28 20:08:02 +0100

    Add test for new pages

---
+++ src/frontend/tests/pages/pui_core.spec.ts
@@ -0,0 +1,19 @@
+import { test } from '../baseFixtures.js';
+import { navigate } from '../helpers.js';
+import { doQuickLogin } from '../login.js';
+
+test('Core User/Group', async ({ page }) => {
+  await doQuickLogin(page);
+
+  // groups
+  await navigate(page, '/core');
+  await page.getByText('System Overview', { exact: true }).click();
+  await page.getByRole('tab', { name: 'Groups' }).click();
+  await page.getByRole('cell', { name: 'all access' }).click();
+  await page.getByText('Group: all access', { exact: true }).click();
+  await page.getByLabel('breadcrumb-1-groups').click();
+
+  // users
+  await page.getByRole('tab', { name: 'Users' }).click();
+  await page.getByRole('cell', { name: 'admin' }).click();
+});
