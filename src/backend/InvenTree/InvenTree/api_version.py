2222c06fe8ecf52f9c7df98ba4ab0837a09a96d4
Oliver Walters <oliver.henry.walters@gmail.com>
2025-03-03 07:44:57 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,16 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 317
+INVENTREE_API_VERSION = 318
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v318 - 2025-03-03 : https://github.com/inventree/InvenTree/pull/9199
+    - Add detail API endpoint for the LabelOutput model
+    - Add detail API endpoint for the ReportOutput model
 
 v317 - 2025-02-26 : https://github.com/inventree/InvenTree/pull/9143
     - Default 'overdue' field to False in Build serializer
