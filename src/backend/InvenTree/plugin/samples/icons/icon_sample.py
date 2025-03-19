78704ab3661061b29bed137a33a3839f5be1b0c4
Lukas <76838159+wolflu05@users.noreply.github.com>
2025-03-19 16:26:29 +0100

    Undo changes to sample icons for now

--- src/backend/InvenTree/plugin/samples/icons/icon_sample.py
+++ src/backend/InvenTree/plugin/samples/icons/icon_sample.py
@@ -1,8 +1,8 @@
 """Sample icon pack plugin to add custom icons."""
 
-# from django.templatetags.static import static
+from django.templatetags.static import static
 
-# from common.icons import IconPack
+from common.icons import IconPack
 from plugin.base.icons.mixins import IconPackMixin
 from plugin.plugin import InvenTreePlugin
 
@@ -19,21 +19,21 @@ class SampleIconPlugin(IconPackMixin, InvenTreePlugin):
     def icon_packs(self):
         """Return a list of custom icon packs."""
         return [
-            # IconPack(
-            #     name='My Custom Icons',
-            #     prefix='my',
-            #     fonts={
-            #         'woff2': static('fontawesome/webfonts/fa-regular-400.woff2'),
-            #         'woff': static('fontawesome/webfonts/fa-regular-400.woff'),
-            #         'truetype': static('fontawesome/webfonts/fa-regular-400.ttf'),
-            #     },
-            #     icons={
-            #         'my-icon': {
-            #             'name': 'My Icon',
-            #             'category': '',
-            #             'tags': ['my', 'icon'],
-            #             'variants': {'filled': 'f0a5', 'cool': 'f073'},
-            #         }
-            #     },
-            # )
+            IconPack(
+                name='My Custom Icons',
+                prefix='my',
+                fonts={
+                    'woff2': static('fontawesome/webfonts/fa-regular-400.woff2'),
+                    'woff': static('fontawesome/webfonts/fa-regular-400.woff'),
+                    'truetype': static('fontawesome/webfonts/fa-regular-400.ttf'),
+                },
+                icons={
+                    'my-icon': {
+                        'name': 'My Icon',
+                        'category': '',
+                        'tags': ['my', 'icon'],
+                        'variants': {'filled': 'f0a5', 'cool': 'f073'},
+                    }
+                },
+            )
         ]
