6ccc3d51acf229b8836e2a4569dbc728ff495005
Oliver Walters <oliver.henry.walters@gmail.com>
2025-03-19 12:01:36 +0000

    Update <StylishText>
    - Re-enable gradient
    - Based on selected highlight color

--- src/frontend/src/components/items/StylishText.tsx
+++ src/frontend/src/components/items/StylishText.tsx
@@ -1,18 +1,49 @@
-import { Text, useMantineTheme } from '@mantine/core';
+import {
+  type MantineSize,
+  Text,
+  darken,
+  getThemeColor,
+  lighten,
+  useMantineColorScheme,
+  useMantineTheme
+} from '@mantine/core';
+import { useMemo } from 'react';
+import { useLocalState } from '../../states/LocalState';
 
-import * as classes from '../../main.css';
-
+// A stylish text component that uses the primary color of the theme
 export function StylishText({
   children,
-  size = 'md'
+  size
 }: Readonly<{
   children: JSX.Element | string;
-  size?: string;
+  size: MantineSize;
 }>) {
+  const { usertheme } = useLocalState();
   const theme = useMantineTheme();
+  const colorScheme = useMantineColorScheme();
+
+  const primary = useMemo(() => {
+    return getThemeColor(usertheme.primaryColor, theme);
+  }, [usertheme.primaryColor, theme]);
+
+  const secondary = useMemo(() => {
+    let secondary = primary;
+    if (colorScheme.colorScheme == 'dark') {
+      secondary = lighten(primary, 0.3);
+    } else {
+      secondary = darken(primary, 0.3);
+    }
+
+    return secondary;
+  }, [usertheme, colorScheme, primary]);
 
   return (
-    <Text size={size} className={classes.signText} c={theme.primaryColor}>
+    <Text
+      fw={700}
+      size={size ?? 'xl'}
+      variant='gradient'
+      gradient={{ from: primary.toString(), to: secondary.toString() }}
+    >
       {children}
     </Text>
   );
