36b0c68b12f37d057f0e8cd04fb8722e1a8faae9
Asterix\Oliver <oliver@currawongeng.com>
2025-03-23 23:39:33 +0000

    Add "alerts" to header

--- src/frontend/src/components/nav/Header.tsx
+++ src/frontend/src/components/nav/Header.tsx
@@ -1,14 +1,20 @@
 import {
   ActionIcon,
+  Alert,
   Container,
   Group,
   Indicator,
+  Menu,
   Tabs,
   Text,
   Tooltip
 } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
-import { IconBell, IconSearch } from '@tabler/icons-react';
+import {
+  IconBell,
+  IconExclamationCircle,
+  IconSearch
+} from '@tabler/icons-react';
 import { useQuery } from '@tanstack/react-query';
 import { type ReactNode, useEffect, useMemo, useState } from 'react';
 import { useMatch, useNavigate } from 'react-router-dom';
@@ -34,7 +40,15 @@ import { NavigationDrawer } from './NavigationDrawer';
 import { NotificationDrawer } from './NotificationDrawer';
 import { SearchDrawer } from './SearchDrawer';
 
+interface AlertInfo {
+  key: string;
+  title: string;
+  message: string;
+}
+
 export function Header() {
+  const user = useUserState();
+
   const [setNavigationOpen, navigationOpen] = useLocalState((state) => [
     state.setNavigationOpen,
     state.navigationOpen
@@ -59,6 +73,22 @@ export function Header() {
   const navbar_message = useMemo(() => {
     return server.customize?.navbar_message;
   }, [server.customize]);
+
+  const [dismissed, setDismissed] = useState<string[]>([]);
+
+  const alerts: AlertInfo[] = useMemo(() => {
+    const _alerts: AlertInfo[] = [];
+
+    if (server?.worker_running == false) {
+      _alerts.push({
+        key: 'worker',
+        title: t`Background Worker`,
+        message: t`The background worker process is not running. Contact your system administrator.`
+      });
+    }
+
+    return _alerts.filter((alert) => !dismissed.includes(alert.key));
+  }, [server, dismissed]);
 
   // Fetch number of notifications for the current user
   const notifications = useQuery({
@@ -156,6 +186,35 @@ export function Header() {
                 </ActionIcon>
               </Tooltip>
             </Indicator>
+            {user.isStaff() && alerts.length > 0 && (
+              <Menu withinPortal={true} position='bottom-end'>
+                <Menu.Target>
+                  <Tooltip position='bottom-end' label={t`Alerts`}>
+                    <ActionIcon
+                      variant='transparent'
+                      aria-label='open-alerts'
+                      color='red'
+                    >
+                      <IconExclamationCircle />
+                    </ActionIcon>
+                  </Tooltip>
+                </Menu.Target>
+                <Menu.Dropdown>
+                  {alerts.map((alert) => (
+                    <Menu.Item key={`alert-item-${alert.key}`}>
+                      <Alert
+                        withCloseButton
+                        color='red'
+                        title={alert.title}
+                        onClose={() => setDismissed([...dismissed, alert.key])}
+                      >
+                        {alert.message}
+                      </Alert>
+                    </Menu.Item>
+                  ))}
+                </Menu.Dropdown>
+              </Menu>
+            )}
             <MainMenu />
           </Group>
         </Group>
