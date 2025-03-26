cd1e3a5773727a20d3497942ce13110124ee90bb
Jacob Felknor <jacobfelknor073@gmail.com>
2025-03-26 16:59:20 +0000

    add search notes and tie user search settings to checkboxes in search drawer

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 327
+INVENTREE_API_VERSION = 328
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v328 - 2025-03-26 :
+    - Adds 'search_notes' parameter to all searchable API endpoints
 
 v327 - 2025-03-20 : https://github.com/inventree/InvenTree/pull/9339
     - Adds "is_mandatory" field to the Plugin API
