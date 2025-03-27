5998a57d8432d73bb853ee259878fecc37d83faf
Oliver Walters <oliver.henry.walters@gmail.com>
2025-03-27 01:52:24 +0000

    Cleanup vite.config.ts

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -17,6 +17,8 @@ if (IS_IN_WSL) {
   console.log('WSL detected: using polling for file system events');
 }
 
+const OUTPUT_DIR = '../../src/backend/InvenTree/web/static/web';
+
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
   return {
@@ -58,7 +60,7 @@ export default defineConfig(({ command, mode }) => {
     base: command == 'build' ? '' : undefined,
     build: {
       manifest: true,
-      outDir: '../../src/backend/InvenTree/web/static/web',
+      outDir: OUTPUT_DIR,
       sourcemap: IS_COVERAGE
     },
     server: {
@@ -70,8 +72,8 @@ export default defineConfig(({ command, mode }) => {
         }
       },
       watch: {
-        // use polling only for WSL as the file system doesn't trigger notifications for Linux apps
-        // ref: https://github.com/vitejs/vite/issues/1153#issuecomment-785467271
+        // Use polling only for WSL as the file system doesn't trigger notifications for Linux apps
+        // Ref: https://github.com/vitejs/vite/issues/1153#issuecomment-785467271
         usePolling: IS_IN_WSL
       }
     }
