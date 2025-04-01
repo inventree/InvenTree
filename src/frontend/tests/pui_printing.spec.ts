57f23badd8b1df3a0f0809bb4a10fc1bbbfef8e8
Lukas <76838159+wolflu05@users.noreply.github.com>
2025-04-01 17:45:32 +0200

    Update pui_printing.spec.ts

--- src/frontend/tests/pui_printing.spec.ts
+++ src/frontend/tests/pui_printing.spec.ts
@@ -137,6 +137,10 @@ test('Report Editing', async ({ browser, request }) => {
     .getByLabel('split-button-preview-options', { exact: true })
     .click();
   const msg = (await consoleLogPromise).args();
+  console.log(msg);
+  for (const arg of msg) {
+    console.log("==>", await arg.jsonValue());
+  }
   expect(await msg[0].jsonValue()).toBe('updatePreview');
   expect((await msg[1].jsonValue())[0]).toBe(newTextareaValue);
 
