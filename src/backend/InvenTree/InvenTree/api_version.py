b4ce88b56f9e428f72617b67f1d7e1bc0e488243
Tristan Le <tristan.tuanle@gmail.com>
2025-04-18 17:13:58 +0000

    Update API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,15 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 338
+INVENTREE_API_VERSION = 339
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v339 -> 2025-04-18 : https://github.com/inventree/InvenTree/pull/9532
+    - Adds "merge" field to the ReportTemplate model
 
 v338 -> 2025-04-15 : https://github.com/inventree/InvenTree/pull/9333
     - Adds oAuth2 support for the API
