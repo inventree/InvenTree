c3d1cd9d53d8a2ae3339a4158603160591547079
Joe Rogers <1337joe@gmail.com>
2025-04-21 08:07:25 -0400

    Bump version again

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,13 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 340
+INVENTREE_API_VERSION = 341
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
-v340 -> 2025-04-21 : https://github.com/inventree/InvenTree/pull/9547
+v341 -> 2025-04-21 : https://github.com/inventree/InvenTree/pull/9547
     - Require pagination limit on list queries
 
 v340 -> 2025-04-15 : https://github.com/inventree/InvenTree/pull/9546
