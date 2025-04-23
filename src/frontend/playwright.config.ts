36a79b63f43b9ecafe2ce8bda2f2db3290bdcb94
Matthias Mair <code@mjmair.com>
2025-04-23 12:47:56 +0200

    reduce diff

--- src/frontend/playwright.config.ts
+++ src/frontend/playwright.config.ts
@@ -48,6 +48,12 @@ export default defineConfig({
   /* Configure projects for major browsers */
   projects: [
     {
+      name: 'chromium',
+      use: {
+        ...devices['Desktop Chrome']
+      }
+    },
+    {
       name: 'firefox',
       use: {
         ...devices['Desktop Firefox']
