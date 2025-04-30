4299338fc12418545d3caa8ec9b68d46b21fb50e
Tristan Le <tristan.tuanle@gmail.com>
2025-04-30 02:55:39 +0000

    Updated migration file

--- src/backend/InvenTree/report/migrations/0031_reporttemplate_merge.py
+++ src/backend/InvenTree/report/migrations/0031_reporttemplate_merge.py
@@ -6,7 +6,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("report", "0029_remove_reportoutput_template_and_more"),
+        ("report", "0030_alter_labeltemplate_model_type_and_more"),
     ]
 
     operations = [
