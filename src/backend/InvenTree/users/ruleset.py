f4a1fb4ce0acc67b971f35a25e605d66b5f42d68
Matthias Mair <code@mjmair.com>
2025-05-07 02:55:47 +0200

    fix missing model props

--- src/backend/InvenTree/users/ruleset.py
+++ src/backend/InvenTree/users/ruleset.py
@@ -95,6 +95,9 @@ def get_ruleset_models() -> dict:
             # common / comms
             'common_emailmessage',
             'common_emailthread',
+            'django_mailbox_mailbox',
+            'django_mailbox_messageattachment',
+            'django_mailbox_message',
         ],
         RuleSetEnum.PART_CATEGORY: [
             'part_partcategory',
