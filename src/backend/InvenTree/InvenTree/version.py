2da0df6eaeaf3ed59cc8c6cc1d651e43571e0935
Oliver <oliver.henry.walters@gmail.com>
2025-06-02 22:22:07 +1000

    Update version.py
    Bump version number to `0.17.13`

--- src/backend/InvenTree/InvenTree/version.py
+++ src/backend/InvenTree/InvenTree/version.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from .api_version import INVENTREE_API_TEXT, INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = '0.17.12'
+INVENTREE_SW_VERSION = '0.17.13'
 
 
 logger = logging.getLogger('inventree')
