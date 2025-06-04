534f54859eec65bdbf50a5639a9a6a605c92c337
Matthias Mair <code@mjmair.com>
2025-06-04 20:59:49 +0200

    Update api_version.py

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,7 +1,7 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 343
+INVENTREE_API_VERSION = 344
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
