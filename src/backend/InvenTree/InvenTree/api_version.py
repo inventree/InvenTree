4b71130ebed3d634dd5da05b0037f96d036a9a66
Oliver <oliver.henry.walters@gmail.com>
2025-06-07 20:08:34 +1000

    [Feature] Override global settings (#9718)
    * Provide mechanism for config to override global settings
    
    * Ensure overrides are observed on save
    
    * Refactor
    
    * Add "read_only" field to serializer
    
    * Prevent editing for read_only settings
    
    * Bump API version
    
    * Update docs
    
    * Secure logs
    
    * Override applies to default_value
    
    * override get_setting method
    
    * Add unit test
    
    * Utilize new approach to override SITE_URL
    
    * Docs updates
    
    * Docs tweaks
    
    * Shortcut for get_global_setting
    
    * Remove previous change
    
    - Allow validation to be performed within the InvenTreeSetting class
    
    * Override INVENTREE_BASE_URL setting
    
    * Handle error on worker boot
    
    * Tweak unit test

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,12 +1,16 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 345
+INVENTREE_API_VERSION = 346
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 
 INVENTREE_API_TEXT = """
+
+v346 -> 2025-06-07 : https://github.com/inventree/InvenTree/pull/9718
+    - Adds "read_only" field to the GlobalSettings API endpoint(s)
+
 v345 -> 2025-06-07 : https://github.com/inventree/InvenTree/pull/9745
     - Adds barcode information to SalesOrderShipment API endpoint
 
