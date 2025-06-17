133bbcfa4be26c37e8555d8592a16b09580d9e99
Oliver Walters <oliver.henry.walters@gmail.com>
2025-06-17 11:19:24 +0000

    Updated docs

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -9,7 +9,9 @@ INVENTREE_API_VERSION = 350
 INVENTREE_API_TEXT = """
 v350 -> 2025-06-17 : https://github.com/inventree/InvenTree/pull/9798
     - Adds "can_build" field to the part requirements API endpoint
+    - Remove "allocated" and "required" fields from the part requirements API endpoint
     - Add detailed serializer to the part requirements API endpoint
+
 
 v349 -> 2025-06-13 : https://github.com/inventree/InvenTree/pull/9574
     - Remove the 'create_child_builds' flag from the BuildOrder creation API endpoint
