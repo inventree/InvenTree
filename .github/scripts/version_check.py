0bfd7aa37550e7c6d885e2955c5b055f794ca87f
Matthias Mair <code@mjmair.com>
2025-06-24 01:14:14 +0200

    remove

--- .github/scripts/version_check.py
+++ .github/scripts/version_check.py
@@ -113,28 +113,6 @@ if __name__ == '__main__':
         print(results[0])
         exit(0)
 
-    if 'do_release' in sys.argv:
-        target = os.getenv('TARGET')
-        highest_tupple = get_existing_release_tags()[0]
-
-        old_version = f'{highest_tupple[0]}.{highest_tupple[1]}.{highest_tupple[2]}'
-        if target == 'master':
-            tag = f'{highest_tupple[0]}.{highest_tupple[1] + 1}.0'
-            old_branch = 'master'
-        elif target == 'stable':
-            tag = f'{highest_tupple[0]}.{highest_tupple[1]}.{highest_tupple[2] + 1}'
-            old_branch = f'{highest_tupple[0]}.{highest_tupple[1]}.x'
-        else:
-            raise ValueError(f"Unknown target '{target}'")
-        new_tag = f'{tag} dev'
-
-        with open(os.getenv('GITHUB_OUTPUT'), 'a', encoding='utf-8') as env_file:
-            env_file.write(f'old_version={old_version}\n')
-            env_file.write(f'old_branch={old_branch}\n')
-            env_file.write(f'tag={tag}\n')
-            env_file.write(f'new_tag={new_tag}\n')
-        exit(0)
-
     # GITHUB_REF_TYPE may be either 'branch' or 'tag'
     GITHUB_REF_TYPE = os.environ['GITHUB_REF_TYPE']
 
