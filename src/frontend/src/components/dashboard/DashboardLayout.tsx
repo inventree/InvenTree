28dc7439a57bc4ec06959065e8ef5f26601fd6f0
Oliver Walters <oliver.henry.walters@gmail.com>
2025-06-27 03:13:48 +0000

    Add divider to dashboard

--- src/frontend/src/components/dashboard/DashboardLayout.tsx
+++ src/frontend/src/components/dashboard/DashboardLayout.tsx
@@ -1,5 +1,5 @@
 import { t } from '@lingui/core/macro';
-import { Alert, Card, Center, Loader, Text } from '@mantine/core';
+import { Alert, Card, Center, Divider, Loader, Text } from '@mantine/core';
 import { useDisclosure, useHotkeys } from '@mantine/hooks';
 import { IconInfoCircle } from '@tabler/icons-react';
 import { useCallback, useEffect, useMemo, useState } from 'react';
@@ -219,6 +219,7 @@ export default function DashboardLayout() {
         editing={editing}
         removing={removing}
       />
+      <Divider p='xs' />
       {layouts && loaded && availableWidgets.loaded ? (
         <>
           {widgetLabels.length == 0 ? (
