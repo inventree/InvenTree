4d659f769a9de4f1fc83026591e372f3def5eaa9
Oliver Walters <oliver.henry.walters@gmail.com>
2025-06-30 00:44:36 +0000

    Prevent clicks when editing dashboard items

--- src/frontend/src/components/dashboard/DashboardWidget.tsx
+++ src/frontend/src/components/dashboard/DashboardWidget.tsx
@@ -55,6 +55,20 @@ export default function DashboardWidget({
             overflowY: 'hidden'
           }}
         >
+          {/* Overlay to prevent mouse events when editing */}
+          {editing && (
+            <Box
+              style={{
+                position: 'absolute',
+                top: 0,
+                right: 0,
+                bottom: 0,
+                left: 0,
+                backgroundColor: 'rgba(255, 255, 255, 0.5)',
+                pointerEvents: 'auto'
+              }}
+            />
+          )}
           {item.render()}
         </Box>
         {removing && (
