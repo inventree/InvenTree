dce4878b1c50037010902c6478351a280ef2d4fb
Oliver Walters <oliver.henry.walters@gmail.com>
2025-06-30 02:43:55 +0000

    Add "hover image" for part display in tables

--- src/frontend/src/tables/ColumnRenderers.tsx
+++ src/frontend/src/tables/ColumnRenderers.tsx
@@ -31,6 +31,7 @@ export function PartColumn({
       <Thumbnail
         src={part?.thumbnail ?? part?.image}
         text={full_name ? part?.full_name : part?.name}
+        hover
       />
       <Group justify='flex-end' wrap='nowrap' gap='xs'>
         {part?.active == false && (
