3752c03f1b3a48e54172fe8d47bff05791366ced
Oliver Walters <oliver.henry.walters@gmail.com>
2025-07-01 12:28:41 +0000

    Log errors during tree rebuild

--- src/backend/InvenTree/stock/tasks.py
+++ src/backend/InvenTree/stock/tasks.py
@@ -13,12 +13,14 @@ def rebuild_stock_item_tree(tree_id=None):
 
     The StockItem tree uses the MPTT library to manage the tree structure.
     """
+    from InvenTree.exceptions import log_error
     from stock.models import StockItem
 
     if tree_id:
         try:
             StockItem.objects.partial_rebuild(tree_id)
         except Exception:
+            log_error('rebuild_stock_item_tree')
             logger.warning('Failed to rebuild StockItem tree')
             # If the partial rebuild fails, rebuild the entire tree
             StockItem.objects.rebuild()
