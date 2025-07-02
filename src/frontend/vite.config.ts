80adb37f2e54a07d09bdcba3c828426cd3f50e12
Oliver Walters <oliver.henry.walters@gmail.com>
2025-07-02 04:43:42 +0000

    Fixes for playwright testing
    - Ensure cookies are completely cleaned  between sessions
    - Fix base URL based on vite command
    - Fix samesite cookie mode
    - Prevent /static/ files being served by web server on :8000

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -21,6 +21,25 @@ const OUTPUT_DIR = '../../src/backend/InvenTree/web/static/web';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
+  let base: string | undefined = undefined;
+
+  // Specify the base URL based on the command
+  // In 'serve' mode, we want to use a base URL of '/web/'
+  // In 'build' mode, we want to use an empty base URL (for static file generation)
+  switch (command) {
+    case 'serve':
+      base = '/web/';
+      break;
+    case 'build':
+      base = '';
+      break;
+    default:
+      base = undefined;
+      break;
+  }
+
+  console.log(`Running Vite in '${command}' mode -> base URL: ${base}`);
+
   return {
     plugins: [
       react({
@@ -57,7 +76,7 @@ export default defineConfig(({ command, mode }) => {
     ],
     // When building, set the base path to an empty string
     // This is required to ensure that the static path prefix is observed
-    base: command == 'build' ? '' : undefined,
+    base: base,
     build: {
       manifest: true,
       outDir: OUTPUT_DIR,
