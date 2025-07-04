6453abb974d02ae2910fc1b25710da63b2088190
Oliver <oliver.henry.walters@gmail.com>
2025-07-04 11:38:21 +1000

    User token table (#9954)
    * Tweak layout of Token dialog
    
    * Render user details in API token table
    
    * Bump API version

--- src/backend/InvenTree/users/serializers.py
+++ src/backend/InvenTree/users/serializers.py
@@ -134,46 +134,6 @@ def generate_roles_dict(roles) -> dict:
     return role_dict
 
 
-class ApiTokenSerializer(InvenTreeModelSerializer):
-    """Serializer for the ApiToken model."""
-
-    in_use = serializers.SerializerMethodField(read_only=True)
-    user = serializers.PrimaryKeyRelatedField(
-        queryset=User.objects.all(), required=False
-    )
-
-    def get_in_use(self, token: ApiToken) -> bool:
-        """Return True if the token is currently used to call the endpoint."""
-        from InvenTree.middleware import get_token_from_request
-
-        request = self.context.get('request')
-        rq_token = get_token_from_request(request)
-        return token.key == rq_token
-
-    class Meta:
-        """Meta options for ApiTokenSerializer."""
-
-        model = ApiToken
-        fields = [
-            'created',
-            'expiry',
-            'id',
-            'last_seen',
-            'name',
-            'token',
-            'active',
-            'revoked',
-            'user',
-            'in_use',
-        ]
-
-    def validate(self, data):
-        """Validate the data for the serializer."""
-        if 'user' not in data:
-            data['user'] = self.context['request'].user
-        return super().validate(data)
-
-
 class GetAuthTokenSerializer(serializers.Serializer):
     """Serializer for the GetAuthToken API endpoint."""
 
@@ -246,6 +206,49 @@ class UserSerializer(InvenTreeModelSerializer):
     email = serializers.EmailField(
         label=_('Email'), help_text=_('Email address of the user'), allow_blank=True
     )
+
+
+class ApiTokenSerializer(InvenTreeModelSerializer):
+    """Serializer for the ApiToken model."""
+
+    in_use = serializers.SerializerMethodField(read_only=True)
+    user = serializers.PrimaryKeyRelatedField(
+        queryset=User.objects.all(), required=False
+    )
+
+    def get_in_use(self, token: ApiToken) -> bool:
+        """Return True if the token is currently used to call the endpoint."""
+        from InvenTree.middleware import get_token_from_request
+
+        request = self.context.get('request')
+        rq_token = get_token_from_request(request)
+        return token.key == rq_token
+
+    class Meta:
+        """Meta options for ApiTokenSerializer."""
+
+        model = ApiToken
+        fields = [
+            'created',
+            'expiry',
+            'id',
+            'last_seen',
+            'name',
+            'token',
+            'active',
+            'revoked',
+            'user',
+            'user_detail',
+            'in_use',
+        ]
+
+    def validate(self, data):
+        """Validate the data for the serializer."""
+        if 'user' not in data:
+            data['user'] = self.context['request'].user
+        return super().validate(data)
+
+    user_detail = UserSerializer(source='user', read_only=True)
 
 
 class GroupSerializer(InvenTreeModelSerializer):
