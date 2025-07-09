c6166d7c4a38e750a0464883db9c5679ea22151c
Oliver <oliver.henry.walters@gmail.com>
2025-07-09 23:32:00 +1000

    Import fix 2 (#9992)
    * Fix return types
    
    * Add getStatusCodeLabel func
    
    * Fix logic for import session drawer
    
    - Properly re-fetch session data
    - Rendering improvements
    
    * Fix icon
    
    * API permission fixes
    
    * Enhanced playwright testing
    
    * Fix playwright tests
    
    * Tweak playwright tests
    
    * Remove unused var
    
    * Tweak playwright tests

--- src/frontend/tests/pui_importing.spec.ts
+++ src/frontend/tests/pui_importing.spec.ts
@@ -50,13 +50,55 @@ test('Importing - BOM', async ({ browser }) => {
 
   await page.getByText('Mapping data columns to database fields').waitFor();
   await page.getByRole('button', { name: 'Accept Column Mapping' }).click();
+  await page.waitForTimeout(500);
 
-  await page.getByText('Processing Data').waitFor();
+  await page.getByText('Importing Data').waitFor();
   await page.getByText('0 / 4').waitFor();
+
+  await page.getByText('Torx head screw, M3 thread, 10.0mm').first().waitFor();
+  await page.getByText('Small plastic enclosure, black').first().waitFor();
+
+  // Select some rows
   await page
-    .getByLabel('Importing DataUpload FileMap')
-    .getByText('002.01-PCBA | Widget Board')
-    .waitFor();
+    .getByRole('row', { name: 'Select record 1 0 Thumbnail' })
+    .getByLabel('Select record')
+    .click();
+  await page
+    .getByRole('row', { name: 'Select record 2 1 Thumbnail' })
+    .getByLabel('Select record')
+    .click();
+
+  // Delete selected rows
+  await page
+    .getByRole('dialog', { name: 'Importing Data Upload File 2' })
+    .getByLabel('action-button-delete-selected')
+    .click();
+  await page.getByRole('button', { name: 'Delete', exact: true }).click();
+
+  await page.getByText('Success', { exact: true }).waitFor();
+  await page.getByText('Items deleted', { exact: true }).waitFor();
+
+  // Edit a row
+  await page
+    .getByRole('row', { name: 'Select record 1 2 Thumbnail' })
+    .getByLabel('row-action-menu-')
+    .click();
+  await page.getByRole('menuitem', { name: 'Edit' }).click();
+  await page.getByRole('textbox', { name: 'number-field-quantity' }).fill('12');
+
+  await page.waitForTimeout(250);
+  await page.getByRole('button', { name: 'Submit' }).click();
+  await page.waitForTimeout(250);
+
+  await page.getByText('0 / 2', { exact: true }).waitFor();
+
+  // Submit a row
+  await page
+    .getByRole('row', { name: 'Select record 1 2 Thumbnail' })
+    .getByLabel('row-action-menu-')
+    .click();
+  await page.getByRole('menuitem', { name: 'Accept' }).click();
+  await page.getByText('1 / 2', { exact: true }).waitFor();
 });
 
 test('Importing - Purchase Order', async ({ browser }) => {
