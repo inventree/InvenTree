e9a69e28e0f920da232ec31ac2fe721be8bba3c0
Oliver Walters <oliver.henry.walters@gmail.com>
2025-07-11 04:00:17 +0000

    Simpilfy test
    - tree_id is somewhat ephemeral

--- src/backend/InvenTree/InvenTree/tests.py
+++ src/backend/InvenTree/InvenTree/tests.py
@@ -51,8 +51,7 @@ class TreeFixtureTest(TestCase):
     def node_string(self, node):
         """Construct a string representation of a tree node."""
         return ':'.join([
-            str(getattr(node, attr))
-            for attr in ['parent', 'tree_id', 'level', 'lft', 'rght']
+            str(getattr(node, attr)) for attr in ['parent', 'level', 'lft', 'rght']
         ])
 
     def run_tree_test(self, model):
