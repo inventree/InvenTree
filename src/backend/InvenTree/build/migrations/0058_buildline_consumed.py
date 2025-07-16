a68ac770512093f8e6146dd81d2ef43455889116
Oliver Walters <oliver.henry.walters@gmail.com>
2025-07-16 02:06:19 +0000

    Fix migrations

--- src/backend/InvenTree/build/migrations/0058_buildline_consumed.py
+++ src/backend/InvenTree/build/migrations/0058_buildline_consumed.py
@@ -7,7 +7,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('build', '0054_build_start_date'),
+        ('build', '0057_build_external'),
     ]
 
     operations = [
