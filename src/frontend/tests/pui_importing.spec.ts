9993f41606826251431c9d812fec05f52cfb921f
Oliver Walters <oliver.henry.walters@gmail.com>
2025-08-20 04:38:46 +0000

    Further playwright fixes

--- src/frontend/tests/pui_importing.spec.ts
+++ src/frontend/tests/pui_importing.spec.ts
@@ -63,7 +63,7 @@ test('Importing - Admin Center', async ({ browser }) => {
   await page.getByRole('button', { name: 'Close' }).click();
 
   // Confirmation of full import success
-  await page.getByRole('cell', { name: '4 / 4 100%' }).first().waitFor();
+  await page.getByRole('cell', { name: '3 / 3' }).first().waitFor();
 
   // Manually delete records
   await page.getByRole('checkbox', { name: 'Select all records' }).click();
