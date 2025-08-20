a274975786dac705625ff5cd0a705c6cf029fb2a
Matthias Mair <code@mjmair.com>
2025-08-20 13:21:03 +0200

    fix: remove mfa task (#10200)
    * fix remove mfa task
    
    * add test for command
    
    * clean up after schema test
    
    * add assert to esure authenticators are really present/removed
    
    * simplify handler
    
    (cherry picked from commit 76dfd62f8493366d92a9be75fdde14be23f18cdb)

---
+++ src/backend/InvenTree/InvenTree/test_commands.py
@@ -0,0 +1,50 @@
+"""Tests for custom InvenTree management commands."""
+
+from pathlib import Path
+
+from django.contrib.auth.models import User
+from django.core.management import call_command
+from django.test import TestCase
+
+
+class CommandTestCase(TestCase):
+    """Test case for custom management commands."""
+
+    def test_schema(self):
+        """Test the schema generation command."""
+        output = call_command('schema', file='schema.yml', verbosity=0)
+        self.assertEqual(output, 'done')
+
+        Path('schema.yml').unlink()  # cleanup
+
+    def test_remove_mfa(self):
+        """Test the remove_mfa command."""
+        # missing arg
+        with self.assertRaises(Exception) as cm:
+            call_command('remove_mfa', verbosity=0)
+        self.assertEqual(
+            'Error: the following arguments are required: mail', str(cm.exception)
+        )
+
+        # no user
+        with self.assertLogs('inventree') as cm:
+            self.assertFalse(
+                call_command('remove_mfa', 'admin@example.org', verbosity=0)
+            )
+        self.assertIn('No user with this mail associated', str(cm[1]))
+
+        # correct removal
+        my_admin = User.objects.create_user(username='admin', email='admin@example.org')
+        my_admin.authenticator_set.create(type='TOTP', data={})
+        self.assertEqual(my_admin.authenticator_set.all().count(), 1)
+        output = call_command('remove_mfa', 'admin@example.org', verbosity=0)
+        self.assertEqual(output, 'done')
+        self.assertEqual(my_admin.authenticator_set.all().count(), 0)
+
+        # two users with same email
+        User.objects.create_user(username='admin2', email='admin@example.org')
+        with self.assertLogs('inventree') as cm:
+            self.assertFalse(
+                call_command('remove_mfa', 'admin@example.org', verbosity=0)
+            )
+        self.assertIn('More than one user found with this mail', str(cm[1]))
