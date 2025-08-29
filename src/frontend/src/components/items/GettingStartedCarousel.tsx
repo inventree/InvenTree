ff7c316f925f70cb938532e5090fa7b676500a25
Oliver Walters <oliver.henry.walters@gmail.com>
2025-08-29 07:20:31 +0000

    Further props updates

--- src/frontend/src/components/items/GettingStartedCarousel.tsx
+++ src/frontend/src/components/items/GettingStartedCarousel.tsx
@@ -39,8 +39,6 @@ export function GettingStartedCarousel({
     <Carousel
       slideSize={{ base: '100%', sm: '50%', md: '33.333333%' }}
       slideGap={{ base: 0, sm: 'md' }}
-      align='start'
-      loop
     >
       {slides}
     </Carousel>
