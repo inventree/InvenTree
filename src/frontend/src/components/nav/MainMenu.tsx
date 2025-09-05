335d87ef16b2760c107641104bba3a2771ecc6ab
Oliver <oliver.henry.walters@gmail.com>
2025-09-05 16:07:32 +1000

    Auth session info (#10271)
    * https://github.com/inventree/InvenTree/pull/6293
    
    * refactor to a shared component
    
    * refactoring container stuff to a wrapper
    
    * move title to wrapper
    
    * move logoff and loader to wrapper
    
    * mvoe functions to general auth
    
    * seperate login and register into seperate pages
    
    * unify auth styling
    
    * rename component
    
    * adapt to new look
    
    * check if registration is enabled
    
    * feat(frontend):add authentication debug window
    
    * clear state on logout
    
    * add reload button
    
    * reduce diff
    
    * export helper
    
    * move hover out
    
    * only show to superusers
    
    * fix state args
    
    * fix merge
    
    * fix merge
    
    * clean up diff
    
    * reduce diff
    
    * re-diff
    
    * fix shallow loading
    
    * fix test
    
    * fix umport
    
    * Move session info to user settings panel
    
    * Restrict to superuser accounts
    
    ---------
    
    Co-authored-by: Matthias Mair <code@mjmair.com>

--- src/frontend/src/components/nav/MainMenu.tsx
+++ src/frontend/src/components/nav/MainMenu.tsx
@@ -17,7 +17,6 @@ import {
   IconUserCog
 } from '@tabler/icons-react';
 import { Link, useNavigate } from 'react-router-dom';
-
 import { useShallow } from 'zustand/react/shallow';
 import { doLogout } from '../../functions/auth';
 import * as classes from '../../main.css';
@@ -32,70 +31,76 @@ export function MainMenu() {
   const { colorScheme, toggleColorScheme } = useMantineColorScheme();
 
   return (
-    <Menu width={260} position='bottom-end'>
-      <Menu.Target>
-        <UnstyledButton className={classes.layoutHeaderUser}>
-          <Group gap={7}>
-            {username() ? (
-              <Text fw={500} size='sm' style={{ lineHeight: 1 }} mr={3}>
-                {username()}
-              </Text>
-            ) : (
-              <Skeleton height={20} width={40} radius={vars.radiusDefault} />
-            )}
-            <IconChevronDown />
-          </Group>
-        </UnstyledButton>
-      </Menu.Target>
-      <Menu.Dropdown>
-        <Menu.Label>
-          <Trans>Settings</Trans>
-        </Menu.Label>
-        <Menu.Item
-          leftSection={<IconUserCog />}
-          component={Link}
-          to='/settings/user'
-        >
-          <Trans>Account Settings</Trans>
-        </Menu.Item>
-        {user?.is_staff && (
+    <>
+      <Menu width={260} position='bottom-end'>
+        <Menu.Target>
+          <UnstyledButton className={classes.layoutHeaderUser}>
+            <Group gap={7}>
+              {username() ? (
+                <Text fw={500} size='sm' style={{ lineHeight: 1 }} mr={3}>
+                  {username()}
+                </Text>
+              ) : (
+                <Skeleton height={20} width={40} radius={vars.radiusDefault} />
+              )}
+              <IconChevronDown />
+            </Group>
+          </UnstyledButton>
+        </Menu.Target>
+        <Menu.Dropdown>
+          <Menu.Label>
+            <Trans>Settings</Trans>
+          </Menu.Label>
           <Menu.Item
-            leftSection={<IconSettings />}
+            leftSection={<IconUserCog />}
             component={Link}
-            to='/settings/system'
+            to='/settings/user'
           >
-            <Trans>System Settings</Trans>
+            <Trans>Account Settings</Trans>
           </Menu.Item>
-        )}
-        {user?.is_staff && (
+          {user?.is_staff && (
+            <Menu.Item
+              leftSection={<IconSettings />}
+              component={Link}
+              to='/settings/system'
+            >
+              <Trans>System Settings</Trans>
+            </Menu.Item>
+          )}
+          {user?.is_staff && (
+            <Menu.Item
+              leftSection={<IconUserBolt />}
+              component={Link}
+              to='/settings/admin'
+            >
+              <Trans>Admin Center</Trans>
+            </Menu.Item>
+          )}
+          {user?.is_staff && <Menu.Divider />}
           <Menu.Item
-            leftSection={<IconUserBolt />}
-            component={Link}
-            to='/settings/admin'
+            onClick={toggleColorScheme}
+            leftSection={
+              colorScheme === 'dark' ? <IconSun /> : <IconMoonStars />
+            }
+            c={
+              colorScheme === 'dark'
+                ? vars.colors.yellow[4]
+                : vars.colors.blue[6]
+            }
           >
-            <Trans>Admin Center</Trans>
+            <Trans>Change Color Mode</Trans>
           </Menu.Item>
-        )}
-        {user?.is_staff && <Menu.Divider />}
-        <Menu.Item
-          onClick={toggleColorScheme}
-          leftSection={colorScheme === 'dark' ? <IconSun /> : <IconMoonStars />}
-          c={
-            colorScheme === 'dark' ? vars.colors.yellow[4] : vars.colors.blue[6]
-          }
-        >
-          <Trans>Change Color Mode</Trans>
-        </Menu.Item>
-        <Menu.Divider />
-        <Menu.Item
-          leftSection={<IconLogout />}
-          onClick={() => {
-            doLogout(navigate);
-          }}
-        >
-          <Trans>Logout</Trans>
-        </Menu.Item>
-      </Menu.Dropdown>
-    </Menu>
+          <Menu.Divider />
+          <Menu.Item
+            leftSection={<IconLogout />}
+            onClick={() => {
+              doLogout(navigate);
+            }}
+          >
+            <Trans>Logout</Trans>
+          </Menu.Item>
+        </Menu.Dropdown>
+      </Menu>
+    </>
   );
 }
