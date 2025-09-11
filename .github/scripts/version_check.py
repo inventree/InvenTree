b998c29179ebd91fb949a02bda84e2d4c0e4fade
Oliver Walters <oliver.henry.walters@gmail.com>
2025-09-11 03:06:22 +0000

    Refactor version_check.py
    - Account for non-standard release tags (rc / dev / etc)
    - Refactor code for extracting version info
    - Add argparse support
    - Update qc_checks.yaml

--- .github/scripts/version_check.py
+++ .github/scripts/version_check.py
@@ -10,6 +10,7 @@ tagged branch:
 
 """
 
+import argparse
 import itertools
 import json
 import os
@@ -21,6 +22,65 @@ import requests
 
 REPO = os.getenv('GITHUB_REPOSITORY', 'inventree/inventree')
 GITHUB_API_URL = os.getenv('GITHUB_API_URL', 'https://api.github.com')
+
+
+def get_src_dir() -> Path:
+    """Return the path to the InvenTree source directory."""
+    here = Path(__file__).parent.absolute()
+    src_dir = here.joinpath('..', '..', 'src', 'backend', 'InvenTree', 'InvenTree')
+
+    if not src_dir.exists():
+        raise FileNotFoundError(
+            f"Could not find InvenTree source directory: '{src_dir}'"
+        )
+
+    return src_dir
+
+
+def get_inventree_version() -> str:
+    """Return the InvenTree version string."""
+    src_dir = get_src_dir()
+    version_file = src_dir.joinpath('version.py')
+
+    if not version_file.exists():
+        raise FileNotFoundError(
+            f"Could not find InvenTree version file: '{version_file}'"
+        )
+
+    with open(version_file, encoding='utf-8') as f:
+        text = f.read()
+
+        # Extract the InvenTree software version
+        results = re.findall(r"""INVENTREE_SW_VERSION = '(.*)'""", text)
+
+        if len(results) != 1:
+            raise ValueError(f'Could not find INVENTREE_SW_VERSION in {version_file}')
+
+        return results[0]
+
+
+def get_api_version() -> str:
+    """Return the InvenTree API version string."""
+    src_dir = get_src_dir()
+    api_version_file = src_dir.joinpath('api_version.py')
+
+    if not api_version_file.exists():
+        raise FileNotFoundError(
+            f"Could not find InvenTree API version file: '{api_version_file}'"
+        )
+
+    with open(api_version_file, encoding='utf-8') as f:
+        text = f.read()
+
+        # Extract the InvenTree software version
+        results = re.findall(r"""INVENTREE_API_VERSION = (.*)""", text)
+
+        if len(results) != 1:
+            raise ValueError(
+                f'Could not find INVENTREE_API_VERSION in {api_version_file}'
+            )
+
+        return results[0].strip().strip('"').strip("'")
 
 
 def version_number_to_tuple(version_string: str) -> tuple[int, int, int, str]:
@@ -75,7 +135,14 @@ def get_existing_release_tags(include_prerelease: bool = True):
         tag = release['tag_name'].strip()
 
         version_tuple = version_number_to_tuple(tag)
-        tags.append(version_tuple)
+
+        if len(version_tuple) >= 4 and version_tuple[3]:
+            # Skip prerelease tags
+            if not include_prerelease:
+                print('-- skipping prerelease tag:', tag)
+                continue
+
+        tags.append(tag)
 
     return tags
 
@@ -95,11 +162,18 @@ def check_version_number(version_string, allow_duplicate=False):
     # Assume that this is the highest release, unless told otherwise
     highest_release = True
 
+    # A non-standard tag cannot be the 'highest' release
+    if len(version_tuple) >= 4 and version_tuple[3]:
+        highest_release = False
+        print(f"-- Version tag '{version_string}' cannot be the highest release")
+
     for release in existing:
-        if release == version_tuple and not allow_duplicate:
+        if version_string == release and not allow_duplicate:
             raise ValueError(f"Duplicate release '{version_string}' exists!")
 
-        if release > version_tuple:
+        release_tuple = version_number_to_tuple(release)
+
+        if release_tuple > version_tuple:
             highest_release = False
             print(f'Found newer release: {release!s}')
 
@@ -107,23 +181,43 @@ def check_version_number(version_string, allow_duplicate=False):
 
 
 if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='InvenTree Version Check')
+    parser.add_argument(
+        '--show-version',
+        action='store_true',
+        help='Print the InvenTree version and exit',
+    )
+    parser.add_argument(
+        '--show-api-version',
+        action='store_true',
+        help='Print the InvenTree API version and exit',
+    )
+    parser.add_argument(
+        '--decrement-api',
+        type=str,
+        default='false',
+        help='Decrement the API version by 1 and print',
+    )
+
+    args = parser.parse_args()
+
+    inventree_version = get_inventree_version()
+    inventree_api_version = int(get_api_version())
+
+    if args.show_version:
+        print(inventree_version)
+        sys.exit(0)
+
+    if args.show_api_version:
+        if args.decrement_api.lower() == 'true':
+            inventree_api_version -= 1
+        print(inventree_api_version)
+        sys.exit(0)
+
     # Ensure that we are running in GH Actions
     if os.environ.get('GITHUB_ACTIONS', '') != 'true':
         print('This script is intended to be run within a GitHub Action!')
         sys.exit(1)
-
-    if 'only_version' in sys.argv:
-        here = Path(__file__).parent.absolute()
-        version_file = here.joinpath(
-            '..', '..', 'src', 'backend', 'InvenTree', 'InvenTree', 'api_version.py'
-        )
-        text = version_file.read_text()
-        results = re.findall(r"""INVENTREE_API_VERSION = (.*)""", text)
-        # If 2. args is true lower the version number by 1
-        if len(sys.argv) > 2 and sys.argv[2] == 'true':
-            results[0] = str(int(results[0]) - 1)
-        print(results[0])
-        exit(0)
 
     # GITHUB_REF_TYPE may be either 'branch' or 'tag'
     GITHUB_REF_TYPE = os.environ['GITHUB_REF_TYPE']
@@ -139,26 +233,7 @@ if __name__ == '__main__':
     print(f'GITHUB_REF_TYPE: {GITHUB_REF_TYPE}')
     print(f'GITHUB_BASE_REF: {GITHUB_BASE_REF}')
 
-    here = Path(__file__).parent.absolute()
-    version_file = here.joinpath(
-        '..', '..', 'src', 'backend', 'InvenTree', 'InvenTree', 'version.py'
-    )
-
-    version = None
-
-    with open(version_file, encoding='utf-8') as f:
-        text = f.read()
-
-        # Extract the InvenTree software version
-        results = re.findall(r"""INVENTREE_SW_VERSION = '(.*)'""", text)
-
-        if len(results) != 1:
-            print(f'Could not find INVENTREE_SW_VERSION in {version_file}')
-            sys.exit(1)
-
-        version = results[0]
-
-    print(f"InvenTree Version: '{version}'")
+    print(f"InvenTree Version: '{inventree_version}'")
 
     # Check version number and look for existing versions
     # If a release is found which matches the current tag, throw an error
@@ -173,7 +248,9 @@ if __name__ == '__main__':
     if GITHUB_BASE_REF == 'stable':
         allow_duplicate = True
 
-    highest_release = check_version_number(version, allow_duplicate=allow_duplicate)
+    highest_release = check_version_number(
+        inventree_version, allow_duplicate=allow_duplicate
+    )
 
     # Determine which docker tag we are going to use
     docker_tags = None
@@ -183,8 +260,10 @@ if __name__ == '__main__':
         version_tag = GITHUB_REF.split('/')[-1]
         print(f"Checking requirements for tagged release - '{version_tag}':")
 
-        if version_tag != version:
-            print(f"Version number '{version}' does not match tag '{version_tag}'")
+        if version_tag != inventree_version:
+            print(
+                f"Version number '{inventree_version}' does not match tag '{version_tag}'"
+            )
             sys.exit
 
         docker_tags = [version_tag, 'stable'] if highest_release else [version_tag]
@@ -195,7 +274,7 @@ if __name__ == '__main__':
 
     else:
         print('Unsupported branch / version combination:')
-        print(f'InvenTree Version: {version}')
+        print(f'InvenTree Version: {inventree_version}')
         print('GITHUB_REF_TYPE:', GITHUB_REF_TYPE)
         print('GITHUB_BASE_REF:', GITHUB_BASE_REF)
         print('GITHUB_REF:', GITHUB_REF)
@@ -205,7 +284,7 @@ if __name__ == '__main__':
         print('Docker tags could not be determined')
         sys.exit(1)
 
-    print(f"Version check passed for '{version}'!")
+    print(f"Version check passed for '{inventree_version}'!")
     print(f"Docker tags: '{docker_tags}'")
 
     target_repos = [REPO.lower(), f'ghcr.io/{REPO.lower()}']
