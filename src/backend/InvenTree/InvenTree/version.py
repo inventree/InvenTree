cbbfcc5994cd21550e3cc67a2dded0d183213073
Oliver Walters <oliver.henry.walters@gmail.com>
2025-09-11 23:25:03 +0000

    1.0.0 rc0
    - Release candidate for testing release process

--- src/backend/InvenTree/InvenTree/version.py
+++ src/backend/InvenTree/InvenTree/version.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from .api_version import INVENTREE_API_TEXT, INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = '1.0.0'
+INVENTREE_SW_VERSION = '1.0.0-rc0'
 
 
 logger = logging.getLogger('inventree')
