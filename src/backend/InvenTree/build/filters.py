0393ea4449b390ef2206afec0e497a6bd8a2106b
Oliver Walters <oliver.henry.walters@gmail.com>
2025-09-26 23:30:44 +0000

    Use Max wrapper

--- src/backend/InvenTree/build/filters.py
+++ src/backend/InvenTree/build/filters.py
@@ -1,13 +1,15 @@
 """Queryset filtering helper functions for the Build app."""
 
-from django.db.models import DecimalField, ExpressionWrapper, F, Sum
+from django.db.models import DecimalField, ExpressionWrapper, F, Max, Sum
 from django.db.models.functions import Coalesce, Greatest
 
 
 def annotate_required_quantity():
     """Annotate the 'required' quantity for each build item in the queryset."""
     return Greatest(
-        ExpressionWrapper(F('quantity') - F('consumed'), output_field=DecimalField()),
+        ExpressionWrapper(
+            Max(F('quantity')) - Max(F('consumed')), output_field=DecimalField()
+        ),
         0,
         output_field=DecimalField(),
     )
