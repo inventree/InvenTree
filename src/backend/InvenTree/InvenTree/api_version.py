cdc97db33f4a248726f3373dcc69f699d22f331b
Oliver Walters <oliver.henry.walters@gmail.com>
2025-10-06 00:54:18 +0000

    Update API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,13 +1,17 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 401
+INVENTREE_API_VERSION = 402
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
 
-v401 -> 2025-10-04 : https://github.com/inventree/InvenTree/pull/10381
+v204 -> 2025-10-06: https://github.com/inventree/InvenTree/pull/10499
+    - Adds ability to partially scrap a build output
+    - Adds ability to partially complete a build output
+
+v401 -> 2025-10-06 : https://github.com/inventree/InvenTree/pull/10381
     - Adds machine properties to machine API endpoints
 
 v400 -> 2025-10-05 : https://github.com/inventree/InvenTree/pull/10486
