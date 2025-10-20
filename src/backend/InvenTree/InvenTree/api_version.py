a02d1011e7a51beac15bd1f780b85a0e7f0d7991
Matthias Mair <code@mjmair.com>
2025-10-20 21:54:40 +0200

    chores(backend): bump various deps (#10624)
    * bump various deps
    
    * align helpers deps
    
    * revert allauth change
    
    * fix style
    
    * bump allauth too
    
    * bum api version as there is a small allauth change

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,11 +1,14 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 412
+INVENTREE_API_VERSION = 413
 
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v413 -> 2025-10-20 : https://github.com/inventree/InvenTree/pull/10624
+    - Upstream fixes to django-allauth schema - no functional changes
 
 v412 -> 2025-10-19 : https://github.com/inventree/InvenTree/pull/10549
     - added a new query parameter for the PartsList api: price_breaks (default: false)
