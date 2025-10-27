6fe6b43481515c7a21d15b088b80688cff49736e
Matthias Mair <code@mjmair.com>
2025-10-27 23:33:10 +0100

    fix registration

--- src/frontend/src/pages/Auth/MFA.tsx
+++ src/frontend/src/pages/Auth/MFA.tsx
@@ -1,10 +1,17 @@
+import {
+  type CredentialRequestOptionsJSON,
+  get,
+  parseRequestOptionsFromJSON
+} from '@github/webauthn-json/browser-ponyfill';
+import { ApiEndpoints, apiUrl } from '@lib/index';
 import { t } from '@lingui/core/macro';
 import { Trans } from '@lingui/react/macro';
-import { Button, Checkbox, Code, TextInput } from '@mantine/core';
+import { Button, Checkbox, TextInput } from '@mantine/core';
 import { useForm } from '@mantine/form';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import { useShallow } from 'zustand/react/shallow';
+import { api } from '../../App';
 import { handleMfaLogin } from '../../functions/auth';
 import { useServerApiState } from '../../states/ServerApiState';
 import { Wrapper } from './Layout';
@@ -17,8 +24,34 @@ export default function Mfa() {
   const [mfa_context] = useServerApiState(
     useShallow((state) => [state.mfa_context])
   );
-
+  const [webauthn_challenge, setWebauthnChallenge] = useState<string | null>();
   const mfa_types = mfa_context?.types || [];
+
+  useEffect(() => {
+    // Extract webauthn challenge if the type is available and not already set
+    if (
+      (mfa_types.includes('webauthn') || mfa_types.includes('webauthn_2fa')) &&
+      !webauthn_challenge
+    ) {
+      api
+        .get(apiUrl(ApiEndpoints.auth_webauthn_login))
+        .catch(() => {})
+        .then((response) => {
+          if (response && response.status === 200) {
+            const challenge = response.data.data.request_options;
+            setWebauthnChallenge(challenge);
+          }
+        });
+    }
+  }, [mfa_types, webauthn_challenge]);
+
+  // try webauthn login automatically if available
+  useEffect(() => {
+    if (webauthn_challenge) {
+      webauthnLoginTry(webauthn_challenge as CredentialRequestOptionsJSON);
+    }
+  }, [webauthn_challenge]);
+
   return (
     <Wrapper titleText={t`Multi-Factor Authentication`} logOff>
       {(mfa_types.includes('recovery_codes') || mfa_types.includes('totp')) && (
@@ -64,4 +97,18 @@ export default function Mfa() {
       </Button>
     </Wrapper>
   );
+}
+
+async function webauthnLoginTry(
+  webauthn_challenge: CredentialRequestOptionsJSON
+) {
+  try {
+    const options = parseRequestOptionsFromJSON(webauthn_challenge);
+    const credential = await get(options);
+    const reauthResp = await api.post(ApiEndpoints.auth_webauthn_login, {
+      credential: credential
+    });
+  } catch (e) {
+    console.error(e);
+  }
 }
