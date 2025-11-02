34436f933d81c2ce09effce3fda56aec6b2b64cb
Oliver <oliver.henry.walters@gmail.com>
2025-11-02 13:18:23 +1100

    Bump version number to 1.1.0 (#10733)
    * Bump version number to 1.1.0
    
    * Add release tag to CHANGELOG.md
    
    * Cleanup
    
    * Remove links

--- src/backend/InvenTree/InvenTree/version.py
+++ src/backend/InvenTree/InvenTree/version.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from .api_version import INVENTREE_API_TEXT, INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = '1.1.0 dev'
+INVENTREE_SW_VERSION = '1.1.0'
 
 
 logger = logging.getLogger('inventree')
