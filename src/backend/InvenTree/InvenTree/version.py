397311bd516cc94c3b50d7c5591958257a0e7878
Oliver <oliver.henry.walters@gmail.com>
2025-11-03 11:16:25 +1100

    Bump InvenTree software version to 1.1.2

--- src/backend/InvenTree/InvenTree/version.py
+++ src/backend/InvenTree/InvenTree/version.py
@@ -18,7 +18,7 @@ from django.conf import settings
 from .api_version import INVENTREE_API_TEXT, INVENTREE_API_VERSION
 
 # InvenTree software version
-INVENTREE_SW_VERSION = '1.1.1'
+INVENTREE_SW_VERSION = '1.1.2'
 
 
 logger = logging.getLogger('inventree')
