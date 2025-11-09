3e1a05b1ad63395e50ef0f4f720fe9f2a152dbe4
Matthias Mair <code@mjmair.com>
2025-11-09 20:09:31 +0100

    cleanup helper

--- src/backend/InvenTree/InvenTree/helpers_mfa.py
+++ src/backend/InvenTree/InvenTree/helpers_mfa.py
@@ -5,14 +5,18 @@ from allauth.mfa.totp.internal import auth as allauth_totp_auth
 
 
 def get_codes(user):
-    """Generate active TOTP and recovery codes for a user."""
+    """Generate active TOTP and recovery codes for a user.
+
+    Args:
+        user: User instance
+
+    Returns:
+        Tuple of (TOTP Authenticator instance, list of recovery codes, TOTP secret)
+    """
     secret = allauth_totp_auth.generate_totp_secret()
     totp_auth = allauth_totp_auth.TOTP.activate(user, secret).instance
     rc_auth = RecoveryCodes.activate(user).instance
 
     # Get usable codes
-    print(totp_auth)
     rc_codes = rc_auth.wrap().get_unused_codes()
-    if len(rc_codes) == 0:
-        raise ValueError('No recovery codes generated')
     return totp_auth, rc_codes, secret
