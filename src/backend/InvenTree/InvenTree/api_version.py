726c9441912761dfcd9630f8130b046f531e5bfe
Matthias Mair <code@mjmair.com>
2025-12-09 00:06:20 +0100

    bump api

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 430
+INVENTREE_API_VERSION = 431
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v431 -> 2025-12-09 : https://github.com/inventree/InvenTree/pull/10983
+    - Adds an API to gather dynamic price information for a Part
 
 v430 -> 2025-12-04 : https://github.com/inventree/InvenTree/pull/10699
     - Removed the "PartParameter" and "PartParameterTemplate" API endpoints
