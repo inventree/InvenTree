64a14d83c5d6d20590f80c6eeffb513d0ad0c6f1
Oliver Walters <oliver.henry.walters@gmail.com>
2025-12-15 00:04:14 +0000

    Fix for data export

--- src/backend/InvenTree/data_exporter/mixins.py
+++ src/backend/InvenTree/data_exporter/mixins.py
@@ -264,10 +264,8 @@ class DataExportViewMixin:
         exporting = kwargs.pop('exporting', None)
 
         if exporting is None:
-            exporting = (
-                self.request.method.lower() in ['options', 'get']
-                and self.is_exporting()
-            )
+            method = str(getattr(self.request, 'method', '')).lower()
+            exporting = method in ['options', 'get'] and self.is_exporting()
 
         if exporting:
             # Override kwargs when initializing the DataExportOptionsSerializer
