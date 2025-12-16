dbb18f6eabe391a4a6e8af70970065283664442a
Matthias Mair <code@mjmair.com>
2025-12-16 21:37:48 +0100

    fix api schema generation

--- src/backend/InvenTree/InvenTree/management/commands/schema.py
+++ src/backend/InvenTree/InvenTree/management/commands/schema.py
@@ -70,8 +70,10 @@ class Command(spectacular.Command):
         # Reformat paths
         for p_name, p_spec in spec['paths'].items():
             # strip path name
-            p_name = p_name.removeprefix(dja_path_prefix).removeprefix(
-                '/_allauth/browser/v1/'
+            p_name = (
+                p_name.removeprefix(dja_path_prefix)
+                .removeprefix('/_allauth/browser/v1/')
+                .removeprefix('/_allauth/app/v1/')
             )
 
             # fix refs
