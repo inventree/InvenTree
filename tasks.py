69c5387273177dfc9acdc8e21c0145e510a548a6
Matthias Mair <code@mjmair.com>
2025-12-17 23:20:05 +0100

    rollback changes

--- tasks.py
+++ tasks.py
@@ -799,10 +799,7 @@ def migrate(c):
     This is a critical step if the database schema have been altered!
     """
     info('Running InvenTree database migrations...')
-    info('Listing commands...')
-    run(c, 'python3 manage.py --help', manage_py_dir(), False)
 
-    info('Making migrations...')
     # Run custom management command which wraps migrations in "maintenance mode"
     manage(c, 'makemigrations')
     manage(c, 'runmigrations', pty=True)
