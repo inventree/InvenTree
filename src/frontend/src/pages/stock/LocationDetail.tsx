3a319955e794d15eacbf7372adc01eccc33bad79
Oliver Walters <oliver.henry.walters@gmail.com>
2026-01-06 03:38:05 +0000

    Frontend support

--- src/frontend/src/pages/stock/LocationDetail.tsx
+++ src/frontend/src/pages/stock/LocationDetail.tsx
@@ -6,7 +6,13 @@ import { getDetailUrl } from '@lib/functions/Navigation';
 import type { StockOperationProps } from '@lib/types/Forms';
 import { t } from '@lingui/core/macro';
 import { Group, Skeleton, Stack, Text } from '@mantine/core';
-import { IconInfoCircle, IconPackages, IconSitemap } from '@tabler/icons-react';
+import {
+  IconInfoCircle,
+  IconListDetails,
+  IconPackages,
+  IconSitemap,
+  IconTable
+} from '@tabler/icons-react';
 import { useMemo, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { api } from '../../App';
@@ -30,6 +36,8 @@ import NavigationTree from '../../components/nav/NavigationTree';
 import { PageDetail } from '../../components/nav/PageDetail';
 import type { PanelType } from '../../components/panels/Panel';
 import { PanelGroup } from '../../components/panels/PanelGroup';
+import ParametersPanel from '../../components/panels/ParametersPanel';
+import SegmentedControlPanel from '../../components/panels/SegmentedControlPanel';
 import LocateItemButton from '../../components/plugins/LocateItemButton';
 import { stockLocationFields } from '../../forms/StockForms';
 import { InvenTreeIcon } from '../../functions/icons';
@@ -42,6 +50,7 @@ import { useStockAdjustActions } from '../../hooks/UseStockAdjustActions';
 import { useUserState } from '../../states/UserState';
 import { PartListTable } from '../../tables/part/PartTable';
 import { StockItemTable } from '../../tables/stock/StockItemTable';
+import StockLocationParametricTable from '../../tables/stock/StockLocationParametricTable';
 import { StockLocationTable } from '../../tables/stock/StockLocationTable';
 
 export default function Stock() {
@@ -161,6 +170,8 @@ export default function Stock() {
     );
   }, [location, instanceQuery]);
 
+  const [sublocationView, setSublocationView] = useState<string>('table');
+
   const locationPanels: PanelType[] = useMemo(() => {
     return [
       {
@@ -169,12 +180,32 @@ export default function Stock() {
         icon: <IconInfoCircle />,
         content: detailsPanel
       },
-      {
+      SegmentedControlPanel({
         name: 'sublocations',
         label: id ? t`Sublocations` : t`Stock Locations`,
         icon: <IconSitemap />,
-        content: <StockLocationTable parentId={id} />
-      },
+        hidden: !user.hasViewPermission(ModelType.stocklocation),
+        selection: sublocationView,
+        onChange: setSublocationView,
+        options: [
+          {
+            value: 'table',
+            label: t`Table View`,
+            icon: <IconTable />,
+            content: <StockLocationTable parentId={id} />
+          },
+          {
+            value: 'parametric',
+            label: t`Parametric View`,
+            icon: <IconListDetails />,
+            content: (
+              <StockLocationParametricTable
+                queryParams={id ? { parent: id } : {}}
+              />
+            )
+          }
+        ]
+      }),
       {
         name: 'stock-items',
         label: t`Stock Items`,
@@ -203,9 +234,14 @@ export default function Stock() {
             }}
           />
         )
-      }
+      },
+      ParametersPanel({
+        model_type: ModelType.stocklocation,
+        model_id: location.pk,
+        hidden: !location.pk
+      })
     ];
-  }, [location, id]);
+  }, [sublocationView, location, id]);
 
   const editLocation = useEditApiFormModal({
     url: ApiEndpoints.stock_location_list,
