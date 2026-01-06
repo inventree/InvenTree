962b8efbced99e07c8a9055e15cd09ff757434a2
Oliver Walters <oliver.henry.walters@gmail.com>
2026-01-06 04:04:40 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 435
+INVENTREE_API_VERSION = 436
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v436 -> 2026-01-06 : https://github.com/inventree/InvenTree/pull/11084
+    - Add generic parameter support for the StockLocation model
 
 v435 -> 2025-12-16 : https://github.com/inventree/InvenTree/pull/11030
     - Adds token refresh endpoint to auth API
