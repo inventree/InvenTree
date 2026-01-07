eba2e3c69457ca3f71d44db43df8b021507e54ad
Oliver Walters <oliver.henry.walters@gmail.com>
2026-01-07 22:57:44 +0000

    Fix more prefetch issues

--- src/backend/InvenTree/company/serializers.py
+++ src/backend/InvenTree/company/serializers.py
@@ -268,13 +268,7 @@ class ManufacturerPartSerializer(
             source='part', many=False, read_only=True, allow_null=True
         ),
         True,
-        prefetch_fields=[
-            Prefetch(
-                'part',
-                queryset=part.models.Part.objects.all().select_related('pricing_data'),
-            ),
-            'part__category',
-        ],
+        prefetch_fields=['part', 'part__pricing_data', 'part__category'],
     )
 
     pretty_name = enable_filter(
@@ -440,12 +434,7 @@ class SupplierPartSerializer(
             label=_('Part'), source='part', many=False, read_only=True, allow_null=True
         ),
         False,
-        prefetch_fields=[
-            Prefetch(
-                'part',
-                queryset=part.models.Part.objects.all().select_related('pricing_data'),
-            )
-        ],
+        prefetch_fields=['part', 'part__pricing_data'],
     )
 
     supplier_detail = enable_filter(
