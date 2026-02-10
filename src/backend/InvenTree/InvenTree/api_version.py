1934bd00ecec4dc2f76a214ec8f0ba3458e050f9
Oliver Walters <oliver.henry.walters@gmail.com>
2026-02-10 12:18:22 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 450
+INVENTREE_API_VERSION = 451
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v451 -> 2026-02-10 : https://github.com/inventree/InvenTree/pull/11276
+    - Adds "install_into_detail" field to the BuildItem API endpoint
 
 v450 -> 2026-02-10 : https://github.com/inventree/InvenTree/pull/11260
     - Adds "part" field to the StockItemTracking model and API endpoints
