033ad420ceb0fb1e985b0f071a1dce13028b2ad7
Oliver <oliver.henry.walters@gmail.com>
2026-02-17 00:22:44 +1100

    Change default barcode type (#11304)
    * Change default barcode type
    
    * Bump plugin version
    
    * Adjust unit testing
    
    * Tweak unit tests
    
    * Tweak docs

--- src/backend/InvenTree/plugin/builtin/barcodes/inventree_barcode.py
+++ src/backend/InvenTree/plugin/builtin/barcodes/inventree_barcode.py
@@ -26,7 +26,7 @@ class InvenTreeInternalBarcodePlugin(SettingsMixin, BarcodeMixin, InvenTreePlugi
     NAME = 'InvenTreeBarcode'
     TITLE = _('InvenTree Barcodes')
     DESCRIPTION = _('Provides native support for barcodes')
-    VERSION = '2.1.0'
+    VERSION = '2.2.0'
     AUTHOR = _('InvenTree contributors')
 
     SETTINGS = {
@@ -37,7 +37,7 @@ class InvenTreeInternalBarcodePlugin(SettingsMixin, BarcodeMixin, InvenTreePlugi
                 ('json', _('JSON barcodes (human readable)')),
                 ('short', _('Short barcodes (space optimized)')),
             ],
-            'default': 'json',
+            'default': 'short',
         },
         'SHORT_BARCODE_PREFIX': {
             'name': _('Short Barcode Prefix'),
