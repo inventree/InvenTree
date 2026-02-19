5bf6b03a02b8a911894e83b00a43f6b25e6d62b8
Oliver Walters <oliver.henry.walters@gmail.com>
2026-02-19 08:23:04 +0000

    Additional exclusions

--- src/backend/InvenTree/InvenTree/ready.py
+++ src/backend/InvenTree/InvenTree/ready.py
@@ -111,6 +111,18 @@ def isGeneratingSchema():
     if isCollectingPlugins():
         return False
 
+    # Additional set of commands which should not trigger schema generation
+    excluded_commands = [
+        'collectstatic',
+        'wait_for_db',
+        'makemessages',
+        'compilemessages',
+        'check',
+    ]
+
+    if any(cmd in sys.argv for cmd in excluded_commands):
+        return False
+
     if 'schema' in sys.argv:
         return True
 
