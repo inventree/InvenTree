02d20816dc197741245d9492908ee357d4b1f489
Oliver Walters <oliver.henry.walters@gmail.com>
2026-02-22 02:08:38 +0000

    Fix api_version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,15 +1,25 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 456
+INVENTREE_API_VERSION = 457
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
 
-v456 -> 2026-02-22 : https://github.com/inventree/InvenTree/pull/11073
+v457 -> 2026-02-22 : https://github.com/inventree/InvenTree/pull/11073
     - Add OptionalField class for cleaner handling of optional fields in serializers
+
+v456 -> 2026-02-20 : https://github.com/inventree/InvenTree/pull/11303
     - Adds "primary" field to the SupplierPart API
     - Removes "default_supplier" field from the Part API
+
+v455 -> 2026-02-19 : https://github.com/inventree/InvenTree/pull/11383
+    - Adds "exists_for_model_id" filter to ParameterTemplate API endpoint
+    - Adds "exists_for_related_model" filter to ParameterTemplate API endpoint
+    - Adds "exists_for_related_model_id" filter to ParameterTemplate API endpoint
+
+v454 -> 2026-02-19 : https://github.com/inventree/InvenTree/pull/11379
+    - Adds "purchase_price" ordering option to StockItem API endpoint
 
 v455 -> 2026-02-19 : https://github.com/inventree/InvenTree/pull/11383
     - Adds "exists_for_model_id" filter to ParameterTemplate API endpoint
