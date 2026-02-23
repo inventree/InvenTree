835814a7657cba25b19dd1b890a29c10e982c7e6
Justus Rijjke <justusrijke@gmail.com>
2026-02-23 14:44:05 +0000

    Added entry to api_version.py

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 457
+INVENTREE_API_VERSION = 458
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v458 -> 2026-02-23 : https://github.com/inventree/InvenTree/pull/11411
+    - Changed PurchaseOrderLine "auto_pricing" default value from true to false
 
 v457 -> 2026-02-11 : https://github.com/inventree/InvenTree/pull/10887
     - Extend the "auto allocate" wizard API to include tracked items
