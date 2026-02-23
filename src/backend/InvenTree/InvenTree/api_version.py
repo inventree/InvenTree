91f50f78606b47e9d154b2e1e67926c1dc93e49f
jmerro <jack@contour3d.com.au>
2026-02-23 13:08:24 +0000

    never trust github conflict resolution

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,20 +1,18 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 458
+INVENTREE_API_VERSION = 459
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
 
-<<<<<<< po-updated-at-field
-v458 -> 2026-02-23 : https://github.com/inventree/InvenTree/pull/11374
+v459 -> 2026-02-23 : https://github.com/inventree/InvenTree/pull/11374
     - Adds "updated_at" field to PurchaseOrder, SalesOrder and ReturnOrder API endpoints
     - Adds "updated_before" and "updated_after" date filters to all three order list endpoints
     - Adds "updated_at" ordering option to all three order list endpoints
-=======
+
 v458 -> 2026-02-22 : https://github.com/inventree/InvenTree/pull/11401
     - siwtches token refresh endpoint to use POST instead of GET (upstream allauth change)
->>>>>>> master
 
 v457 -> 2026-02-11 : https://github.com/inventree/InvenTree/pull/10887
     - Extend the "auto allocate" wizard API to include tracked items
