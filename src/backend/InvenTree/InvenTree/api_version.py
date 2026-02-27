a9fdcf2ee6c7b3d8feb14952390d968c1453b9fb
Matthias Mair <code@mjmair.com>
2026-02-27 01:11:58 +0100

    bump API

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 459
+INVENTREE_API_VERSION = 460
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v460 -> 2026-02-27 : https://github.com/inventree/InvenTree/pull/9604
+    - Refactors the user API endpoint to be less ambiguous in their naming
 
 v459 -> 2026-02-23 : https://github.com/inventree/InvenTree/pull/11411
     - Changed PurchaseOrderLine "auto_pricing" default value from true to false
