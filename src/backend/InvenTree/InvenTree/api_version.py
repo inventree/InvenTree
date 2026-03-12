1be08e4420bb0a8ab5e1d864f98d26e7e91404b5
Oliver <oliver.henry.walters@gmail.com>
2026-03-12 18:03:31 +1100

    [API] Allow filtering ScheduledTask objects by name (#11497)
    * [API] Allow filtering ScheduledTask objects by name
    
    * Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 461
+INVENTREE_API_VERSION = 462
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v462 -> 2026-03-12 : https://github.com/inventree/InvenTree/pull/11497
+    - Allow "ScheduledTask" API endpoint to be filtered by "name" field
 
 v461 -> 2026-03-10 : https://github.com/inventree/InvenTree/pull/11479
     - Adds option to copy parameters when duplicating an order via the API
