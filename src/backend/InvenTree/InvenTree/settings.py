ccc348aa20d407585b31d4382ac3860d2cb47de0
Matthias Mair <code@mjmair.com>
2026-03-29 05:27:05 +0200

    feat(backend): add inventree version to every response (#11611)
    * feat(backend): add inventree version to every response
    
    * add more info
    
    (cherry picked from commit e3c9a35bae44c3b9f955325516557eb6df7c7603)

--- src/backend/InvenTree/InvenTree/settings.py
+++ src/backend/InvenTree/InvenTree/settings.py
@@ -362,6 +362,7 @@ MIDDLEWARE = CONFIG.get(
         'InvenTree.middleware.InvenTreeRequestCacheMiddleware',  # Request caching
         'InvenTree.middleware.InvenTreeHostSettingsMiddleware',  # Ensuring correct hosting/security settings
         'django_structlog.middlewares.RequestMiddleware',  # Structured logging
+        'InvenTree.middleware.InvenTreeVersionHeaderMiddleware',
     ],
 )
 
