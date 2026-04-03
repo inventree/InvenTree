614664c2498fccaf1e47356d3205d094298d2d9f
Oliver Walters <oliver.henry.walters@gmail.com>
2026-04-03 08:36:21 +0000

    Refactor UI display

--- src/frontend/src/pages/part/PartDetail.tsx
+++ src/frontend/src/pages/part/PartDetail.tsx
@@ -325,55 +325,24 @@ export default function PartDetail() {
       refetchOnMount: true
     });
 
-  // Fetch information on part revision
+  const revisionsEnabled = useMemo(
+    () => globalSettings.isSet('PART_ENABLE_REVISION'),
+    [globalSettings]
+  );
+
+  // Fetch information on parts which are revisions of *this* part
   const partRevisionQuery = useQuery({
     refetchOnMount: true,
-    queryKey: [
-      'part_revisions',
-      part.pk,
-      part.revision_of,
-      part.revision_count
-    ],
-    queryFn: async () => {
-      if (!part.revision_of && !part.revision_count) {
-        return [];
-      }
-
-      const revisions = [];
-
-      // First, fetch information for the top-level part
-      if (part.revision_of) {
-        await api
-          .get(apiUrl(ApiEndpoints.part_list, part.revision_of))
-          .then((response) => {
-            revisions.push(response.data);
-          });
-      } else {
-        revisions.push(part);
-      }
-
-      const url = apiUrl(ApiEndpoints.part_list);
-
-      await api
-        .get(url, {
+    enabled: revisionsEnabled && !!part && !!part.revision_count,
+    queryKey: ['part_revisions', part.pk, part.revision_count],
+    queryFn: async () =>
+      api
+        .get(apiUrl(ApiEndpoints.part_list), {
           params: {
-            revision_of: part.revision_of || part.pk
+            revision_of: part.pk
           }
         })
-        .then((response) => {
-          switch (response.status) {
-            case 200:
-              response.data.forEach((r: any) => {
-                revisions.push(r);
-              });
-              break;
-            default:
-              break;
-          }
-        });
-
-      return revisions;
-    }
+        .then((response) => response.data)
   });
 
   const partRevisionOptions: any[] = useMemo(() => {
@@ -393,26 +362,14 @@ export default function PartDetail() {
       };
     });
 
-    // Add this part if not already available
-    if (!options.find((o) => o.value == part.pk)) {
-      options.push({
-        value: part.pk,
-        label: part.full_name,
-        part: part
-      });
-    }
-
     return options.sort((a, b) => {
       return `${a.part.revision}`.localeCompare(b.part.revision);
     });
   }, [part, partRevisionQuery.isFetching, partRevisionQuery.data]);
 
   const enableRevisionSelection: boolean = useMemo(() => {
-    return (
-      partRevisionOptions.length > 0 &&
-      globalSettings.isSet('PART_ENABLE_REVISION')
-    );
-  }, [partRevisionOptions, globalSettings]);
+    return partRevisionOptions.length > 0 && revisionsEnabled;
+  }, [partRevisionOptions, revisionsEnabled]);
 
   const detailsPanel = useMemo(() => {
     if (instanceQuery.isFetching) {
