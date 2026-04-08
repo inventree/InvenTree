4665876e83a2cdb0dbe8cac64a5882645db6a932
Oliver Walters <oliver.henry.walters@gmail.com>
2026-04-08 06:48:08 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 473
+INVENTREE_API_VERSION = 474
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v474 -> 2026-04-08 : https://github.com/inventree/InvenTree/pull/11693
+    - Adds DataImportMixin to the ManufacturerPartList API endpoint
 
 v473 -> 2026-04-08 : https://github.com/inventree/InvenTree/pull/11692
     - Adds "line" field to PurchaseOrderLineItem and PurchaseOrderExtraLineItem API endpoints
