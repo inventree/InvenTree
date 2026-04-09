93443c36f0b3ed607868610009364c57dd301d1f
Oliver Walters <oliver.henry.walters@gmail.com>
2026-04-09 12:04:48 +0000

    Tweak api version entry

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -7,7 +7,7 @@ INVENTREE_API_VERSION = 476
 INVENTREE_API_TEXT = """
 
 v476 -> 2026-04-09 : https://github.com/inventree/InvenTree/pull/11705
-    - Adds sorting / filtering / searching functionality to the selectionentry API endpoint
+    - Adds sorting / filtering / searching functionality to the SelectionListEntry API endpoint
 
 v475 -> 2026-04-09 : https://github.com/inventree/InvenTree/pull/11702
     - Adds "updated" and "updated_by" fields to the LabelTemplate and ReportTemplate API endpoints
