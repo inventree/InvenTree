0c55bdfa21d5154fa96bdb67556d286c24c12c9b
Matthias Mair <code@mjmair.com>
2026-05-06 01:22:36 +0200

    Lock notes on orders if they are completed Fixes #11879

--- src/frontend/src/pages/build/BuildDetail.tsx
+++ src/frontend/src/pages/build/BuildDetail.tsx
@@ -569,7 +569,9 @@ export default function BuildDetail() {
       NotesPanel({
         model_type: ModelType.build,
         model_id: build.pk,
-        has_note: !!build.notes
+        has_note: !!build.notes,
+        // TODO @matmair - change API to include a "locked" attribute that we can check here
+        editable: build.status == 40 ? false : undefined
       })
     ];
   }, [
--- src/frontend/src/pages/purchasing/PurchaseOrderDetail.tsx
+++ src/frontend/src/pages/purchasing/PurchaseOrderDetail.tsx
@@ -410,7 +410,9 @@ export default function PurchaseOrderDetail() {
       NotesPanel({
         model_type: ModelType.purchaseorder,
         model_id: order.pk,
-        has_note: !!order.notes
+        has_note: !!order.notes,
+        // TODO @matmair - change API to include a "locked" attribute that we can check here
+        editable: order.status == 30 ? false : undefined
       })
     ];
   }, [order, id, user]);
