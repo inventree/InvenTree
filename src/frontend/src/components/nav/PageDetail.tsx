57280556667fd1057cb2a7cbbd8f40676576c0b8
Matthias Mair <code@mjmair.com>
2026-05-16 18:23:55 +0200

    reduce diff

--- src/frontend/src/components/nav/PageDetail.tsx
+++ src/frontend/src/components/nav/PageDetail.tsx
@@ -1,7 +1,8 @@
-import { shortenString } from '@lib/index';
-import { StylishText } from '@lib/index';
 import { Group, Paper, Space, Stack, Text } from '@mantine/core';
 import { useHotkeys } from '@mantine/hooks';
+
+import { StylishText } from '@lib/components/StylishText';
+import { shortenString } from '@lib/functions/String';
 import { Fragment, type ReactNode, useMemo } from 'react';
 import { usePluginUIFeature } from '../../hooks/UsePluginUIFeature';
 import { useUserSettingsState } from '../../states/SettingsStates';
