754a4c2e742b0ecf8b6e8eeb079a249b8ef3c8f4
Oliver Walters <oliver.henry.walters@gmail.com>
2026-05-24 09:42:19 +0000

    Fix migration conflict

--- src/backend/InvenTree/common/migrations/0044_note.py
+++ src/backend/InvenTree/common/migrations/0044_note.py
@@ -9,7 +9,7 @@ from django.db import migrations, models
 class Migration(migrations.Migration):
 
     dependencies = [
-        ("common", "0041_auto_20251203_1244"),
+        ("common", "0043_auto_20260518_1206"),
         ("contenttypes", "0002_remove_content_type_name"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
