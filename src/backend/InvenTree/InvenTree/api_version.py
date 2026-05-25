3112ee6a9708e8aa18a4b03f022d014f72db40ad
Oliver Walters <oliver.henry.walters@gmail.com>
2026-05-25 03:16:14 +0000

    Update API and CHANGELOG

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 494
+INVENTREE_API_VERSION = 495
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v495 -> 2026-05-25 : https://github.com/inventree/InvenTree/pull/12000
+    - Adds "auto-allocate" API endpoint for sales orders
 
 v494 -> 2026-05-23 : https://github.com/inventree/InvenTree/pull/11990
     - Offload build output operations to a background task, and return a task ID which can be used to monitor the progress of the task
