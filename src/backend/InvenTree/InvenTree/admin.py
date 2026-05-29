c47d3e78df7628d3f300305aff927fb9c4f91c04
Oliver Walters <oliver.henry.walters@gmail.com>
2026-05-29 07:25:22 +0000

    Read-only ScheduledTask admin
    - Prevent admin users creating / editing / deleting scheduled tasks

--- src/backend/InvenTree/InvenTree/admin.py
+++ src/backend/InvenTree/InvenTree/admin.py
@@ -3,6 +3,8 @@
 from django.contrib import admin
 from django.http.request import HttpRequest
 
+from django_q.admin import ScheduleAdmin
+from django_q.models import Schedule
 from djmoney.contrib.exchange.admin import RateAdmin
 from djmoney.contrib.exchange.models import Rate
 
@@ -17,3 +19,23 @@ class CustomRateAdmin(RateAdmin):
 
 admin.site.unregister(Rate)
 admin.site.register(Rate, CustomRateAdmin)
+
+
+class ReadOnlyScheduleAdmin(ScheduleAdmin):
+    """Read-only admin interface for django-q Schedule objects."""
+
+    def has_add_permission(self, request: HttpRequest) -> bool:
+        """Prevent adding new Schedule objects."""
+        return False
+
+    def has_change_permission(self, request: HttpRequest, obj=None) -> bool:
+        """Prevent changing existing Schedule objects."""
+        return False
+
+    def has_delete_permission(self, request: HttpRequest, obj=None) -> bool:
+        """Prevent deleting Schedule objects."""
+        return False
+
+
+admin.site.unregister(Schedule)
+admin.site.register(Schedule, ReadOnlyScheduleAdmin)
