f0615a431cd065c6c3990e003b65fbb1f25a89b7
Matthias Mair <code@mjmair.com>
2026-06-17 22:31:51 +0200

    ensure session is not overwritten

--- src/backend/InvenTree/importer/serializers.py
+++ src/backend/InvenTree/importer/serializers.py
@@ -222,6 +222,8 @@ class DataImportAcceptRowSerializer(serializers.Serializer):
             row.validate(commit=True, request=request)
 
         if session := self.context.get('session', None):
+            # ensure current state is available
+            session.refresh_from_db()
             session.check_complete()
 
         return rows
