5de14b1ef289dbc419765ea49dac8b7729900ce0
Matthias Mair <code@mjmair.com>
2026-06-20 00:52:55 +0200

    various fixes

--- src/backend/InvenTree/stock/api.py
+++ src/backend/InvenTree/stock/api.py
@@ -18,6 +18,7 @@ from rest_framework.generics import GenericAPIView
 from rest_framework.response import Response
 from rest_framework.serializers import ValidationError
 
+import common.filters
 import common.models
 import common.settings
 import InvenTree.helpers
