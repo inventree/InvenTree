327b1a7900f583bd743d21333d8c24c4f9a8437f
Oliver Walters <oliver.henry.walters@gmail.com>
2026-06-29 03:28:13 +0000

    Switch to date type

--- src/frontend/src/pages/stock/StockDetail.tsx
+++ src/frontend/src/pages/stock/StockDetail.tsx
@@ -431,20 +431,20 @@ export default function StockDetail() {
         hidden: !stockitem.packaging
       },
       {
-        type: 'text',
+        type: 'date',
         name: 'creation_date',
         icon: 'calendar',
         label: t`Created`,
         hidden: !stockitem.creation_date
       },
       {
-        type: 'text',
+        type: 'date',
         name: 'updated',
         icon: 'calendar',
         label: t`Last Updated`
       },
       {
-        type: 'text',
+        type: 'date',
         name: 'stocktake_date',
         icon: 'calendar',
         label: t`Last Stocktake`,
