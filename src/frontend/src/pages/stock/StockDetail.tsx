71f717642f8b0bc1dcc957b6fff4f42872212309
Oliver <oliver.henry.walters@gmail.com>
2026-06-29 16:01:33 +1000

    [UI] Fix stock dates (#12272)
    * [UI] Fix stock dates
    
    - Display creation date
    - Display stocktake date
    
    * Switch to date type

--- src/frontend/src/pages/stock/StockDetail.tsx
+++ src/frontend/src/pages/stock/StockDetail.tsx
@@ -439,17 +439,24 @@ export default function StockDetail() {
         hidden: !stockitem.barcode_data
       },
       {
-        type: 'text',
+        type: 'date',
+        name: 'creation_date',
+        icon: 'calendar',
+        label: t`Created`,
+        hidden: !stockitem.creation_date
+      },
+      {
+        type: 'date',
         name: 'updated',
         icon: 'calendar',
         label: t`Last Updated`
       },
       {
-        type: 'text',
-        name: 'stocktake',
+        type: 'date',
+        name: 'stocktake_date',
         icon: 'calendar',
         label: t`Last Stocktake`,
-        hidden: !stockitem.stocktake
+        hidden: !stockitem.stocktake_date
       }
     ];
 
