6111aace1f7fda656016b06f85e43a5e4c22d097
Oliver <oliver.henry.walters@gmail.com>
2026-06-30 18:10:40 +1000

    [UI] Table field refactor (#12274)
    * Use callback funcs
    
    * Don't use idx to identify rows
    
    * Add debug function for finding why a component re-rendered
    
    * Do not pass 'control' through to each row
    
    * Prevent unnecessary re-rendering of table rows
    
    * Adjust order of operations for hooks
    
    * Keep props hidden
    
    * Use lightweight NumberInput
    
    * Use NumberInput elsewhere
    
    * Add comment
    
    * use rowId instead of idx
    
    * Generic row memos
    
    * Compare errors too
    
    * Fix for BomItemSubstituteRow
    
    * Adjust more forms
    
    * memoize quantity
    
    * Memoize build lines
    
    * Fix re-rendering issues for build allocation
    
    * Fix for useConsumeBuildLinesForm
    
    * Fix for transfer order table
    
    * Fix useReceiveLineItems
    
    * Remove memoized pattern
    
    * Fix row keys
    
    * Cleanup
    
    * Create useStockItems hook for memoizing items
    
    * Refactoring
    
    * More refactoring
    
    * Remove obj reference
    
    - preventing shallow comparison from working
    
    * Add error message to useWhyDidYouUpdate
    
    * Cleanup
    
    * Cleanup dead code
    
    * Adjust modal width
    
    * Change attr name
    
    * Remove autoFillFilters prop
    
    * Adjustments for serialized stock
    
    * Fix typing
    
    * Bump frontend version
    
    * Adjustments for playwright testing
    
    * Fix ref issue
    
    * Remove debug entry
    
    * Update CHANGELOG.md
    
    * Reintroduce index to table header
    
    * Refactor common component

--- src/frontend/src/pages/stock/StockDetail.tsx
+++ src/frontend/src/pages/stock/StockDetail.tsx
@@ -805,7 +805,6 @@ export default function StockDetail() {
   const stockOperationProps: StockOperationProps = useMemo(() => {
     return {
       items: [stockitem],
-      model: ModelType.stockitem,
       refresh: () => {
         const location = stockitem?.location;
         refreshInstancePromise().then((response) => {
@@ -830,6 +829,8 @@ export default function StockDetail() {
     formProps: stockOperationProps,
     delete: false,
     changeBatch: false,
+    add: !stockitem.serial,
+    remove: !stockitem.serial,
     assign: !!stockitem.in_stock && stockitem.part_detail?.salable,
     return: !!stockitem.consumed_by || !!stockitem.customer,
     merge: false
