ed074810a41da1d2f053d37c8ee6ebe1a476afb9
Oliver Walters <oliver.henry.walters@gmail.com>
2026-06-30 14:01:07 +0000

    Support ReturnOrder

--- src/frontend/src/components/previews/PreviewType.tsx
+++ src/frontend/src/components/previews/PreviewType.tsx
@@ -2,6 +2,7 @@ import { ModelType } from '@lib/enums/ModelType';
 import type { ReactNode } from 'react';
 import { PartPreviewComponent } from './models/PartPreview';
 import { PurchaseOrderPreviewComponent } from './models/PurchaseOrderPreview';
+import { ReturnOrderPreviewComponent } from './models/ReturnOrderPreview';
 import { SalesOrderPreviewComponent } from './models/SalesOrderPreview';
 import { StockPreviewComponent } from './models/StockPreview';
 
@@ -34,6 +35,8 @@ export function getPreviewComponentForModel({
       return PurchaseOrderPreviewComponent({ instance, modelId });
     case ModelType.salesorder:
       return SalesOrderPreviewComponent({ instance, modelId });
+    case ModelType.returnorder:
+      return ReturnOrderPreviewComponent({ instance, modelId });
     default:
       return null;
   }
---
+++ src/frontend/src/components/previews/models/ReturnOrderPreview.tsx
@@ -0,0 +1,25 @@
+import { t } from '@lingui/core/macro';
+import { ReturnOrderDetailsPanel } from '../../../pages/sales/ReturnOrderDetailsPanel';
+import type { PreviewType } from '../PreviewType';
+
+export function ReturnOrderPreviewComponent({
+  instance,
+  modelId
+}: Readonly<{
+  instance: any;
+  modelId: number;
+}>): PreviewType {
+  const customer = instance?.customer_detail?.name ?? instance?.customer_name;
+  const ref = instance?.reference ?? `#${modelId}`;
+
+  let title = `${t`Return Order`} ${ref}`;
+
+  if (customer) {
+    title += ` (${customer})`;
+  }
+
+  return {
+    title,
+    preview: <ReturnOrderDetailsPanel instance={instance} />
+  };
+}
