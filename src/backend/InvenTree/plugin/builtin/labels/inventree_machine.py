a70aad2e0b12ac0133bf8777287e40635690e7b3
Dean Gardiner <me@dgardiner.net>
2026-07-03 16:42:58 +1200

    Provide current `machine` to `LabelPrinterBaseDriver.get_printing_options_serializer`

--- src/backend/InvenTree/plugin/builtin/labels/inventree_machine.py
+++ src/backend/InvenTree/plugin/builtin/labels/inventree_machine.py
@@ -187,11 +187,11 @@ class InvenTreeLabelPlugin(LabelPrintingMixin, InvenTreePlugin):
 
         def get_driver_options(self, fields):
             """Returns the selected machines serializer."""
-            _, driver = get_machine_and_driver(fields['machine'])
+            machine, driver = get_machine_and_driver(fields['machine'])
 
             if driver is None:
                 return None
 
             return driver.get_printing_options_serializer(
-                self.context['request'], context=self.context
+                self.context['request'], context=self.context, machine=machine
             )
