5df2adae6da3eba4fe7b7df1d8515a689f940e60
Miklós Márton <martonmiklosqdev@gmail.com>
2026-07-03 23:58:53 +0200

    Fix OpenAPI multipart file fields to use binary format (#12298)
    * Fix OpenAPI multipart file fields to use binary format
    
    Fixes #11246
    
    * Update API version
    
    * fix api version
    
    ---------
    
    Co-authored-by: Matthias Mair <code@mjmair.com>

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 514
+INVENTREE_API_VERSION = 515
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v515 -> 2026-07-03 : https://github.com/inventree/InvenTree/pull/12298
+    - Change the file fields definition to binary (from uri) in the upload requests
 
 v514 -> 2026-07-02 : https://github.com/inventree/InvenTree/pull/12294
     - Adds "duplicate" field to the BuildOrder, Company, ManufacturerPart, SupplierPart and SalesOrderShipment API endpoints
