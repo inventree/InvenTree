760f63cf5c95ca89cd02e0046663bb3d04c5115c
Matthias Mair <code@mjmair.com>
2026-07-07 23:57:16 +0200

    fix return code

--- src/backend/InvenTree/order/api.py
+++ src/backend/InvenTree/order/api.py
@@ -505,7 +505,7 @@ class PurchaseOrderViewSet(
         serializer = self.get_serializer(data=request.data)
         serializer.is_valid(raise_exception=True)
         serializer.save()
-        return Response(serializer.data, status=status.HTTP_200_OK)
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
 
     @extend_schema(responses={201: stock_serializers.StockItemSerializer(many=True)})
     @action(
