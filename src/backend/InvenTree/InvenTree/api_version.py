8d46b9fa26a3fa8452d1c4b51acf4e8592cb76bc
Oliver Walters <oliver.henry.walters@gmail.com>
2026-07-09 12:22:19 +0000

    Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 517
+INVENTREE_API_VERSION = 518
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v518 -> 2026-07-09 : https://github.com/inventree/InvenTree/pull/12341
+    - Enable import of internal part prices via the API
 
 v517 -> 2026-07-08 : https://github.com/inventree/InvenTree/pull/12336
     - Fix currency code options for the PartPricing  model and API endpoints
