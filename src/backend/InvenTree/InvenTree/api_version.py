ea68eaf2587f5c978fb787b30fa4c5d91603f9d5
Matthias Mair <code@mjmair.com>
2026-07-23 23:26:05 -0700

    chore(backend): major allauth bump (#12454)
    * mayjor allauth bump
    
    * add PR link

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 526
+INVENTREE_API_VERSION = 527
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v527 -> 2026-07-24 : https://github.com/inventree/InvenTree/pull/12454
+    - Extend API with dedicated login-by-code re-send endpoint
 
 v526 -> 2026-07-24 : https://github.com/inventree/InvenTree/pull/12463
     - Fix for data export on SalesOrderAllocation API endpoint
