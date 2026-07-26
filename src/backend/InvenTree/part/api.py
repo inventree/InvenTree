a32624605a5804fe88557b63068382a1176b5f60
Oliver Walters <oliver.henry.walters@gmail.com>
2026-07-26 13:21:40 +0000

    Add "on order" filter for Part model
    - Added API filter
    - Added table filter
    - Added table column

--- src/backend/InvenTree/part/api.py
+++ src/backend/InvenTree/part/api.py
@@ -784,6 +784,15 @@ class PartFilter(FilterSet):
             return queryset.filter(Q(unallocated_stock__gt=0))
         return queryset.filter(Q(unallocated_stock__lte=0))
 
+    # on_order filter
+    on_order = rest_filters.BooleanFilter(label='On order', method='filter_on_order')
+
+    def filter_on_order(self, queryset, name, value):
+        """Filter by whether the Part has any stock on order."""
+        if str2bool(value):
+            return queryset.filter(Q(ordering__gt=0))
+        return queryset.filter(Q(ordering__lte=0))
+
     convert_from = rest_filters.ModelChoiceFilter(
         label='Can convert from',
         queryset=Part.objects.all(),
