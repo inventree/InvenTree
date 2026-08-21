9d19b32b16d7424d349644b4fb8d9c3377d40365
Matthias Mair <code@mjmair.com>
2026-08-21 23:46:26 +0200

    feat(backend): rename filter (#12672)
    * feat(backend): rename filter
    
    * fix filter name
    
    * fix parameter used in test
    
    * remove temporary fix added in https://github.com/inventree/InvenTree/pull/12668
    
    * remove unneeded filter redef
    
    * add changelog
    
    ---------
    
    Co-authored-by: Oliver <oliver.henry.walters@gmail.com>

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 533
+INVENTREE_API_VERSION = 534
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v534 -> 2026-08-21 : https://github.com/inventree/InvenTree/pull/12672
+    - rename 'tags' filter to 'tag_name' to avoid name clash with the 'tags' field on various API endpoints
 
 v533 -> 2026-08-18 : https://github.com/inventree/InvenTree/pull/12317
     - Refactors the PurchaseOrder, PurchaseOrderLineItem and PurchaseOrderExtraLine API endpoints to use DRF viewsets
