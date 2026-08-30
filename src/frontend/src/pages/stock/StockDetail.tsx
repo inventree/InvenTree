8002367ac26d93139ecae6ce94fca8da27f5ae52
Oliver <oliver.henry.walters@gmail.com>
2026-08-30 11:44:07 +1000

    Stock location fix (#12742)
    * Fill out stock location based on part default
    
    * Add playwright tests
    
    (cherry picked from commit 871147d93639df704751d5b355d9ce3dc32b1d56)

--- src/frontend/src/pages/stock/StockDetail.tsx
+++ src/frontend/src/pages/stock/StockDetail.tsx
@@ -374,6 +374,7 @@ export default function StockDetail() {
 
   const duplicateStockItemFields = useStockFields({
     create: true,
+    locationId: stockitem.location,
     modalId: 'duplicate-stock-item'
   });
 
