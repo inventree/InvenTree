aadd27d217e3bd8f3165414f8b33f56e8500f901
Oliver <oliver.henry.walters@gmail.com>
2026-09-03 15:27:44 +1000

    Prevent delete operation on /api/user/me/ (#12770)
    * Prevent delete operation on /api/user/me/
    
    * Bump API version

--- src/backend/InvenTree/InvenTree/api_version.py
+++ src/backend/InvenTree/InvenTree/api_version.py
@@ -1,10 +1,13 @@
 """InvenTree API version information."""
 
 # InvenTree API version
-INVENTREE_API_VERSION = 540
+INVENTREE_API_VERSION = 541
 """Increment this API version number whenever there is a significant change to the API that any clients need to know about."""
 
 INVENTREE_API_TEXT = """
+
+v541 -> 2026-09-03 : https://github.com/inventree/InvenTree/pull/12770
+    - Prevent DELETE operation against the /api/user/me/ endpoint
 
 v540 -> 2026-09-04 : https://github.com/inventree/InvenTree/pull/12773
     - Adds a "system_state" field to the info endpoint for non-critical general system state information
