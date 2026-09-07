edb4b083de8852e8b602f2d66be30ba0623a9ac9
root <root@vmi3152815.contaboserver.net>
2026-09-07 13:15:15 +0200

    fix(ci): pin demo dataset to stable branch instead of main

--- tasks.py
+++ tasks.py
@@ -1859,7 +1859,7 @@ def setup_test(
     use_ssh: bool = False,
     verbose: bool = False,
     path: str = 'inventree-demo-dataset',
-    branch='main',
+    branch='stable',
 ):
     """Setup a testing environment."""
     from src.backend.InvenTree.InvenTree.config import (  # type: ignore[import]
