020177e92a3dabe27b9a1e5f53ccd4d8c4f736c9
Oliver <oliver.henry.walters@gmail.com>
2026-09-18 07:39:18 +1000

    Display user fix (#12869)
    * Add 'created-by' badge to order pages
    
    * Update playwright tests
    
    * fix icons
    
    (cherry picked from commit e37045ac411a1b636002cbe4af4ddf6c06161fa0)

--- src/frontend/tests/pages/pui_sales.spec.ts
+++ src/frontend/tests/pages/pui_sales.spec.ts
@@ -68,6 +68,12 @@ test('Sales Orders - Tabs', async ({ browser }) => {
     .getByLabel('Order Details')
     .getByText('Selling some stuff')
     .waitFor();
+
+  // Creator of the order should be displayed
+  await expect(page.getByRole('row', { name: 'Created By' })).toContainText(
+    'admin'
+  );
+
   await loadTab(page, 'Line Items');
   await loadTab(page, 'Shipments');
   await loadTab(page, 'Build Orders');
@@ -82,6 +88,12 @@ test('Sales Orders - Tabs', async ({ browser }) => {
   await loadTab(page, 'Return Orders');
   await page.getByRole('cell', { name: 'RMA-' }).click();
   await page.getByText('RMA-0001', { exact: true }).waitFor();
+
+  // Creator of the order should be displayed
+  await expect(page.getByRole('row', { name: 'Created By' })).toContainText(
+    'admin'
+  );
+
   await loadTab(page, 'Line Items');
   await loadTab(page, 'Attachments');
   await loadTab(page, 'Notes');
